a2519c4c47b8a1fbb2da7fa8bef1132a609cb532
fjakobs <fjakobs@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2007-06-15 12:46:06 +0000

    add upload widget by dietrich streifert

---
+++ trunk/qooxdoo-contrib/contributions/UploadWidget/Manifest.js
@@ -0,0 +1,24 @@
+{
+  
+  name : "SkeletonWidget",
+  
+  description : "A Sample widget. This widget can be used as basis for other contributions",
+  
+  authors : [
+    {
+      name : "",
+      email : ""
+    },
+  ],
+  
+  license : "EPL/LGPL",
+      
+  versions :
+  {
+    "0.1" :
+    {
+      "qooxdoo-versions": ["0.7", "0.7.1-pre", "trunk"]
+    }
+  }
+  
+}
