5506a651d069dfa82a801fd1ec14b34546fb3015
fjakobs <fjakobs@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2007-06-19 09:53:21 +0000

    work on contrib tools

--- trunk/qooxdoo-contrib/contributions/ProgressBar/0.1/Manifest.js
+++ trunk/qooxdoo-contrib/contributions/ProgressBar/0.1/Manifest.js
@@ -12,5 +12,6 @@
   "license" : "EPL/LGPL",
       
   "version" : "0.1",
-  "qooxdoo-versions": ["0.7", "0.7.1-pre", "trunk"]
+  "qooxdoo-versions": ["0.7", "0.7.1-pre", "trunk"],
+  "namespace" : "progressbar"
 }
