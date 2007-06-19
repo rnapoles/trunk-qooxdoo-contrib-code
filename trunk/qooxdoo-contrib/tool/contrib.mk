6dd2da9f98cebfb51d56f30dcb0713af36cb7a25
fjakobs <fjakobs@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2007-06-19 10:01:14 +0000

    minor fix

--- trunk/qooxdoo-contrib/tool/contrib.mk
+++ trunk/qooxdoo-contrib/tool/contrib.mk
@@ -1,3 +1,6 @@
+CONTRIB_UTIL = python $(QOOXDOO_CONTRIB_PATH)/tools/contrib.py
+
+
 ifneq ($(CONTRIBS),)
 
   EXTERNAL_CLASS_PATH = $(patsubst %,--class-path %/source/class, $(CONTRIBS))
@@ -11,17 +14,22 @@ ifneq ($(CONTRIBS),)
   # --use-setting $(EXTERNAL_NAMESPACE).resourceUri:$(APPLICATION_HTML_TO_ROOT_URI)/resource/$(EXTERNAL_NAMESPACE)
   EXTERNAL_BUILD_SETTINGS = $(patsubst %, --use-setting %, $(join $(patsubst %, %.resourceUri:$(APPLICATION_HTML_TO_ROOT_URI)/resource/, $(CONTRIBS_NAMESPACES)), $(CONTRIBS_NAMESPACES)))
 
-  APPLICATION_ADDITIONAL_BUILD_OPTIONS += \
-    --copy-resources \
-    $(patsubst %,--resource-input %/source/resource, $(CONTRIBS)) \
-    $(patsubst %,--resource-output $(APPLICATION_BUILD_PATH)/resource/%, $(CONTRIBS_NAMESPACES)) \
-    $(EXTERNAL_BUILD_SETTINGS) \
-    $(EXTERNAL_CLASS_PATH) \
-    $(EXTERNAL_CLASS_URI)
+  #APPLICATION_ADDITIONAL_BUILD_OPTIONS += \
+  #  --copy-resources \
+  #  $(patsubst %,--resource-input %/source/resource, $(CONTRIBS)) \
+  #  $(patsubst %,--resource-output $(APPLICATION_BUILD_PATH)/resource/%, $(CONTRIBS_NAMESPACES)) \
+  #  $(EXTERNAL_BUILD_SETTINGS) \
+  #  $(EXTERNAL_CLASS_PATH) \
+  #  $(EXTERNAL_CLASS_URI)
 
-  APPLICATION_ADDITIONAL_SOURCE_OPTIONS += \
-    $(EXTERNAL_SOURCE_SETTINGS) \
-    $(EXTERNAL_CLASS_PATH) \
-    $(EXTERNAL_CLASS_URI)
+  #APPLICATION_ADDITIONAL_SOURCE_OPTIONS += \
+  #  $(EXTERNAL_SOURCE_SETTINGS) \
+  #  $(EXTERNAL_CLASS_PATH) \
+  #  $(EXTERNAL_CLASS_URI)
+
+	MANIFESTS = $(patsubst %, --manifest %s/Manifest.js , $(CONTRIBS))
+
+	APPLICATION_ADDITIONAL_BUILD_OPTIONS += `$(CONTRIB_UTIL) $(MANIFESTS) --build-flags`
+	APPLICATION_ADDITIONAL_SOURCE_OPTIONS += `$(CONTRIB_UTIL) $(MANIFESTS) --source-flags`
 
 endif
