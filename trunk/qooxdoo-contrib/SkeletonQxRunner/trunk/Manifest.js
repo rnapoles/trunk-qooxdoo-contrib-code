2ee520906deb2d8961536bf92fc7836629a462f6
thron7 <thron7@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2007-07-27 11:07:35 +0000

    New skeleton for TestRunner/DemoBrowser-like applications.

---
+++ trunk/qooxdoo-contrib/SkeletonQxRunner/trunk/Manifest.js
@@ -0,0 +1,22 @@
+{
+  "name" : "SkeletonQxRunner",
+  "namespace" : "qxapp",
+  
+  "summary" : "Skeleton for TestRunner/DemoBrowser-like applications",
+  "description" : "...",
+  "keywords" : ["widget"],
+  "homepage" : "http://qooxdoo.org/documentation/contrib",
+  
+  "authors" : [
+    {
+      "name" : "Thomas Herchenroeder (thron7)"
+      "name" : "Fabian Jakobs (fjakobs)"
+    }
+  ],
+  
+  "license" : "LGPL",
+  "version" : "trunk",
+  
+  "qooxdoo-versions": ["0.7.1"]
+  
+}
---
+++ trunk/qooxdoo-contrib/SkeletonQxRunner/trunk/source/class/qxapp/Application.js
@@ -0,0 +1,111 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2007 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Thomas Herchenroeder (thron7)
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#module(qxapp)
+#resource(qxapp.css:css)
+#resource(qxapp.image:image)
+
+#embed(qx.icontheme/16/*)
+#embed(qxapp.image/*)
+#embed(qxapp.css/*)
+
+************************************************************************ */
+
+/**
+ * The main application class.
+ */
+qx.Class.define("qxapp.Application",
+{
+  extend : qx.application.Gui,
+
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    /**
+     * TODOC
+     *
+     * @type member
+     * @return {void}
+     */
+    main : function()
+    {
+      this.base(arguments);
+
+      // Define alias for custom resource path
+      qx.io.Alias.getInstance().add("qxapp", qx.core.Setting.get("qxapp.resourceUri"));
+
+      // Include CSS files
+      qx.html.StyleSheet.includeFile(qx.io.Alias.getInstance().resolve("qxapp/css/style.css"));
+      qx.html.StyleSheet.includeFile(qx.io.Alias.getInstance().resolve("qxapp/css/sourceview.css"));
+
+      // Initialize the viewer
+      this.viewer = new qxapp.AppFrame;
+      this.viewer.addToDocument();
+
+      // Load data file
+      qx.client.Timer.once(this._load, this, 0);
+    },
+
+
+    /**
+     * TODOC
+     *
+     * @type member
+     * @return {void}
+     */
+    _load : function() {
+      this.viewer.dataLoader();
+    }
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     SETTINGS
+  *****************************************************************************
+  */
+
+  settings : { "qxapp.resourceUri" : "./resource" },
+
+
+
+
+  /*
+  *****************************************************************************
+     DESTRUCTOR
+  *****************************************************************************
+  */
+
+  destruct : function() {
+    this._disposeObjects("viewer");
+  }
+});
