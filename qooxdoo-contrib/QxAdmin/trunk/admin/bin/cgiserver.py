e258b1bee40e952a37acf06e8a2270cff2ed67b8
thron7 <thron7@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2007-08-13 17:24:17 +0000

    Re-org of tool/ dir (to better comply with target environment in skeleton).

---
+++ qooxdoo-contrib/QxAdmin/trunk/admin/bin/cgiserver.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import CGIHTTPServer
+import BaseHTTPServer
+
+
+class Handler(CGIHTTPServer.CGIHTTPRequestHandler):
+    cgi_directories = ["/tool"]
+
+
+PORT = 8000
+
+httpd = BaseHTTPServer.HTTPServer(("", PORT), Handler)
+print "serving at port", PORT
+httpd.serve_forever()
