2b45ee75fd768eb87a68738743cd07b507dd2fe3
thron7 <thron7@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2007-08-14 12:57:00 +0000

    Windows start script for QxAdmin

---
+++ trunk/qooxdoo-contrib/QxAdmin/trunk/admin/bin/startme.bat
@@ -0,0 +1,18 @@
+@echo off
+:: cd to Makefile dir
+echo. Changing directory
+for /f %%i in ('cd') do set opwd=%%i
+cd ..\..
+if not exist "Makefile" (
+  echo. Looks like the wrong directory _no Makefile_ aborting ...
+  cd %opwd%
+  exit /b 1
+)
+:: start mini web server
+echo. Starting mini web server
+start python admin/bin/cgiserver.py
+:: load admin url in browser
+echo. Launching admin url in browser
+start http://localhost:8000/admin/
+cd %opwd%
+
