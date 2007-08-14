2a12db25a4ac85ee8cfdfdc8010ebc571587a0d2
thron7 <thron7@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2007-08-14 14:29:31 +0000

    Check for Python

--- trunk/qooxdoo-contrib/QxAdmin/trunk/admin/bin/startme.bat
+++ trunk/qooxdoo-contrib/QxAdmin/trunk/admin/bin/startme.bat
@@ -1,19 +1,28 @@
-@echo off
+  @echo off
+  set rc=0
 :: cd to Makefile dir
-echo. Changing directory
-for /f %%i in ('cd') do set opwd=%%i
-cd ..\..
-if not exist "Makefile" (
-  echo. Looks like the wrong directory _no Makefile_ aborting ...
-  cd %opwd%
-  exit /b 1
-)
+  echo. Changing directory
+  for /f %%i in ('cd') do set opwd=%%i
+  cd ..\..
+  if not exist "Makefile" (
+    echo. Looks like the wrong directory _no Makefile_ - aborting ...
+    set rc=1
+    goto:END
+  )
 :: start mini web server
-echo. Starting mini web server
-start python admin/bin/cgiserver.py
+  python -V >nul 2>&1
+  if not errorlevel 0 (
+    echo. Python not in your pathi - aborting ...
+    set rc=2
+    goto:END
+  )
+  echo. Starting mini web server
+  start "Use Ctrl-Break to terminate" python admin/bin/cgiserver.py
 :: load admin url in browser
-echo. Launching admin url in browser
-start http://localhost:8000/admin/
-:: back to initial dir
-cd %opwd%
+  echo. Launching admin url in browser
+  start http://localhost:8000/admin/
+:: clean-up
+  :END
+  cd %opwd%
+  exit /b %rc%
 
