02e24a8f67fa60f17d0effbb866f7d96aa4b0dfa
thron7 <thron7@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2007-08-16 15:50:16 +0000

    Optimized search for cygwin/python

--- qooxdoo-contrib/QxAdmin/trunk/admin/bin/startme.bat
+++ qooxdoo-contrib/QxAdmin/trunk/admin/bin/startme.bat
@@ -1,5 +1,6 @@
   @echo off
   setlocal ENABLEDELAYEDEXPANSION
+  set DEBUG=0
   set rc=0
   set pybin=python
 :: cd to Makefile dir
@@ -20,18 +21,13 @@
   :: try to locate python within cygwin
   echo. No python in your path; trying to locate python within cygwin ...
   echo. ... this may take some moments
-  call :_dot " ."
-  rem cmd /c exit /b 19
-  rem for /f %%L in (admin\bin\drives.txt) do (
-  for %%L in (c: d: e:) do (
-    call :_SearchCygwin "%%L"
-    set SearchCygwinFound >nul 2>&1
-    if !errorlevel!==0 (
-      echo.
-      echo. Found cygwin as !SearchCygwinFound!, containing python
-      set pybin=!SearchCygwinFound!\bin\python.exe
-      goto:GotPython
-    )
+  call :_searchPython
+  set SearchCygwinFound >nul 2>&1
+  if !errorlevel!==0 (
+    echo.
+    echo. Found cygwin as !SearchCygwinFound!, containing python
+    set pybin=!SearchCygwinFound!\bin\python.exe
+    goto:GotPython
   )
   :: no python - giving up
   echo.
@@ -41,7 +37,11 @@
 
   :GotPython
   echo. Starting mini web server
-  start "Use Ctrl-Break to terminate" %pybin% admin/bin/cgiserver.py
+  if %DEBUG%==1 (
+    start "Use Ctrl-Break to terminate" %pybin% admin/bin/cgiserver.py
+  ) else (
+    start "Use Ctrl-Break to terminate" /b %pybin% admin/bin/cgiserver.py >nul 2>&1
+  )
   echo. Wait a few seconds for the web server
   call :_sleepDot 3
   :: TODO check success
@@ -52,7 +52,7 @@
   :END
   cd %opwd%
   endlocal
-  pause
+  call :_shutdown
   exit /b %rc%
 
 :_SearchCygwin
@@ -64,7 +64,7 @@
       set myfound=%%G
     ) 
   )
-  endlocal & set SearchCygwinFound=%myfound%
+  endlocal & set _SearchCygwinFound=%myfound%
   goto:EOF
 
 :_dot
@@ -93,6 +93,50 @@
 
 :_errReset
   cmd /c exit /b 0
+  goto:EOF
+
+:_shutdown
+  setlocal
+  rem taskkill /f /im python.exe
+  rem tasklist /fi "imagename eq python.exe"
+  rem TODO: if you call this script on an existing shell, and you dont use
+  ::  use ctrl-break, the python process will continue to run
+  echo. Press Ctrl-Break^<RET^> to terminate the application ...
+  pause >nul
+  endlocal
+  goto:EOF
+
+:_searchPython
+  setlocal
+  call :_dot " ."
+  :: try likely paths
+  :: HOMEDRIVE\cygwin ProgramFiles\cygwin SystemDrive\cygwin
+  for %%L in (%ProgramFiles% %HOMEDRIVE% %SystemDrive%) do (
+    call :_dot "."
+    if exist "%%L\cygwin\bin\python.exe" (
+      call :_setCygwinFound "%%L\cygwin"
+      goto:f1End
+    )
+  )
+  :: do an exhaustive search of some paths
+  rem for /f %%L in (admin\bin\drives.txt) do (
+  for %%L in (%ProgramFiles% %HOMEDRIVE% %SystemDrive% c: d: e:) do (
+    call :_dot "."
+    call :_SearchCygwin "%%L"
+    set _SearchCygwinFound >nul 2>&1
+    if !errorlevel!==0 (
+      call :_setCygwinFound "%_SearchCygwinFound%"
+      set _SearchCygwinFound=
+      goto:f1End
+    )
+  )
+  :f1End
+  endlocal
+  goto:EOF
+
+:_setCygwinFound
+  setlocal
+  endlocal & set SearchCygwinFound=%1
   goto:EOF
 
 :EOF
