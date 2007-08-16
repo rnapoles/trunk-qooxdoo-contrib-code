0ef1683af8a2c63aba4379f4687d052a65f67467
thron7 <thron7@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2007-08-16 12:10:39 +0000

    Better feedback, wait before browser launch

--- qooxdoo-contrib/QxAdmin/trunk/admin/bin/startme.bat
+++ qooxdoo-contrib/QxAdmin/trunk/admin/bin/startme.bat
@@ -19,19 +19,22 @@
   )
   :: try to locate python within cygwin
   echo. No python in your path; trying to locate python within cygwin ...
-  echo. ... this may take some moments.
+  echo. ... this may take some moments
+  call :_dot " ."
   rem cmd /c exit /b 19
   rem for /f %%L in (admin\bin\drives.txt) do (
   for %%L in (c: d: e:) do (
     call :_SearchCygwin "%%L"
     set SearchCygwinFound >nul 2>&1
     if !errorlevel!==0 (
+      echo.
       echo. Found cygwin as !SearchCygwinFound!, containing python
       set pybin=!SearchCygwinFound!\bin\python.exe
       goto:GotPython
     )
   )
   :: no python - giving up
+  echo.
   echo. Could not find python in your environment - aborting ...
   set rc=2
   goto:END
@@ -39,6 +42,8 @@
   :GotPython
   echo. Starting mini web server
   start "Use Ctrl-Break to terminate" %pybin% admin/bin/cgiserver.py
+  echo. Wait a few seconds for the web server
+  call :_sleepDot 3
   :: TODO check success
 :: load admin url in browser
   echo. Launching admin url in browser
@@ -54,11 +59,40 @@
   setlocal
   rem echo. IN SUBROUTINE: %1
   for /r %1\ /d %%G in (*cygwin) do (
+    call :_dot "."
     if exist "%%G\bin\python.exe" (
       set myfound=%%G
     ) 
   )
   endlocal & set SearchCygwinFound=%myfound%
+  goto:EOF
+
+:_dot
+  setlocal
+  <nul (set/p xxx=%1)
+  endlocal
+  cmd /c exit /b 0
+  goto:EOF
+
+:_sleep
+  setlocal
+  ping -n %1 127.0.0.1 >nul
+  endlocal
+  goto:EOF
+
+:_sleepDot
+  setlocal
+  for /l %%Y in (1,1,%1) do (
+    call :_sleep 1
+    call :_dot "."
+  )
+  call :_errReset
+  echo.
+  endlocal
+  goto:EOF
+
+:_errReset
+  cmd /c exit /b 0
   goto:EOF
 
 :EOF
