ffafcb438a7481d7e17ecbd7bfd49c7e30606669
thron7 <thron7@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2007-09-06 15:21:49 +0000

    Adaptions for cygwin

--- qooxdoo-contrib/QxAdmin/trunk/admin/bin/cgiserver.py
+++ qooxdoo-contrib/QxAdmin/trunk/admin/bin/cgiserver.py
@@ -3,6 +3,8 @@
 import CGIHTTPServer
 import BaseHTTPServer
 
+import os, sys
+
 
 class Handler(CGIHTTPServer.CGIHTTPRequestHandler):
     cgi_directories = ["/admin/bin"]
@@ -11,5 +13,8 @@ class Handler(CGIHTTPServer.CGIHTTPRequestHandler):
 PORT = 8000
 
 httpd = BaseHTTPServer.HTTPServer(("", PORT), Handler)
+# fix PATH on cygwin
+if sys.platform == 'cygwin':
+    os.environ['PATH'] = '/usr/bin:'+os.environ['PATH']
 print "serving at port", PORT
 httpd.serve_forever()
