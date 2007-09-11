9a9baa839b582cd28b866ec4ee50b62fa9ebc469
mlhartme <mlhartme@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2007-09-11 08:49:38 +0000

    fix

--- trunk/qooxdoo-contrib/QWT/trunk/src/hudson.sh
+++ trunk/qooxdoo-contrib/QWT/trunk/src/hudson.sh
@@ -16,6 +16,6 @@ export MAVEN_OPTS="-Xmx112m -client"
 export INSTALL_OPTS="--batch-mode -Dall"
 rm -rf $HOME/.m2/repository
 ./install.sh || exit 1
-mvn site site:deploy || exit 1
-mvn checkstyle:check || exit 1
+mvn site site:deploy --batch-mode || exit 1
+mvn checkstyle:check --batch-mode || exit 1
 
