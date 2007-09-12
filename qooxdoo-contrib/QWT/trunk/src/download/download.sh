e9a3e9c50ff124ce7fe9edd9ad7b6ffbbb727f76
mlhartme <mlhartme@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2007-09-12 10:53:02 +0000

    fixes

--- qooxdoo-contrib/QWT/trunk/src/download/download.sh
+++ qooxdoo-contrib/QWT/trunk/src/download/download.sh
@@ -2,8 +2,7 @@
 mkdir tmp-qwt || exit 1
 cd tmp-qwt
 wget -q http://qooxdoo-contrib.svn.sf.net/viewvc/*checkout*/qooxdoo-contrib/trunk/qooxdoo-contrib/QWT/trunk/src/download/pom.xml || exit 1
-mvn dependency:resolve || exit 1
-mvn dependency:resolve-plugins || exit 1
+mvn || exit 1
 cd ..
 rm -rf tmp-qwt
 
