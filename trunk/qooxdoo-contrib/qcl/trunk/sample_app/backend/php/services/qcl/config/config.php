cb4fb0e65a0eecaae4048ef01d8c2ed0fa630fba
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2007-09-12 07:05:11 +0000

    more work on qcl.config backend, not yet functional

--- trunk/qooxdoo-contrib/qcl/trunk/sample_app/backend/php/services/qcl/config/config.php
+++ trunk/qooxdoo-contrib/qcl/trunk/sample_app/backend/php/services/qcl/config/config.php
@@ -3,6 +3,22 @@ require_once SERVICE_PATH . "qcl/jsonrpc/object.php";
 
 /**
  * abstract class for classes that implement configuration management
+ * 
+ * the idea of the class is the following:
+ * - configuration entries (properties) have a name(key), a type, and a value
+ * - types accepted currently are string, number and boolean (others might follow 
+ *   if there is a need)
+ * - for each configuration entry, you can also set a read and a write permission.
+ *   if set, the active user requesting an action must have the corresponding 
+ *   permission.
+ * - each configuration entry can also have a user variant, i.e. users can, 
+ *   if allowed, create their own versions of the entry. When they edit
+ *   this entry, they will all other variants will be left untouched including
+ *   the default value, to which the user variant can be reset.
+ * - on the client, a qcl.config.manager singleton object takes care of retrieval
+ *   and update of the config entries. on login, all or a subset of the configuration 
+ *   entries that an individual user has access to will be sent to the client and 
+ *   cached there.  
  */
 
 class qcl_config extends qcl_jsonrpc_object
@@ -45,8 +61,8 @@ class qcl_config extends qcl_jsonrpc_object
 	//-------------------------------------------------------------   
 
 	/**
-	 * creates a config property, overwriting any previous entry unless the 
-	 * entry is a user variant of the base entry.
+	 * creates a config property, overwriting any previous entry
+	 * requires permission qcl.config.permissions.manage
 	 * 
 	 * @param string $name The name of the property (i.e., myapplication.config.locale)
 	 * @param string $type The type of the property (string|number|object|boolean)
@@ -54,26 +70,27 @@ class qcl_config extends qcl_jsonrpc_object
 	 * 		  and read this property (optional)
 	 * @param string $permissionWrite The permission name that is needed to access 
 	 * 		  and read this property (optional)
-	 * @param mixed $userRef if this property has a user-defined variant, 
-	 * 		  it is stored as an additional entry with the user reference (usually user
-	 * 		  id) (optional)
+	 * @param boolean $allowUserVariants If true, allow users to create their 
+	 * 		  own variant of the configuration setting 
 	 * @return true if success or false if there was an error
 	 */
-	function create($name, $type, $permissionRead=null, $permissionWrite=null, $userRef=null ){}
+	function create($name, $type, $permissionRead=null, $permissionWrite=null, $allowUserVariants=false ){}
 
 	/**
-	 * gets config property value. 
-	 * raise an error if the active user does not have write permission for this property.
-	 * @param string $name The name of the property (i.e., myapplication.config.locale) 
-	 * @return value of property or null if value does not exist.
+	 * deletes a config property completely or only its user variant 
+	 * requires permission qcl.config.permissions.manage
+	 * 
+	 * @param string $name The name of the property (i.e., myapplication.config.locale)
+	 * @return true if success or false if there was an error
 	 */
-	function delete($name, $userRef){} 	
+	function delete($name ){} 	
 
 	/**
-	 * gets all config property value that are readable by the active user 
+	 * gets all config property value that are readable by the active user
+	 * @param string $mask return only a subset of entries that start with $mask
 	 * @return array Array, with property names as key 
 	 */
-	function getAll(){} 	
+	function getAll($mask){} 	
 
 	/**
 	 * gets config property value. 
@@ -84,14 +101,49 @@ class qcl_config extends qcl_jsonrpc_object
 	function get($name){} 	 
  
 	/**
-	 * sets config property values
-	 * raise an error if the active user does not have write permission for this property
-	 * or if the type of the value does not match the type of the property.  
+	 * sets config property
 	 * @param string $name The name of the property (i.e., myapplication.config.locale)
-	 * @param mixed $value The value of the property. 
+	 * @param string $value The value of the property. 
+	 * @param boolean $defaultValue If true, set the key's default value for keys that allow
+	 *  	user variants. This is necessary so that the admin can change the defaults instead
+	 * 		of editing her/his own variant.
 	 * @return true if success or false if there was an error
 	 */
-	function set($name,$value){}
+	function set($name,$value,$defaultValue=false){}
 
+	/**
+	 * resets a property user variant to its original value
+	 * raise an error if the active user does not have write permission for this property  
+	 * @param string $name The name of the property (i.e., myapplication.config.locale)
+	 * @return true if success or false if there was an error
+	 */
+	function reset($name){}
+	
+	/**
+	 * gets required permission name for read access to config property
+	 * @param string $name name of configuration key
+	 */
+	function getPermissionRead($name){}
+	
+	/**
+	 * gets required permission name for write access to config property
+	 * @param string $name name of configuration key
+	 */
+	function getPermissionWrite($name){}
+
+	/**
+	 * checks if current user has the right to access the configuration 
+	 * property with read priviledges
+	 * @param string $name name of configuration key 
+	 */
+	 function hasReadAccess($name){}
+	
+	/**
+	 * checks if current user has the right to access the configuration 
+	 * property with write priviledges
+	 * @param string $name name of configuration key 
+	 */
+	 function hasWriteAccess($name){}	
+	
 }
 
--- trunk/qooxdoo-contrib/qcl/trunk/sample_app/backend/php/services/qcl/config/db.php
+++ trunk/qooxdoo-contrib/qcl/trunk/sample_app/backend/php/services/qcl/config/db.php
@@ -36,14 +36,87 @@ class qcl_config_db extends qcl_config
 		parent::__construct();
 		$this->db = qcl_db::getDbObject($this->ini);
 	}
-
+	
+	/**
+	 * gets the database record for the config entry and for the specific user
+	 * @param string $name
+	 * @param mixed $userRef
+	 */
+	function _getRow($name,$userRef=null)
+	{			
+		if ( $userRef !== null )
+		{
+			// user reference given, this is usually only the
+			// case if a manager edits the configuration
+			$userId = $this->user->getByRef($userRef);
+			$row = $this->db->getRow("
+				SELECT * 
+				FROM {$this->table}
+				WHERE {$this->key_name} = '$name'
+				AND {$this->key_userId} = $userId
+			");
+			
+			// admins can read default value
+			if ( $userId == 0 and $this->user->hasPermission ("qcl.config.permissions.manage") )
+			{
+				return $row;
+			}
+			
+			// active user is allowed to acces their own data
+			if ( $row['id'] == $userId )
+			{
+				return $row;
+			}
+			if( $permissionRead = $row[$this->key_permissionRead] )
+			{
+				$this->user->requirePermisson($permissionRead);
+			}
+			return $row;
+		}
+		else
+		{
+			// no user reference given, assume active user
+			$activeUser 	= $this->user->getActiveUser(); 
+			$activeUserId 	= $activeUser[$this->user->key_id];
+	
+			// get all rows containing key name
+			$rows = $this->db->getAllRows("
+				SELECT * 
+				FROM {$this->table}
+				WHERE {$this->key_name} = '$name'
+					AND 
+					(
+						`{$this->key_userId}` = $activeUserId
+						OR `{$this->key_userId}` = 0
+						OR `{$this->key_userId}` = null
+					)
+				ORDER BY `{$this->key_userId}`
+			");
+			
+			if ( count($rows) )
+			{
+				// config entry has variants, return user variant
+				// since user can access own data
+				return $rows[1];
+			}
+			
+			// only a non-variant or default entry available, check permission
+			if( $permissionRead = $rows[0][$this->key_permissionRead] )
+			{
+				$this->user->requirePermisson($permissionRead);
+			}
+			return $rows[0];
+		}
+	}
+	
+	
 	//-------------------------------------------------------------
    	// public non-rpc methods 
 	//-------------------------------------------------------------    
  
 	/**
-	 * creates a config property, overwriting any previous entry unless the 
-	 * entry is a user variant of the base entry.
+	 * creates a config property, overwriting any previous entry
+	 * requires permission "qcl.config.permissions.manage"
 	 * 
 	 * @param string $name The name of the property (i.e., myapplication.config.locale)
 	 * @param string $type The type of the property (string|number|object|boolean)
@@ -51,127 +124,180 @@ class qcl_config_db extends qcl_config
 	 * 		  and read this property (optional)
 	 * @param string $permissionWrite The permission name that is needed to access 
 	 * 		  and read this property (optional)
-	 * @param mixed $userRef if this property has a user-defined variant, 
-	 * 		  it is stored as an additional entry with the user reference (usually user
-	 * 		  id) (optional)
-	 * @return true if success or false if there was an error
+	 * @param boolean $allowUserVariants If true, allow users to create their 
+	 * 		  own variant of the configuration setting 
+	 * @return true if success 
 	 */
-	function create($name, $type, $permissionRead=null, $permissionWrite=null, $userRef=null )
+	function create($name, $type, $permissionRead=null, $permissionWrite=null, $allowUserVariants=false )
 	{
+		$this->user->requirePermission("qcl.config.permissions.manage");
 		
-		// delete whole entry or user variant, also checks write access
-		$this->delete($name,$userRef);
+		$permissionRead  = $permissionRead ? "'$permissionRead'" : null;
+		$permissionWrite = $permissionWrite ? "'$permissionWrite'" : null;
+
+		// delete all previous entries
+		$this->db->execute("
+			DELETE FROM `{$this->table}`
+			WHERE  `{$this->key_name}` = '$name'
+		");			
 		
-		// create new entry or user variant
-		if ( $userRef === null)
-		{
-			$this->execute("
-				INSERT INTO `{$this->table}` 
-					(`{$this->key_name}`,`{$this->key_type}`,`{$this->key_permissionRead}`,`{$this->key_permissionWrite}`) 
-				VALUES 
-					('$name','$type'," . 
-						( $permissionRead ? "'$permissionRead'," : "null," ) .
-						( $permissionWrite ? "'$permissionWrite'" : "null)" ) 
-			);
-		}
-		else
-		{
-			$userId = $this->user->getByRef($userRef);
-			$this->execute("
-				INSERT INTO `{$this->table}` 
-					(`{$this->key_name}`,`{$this->key_type}`,`{$this->key_permissionRead}`,`{$this->key_permissionWrite}`) 
-				VALUES 
-					('$name','$type'," . 
-						( $permissionRead ? "'$permissionRead'," : "null," ) .
-						( $permissionWrite ? "'$permissionWrite'" : "null)" ) ."
-				WHERE `{$this->key_userId}` = $userId
-			");			
-		}
+		// create new entry
+		$userId = $allowUserVariants ? 0 : null;
+		
+		$this->db->execute("
+			INSERT INTO `{$this->table}` 
+				(`{$this->key_name}`,`{$this->key_type}`,
+				`{$this->key_permissionRead}`,`{$this->key_permissionWrite}`,
+				`{$this->key_userId}`) 
+			VALUES 
+				('$name','$type',$permissionRead,$permissionWrite,$userId)
+		");					
+
 		return true;
 	} 
 
 	/**
 	 * deletes a config property completely or only its user variant 
+	 * requires permission qcl.config.permissions.manage
 	 * 
 	 * @param string $name The name of the property (i.e., myapplication.config.locale)
-	 * @param mixed $userRef if the user-defined variant should be deleted (optional)
 	 * @return true if success or false if there was an error
 	 */
-	function delete($name, $userRef=null )
+	function delete($name )
 	{
-		$this->requireWriteAccess($name);
-		
-		if ( $userRef === null)
-		{
-			$this->execute("
-				DELETE FROM `{$this->table}` 
-				WHERE `{$this->key_name}` = $name 
-			");	
-		}
-		else
-		{
-			$userId = $this->user->getByRef($userRef);
-			$this->execute("
-				DELETE FROM `{$this->table}` 
-				WHERE `{$this->key_name}` = $name
-				AND `{$this->key_userId}` = $userId 
-			");	
-		}
+		$this->user->requirePermission("qcl.config.permissions.manage"); 
+		$this->db->execute("
+			DELETE FROM `{$this->table}` 
+			WHERE `{$this->key_name}` = '$name'
+		"); 
 		return true;
 	} 
- 
+	 
 	/**
 	 * gets config property value
 	 * @param string $name The name of the property (i.e., myapplication.config.locale) 
 	 * @return value of property or null if value does not exist.
 	 */
-	function get($name,$value)
+	function get($name)
 	{
-		
+		$row = $this->_getRow($name);
+		return $row[$this->key_value];	
 	} 	  
  
 	/**
 	 * sets config property
 	 * @param string $name The name of the property (i.e., myapplication.config.locale)
 	 * @param string $value The value of the property. 
+	 * @param boolean $defaultValue If true, set the key's default value for keys that allow
+	 *  	user variants. This is necessary so that the admin can change the defaults instead
+	 * 		of editing her/his own variant.
 	 * @return true if success or false if there was an error
 	 */
-	function set($name,$value)
+	function set($name,$value,$defaultValue=false)
 	{
-		$res = $this->db->getAllRows("SELECT id FROM config WHERE name = '$name'");
-		$id  = $res[0]['id'];
-		if ( $id )
+		// if we set the default value, we need to retrieve 
+		if ( $defaultValue )
 		{
-			$this->execute("UPDATE config SET value = '$value' WHERE id = $id ");
+			$row = $this->_getRow($name,0);	
 		}
 		else
 		{
-			$this->execute("INSERT INTO config (name,value) VALUES ('$name','$value')");
-			return $this->getLastInsertId();
+			$row = $this->_getRow($name);
 		}
+		
+		if ( ! count($row) )
+		{
+			$this->triggerError("qcl_config::set : no config key '$name' ");
+		}
+		
+		$id				 = $row[$this->key_id];
+		$type			 = $row[$this->key_type];
+		$permissionWrite = $row[$this->key_permissionWrite];
+		$userId			 = $row[$this->key_userId];
+		$activeUser 	 = $this->user->getActiveUser(); 
+		$activeUserId 	 = $activeUser[$this->user->key_id];
+		
+		// users can set their own entry variant with no further checking
+		if ( $userId != $activeUserId )
+		{
+			// but others need to check permission
+			$this->requirePermission( $permissionWrite );
+			
+			// create variant if necessary
+			if ( $userId == 0 and ! $defaultValue )
+			{
+				unset($row[$this->key_id]);
+				$id = $this->db->insert($this->table,$row);
+				$row[$this->key_id] = $id;
+			}
+			
+			// set default value
+			if ( $userId == 0 and $defaultValue )
+			{
+				$this->requirePermission("qcl.config.permissions.manage");
+			}				
+		}		
+				
+		// type checking
+		$type_error = false;
+		switch ( $row[$this->key_type] )
+		{
+			case "string" 	: if ( ! is_string($value) ) 	$type_error = true; break;
+			case "number" 	: if ( ! is_numeric($value) ) 	$type_error = true; break;
+			case "boolean" 	: if ( ! is_bool($value) ) 		$type_error = true; break;
+		}
+		if ( $type_error )
+		{
+			$this->triggerError("cql_config::set : Invalid type for '$name' (type '$type').");
+		}
+		
+		// all checks have been passed, set value
+		$this->db->execute("
+			UPDATE `{$this->table}` 
+			SET `{$this->key_value}` = '$value' 
+			WHERE `{$this->key_id}` = $id
+		");
+		return true;
 	}
 	
 	/**
 	 * gets required permission name for read access to config property
-	 * @param string $name
+	 * @param string $name name of configuration key
 	 */
 	function getPermissionRead($name)
 	{
-		$userId = 0;
-		$this->getRow("
-			SELECT * 
-			FROM `{$this->table}` 
-			WHERE `{$this->key_name}` = $name 
-		");	
+		$row = $this->_getRow($name);
+		return $row[$this->key_permissionRead];
 	}
 	
 	/**
+	 * gets required permission name for write access to config property
+	 * @param string $name name of configuration key
+	 */
+	function getPermissionWrite($name)
+	{
+		$row = $this->_getRow($name);
+		return $row[$this->key_permissionRead];
+	}
+
+	/**
 	 * checks if current user has the right to access the configuration 
 	 * property with read priviledges
+	 * @param string $name name of configuration key 
+	 */
+	 function hasReadAccess($name)
+	 {
+		return $this->user->hasPermission($this->getPermissionRead($name));
+	 }
+	
+	/**
+	 * checks if current user has the right to access the configuration 
+	 * property with write priviledges
+	 * @param string $name name of configuration key 
 	 */
 	 function hasWriteAccess($name)
 	 {
-		//
+		return $this->user->hasPermission($this->getPermissionWrite($name));
 	 }
 }
 
