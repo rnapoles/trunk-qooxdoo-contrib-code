6030fe123bafcdbbde3d90d0529c6989202c51e0
mlhartme <mlhartme@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2007-09-12 15:14:52 +0000

    revert plugin changes

--- qooxdoo-contrib/QWT/trunk/src/download/download.sh
+++ qooxdoo-contrib/QWT/trunk/src/download/download.sh
@@ -3,7 +3,6 @@ mkdir tmp-qwt || exit 1
 cd tmp-qwt
 wget -q http://qooxdoo-contrib.svn.sf.net/viewvc/*checkout*/qooxdoo-contrib/trunk/qooxdoo-contrib/QWT/trunk/src/download/pom.xml || exit 1
 mvn || exit 1
-mvn plugin:updateRegistry || exit 1
 cd ..
 rm -rf tmp-qwt
 
