fe7ff9028b94158d4517acfdc4db5495e782b4f7
mlhartme <mlhartme@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2007-09-12 06:59:17 +0000

    editing

--- qooxdoo-contrib/QWT/trunk/src/download/download.sh
+++ qooxdoo-contrib/QWT/trunk/src/download/download.sh
@@ -1,2 +1,8 @@
 #/bin/sh
+mkdir tmp
+cd tmp
+wget -q http://qooxdoo-contrib.svn.sf.net/viewvc/*checkout*/qooxdoo-contrib/trunk/qooxdoo-contrib/QWT/trunk/src/download/pom.xml
 mvn dependency:resolve
+cd ..
+rm -rf tmp
+
