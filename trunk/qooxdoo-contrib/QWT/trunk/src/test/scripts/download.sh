d0104490220963970308dbf5f32b6d37ef813a89
mlhartme <mlhartme@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2007-09-13 07:22:22 +0000

    fix

--- trunk/qooxdoo-contrib/QWT/trunk/src/test/scripts/download.sh
+++ trunk/qooxdoo-contrib/QWT/trunk/src/test/scripts/download.sh
@@ -1,11 +1,12 @@
 #/bin/sh
 export DIR=tmp-download
 export MAVEN_OPTS="-Xmx96m -client"
+rm -rf $HOME/.m2/repository
 rm -rf $DIR
-mkdir $DIR
-cd $DIR
+mkdir $DIR || exit 1
 # can't use wget, it's not available on all machines:
-cp src/download/pom.xml .
+cp src/download/pom.xml $DIR
+cd $DIR || exit 1
 mvn || exit 1
 mvn qwt:new -Dpackage=foo.bar || exit 1
 cd bar
