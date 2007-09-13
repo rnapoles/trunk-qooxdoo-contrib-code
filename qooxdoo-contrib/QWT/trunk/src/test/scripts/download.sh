5d78eebe3399e68a9f9ad5c7398d2509c2823aea
mlhartme <mlhartme@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2007-09-13 07:04:23 +0000

    wipe old download dir

--- qooxdoo-contrib/QWT/trunk/src/test/scripts/download.sh
+++ qooxdoo-contrib/QWT/trunk/src/test/scripts/download.sh
@@ -1,6 +1,7 @@
 #/bin/sh
 export DIR=tmp-download
 export MAVEN_OPTS="-Xmx96m -client"
+rm -rf $DIR
 mkdir $DIR
 cd $DIR
 wget http://qooxdoo-contrib.svn.sourceforge.net/viewvc/*checkout*/qooxdoo-contrib/trunk/qooxdoo-contrib/QWT/trunk/src/download/pom.xml
