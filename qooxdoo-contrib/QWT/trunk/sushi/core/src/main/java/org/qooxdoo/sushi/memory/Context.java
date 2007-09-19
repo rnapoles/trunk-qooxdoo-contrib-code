300d0bc2d83d67093af2edc2b0638bffb23eed37
mlhartme <mlhartme@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2007-09-19 14:22:26 +0000

    fix checkstyle

--- qooxdoo-contrib/QWT/trunk/sushi/core/src/main/java/org/qooxdoo/sushi/memory/Context.java
+++ qooxdoo-contrib/QWT/trunk/sushi/core/src/main/java/org/qooxdoo/sushi/memory/Context.java
@@ -23,7 +23,6 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
