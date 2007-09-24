e810f7e2a5037cd8b2d7287afdb2751c4d69641d
mlhartme <mlhartme@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2007-09-24 10:11:03 +0000

    bump version

--- qooxdoo-contrib/QWT/trunk/src/mirror.sh
+++ qooxdoo-contrib/QWT/trunk/src/mirror.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-scp -r mlhartme@shell.sf.net:/home/groups/q/qo/qooxdoo-contrib/htdocs/maven/repository repo
+scp -r mlhartme@shell.sf.net:/home/groups/q/qo/qooxdoo-contrib/htdocs/maven/repository/org/eclipse eclipse
 cd repo/org
 scp -r qooxdoo billy@devel.schlund.de:public_html/repository/org/qooxdoo
 
