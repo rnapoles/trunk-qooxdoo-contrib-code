d6f1d4507d076dd11276b09050ffc7efca73136d
mlhartme <mlhartme@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2007-10-01 13:04:12 +0000

    mirror site

--- trunk/qooxdoo-contrib/QWT/trunk/src/mirror.sh
+++ trunk/qooxdoo-contrib/QWT/trunk/src/mirror.sh
@@ -1,7 +1,13 @@
 #/bin/sh
-# TODO: merge into existing directories
-export REPO=$HOME/qooxdoo-repo
+
+export ROOT=$HOME/mirror
+export REPO=$ROOT/repository
+export SITE=$ROOT/site
+
+mkdir $ROOT
+mkdir $REPO
 mkdir $REPO/eclipse
+mkdir $SITE
 
 echo 
 echo Transfer from Billy
@@ -9,6 +15,7 @@ echo
 
 rsync -v -a --delete --rsh="ssh -l billy" billy@devel.schlund.de:public_html/repository/org/qooxdoo $REPO || exit 1
 rsync -v -a --delete --rsh="ssh -l billy" billy@devel.schlund.de:public_html/repository/org/eclipse/base $REPO/eclipse || exit 1
+rsync -v -a --delete --rsh="ssh -l billy" billy@devel.schlund.de:public_html/sites/qooxdoo $SITE || exit 1
 
 echo Done. 
 echo Press return to continue.
@@ -19,3 +26,5 @@ echo Transfer to Sourceforge
 echo
 
 rsync -v -a --delete --rsh="ssh -l mlhartme" $REPO/ mlhartme@shell.sourceforge.net:/home/groups/q/qo/qooxdoo-contrib/htdocs/maven/repository/org/
+rsync -v -a --delete --rsh="ssh -l mlhartme" $SITE/qooxdoo/ mlhartme@shell.sourceforge.net:/home/groups/q/qo/qooxdoo-contrib/htdocs/maven/sites/qooxdoo/
+
