c7a9e4b0dc56363635af176f42125152b6e5d717
mlhartme <mlhartme@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2007-10-05 14:05:56 +0000

    download test

--- qooxdoo-contrib/QWT/trunk/toolkit/src/test/scripts/download.sh
+++ qooxdoo-contrib/QWT/trunk/toolkit/src/test/scripts/download.sh
@@ -1,24 +1,24 @@
 #/bin/sh
-mkdir target
-cd target || exit 1
-export DIR=download
+export SCRIPTS=`pwd`
+echo scripts: $SCRIPTS
+
+export DIR=/tmp/download
 export MAVEN_OPTS="-Xmx56m -client"
-rm -rf $HOME/.m2/repository
-rm $HOME/.m2/settings.xml   # failure is ok!
-cp $HOME/.m2/initial-settings.xml $HOME/.m2/settings.xml  # failure is ok!
+export FIX="--batch-mode -s $SCRIPTS/download-settings.xml"
+rm -rf $HOME/.m2/qwt-download-repository  # failure is ok
 rm -rf $DIR
 mkdir $DIR || exit 1
 # can't use wget, it's not available on all machines:
-cp ../src/download/pom.xml $DIR
+cp $SCRIPTS/../src/download/pom.xml $DIR
 cd $DIR || exit 1
-mvn --batch-mode || exit 1
-mvn --batch-mode qx:new -Dpackage=foo.bar || exit 1
+mvn $FIX || exit 1
+rm pom.xml
+mvn $FIX qx:new -Dpackage=foo.bar || exit 1
 cd bar
 # run 'package', not 'compile -- to test memory consumption
-mvn --batch-mode clean package || exit 1
-mvn --batch-mode qx:uninstall || exit 1
-cd /
-mvn --batch-mode qx:help && exit 1
-
+mvn $FIX clean package || exit 1
+mvn $FIX qx:uninstall || exit 1
+cd ..
+mvn $FIX && exit 1
 exit 0
 
