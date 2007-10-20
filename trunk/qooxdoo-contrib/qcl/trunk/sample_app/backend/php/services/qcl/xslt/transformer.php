20fc28ce0d838a7110e6ddad3f8bfb3285dfcc5f
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2007-10-20 14:57:42 +0000

    changed to domxml extension

--- trunk/qooxdoo-contrib/qcl/trunk/sample_app/backend/php/services/qcl/xslt/transformer.php
+++ trunk/qooxdoo-contrib/qcl/trunk/sample_app/backend/php/services/qcl/xslt/transformer.php
@@ -16,14 +16,13 @@ class qcl_xslt_transformer extends qcl_jsonrpc_model
     
 	/**
 	 * transforms xml data with xsl stylesheet
-	 * @param mixed $xml string or filename of xml file to transform
-	 * @param mixed $xsl string or filename of xslt file to transform xml with
-	 * @param array $parameters an associated array to pass to the xsl as top-level parameters
-	 * @param string $fileBase file base for stylesheets 
-	 * @param string $target optional filename to write xml to
+	 * @param mixed 	$xml 		string or filename of xml file to transform
+	 * @param mixed 	$xsl 		string or filename of xslt file to transform xml with
+	 * @param array 	$params 	an associated array to pass to the xsl as top-level parameters
+	 * @param string 	$debugfile 	file to write debug information to 
 	 * @return string transformed xml
 	 */
-    function transform($xml,$xsl,$parameters=null,$filebase=null,$target=null)
+    function transform($xml,$xsl,$params=null,$debugfile=null)
     {
         
 		if ( function_exists( "domxml_open_file" ) )
@@ -33,78 +32,47 @@ class qcl_xslt_transformer extends qcl_jsonrpc_model
 			$domXmlObj 	= @is_file($xml) ? 
 				domxml_open_file($xml): 
 				domxml_open_mem ($xml);
+			if ( ! is_object($domXmlObj) )
+			{
+				file_put_contents($debugfile,$xml);
+				$this->raiseError("Invalid xml data");
+				
+			}
+			
 			$domXsltObj = @is_file($xsl) ? 
 				domxml_xslt_stylesheet_file($xsl) : 
 				domxml_xslt_stylesheet_doc(domxml_open_mem ($xsl));
 			
+			if ( ! is_object($domXmlObj) )
+			{
+				file_put_contents($debugfile,$xsl);
+				$this->raiseError("Invalid xsl data");
+			}
+			
+			$domTranObj = $domXsltObj->process($domXmlObj,$params,false,$debugfile);
+			
 			// process
-			$domTranObj = $domXsltObj->process($domXmlObj);
-			return $domXsltObj->result_dump_mem($domTranObj);			
+			if ( is_object ($domTranObj) )
+			{
+				return $domXsltObj->result_dump_mem($domTranObj);		
+			}
+			else
+			{
+				$message = "Transformation failed";
+				if ($debugfile)
+				{
+					$message .= file_get_contents($debugfile);
+				}
+				$this->raiseError($message);	
+			}	
 			
 		}
 		else
 		{
-	        // php-xslt extension with sablotron
-	        // just a headache! install domxml extension!!
-	        
-	        // prepare arguments
-	        $arguments = array();
-	        if ( @is_file ($xml) )
-	        {        
-				$xml = join('', file($xml) );
-	        }
-	        
-	        if ( @is_file ($xsl) )
-	        {
-	        	$xsl = join('', file($xsl) );
-	        }
-	
-	        $arg_buffer = array('/xml' => $xml, '/xsl' => $xsl);
-	        
-	        // Allocate a new XSLT processor
-	        $xh = xslt_create();
-	        
-	        // set file base
-	        if ( $filebase )
-	        { 
-	        	xslt_set_base ( $xh, 'file://' . $filebase );
-	        } 
-	        
-	        // Make $this object the callback resolver and set error handler
-	       	xslt_set_object($xh, $this);
-	        xslt_set_error_handler($xh, "error_handler");
-	        
-	        // logging
-	        $this->logfile = SERVICE_PATH . "bibliograph/var/log/xslt.log";
-	        @unlink ( $this->logfile );
-	        
-	        xslt_set_log($xh, true);
-			xslt_set_log($xh, $this->logfile );
-	        
-	        // Process the document
-	        $result = @xslt_process(
-	        	$xh, 'arg:/xml', 'arg:/xsl', $target, $arg_buffer, $parameters 
-	        );
-	        
-	        xslt_free($xh);
-	        
-	        return $result;			
+			$this->raiseError("dom_xml extension is not installed.");
 		}
 
     }
 
-	/**
-	 * error handler for sablotron
-	 */
-	function error_handler($handler, $errno, $level, $info)
-   	{
-		$error = "XSLT error: \n";
-		foreach ($info as $key => $value )
-		{
-			$error .= addslashes("$key: $value") . "\n";	
-		}
-		$error .= file_get_contents($this->logfile);
-		$this->log($error);
-		$this->raiseError($error);
-	}
+
 }
