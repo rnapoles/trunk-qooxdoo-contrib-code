9a256326d25132b9a3bae1774d0804b99321a265
martinwittemann <martinwittemann@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2007-11-05 15:33:55 +0000

    Added some function to the DOM view.

--- trunk/qooxdoo-contrib/Inspector/trunk/source/class/inspector/console/DomViewHtml.js
+++ trunk/qooxdoo-contrib/Inspector/trunk/source/class/inspector/console/DomViewHtml.js
@@ -42,9 +42,9 @@ qx.Class.define("inspector.console.DomViewHtml", {
     this._htmlEmbed.setWidth("100%");
     this._htmlEmbed.setHeight(174);
     this.add(this._htmlEmbed);
-	
-	// creaete the array used to  stor the naviagating path
-	this._objectsArray = [];
+    
+    // creaete the array used to  stor the naviagating path
+    this._objectsArray = [];
   },
 
 
@@ -59,13 +59,13 @@ qx.Class.define("inspector.console.DomViewHtml", {
        ATTRIBUTES
     *********************************
     */
-	// reference to the console
+    // reference to the console
     _console: null,
    
     // the main element
     _htmlEmbed: null,
-	
-	_objectsArray: null,
+    
+    _objectsArray: null,
     
     /*
     *********************************
@@ -80,50 +80,55 @@ qx.Class.define("inspector.console.DomViewHtml", {
       return [this, this._htmlEmbed];
     },
     
-	
+    
     setMainElementDeltaHeight: function(delta) {
       this._htmlEmbed.setHeight(this._htmlEmbed.getHeight() + delta);
     },
     
-	
+    
     focus: function() {
       // do nothing
     },
 
 
-	/**
-	 * Set a new object to inspect.
-	 * @param object {Object} The object to inspect.
-	 */
-	setObject: function(object) {
-	    this._htmlEmbed.setHtml(this._getHtmlToObject(object, 0));
+    /**
+     * Set a new object to inspect.
+     * @param object {Object} The object to inspect.
+     */
+    setObject: function(object) {
+        this._htmlEmbed.setHtml(this._getHtmlToObject(object, 0));
     },
-	
-	
-	/**
-	 * Sets a new object in the dom view.
-	 * @internal
-	 * @param index {Number} The index in the objects array.  
-	 * @param key {Object} The name of the value of the indexed object.
-	 */
-	setObjectByIndex: function(index, key) {
-		// if a key is given (selection of a object as a value) 
-		if (key) {
-			// select the given value object
-			var newQxObject = this._objectsArray[index][key];
-			// set the new object with a higher index
-			this._htmlEmbed.setHtml(this._getHtmlToObject(newQxObject, (index) + 1));
-		// if only a index is given (selection wia the back button)
-		} else {
-			// select the stored object in the array
-			var newQxObject = this._objectsArray[index];
-			// show the selected array with the current index
-			this._htmlEmbed.setHtml(this._getHtmlToObject(newQxObject, index));	
-		}
-	},
-	
-	
-	/**
+    
+    
+    /**
+     * Sets a new object in the dom view.
+     * @internal
+     * @param index {Number} The index in the objects array.  
+     * @param key {Object} The name of the value of the indexed object.
+     */
+    setObjectByIndex: function(index, key) {
+        try {
+            // if a key is given (selection of a object as a value) 
+            if (key) {
+                // select the given value object
+                var newQxObject = this._objectsArray[index][key];
+                // set the new object with a higher index
+                this._htmlEmbed.setHtml(this._getHtmlToObject(newQxObject, (index) + 1));
+            // if only a index is given (selection wia the back button)
+            } else {
+                // select the stored object in the array
+                var newQxObject = this._objectsArray[index];
+                // show the selected array with the current index
+                this._htmlEmbed.setHtml(this._getHtmlToObject(newQxObject, index));    
+            }
+        } catch (e) {
+            alert("Can not select this Object: " + e);
+        }
+        
+    },
+    
+    
+    /**
      * Clears the whole dom view.
      */
     clear: function() {
@@ -135,54 +140,157 @@ qx.Class.define("inspector.console.DomViewHtml", {
        PROTECTED
     *********************************
     */    
-		/**
-	 * Returns a html String which contains table of the objects porperties and values.
-	 * @internal
-	 * @param qxObject {Object} The object to return the html from.
-	 * @param index {Object} The index of the object path.
-	 */
+        /**
+     * Returns a html String which contains table of the objects porperties and values.
+     * @internal
+     * @param qxObject {Object} The object to return the html from.
+     * @param index {Object} The index of the object path.
+     */
     _getHtmlToObject: function(qxObject, index) {
-	  // create an empty string
+      // create an empty string
       var returnString = "";
-	  
-	  // if it is not the first object to see
-	  if (index > 0) {
-	  	// print the back button
-	  	returnString += "<div onclick='" + 
-							"inspector.Inspector.getInstance().inspectObjectByDomSelecet(" + (index-1) + ")'" + 
-					    " style='cursor: pointer; padding: 5px;'> &lt;&lt; Back</div>";
-	  }
-  
-	  // save the object a path array
-	  this._objectsArray[index] = qxObject;
-	  
-	  // go threw all properties of the object
-      for (var key in qxObject) {	  	
+
+      // save the object a path array
+      this._objectsArray[index] = qxObject;
+      
+      returnString += this._getReturnPath(index);
+      
+      // flat used to signal if porperties were print out
+      var nothingToShow = true;      
+      // go threw all properties of the object
+      for (var key in qxObject) {      
+        // kar that there has been a property printed out
+        nothingToShow = false;
         // start the table
-        returnString += "<div><table width='100%'>";
-		// if it is not an object
+        returnString += "<div style='font-family: Helvetica; margin-bottom: -5px'><table width='100%'>";
+
+        // if it is not an object
         if (!(qxObject[key] instanceof Object)) {
-          returnString += "<tr><td width='18'></td><td width='30%'><b>" + key + "</b></td>";
-          returnString += "<td><span style='color: white; background-color: #999999; border: 1px #666666 solid';>" + null + "</span></td></tr>";
+          returnString += "<tr><td width='40%'><font size='2'>" + key + "</font></td>";
+          
+          // if the value is null
+          if (qxObject[key] == null) {
+              returnString += "<td><span style='color: white; background-color: #999999; border: 1px #666666 solid;'><font size='2'>" + qxObject[key] + "</font></span></td></tr>";
+          } else if (typeof qxObject[key] == "string"){
+              returnString += "<td><span style='color: red;'><font size='2'>&quot;" + qxObject[key] + "&quot;</font></span></td></tr>";              
+          } else {
+                returnString += "<td><span style='color: darkblue;'><font size='2'>"  + qxObject[key] + "</font></span></td></tr>";
+          }
 
         // if it is an object          
         } else {
-		  // print out the objects key          
-		  returnString += "<tr>" + 
-                            "<td width='18' valign='top'>" + 
-                              "<img src='" + qx.io.Alias.getInstance().resolve("inspector/image/open.gif") + "' style='cursor: pointer;' onclick='" + 
-                                "inspector.Inspector.getInstance().inspectObjectByDomSelecet(" + index + ", \"" + key + "\")" + 
-                              "'>" + 
-                            "</td>" + 
-                            "<td width='30%' valign='top'>" + 
-                              "<b>" + key + "</b>" + 
+          // print out the objects key          
+          returnString += "<tr>" + 
+                            "<td width='40%' valign='top'><font size='2'><b>" + key + "</b></font>" +  
                             "</td>";
-		  // print out the objects value
-          returnString += "<td><span style='color: green;'>" + qxObject[key] + "</span></td></tr>";          
+          // print out the objects value
+          returnString += "<td>" + this._getObject(qxObject[key], index, key) + "</td></tr>";          
+
         }
-	    // end the table 
+        // end the table 
         returnString += "</table></div>";
       }
+      
+      // if there is no property
+      if (nothingToShow) {
+          returnString += "<div style='padding: 10px; color: #999999; font-family: Helvetica;'>There are no properties to show for this object.</div>";
+      }
+      
+      // if the current object is a function
+      if (qxObject instanceof Function) {
+          // print out the toString stuff (function code)
+          returnString += "<div style='margin-top: 10px; padding: 10px;  border-top: 1px #666666 solid; color: #666666; font-family: Helvetica;'><font size='2'>" + qxObject + "</font></div>";
+      }
+      
+      return returnString;
+    },
+    
+    
+    /**
+     * Creates a html div as a string. This html shows a
+     * path back threw the selected objects.
+     * @param index {Number} The current index of the path.
+     */
+    _getReturnPath: function(index) {
+      // print the path to go back
+      var returnString = "<div style='font: bold 12px Helvetica; border-bottom: 1px #555555 solid; padding: 5px; background-color: #000000; color: #FFFFFF;'>";  
+      
+      // go threw the existing path
+      for (var i = 0; i <= index; i++) {
+        // if it is the current item
+          if (i == index) {
+            returnString += " &raquo; <span style='color: #666666;'>"
+        } else {
+              // print out every item of the path
+            returnString += " &raquo; <span style='cursor: pointer;' onclick='" + 
+                            "inspector.Inspector.getInstance().inspectObjectByDomSelecet(" + i + ")'>";            
+        }
+        // do print the functions as function()
+        if (this._objectsArray[i] instanceof Function) {
+            returnString += "function()";    
+        // check for arrays
+        } else if (this._objectsArray[i] instanceof Array) {
+            returnString += "<b>Array</b>";
+        } else {
+            returnString += this._objectsArray[i];
+        }
+        
+        returnString += "</span>";
+      }
+      // end tht leading div
+      returnString += "</div>";
+      
+      return returnString;
+    },
+    
+    
+    /**
+     * Returns a html stringcontaining a link to select the given object.
+     * @param object {Object} The object to select.
+     * @param index {Number} The index in the path to the selectd object.
+     * @param index {String} The porperty to select the new object.
+     */
+    _getObject: function(object, index, key) {
+      var returnString = "<span style='color: green; cursor: pointer;' onclick='" +
+                             "inspector.Inspector.getInstance().inspectObjectByDomSelecet(" + index + ", \"" + key + "\")" + 
+                          "'><font size='2'>";
+                          
+      // if it is a function
+      if (object instanceof Function) {
+          returnString += "function()";
+              
+      // if it is an array
+      } else if (object instanceof Array) {
+        returnString += "[ ";
+          // if it has more than two elements
+          if (object.length > 2) {
+              // print out the first tow elements
+            for (var j = 0; j < 2; j++) {
+                returnString += object[j] + ", ";
+            }
+            // print out a message that there are more
+            returnString += " ... <b>" + (object.length - 2) + " more</b> ]";
+        // if it is an empty array                
+        } else if (object.length == 0) {
+            returnString += " ]";
+        // if there are one or two elements
+        } else {
+            // print out all containing elements
+            for (var j = 0; j < 2 && j < object.length; j++) {
+                returnString += object[j];
+                // print out the comma only if it is not the last element
+                if (j != 1 && j != object.length - 1) {
+                    returnString +=  ", ";
+                }
+            }
+            returnString += " ]";            
+        }
+        
+      // if it is a regular object
+      } else {
+          returnString += object;
+      }      
+      returnString += "</font></span>";
       
       return returnString;
     }
