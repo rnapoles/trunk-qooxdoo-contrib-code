02c00dd6bcf939bf72fa8aa19f2708c0728134d8
mlhartme <mlhartme@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2007-11-14 14:33:38 +0000

    test fixes

--- qooxdoo-contrib/QWT/trunk/sushi/core/src/main/java/org/qooxdoo/sushi/io/OS.java
+++ qooxdoo-contrib/QWT/trunk/sushi/core/src/main/java/org/qooxdoo/sushi/io/OS.java
@@ -1,38 +1,37 @@
 /* ************************************************************************
-   
+
    qooxdoo - the new era of web development
-   
+
    http://qooxdoo.org
-   
+
    Copyright:
      2006-2007 1&1 Internet AG, Germany, http://www.1and1.org
-   
+
    License:
      LGPL: http://www.gnu.org/licenses/lgpl.html
      EPL: http://www.eclipse.org/org/documents/epl-v10.php
      See the LICENSE file in the project's top-level directory for details.
-   
+
    Authors:
-     * Michael Hartmeier (mlhartme)
-   
+ * Michael Hartmeier (mlhartme)
+
  ************************************************************************ */
 
 package org.qooxdoo.sushi.io;
 
 public enum OS {
-    MAC("Mac", "$", "", "\n", "-f", "%Op"),
-    WINDOWS("Windows", "%", "%", "\r\n", "/f", "%a"),
-    LINUX("Linux", "$", "", "\n", "--format", "%a");
+    MAC("Mac", "$", "", "\n", "-f", "%Op"), WINDOWS("Windows", "%", "%",
+            "\r\n", "/f", "%a"), LINUX("Linux", "$", "", "\n", "--format", "%a");
 
     private static OS detect() {
         String name;
 
         name = System.getProperty("os.name");
         for (OS os : values()) {
-    		if (name.contains(os.substring)) {
-    			return os;
-    		}
-    	}
+            if (name.contains(os.substring)) {
+                return os;
+            }
+        }
         throw new IllegalArgumentException("unkown os:" + name);
     }
 
@@ -43,17 +42,17 @@ public enum OS {
     private final String variableSuffix;
     public final String lineSeparator;
     public final String[] stat;
-    
-    private OS(String substring, String variablePrefix, String variableSuffix, 
-    		String lineSeparator, String... stat) {
-    	this.substring = substring;
-    	this.variablePrefix = variablePrefix;
-    	this.variableSuffix = variableSuffix;
-    	this.lineSeparator = lineSeparator;
+
+    private OS(String substring, String variablePrefix, String variableSuffix,
+            String lineSeparator, String... stat) {
+        this.substring = substring;
+        this.variablePrefix = variablePrefix;
+        this.variableSuffix = variableSuffix;
+        this.lineSeparator = lineSeparator;
         this.stat = stat;
     }
-    
+
     public String variable(String name) {
-    	return variablePrefix + name + variableSuffix;
+        return variablePrefix + name + variableSuffix;
     }
 }
