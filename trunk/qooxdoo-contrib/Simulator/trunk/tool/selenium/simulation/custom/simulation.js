1c04c51209ca21816e9ee252eb7838c3874678bb
thron7 <thron7@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2007-12-11 18:38:50 +0000

    parameterized custom/simulation.js for keeping the browser open after tests (or not)

--- trunk/qooxdoo-contrib/Simulator/trunk/tool/selenium/simulation/custom/simulation.js
+++ trunk/qooxdoo-contrib/Simulator/trunk/tool/selenium/simulation/custom/simulation.js
@@ -31,25 +31,46 @@ var config =
     sleep : 2000
 };
 
-function simulation()
+function simulation(keepBrowser)
 {
-  sel.qxClick('button');
-  sel.stop();
+  sel.qxClick('button'); // using HTML id 'button'
+  sel.qxClick('First');  // using HTML id 'First'
+  sel.qxClick('Second'); // using HTML id 'Second'
+
+  sel.type('tf',"");     // clear textfield
+  sel.qxClick('qx=thefatbutton');  // click button again, using qooxdoo UserData
+  sel.qxClick('qxh=app:c1/c2/c3/First'); // first tab, using object nesting
+  sel.qxClick('qxh=*/[@label="Second Tab"]'); // second tab, using qooxdoo widget hierarchy
+  if (!keepBrowser)
+  {
+    sel.stop();  // this would tear down the browser
+  }
 }
 
 function init()
 {
+  // create Selenium object
   sel = new QxSelenium(config.serverHost, config.serverPort, 
       config.browserStartCommand, config.browserUrl);
   
   sel.setSpeed(config.speed);
   
-  sel.start();
-  sel.open(config.browserUrl + config.appUrl);
+  sel.start(); // this launches the browser
+  sel.open(config.browserUrl + config.appUrl);  // this opens the AUT
   
-  Packages.java.lang.Thread.sleep(config.sleep);
+  Packages.java.lang.Thread.sleep(config.sleep); // pausing is often helpful
 }
+
+var keepBrowser = false;
 
 init();
 
-simulation();
+for (var i in arguments)
+{
+  if (arguments[i]=="-keepBrowser")
+  {
+    keepBrowser = true;
+  }
+}
+
+simulation(keepBrowser);
