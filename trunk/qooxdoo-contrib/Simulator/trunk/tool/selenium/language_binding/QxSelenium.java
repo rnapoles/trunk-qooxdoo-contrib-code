527a79c25a2efeaefa975447a49c9571db2cbcfd
thron7 <thron7@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2007-12-19 17:59:49 +0000

    add module header

--- trunk/qooxdoo-contrib/Simulator/trunk/tool/selenium/language_binding/QxSelenium.java
+++ trunk/qooxdoo-contrib/Simulator/trunk/tool/selenium/language_binding/QxSelenium.java
@@ -1,3 +1,36 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2007 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Thomas Herchenroeder (thron7)
+
+************************************************************************ */
+
+qx.Class.define("qxunit.runner._mod_head",
+{
+  extend : qx.core.Object,
+
+  construct : function()
+  {
+    this.base(arguments);
+  },
+
+  members : {
+
+  }
+});
+
 package com.thoughtworks.selenium;
 
 public class QxSelenium extends DefaultSelenium
