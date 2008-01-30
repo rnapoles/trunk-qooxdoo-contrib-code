7c6e551593f7882c2dc4f05fdce8e8db3f369662
mlhartme <mlhartme@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-01-30 06:43:20 +0000

    adjust

--- qooxdoo-contrib/QWT/trunk/src/mirror.sh
+++ qooxdoo-contrib/QWT/trunk/src/mirror.sh
@@ -21,7 +21,7 @@ echo
 echo Transfer from Billy
 echo
 
-rsync -v -a --delete --rsh="ssh -l billy" billy@devel.schlund.de:public_html/sites/qooxdoo $SITE || exit 1
+rsync -v -a --delete --rsh="ssh -l billy" billy@devel.schlund.de:public_html/sites/qooxdoo-0.2.0 $SITE || exit 1
 rsync -v -a --delete --rsh="ssh -l billy" billy@devel.schlund.de:public_html/repository/org/qooxdoo $REPO || exit 1
 rsync -v -a --delete --rsh="ssh -l billy" billy@devel.schlund.de:public_html/repository/org/eclipse/base $REPO/eclipse || exit 1
 
@@ -33,6 +33,6 @@ echo
 echo Transfer to Sourceforge
 echo
 
-rsync -v -a --delete --rsh="ssh -l mlhartme" $SITE/qooxdoo/ mlhartme@shell.sourceforge.net:/home/groups/q/qo/qooxdoo-contrib/htdocs/maven/sites/qooxdoo/
+rsync -v -a --delete --rsh="ssh -l mlhartme" $SITE/qooxdoo-0.2.0/ mlhartme@shell.sourceforge.net:/home/groups/q/qo/qooxdoo-contrib/htdocs/maven/sites/qooxdoo-0.2.0/
 rsync -v -a --delete --rsh="ssh -l mlhartme" $REPO/ mlhartme@shell.sourceforge.net:/home/groups/q/qo/qooxdoo-contrib/htdocs/maven/repository/org/
 
