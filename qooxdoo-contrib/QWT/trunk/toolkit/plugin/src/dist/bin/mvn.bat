2298c0927e2476ecaba72c874165ae5ab85784e8
mlhartme <mlhartme@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-01-30 06:42:42 +0000

    fix problem with blanks in path

--- qooxdoo-contrib/QWT/trunk/toolkit/plugin/src/dist/bin/mvn.bat
+++ qooxdoo-contrib/QWT/trunk/toolkit/plugin/src/dist/bin/mvn.bat
@@ -7,12 +7,13 @@ if "%OS%"=="Windows_NT" SET QWT_HOME=%~dp0..
 if not "%QWT_HOME%"=="" goto start
 
 echo.
-echo Cannot locate QWT_HOME, please set a QWT_HOME environment variable.
+echo Cannot locate QWT_HOME, please define a QWT_HOME environment variable pointing
+echo to the install directory.
 echo.
 goto end
 
 :start
 set QWT_OPTS="-Dmaven.repo.local=%QWT_HOME%\repository" "-Dorg.apache.maven.global-settings=%QWT_HOME%\bin\settings.xml"
-%QWT_HOME%\apache-maven-2.0.8\bin\mvn %QWT_OPTS% %*
+"%QWT_HOME%\apache-maven-2.0.8\bin\mvn" %QWT_OPTS% %*
 
 :end
