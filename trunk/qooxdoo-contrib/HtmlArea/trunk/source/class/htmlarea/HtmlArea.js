7b03f5326891e7db35e735ac94130add5a00b3b8
mhaitz <mhaitz@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-02-22 15:51:30 +0000

    Bug 372: fixed scrollbars

--- trunk/qooxdoo-contrib/HtmlArea/trunk/source/class/htmlarea/HtmlArea.js
+++ trunk/qooxdoo-contrib/HtmlArea/trunk/source/class/htmlarea/HtmlArea.js
@@ -505,7 +505,7 @@ qx.Class.define("htmlarea.HtmlArea",
         doctype : '<!' + 'DOCTYPE html PUBLIC "-/' + '/W3C/' + '/DTD XHTML 1.0 Transitional/' + '/EN" "http:/' + '/www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">',
         html    : '<html xmlns="http:/' + '/www.w3.org/1999/xhtml" xml:lang="en" lang="en">',
         meta    : '<meta http-equiv="Content-type" content="text/html; charset=UTF-8" /><title></title>',
-        style   : 'html { overflow-y: auto; margin:0px; padding:0px; } body { background-color:transparent; background-image:none; margin:0px; padding:1px; width:100%; height:100%; }',
+        style   : 'html { margin:0px; padding:5px; } body { background-color:transparent; overflow-y: auto; background-image:none; margin:0px; padding:0px; width:100%; height:100%; }',
         body    : '<body id="bodyElement">\n',
         footer  : '</body></html>'
       }
