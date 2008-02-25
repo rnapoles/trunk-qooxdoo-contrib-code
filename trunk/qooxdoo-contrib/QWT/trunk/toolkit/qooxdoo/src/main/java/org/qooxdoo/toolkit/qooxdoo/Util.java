a4ca2c6b8259dfa5d3372a85a5cbbd8eecc09f91
mlhartme <mlhartme@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-02-25 07:29:55 +0000

    alert button

---
+++ trunk/qooxdoo-contrib/QWT/trunk/toolkit/qooxdoo/src/main/java/org/qooxdoo/toolkit/qooxdoo/Util.java
@@ -0,0 +1,9 @@
+package org.qooxdoo.toolkit.qooxdoo;
+
+/** 
+ * Helper class to privide JavaScript features in Java. 
+ */
+public class Util {
+    /** @native alert(msg) */
+    public static native void alert(String msg);
+}
