10cbb556385ac0825ff65b5b2ed3ecfb7a8e5094
mlhartme <mlhartme@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-03-17 15:30:38 +0000

    better tests

--- qooxdoo-contrib/QWT/trunk/toolkit/qooxdoo/src/test/java/org/qooxdoo/toolkit/qooxdoo/LoadFullTest.java
+++ qooxdoo-contrib/QWT/trunk/toolkit/qooxdoo/src/test/java/org/qooxdoo/toolkit/qooxdoo/LoadFullTest.java
@@ -19,31 +19,59 @@
 
 package org.qooxdoo.toolkit.qooxdoo;
 
-import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.qooxdoo.sushi.io.IO;
 import org.qooxdoo.toolkit.repository.Module;
 import org.qooxdoo.toolkit.repository.Repository;
 import org.qooxdoo.toolkit.rhino.QooxdooEngine;
-import org.qooxdoo.sushi.io.IO;
 
+@RunWith(Parameterized.class)
 public class LoadFullTest {
-    @Test
-    public void load() throws Exception {
-        IO io;
-        Repository repository;
-        QooxdooEngine engine;
-
+    private static IO io;
+    private static Repository repository;
+    
+    @Parameters
+    public static List<Module[]> modules() {
+        List<Module[]> modules;
+        
         io = new IO();
         repository = Qooxdoo.load(io).repository;
+        modules = new ArrayList<Module[]>();
         for (Module module : repository) {
             if (module.getName().startsWith("qx.")) {
-                engine = new QooxdooEngine(io);
-                try {
-                    engine.load(module);
-                } catch (Exception e) {
-                    throw new RuntimeException(module.getName(), e);
-                }
+                modules.add(new Module[] { module });
             }
         }
+        return modules;
+    }
+    
+    //--
+    
+    private Module module;
+    
+    public LoadFullTest(Module module) {
+        this.module = module;
+    }
+
+    public String getName() {
+        return "name " + module.getName();
+    }
+    
+    public String toString() {
+        return "toString " + module.getName();
+    }
+    
+    @Test
+    public void run() throws Exception {
+        QooxdooEngine engine;
+
+        engine = new QooxdooEngine(io);
+        engine.load(module);
     }
 }
