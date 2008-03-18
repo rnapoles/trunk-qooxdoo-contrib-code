07094b69e054a02afb60897379a880f0b94e56e2
mlhartme <mlhartme@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-03-18 10:35:26 +0000

    improved test output

--- qooxdoo-contrib/QWT/trunk/toolkit/qooxdoo/src/test/java/org/qooxdoo/toolkit/qooxdoo/LoadFullTest.java
+++ qooxdoo-contrib/QWT/trunk/toolkit/qooxdoo/src/test/java/org/qooxdoo/toolkit/qooxdoo/LoadFullTest.java
@@ -19,54 +19,23 @@
 
 package org.qooxdoo.toolkit.qooxdoo;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 import org.qooxdoo.sushi.io.IO;
 import org.qooxdoo.toolkit.repository.Module;
-import org.qooxdoo.toolkit.repository.Repository;
 import org.qooxdoo.toolkit.rhino.QooxdooEngine;
 
-@RunWith(Parameterized.class)
+// JUnit Parameters doesn't print proper test names
+@RunWith(Modules.class)
 public class LoadFullTest {
-    private static IO io;
-    private static Repository repository;
+    private final IO io;
+    private final Module module;
     
-    @Parameters
-    public static List<Module[]> modules() {
-        List<Module[]> modules;
-        
-        io = new IO();
-        repository = Qooxdoo.load(io).repository;
-        modules = new ArrayList<Module[]>();
-        for (Module module : repository) {
-            if (module.getName().startsWith("qx.")) {
-                modules.add(new Module[] { module });
-            }
-        }
-        return modules;
-    }
-    
-    //--
-    
-    private Module module;
-    
-    public LoadFullTest(Module module) {
+    public LoadFullTest(IO io, Module module) {
+        this.io = io;
         this.module = module;
     }
 
-    public String getName() {
-        return "name " + module.getName();
-    }
-    
-    public String toString() {
-        return "toString " + module.getName();
-    }
-    
     @Test
     public void run() throws Exception {
         QooxdooEngine engine;
