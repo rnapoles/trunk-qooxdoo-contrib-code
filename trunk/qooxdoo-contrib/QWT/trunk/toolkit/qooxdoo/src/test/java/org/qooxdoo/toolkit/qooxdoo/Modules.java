0ddafd95979b2d6d497d797a7c6e42010c9f4e25
mlhartme <mlhartme@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-03-18 10:35:26 +0000

    improved test output

---
+++ trunk/qooxdoo-contrib/QWT/trunk/toolkit/qooxdoo/src/test/java/org/qooxdoo/toolkit/qooxdoo/Modules.java
@@ -0,0 +1,101 @@
+package org.qooxdoo.toolkit.qooxdoo;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+import org.junit.Assert;
+import org.junit.internal.runners.ClassRoadie;
+import org.junit.internal.runners.CompositeRunner;
+import org.junit.internal.runners.InitializationError;
+import org.junit.internal.runners.JUnit4ClassRunner;
+import org.junit.internal.runners.MethodValidator;
+import org.junit.internal.runners.TestClass;
+import org.junit.runner.notification.RunNotifier;
+import org.qooxdoo.sushi.io.IO;
+import org.qooxdoo.toolkit.repository.Module;
+import org.qooxdoo.toolkit.repository.Repository;
+
+public class Modules extends CompositeRunner {
+    private final TestClass modulesTest;
+
+    public Modules(Class<?> klass) throws Exception {
+        super(klass.getName());
+
+        IO io;
+        Repository repository;
+        
+        io = new IO();
+        repository = Qooxdoo.load(io).repository;
+
+        modulesTest= new TestClass(klass);
+        
+        MethodValidator methodValidator= new MethodValidator(modulesTest);
+        methodValidator.validateStaticMethods();
+        methodValidator.validateInstanceMethods();
+        methodValidator.assertValid();
+        
+        for (Module module : repository) {
+            if (module.getName().startsWith("qx.")) {
+                add(new ModuleRunner(modulesTest, io, module));
+            }
+        }
+    }
+    
+    @Override
+    public void run(final RunNotifier notifier) {
+        new ClassRoadie(notifier, modulesTest, getDescription(), new Runnable() {
+            public void run() {
+                runChildren(notifier);
+            }
+        }).runProtected();
+    }
+    
+    public static class ModuleRunner extends JUnit4ClassRunner {
+        private final IO io;
+        private final Module module;
+        private final Constructor<?> create;
+
+        ModuleRunner(TestClass testClass, IO io, Module module) throws InitializationError {
+            super(testClass.getJavaClass());
+
+            this.io = io;
+            this.module = module;
+            this.create= getOnlyConstructor();
+        }
+
+        @Override
+        protected Object createTest() throws Exception {
+            return create.newInstance(io, module);
+        }
+        
+        @Override
+        protected String getName() {
+            return String.format(module.getName());
+        }
+        
+        @Override
+        protected String testName(final Method method) {
+            return String.format(module.getName() + "." + method.getName());
+        }
+
+        private Constructor<?> getOnlyConstructor() {
+            Constructor<?>[] constructors= getTestClass().getJavaClass().getConstructors();
+            Assert.assertEquals(1, constructors.length);
+            return constructors[0];
+        }
+        
+        @Override
+        protected void validate() throws InitializationError {
+            // do nothing: validated before.
+        }
+        
+        @Override
+        public void run(RunNotifier notifier) {
+            runMethods(notifier);
+        }
+    }
+}
