a7c56256fcc420134c79bd94a11c6b50d7a72f66
mhaitz <mhaitz@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-03-31 14:41:17 +0000

    correct iframe size (ie/ff switch)

--- qooxdoo-contrib/HtmlArea/trunk/source/class/htmlarea/HtmlArea.js
+++ qooxdoo-contrib/HtmlArea/trunk/source/class/htmlarea/HtmlArea.js
@@ -532,7 +532,13 @@ qx.Class.define("htmlarea.HtmlArea",
         doctype : '<!' + 'DOCTYPE html PUBLIC "-/' + '/W3C/' + '/DTD XHTML 1.0 Transitional/' + '/EN" "http:/' + '/www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">',
         html    : '<html xmlns="http:/' + '/www.w3.org/1999/xhtml" xml:lang="en" lang="en"><title></title>',
         meta    : '<meta http-equiv="Content-type" content="text/html; charset=UTF-8" />',
-        style   : 'html { width:100%; height:100%; margin:0px; padding:5px; overflow-y: auto; } body { background-color:transparent; overflow:show; background-image:none; margin:0px; padding:0px; padding-right: 5px; }',
+        style   : qx.core.Variant.select("qx.client",
+        {
+           "mshtml"  : 'html { margin:0px; padding:0px; overflow: hidden; } ' +
+                       'body { width:100%; height:100%;background-color:transparent; overflow:show; background-image:none; margin:0px; padding:5px; }',
+           "default" : 'html { width:100%; height:100%;margin:0px; padding:0px; overflow-y: auto; overflow-x: hidden; } ' +
+                       'body { background-color:transparent; overflow:show; background-image:none; margin:0px; padding:5px; }'
+        }),
         body    : '<body id="bodyElement">\n',
         footer  : '</body></html>'
       }
