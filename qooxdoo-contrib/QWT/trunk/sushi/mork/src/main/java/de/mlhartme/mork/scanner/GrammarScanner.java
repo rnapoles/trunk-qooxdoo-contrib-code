9c03a8fb41ecab55e651596813a865f5f2fb91c8
mlhartme <mlhartme@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-04-02 05:54:21 +0000

    qooxdoo file header

--- qooxdoo-contrib/QWT/trunk/sushi/mork/src/main/java/de/mlhartme/mork/scanner/GrammarScanner.java
+++ qooxdoo-contrib/QWT/trunk/sushi/mork/src/main/java/de/mlhartme/mork/scanner/GrammarScanner.java
@@ -1,13 +1,21 @@
-// §{{header}}:
-//
-// This is file src/de/mlhartme/mork/scanner/GrammarScanner.java,
-// Mork version 0.6 Copyright (c) 1998-2002  Michael Hartmeier
-// Mork is licensed under the terms of the GNU Lesser General Public License.
-// It is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE. See the file license.txt for details.
-//
-// §.
+/* ************************************************************************
+   
+   qooxdoo - the new era of web development
+   
+   http://qooxdoo.org
+   
+   Copyright:
+     2006-2007 1&1 Internet AG, Germany, http://www.1and1.org
+   
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+   
+   Authors:
+     * Michael Hartmeier (mlhartme)
+   
+ ************************************************************************ */
 
 package de.mlhartme.mork.scanner;
 
