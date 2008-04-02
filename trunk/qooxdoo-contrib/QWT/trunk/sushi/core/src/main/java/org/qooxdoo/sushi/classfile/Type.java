3420c59972e87dc8f6ada9a9505942baba9651ea
mlhartme <mlhartme@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-04-02 09:52:27 +0000

    tweaks

--- trunk/qooxdoo-contrib/QWT/trunk/sushi/core/src/main/java/org/qooxdoo/sushi/classfile/Type.java
+++ trunk/qooxdoo-contrib/QWT/trunk/sushi/core/src/main/java/org/qooxdoo/sushi/classfile/Type.java
@@ -37,14 +37,13 @@ public class Type implements Bytecodes, Constants {
     public final int arrayLoad;
     public final int arrayStore;
 
-    private Type(int id, char descriptor,
-                 Class<?> type, Class<?> wrapper,
+    private Type(int id, char descriptor, Class<?> wrapper,
                  Object zero, Object zeroLdc, int size,
                  int load, int store, int arrayLoad, int arrayStore) {
         this.id = id;
+        this.type = getPrimitive(wrapper);
         this.name = (type == null ? null : type.getName());
         this.descriptor = descriptor;
-        this.type = type;
         this.wrapper = wrapper;
         this.zero = zero;
         this.zeroLdc = zeroLdc;
@@ -55,51 +54,56 @@ public class Type implements Bytecodes, Constants {
         this.arrayStore = arrayStore;
     }
 
+    private static Class<?> getPrimitive(Class<?> wrapper) {
+        if (wrapper == null) {
+            return null;
+        }
+        try {
+            return (Class<?>) wrapper.getDeclaredField("TYPE").get(null);
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException(e);
+        } catch (SecurityException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
     private static final Integer ZERO = new Integer(0);
 
     public static final Type[] PRIMITIVES = {
         // primitive types
-        new Type(T_VOID,      'V', Void.TYPE,      Void.class,
-            null,                    null,          0,
-            -1, -1, -1, -1),
+            new Type(T_VOID, 'V', Void.class, null, null, 0, -1, -1, -1, -1),
 
-        new Type(T_BOOLEAN,   'Z', Boolean.TYPE,   Boolean.class,
-            new Boolean(false),      ZERO,          1,
-            ILOAD, ISTORE, BALOAD, BASTORE),
+            new Type(T_BOOLEAN, 'Z', Boolean.class, new Boolean(false), ZERO,
+                    1, ILOAD, ISTORE, BALOAD, BASTORE),
             // bytecode has no instruction to access boolean arrays,
             // they are accessed as byte arrays
 
-        new Type(T_CHAR,      'C', Character.TYPE, Character.class,
-            new Character((char) 0), ZERO,          1,
-            ILOAD, ISTORE, CALOAD, CASTORE),
+            new Type(T_CHAR, 'C', Character.class, new Character((char) 0),
+                    ZERO, 1, ILOAD, ISTORE, CALOAD, CASTORE),
 
-        new Type(T_FLOAT,     'F', Float.TYPE,     Float.class,
-            new Float(0),            new Float(0),  1,
-            FLOAD, FSTORE, FALOAD, FASTORE),
+            new Type(T_FLOAT, 'F', Float.class, new Float(0), new Float(0), 1,
+                    FLOAD, FSTORE, FALOAD, FASTORE),
 
-        new Type(T_DOUBLE,    'D', Double.TYPE,    Double.class,
-            new Double(0),           new Double(0), 2,
-            DLOAD, DSTORE, DALOAD, DASTORE),
+            new Type(T_DOUBLE, 'D', Double.class, new Double(0), new Double(0),
+                    2, DLOAD, DSTORE, DALOAD, DASTORE),
 
-        new Type(T_BYTE,      'B', Byte.TYPE,      Byte.class,
-            new Byte((byte) 0),      ZERO,          1,
-            ILOAD, ISTORE, BALOAD, BASTORE),
+            new Type(T_BYTE, 'B', Byte.class, new Byte((byte) 0), ZERO, 1,
+                    ILOAD, ISTORE, BALOAD, BASTORE),
 
-        new Type(T_SHORT,     'S', Short.TYPE,     Short.class,
-            new Short((short) 0),    ZERO,          1,
-            ILOAD, ISTORE, SALOAD, SASTORE),
+            new Type(T_SHORT, 'S', Short.class, new Short((short) 0), ZERO, 1,
+                    ILOAD, ISTORE, SALOAD, SASTORE),
 
-        new Type(T_INT,       'I', Integer.TYPE,   Integer.class,
-            ZERO,                    ZERO,          1,
-            ILOAD, ISTORE, IALOAD, IASTORE),
+            new Type(T_INT, 'I', Integer.class, ZERO, ZERO, 1, ILOAD, ISTORE,
+                    IALOAD, IASTORE),
 
-        new Type(T_LONG,      'J', Long.TYPE,      Long.class,
-            new Long(0),             new Long(0),   2,
-            LLOAD, LSTORE, LALOAD, LASTORE),
+            new Type(T_LONG, 'J', Long.class, new Long(0), new Long(0), 2,
+                    LLOAD, LSTORE, LALOAD, LASTORE),
     };
 
     public static final Type REFERENCE =
-        new Type(T_REFERENCE, 'L', null,           null,
-                 null,                    null,          1,
-                 ALOAD, ASTORE, AALOAD, AASTORE);
+        new Type(T_REFERENCE, 'L', null, null, null, 1, ALOAD, ASTORE, AALOAD, AASTORE);
 }
