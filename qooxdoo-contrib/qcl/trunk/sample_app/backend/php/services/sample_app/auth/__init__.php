e9adde6edc88bb0fe99003be03b8bd5bd78792be
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-04-02 20:04:25 +0000

    removed sample app which was broken anyways

--- qooxdoo-contrib/qcl/trunk/sample_app/backend/php/services/sample_app/auth/__init__.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once  SERVICE_PATH . "qcl/auth/__init__.php";
-require_once  SERVICE_PATH . "sample_app/auth/user.php";
-require_once  SERVICE_PATH . "sample_app/auth/role.php";
-require_once  SERVICE_PATH . "sample_app/auth/permission.php";
-
-?>
