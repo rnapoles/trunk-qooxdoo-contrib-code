dfc7f9fcd73b94923a3afc609d22dd613b48d7e2
mlhartme <mlhartme@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-04-02 09:44:21 +0000

    tweaks

--- trunk/qooxdoo-contrib/QWT/trunk/sushi/core/src/main/java/org/qooxdoo/sushi/classfile/Type.java
+++ trunk/qooxdoo-contrib/QWT/trunk/sushi/core/src/main/java/org/qooxdoo/sushi/classfile/Type.java
@@ -37,12 +37,12 @@ public class Type implements Bytecodes, Constants {
     public final int arrayLoad;
     public final int arrayStore;
 
-    private Type(int id, String name, char descriptor,
+    private Type(int id, char descriptor,
                  Class<?> type, Class<?> wrapper,
                  Object zero, Object zeroLdc, int size,
                  int load, int store, int arrayLoad, int arrayStore) {
         this.id = id;
-        this.name = name;
+        this.name = (type == null ? null : type.getName());
         this.descriptor = descriptor;
         this.type = type;
         this.wrapper = wrapper;
@@ -59,47 +59,47 @@ public class Type implements Bytecodes, Constants {
 
     public static final Type[] PRIMITIVES = {
         // primitive types
-        new Type(T_VOID,      "void",    'V', Void.TYPE,      Void.class,
+        new Type(T_VOID,      'V', Void.TYPE,      Void.class,
             null,                    null,          0,
             -1, -1, -1, -1),
 
-        new Type(T_BOOLEAN,   "boolean", 'Z', Boolean.TYPE,   Boolean.class,
+        new Type(T_BOOLEAN,   'Z', Boolean.TYPE,   Boolean.class,
             new Boolean(false),      ZERO,          1,
             ILOAD, ISTORE, BALOAD, BASTORE),
             // bytecode has no instruction to access boolean arrays,
             // they are accessed as byte arrays
 
-        new Type(T_CHAR,      "char",    'C', Character.TYPE, Character.class,
+        new Type(T_CHAR,      'C', Character.TYPE, Character.class,
             new Character((char) 0), ZERO,          1,
             ILOAD, ISTORE, CALOAD, CASTORE),
 
-        new Type(T_FLOAT,     "float",   'F', Float.TYPE,     Float.class,
+        new Type(T_FLOAT,     'F', Float.TYPE,     Float.class,
             new Float(0),            new Float(0),  1,
             FLOAD, FSTORE, FALOAD, FASTORE),
 
-        new Type(T_DOUBLE,    "double",  'D', Double.TYPE,    Double.class,
+        new Type(T_DOUBLE,    'D', Double.TYPE,    Double.class,
             new Double(0),           new Double(0), 2,
             DLOAD, DSTORE, DALOAD, DASTORE),
 
-        new Type(T_BYTE,      "byte",    'B', Byte.TYPE,      Byte.class,
+        new Type(T_BYTE,      'B', Byte.TYPE,      Byte.class,
             new Byte((byte) 0),      ZERO,          1,
             ILOAD, ISTORE, BALOAD, BASTORE),
 
-        new Type(T_SHORT,     "short",   'S', Short.TYPE,     Short.class,
+        new Type(T_SHORT,     'S', Short.TYPE,     Short.class,
             new Short((short) 0),    ZERO,          1,
             ILOAD, ISTORE, SALOAD, SASTORE),
 
-        new Type(T_INT,       "int",     'I', Integer.TYPE,   Integer.class,
+        new Type(T_INT,       'I', Integer.TYPE,   Integer.class,
             ZERO,                    ZERO,          1,
             ILOAD, ISTORE, IALOAD, IASTORE),
 
-        new Type(T_LONG,      "long",    'J', Long.TYPE,      Long.class,
+        new Type(T_LONG,      'J', Long.TYPE,      Long.class,
             new Long(0),             new Long(0),   2,
             LLOAD, LSTORE, LALOAD, LASTORE),
     };
 
     public static final Type REFERENCE =
-        new Type(T_REFERENCE, null,      'L', null,           null,
+        new Type(T_REFERENCE, 'L', null,           null,
                  null,                    null,          1,
                  ALOAD, ASTORE, AALOAD, AASTORE);
 }
