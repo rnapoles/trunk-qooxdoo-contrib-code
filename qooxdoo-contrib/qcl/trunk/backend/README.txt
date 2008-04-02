d494b342a89ec4f289102eb026cbf2fe31c636bd
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-04-02 20:01:47 +0000

    still moving  qcl PHP backend

--- qooxdoo-contrib/qcl/trunk/backend/README.txt
+++ qooxdoo-contrib/qcl/trunk/backend/README.txt
@@ -1,14 +1,2 @@
 
-Backend for qcl
----------------
-
-Currently, there is only a php backend for qcl. It can be found in 
-sampe_app/backend/php, since the backend can only meaningfully be
-developed in the context of a real application. the general classes
-are contained in sample_app/backend/php/services/qcl, the application-
-specific classes (which extend the qcl_... classes and make their 
-methods available as services) are located in 
-sample_app/backend/php/services/sample_app. 
-
-There is doxygen documentation available in sample_app/backend/php/docs.
-More documentation to follow as the project evolves. 
+ todo.
---
+++ qooxdoo-contrib/qcl/trunk/backend/php/services/test.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * page to test jsonrpc php backend
+ * @author Christian Boulanger (cboulanger)
+ */
+
+if ( $_POST )
+{
+	$url  ="index.php?_ScriptTransport_id=1&_ScriptTransport_data={";
+	$url .="%22service%22:%22" . $_POST['service'] . "%22,%22method%22:%22" . $_POST['method']. "%22,";
+	$url .="%22id%22:3,%22params%22:[";
+	$params = array();
+	foreach ( $_POST['params'] as $param )
+	{
+		if ( $param !== "" )
+		{
+			if( is_numeric($param) or strstr("true false null", $param) 
+				or preg_match("/^[{[].*[}\]]$/",$param) )
+			{
+				$params[] = stripslashes($param);
+			}
+			else
+			{
+				$params[] = '"' . $param . '"';
+			}
+		}
+	}
+	$url .= implode(",", $params) . "],%22server_data%22}";
+	header("location: $url");
+	exit;	
+}
+?>
+
+<html>
+<head>
+<title>qooxdoo JSONRPC Backend Test</title>
+</head>
+<body>
+
+  <h1>qooxdoo JSONRPC Backend Test</h1>
+  <form action="test.php" target="responseIframe" 
+  		method="post">
+  	
+  <table>
+    <tr>
+      <td><b>Service name</b></td>
+      <td>
+  		<input type="text" name="service" />
+ 	  </td>
+    </tr>
+    <tr>
+      <td><b>Method name</b></td>
+      <td>
+  		<input type="text" name="method" />
+ 	  </td>      
+    </tr>
+    <tr>
+      <td><b>Parameter 1</b></td>
+      <td>
+  		<input type="text" name="params[]" />
+ 	  </td>      
+    </tr>
+    <tr>
+      <td><b>Parameter 2</b></td>
+      <td>
+  		<input type="text" name="params[]" />
+ 	  </td>      
+    </tr>
+    <tr>
+      <td><b>Parameter 3</b></td>
+      <td>
+  		<input type="text" name="params[]" />
+ 	  </td>      
+    </tr>
+     <tr>
+      <td><b>Parameter 4</b></td>
+      <td>
+  		<input type="text" name="params[]" />
+ 	  </td>      
+    </tr>   
+  </table>
+  
+  <input type="submit" value="Submit"/>
+  
+  </form>
+  
+  <iframe name="responseIframe" height="200" width="100%" scrolling="yes"/> 
+
+</body>
+</html>
+  
