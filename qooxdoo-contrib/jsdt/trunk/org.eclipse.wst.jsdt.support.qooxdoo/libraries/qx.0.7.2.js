9df7e717be45572226d5b978b70077fc947bf98e
mkempka <mkempka@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-04-03 12:39:05 +0000

    Initial import.

---
+++ qooxdoo-contrib/jsdt/trunk/org.eclipse.wst.jsdt.support.qooxdoo/libraries/qx.0.7.2.js
@@ -0,0 +1,26 @@
+/**
+* Object Window
+* @super Global
+* @type constructor
+* @class Window
+* @since FireFox 2.0.0.3
+*/
+function qx(){};
+qx.prototype=new Object();
+/**
+* property Class
+* @type Object
+* @class qx
+* @since qooxdoo 0.7.2
+*/
+qx.Class=new Object();
+
+/**
+* function define(arg1)
+* @type String
+* @param name String
+* @param config Object
+* @class Class
+* @since FireFox 2.0.0.3
+*/
+qx.Class.define=new function(name, config){};
