39a49a77b6cf076b5fa3908bc4c36a074d0f93ba
mkempka <mkempka@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-04-07 16:26:11 +0000

    new test

---
+++ qooxdoo-contrib/jsdt/trunk/org.eclipse.wst.jsdt.qooxdoo.validation.tests/src/org/eclipse/wst/jsdt/qooxdoo/functional/This_PDETest.java
@@ -0,0 +1,160 @@
+/*******************************************************************************
+ * Deutsche Post Com GmbH Tulpenfeld 9 53113 Bonn Germany (c) Copyright 2008 by
+ * Deutsche Post Com GmbH ALL RIGHTS RESERVED
+ * -----------------------------------------------------------------------------
+ * $Id: StaticMembers_PDETest.java,v 1.0 Feb 11, 2008 user Exp $
+ ******************************************************************************/
+package org.eclipse.wst.jsdt.qooxdoo.functional;
+
+import java.io.ByteArrayInputStream;
+
+import junit.framework.Assert;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.widgets.Widget;
+import org.eclipse.ui.part.FileEditorInput;
+import org.eclipse.wst.jsdt.core.JavaCore;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import abbot.swt.finder.WidgetFinderImpl;
+import abbot.swt.finder.generic.Matcher;
+import abbot.swt.finder.generic.MultipleFoundException;
+import abbot.swt.finder.generic.NotFoundException;
+import abbot.swt.finder.matchers.WidgetClassMatcher;
+import de.mkempka.sammy.operators.SammyProperties;
+import de.mkempka.sammy.operators.ShellOperator;
+import de.sammy.mkempka.ide.SammyIDE;
+import de.sammy.mkempka.ide.operators.TextEditorOperator;
+
+public class This_PDETest extends Assert {
+
+  private SammyIDE sammy;
+  private IFile file;
+  private String fileContents = "qx.Class.define(\"Application\", {\n"
+                                + "construct : function(x) {\n"
+                                + "            },\n"
+                                + "properties : {\n"
+                                + "    width : { check : \"Number\" }\n"
+                                + "  },\n"
+                                + "members : {\n"
+                                + "    bar : function(x,y) {\n"
+                                + "    },\n"
+                                + "    foo : function(x) {\n"
+                                + "    },\n"
+                                + "    _baz : 3\n"
+                                + "  }\n"
+                                + "});\n"
+                                + "\n";
+
+  @Before
+  public void setUp() throws Exception {
+    this.sammy = new SammyIDE();
+    setupProject();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    sammy.cleanUp();
+  }
+
+  @Test
+  public void resolvedInFoo() throws Exception {
+    sammy.openEditor( new FileEditorInput( this.file ) );
+    TextEditorOperator teo = new TextEditorOperator( "Application.js",
+                                                     getProperties() );
+    teo.setCurserAfter( "foo : function(x) {" );
+    checkMembersFound( teo );
+  }
+
+  @Test
+  public void resolvedInBar() throws Exception {
+    sammy.openEditor( new FileEditorInput( this.file ) );
+    TextEditorOperator teo = new TextEditorOperator( "Application.js",
+                                                     getProperties() );
+    teo.setCurserAfter( "bar : function(x,y) {" );
+    checkMembersFound( teo );
+  }
+
+  @Test
+  public void resolvedInConstructor() throws Exception {
+    sammy.openEditor( new FileEditorInput( this.file ) );
+    TextEditorOperator teo = new TextEditorOperator( "Application.js",
+                                                     getProperties() );
+    teo.setCurserAfter( "construct : function(x) {" );
+    sammy.stopAndShowChangesToUser();
+    checkMembersFound( teo );
+  }
+
+  private void checkMembersFound( TextEditorOperator teo )
+    throws NotFoundException, MultipleFoundException
+  {
+    teo.type( "this." );
+    teo.triggerAutoCompletion();
+    ShellOperator autoCompletionShell = new ShellOperator();
+    Table suggestions = findTable( autoCompletionShell );
+    checkMethods( suggestions );
+    checkField( suggestions );
+    checkProperties( suggestions );
+  }
+
+  private void checkProperties( Table suggestions ) {
+    assertNotNull( findItem( suggestions, "getWidth(" ) );
+    assertNotNull( findItem( suggestions, "setWidth(" ) );
+  }
+
+  private void checkMethods( Table suggestions ) {
+    assertNotNull( findItem( suggestions, "foo(" ) );
+    assertNotNull( findItem( suggestions, "bar(" ) );
+  }
+
+  private void checkField( Table suggestions ) {
+    assertNotNull( findItem( suggestions, "_baz" ) );
+  }
+
+  private Table findTable( ShellOperator autoCompletionShell )
+    throws NotFoundException, MultipleFoundException
+  {
+    Matcher<Widget> matcher = new WidgetClassMatcher( Table.class, true );
+    Table suggestions = ( Table )WidgetFinderImpl.getDefault()
+      .find( autoCompletionShell.getSource(), matcher );
+    return suggestions;
+  }
+
+  private SammyProperties getProperties() {
+    SammyProperties properties = new SammyProperties();
+    properties.setTimeoutInMillies( 10000L );
+    return properties;
+  }
+
+  private TableItem findItem( Table table, String itemPrefix ) {
+    TableItem result = null;
+    for( TableItem each : table.getItems() ) {
+      if( each.getText().startsWith( itemPrefix ) ) {
+        result = each;
+      }
+    }
+    return result;
+  }
+
+  private void setupProject() throws CoreException {
+    IProject project = sammy.createGenericProject();
+    QxProjectUtil.addJSDTNature( new NullProgressMonitor(), project );
+    QxProjectUtil.addDefaultLibToLibraryPath( new NullProgressMonitor(),
+                                              project );
+    this.file = project.getFile( "Application.js" );
+    this.file.create( new ByteArrayInputStream( fileContents.getBytes() ),
+                      true,
+                      new NullProgressMonitor() );
+    QxProjectUtil.createUserLibrary( JavaCore.create( project ) );
+  }
+}
+/*******************************************************************************
+ * $Log: StaticMembers_PDETest.java,v $
+ ******************************************************************************/
