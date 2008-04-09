8a2718af59e3dbce7be1c871da5c671645936cf2
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-04-09 18:22:18 +0000

    Changes to the session controller

--- qooxdoo-contrib/qcl/trunk/backend/php/services/qcl/session/model.php
+++ qooxdoo-contrib/qcl/trunk/backend/php/services/qcl/session/model.php
@@ -38,13 +38,19 @@ class qcl_session_model extends qcl_db_model
   //-------------------------------------------------------------
 	
 	/**
-	 * registers a session
+	 * registers a session. adds an entry with the current timestamp if the session 
+	 * doesn't exist, otherwise leaves the last action
 	 * @return void
 	 * @param string $sessionId 
 	 * @param string $userId 
 	 */
   function registerSession( $sessionId, $userId )
   {
+    $this->db->execute("
+      DELETE FROM `{$this->table}`
+      WHERE `{$this->key_namedId}` = '$sessionId'
+      AND   `{$this->key_user}`   != '$userId' 
+    ");
     $this->insert( array (
       $this->key_namedId => $sessionId,
       $this->key_user    => $userId
@@ -81,17 +87,17 @@ class qcl_session_model extends qcl_db_model
   /**
    * adds a message for all client except the requesting client
    * @return void
-   * @param $sessionId Object
-   * @param $message Object
-   * @param $data Object
+   * @param string $sessionId 
+   * @param string $message 
+   * @param mixed  $data
    */
   function addMessageBroadcast( $sessionId, $message, $data )
   {
-    $serializedData = addSlashes(serialize($data));
+    $serializedData = addSlashes(serialize(array($message,$data)));
+    $boundary = "af8asdf8h3o434af3h";
     $this->db->execute("
       UPDATE `{$this->table}`
-      SET `{$this->key_messages}` = CONCAT(`{$this->key_messages}`,'[[$message][$serializedData]]') 
-      WHERE `{$this->key_namedId}` != '$sessionId'
+      SET `{$this->key_messages}` = CONCAT(`{$this->key_messages}`,'$boundary$serializedData') 
     ");
   }
   
@@ -107,23 +113,33 @@ class qcl_session_model extends qcl_db_model
       FROM `{$this->table}`
       WHERE `{$this->key_namedId}` = '$sessionId'
     ");
-    preg_match_all( "/\[\[([^\]]+)\]\[([^\]]+)\]\]/", $msgData, $matches );
+   
+    $this->info($msgData);
     $messages = array();
-    for( $i=0; $i<count($matches[0]); $i++)
+        
+    if ( trim($msgData) )
     {
-      if ( $matches[1][$i] )
+      $boundary = "af8asdf8h3o434af3h";
+      $serializedVars = explode($boundary,$msgData);
+
+      foreach ( $serializedVars as $serializedData )
       {
-        $messages[] = array(
-          'name'  => $matches[1][$i],
-          'data'  => unserialize($matches[2][$i])
-        );        
+        $data = unserialize($serializedData);
+        if ( is_array($data) )
+        {
+          $messages[] = array(
+            'name'  => $data[0],
+            'data'  => $data[1]
+          );
+        }        
       }
+      $this->info($messages);
+      $this->db->execute("
+        UPDATE `{$this->table}`
+        SET `{$this->key_messages}` = ''
+        WHERE `{$this->key_namedId}` = '$sessionId'
+      ");
     }
-    $this->db->execute("
-      UPDATE `{$this->table}`
-      SET `{$this->key_messages}` = ''
-      WHERE `{$this->key_namedId}` = '$sessionId'
-    ");
     return $messages;
   }
   
