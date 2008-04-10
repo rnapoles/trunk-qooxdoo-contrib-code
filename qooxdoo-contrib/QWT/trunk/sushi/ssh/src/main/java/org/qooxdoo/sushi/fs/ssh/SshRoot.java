aeaccfd0e3cf9215097bb0d0ad2919d90ec747cd
mlhartme <mlhartme@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-04-10 05:53:04 +0000

    merge host into root

--- qooxdoo-contrib/QWT/trunk/sushi/ssh/src/main/java/org/qooxdoo/sushi/fs/ssh/SshRoot.java
+++ qooxdoo-contrib/QWT/trunk/sushi/ssh/src/main/java/org/qooxdoo/sushi/fs/ssh/SshRoot.java
@@ -19,18 +19,44 @@
 
 package org.qooxdoo.sushi.fs.ssh;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+
 import org.qooxdoo.sushi.fs.Root;
+import org.qooxdoo.sushi.fs.Settings;
+import org.qooxdoo.sushi.fs.file.FileNode;
+import org.qooxdoo.sushi.io.MultiOutputStream;
+import org.qooxdoo.sushi.util.ExitCode;
 
+import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
+import com.jcraft.jsch.UserInfo;
 
-public class SshRoot implements Root {
+public class SshRoot implements Root, UserInfo {
     private final SshFilesystem filesystem;
     private final ChannelSftp channel;
+    private final String user;
+    private final FileNode privateKey;
+    private final String passphrase;
+    private final String machine;
+    private final Session session;
+    private final Settings settings;
     
-    public SshRoot(SshFilesystem filesystem, ChannelSftp channel) {
+    public SshRoot(SshFilesystem filesystem, String machine, String user, FileNode privateKey, String passphrase, int timeout) 
+    throws JSchException {
         this.filesystem = filesystem;
-        this.channel = channel;
+        this.user = user;
+        this.privateKey = privateKey;
+        this.passphrase = passphrase;
+        this.machine = machine;
+        this.settings = filesystem.getIO().settings;
+        this.session = login(filesystem.getJSch(), machine);
+        this.session.connect(timeout);
+        this.channel = (ChannelSftp) session.openChannel("sftp");
+        this.channel.connect();
     }
     
     public SshFilesystem getFilesystem() {
@@ -38,9 +64,6 @@ public class SshRoot implements Root {
     }
 
     public String getId() {
-        Session session;
-        
-        session = channel.getSession();
         return "//" + session.getUserName() + "@" + session.getHost() + "/";
     }
 
@@ -67,4 +90,85 @@ public class SshRoot implements Root {
     public int hashCode() {
         return getId().hashCode();
     }
+    
+    public void close() {
+        session.disconnect();
+    }
+
+    public Exec begin(boolean tty, String ... command) throws JSchException {
+        return begin(tty, MultiOutputStream.createNullStream(), command);
+    }
+    
+    public Exec begin(boolean tty, OutputStream out, String ... command) throws JSchException {
+        return Exec.begin(this, tty, (ChannelExec) session.openChannel("exec"), out, command);
+    }
+    
+    public String exec(String ... command) throws JSchException, ExitCode {
+        return exec(true, command);
+    }
+    
+    public String exec(boolean tty, String ... command) throws JSchException, ExitCode {
+        ByteArrayOutputStream out;
+
+        out = new ByteArrayOutputStream();
+        try {
+            begin(tty, out, command).end();
+        } catch (ExitCode e) {
+            throw new ExitCode(e.call, e.code, settings.string(out));
+        }
+        return settings.string(out);
+    }
+    
+    public String getUser() {
+        return user;
+    }
+
+    public Session login(JSch jsch, String machine) throws JSchException {
+        Session session;
+        
+        jsch.addIdentity(privateKey.getAbsolute());
+        session = jsch.getSession(user, machine, 22);
+        session.setUserInfo(this);
+        return session;
+    }
+
+    
+    public String getMachine() {
+        return machine;
+    }
+    
+    //-- interface implementation 
+    
+    public String getPassphrase(String message) {
+        throw new IllegalStateException(message);
+    }
+
+    public String getPassword() {
+        throw new IllegalStateException();
+    }
+
+    public boolean prompt(String str) {
+        throw new IllegalStateException(str);
+    }
+
+    public String getPassphrase() {
+        return passphrase;
+    }
+
+    public boolean promptPassphrase(String prompt) {
+        return true; // use passphrase auth
+    }
+
+    public boolean promptPassword(String prompt) {
+        return false; // don't use password
+    }
+
+    public boolean promptYesNo(String message) {
+        return true;
+    }
+
+    public void showMessage(String message) {
+        System.out.println("showMessage " + message);
+    }
+    
 }
