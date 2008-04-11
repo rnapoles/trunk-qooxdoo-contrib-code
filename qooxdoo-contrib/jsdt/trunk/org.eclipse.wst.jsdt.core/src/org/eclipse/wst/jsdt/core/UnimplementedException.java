c32472809885b7aaf225ebca53ee8e9f7f812a6d
mkempka <mkempka@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-04-11 08:03:58 +0000

    initial commit from dev.eclipse.org

---
+++ qooxdoo-contrib/jsdt/trunk/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/core/UnimplementedException.java
@@ -0,0 +1,18 @@
+package org.eclipse.wst.jsdt.core;
+
+public class UnimplementedException extends RuntimeException {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UnimplementedException() {
+		super();
+	}
+
+	public UnimplementedException(String arg0) {
+		super(arg0);
+	}
+
+}
