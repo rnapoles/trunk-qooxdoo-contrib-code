5c05a3538c27e0d69a9fa41e23f7294a22f46b32
mkempka <mkempka@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-04-11 08:13:50 +0000

    initial commit from dev.eclipse.org

---
+++ qooxdoo-contrib/jsdt/trunk/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/core/infer/InferredMember.java
@@ -0,0 +1,16 @@
+package org.eclipse.wst.jsdt.core.infer;
+
+import org.eclipse.wst.jsdt.internal.compiler.ast.ASTNode;
+
+public abstract class InferredMember extends ASTNode{
+
+	public char [] name;
+	public InferredType inType;
+	public int nameStart;
+	public boolean isStatic = false;
+
+	public boolean isInferred()
+	{
+		return true;
+	}
+}
