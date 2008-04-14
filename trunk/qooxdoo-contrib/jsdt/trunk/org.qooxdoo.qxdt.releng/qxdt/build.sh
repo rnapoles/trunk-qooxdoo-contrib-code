44e3dfb06ee2f430034035229d39ec35e7954b5f
mkempka <mkempka@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-04-14 15:14:32 +0000

    Initial import.

---
+++ trunk/qooxdoo-contrib/jsdt/trunk/org.qooxdoo.qxdt.releng/qxdt/build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# configure paths
+export launcher=`find ~/eclipse -name "org.eclipse.equinox.launcher_*.jar" | sort | tail -1`
+export tmpdir=~/.hudson/jobs/qxdt/workspace/build
+export homedir=~/qxdt
+
+# ensure we have X-Server for UI-tests
+user=`whoami`
+havex=`ps -u $user | grep tightvnc | wc -l`
+if [ "$havex" = "0" ]; then
+  vncserver :1
+fi
+export DISPLAY=:1
+
+# nuke old output
+test -d "$tmpdir" && rm -rf "$tmpdir"
+test ! -d "$tmpdir" && mkdir "$tmpdir"
+
+# build
+cd "$homedir"
+java -jar $launcher -application org.eclipse.pde.build.Build
---
+++ trunk/qooxdoo-contrib/jsdt/trunk/org.qooxdoo.qxdt.releng/qxdt/readme.txt
@@ -0,0 +1,8 @@
+A reminder from yourself ;-)
+
+Do not forget to propagate changes to the build machine, if you change
+any file in this directory.
+
+1. Log onto the build machine (currently build@venus.ka.innoopract)
+2. cd qxdt
+3. svn update
