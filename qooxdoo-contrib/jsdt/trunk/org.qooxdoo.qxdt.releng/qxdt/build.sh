85325e0afcd07401940b9d8698bdecb2770a2682
mkempka <mkempka@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-04-15 12:21:01 +0000

    build succeeds for jsdt and qxdt

--- qooxdoo-contrib/jsdt/trunk/org.qooxdoo.qxdt.releng/qxdt/build.sh
+++ qooxdoo-contrib/jsdt/trunk/org.qooxdoo.qxdt.releng/qxdt/build.sh
@@ -4,7 +4,7 @@
 export launcher=`find ~/eclipse -name "org.eclipse.equinox.launcher_*.jar" | sort | tail -1`
 export tmpdir=~/.hudson/jobs/qxdt/workspace/build
 export homedir=~/qxdt
-export maps_location=https://qooxdoo-contrib.svn.sourceforge.net/svnroot/qooxdoo-contrib/trunk/qooxdoo-contrib/jsdt/trunk/org.qooxdoo.qxdt.releng/maps
+
 # ensure we have X-Server for UI-tests
 user=`whoami`
 havex=`ps -u $user | grep tightvnc | wc -l`
@@ -18,8 +18,5 @@ test -d "$tmpdir" && rm -rf "$tmpdir"
 test ! -d "$tmpdir" && mkdir "$tmpdir"
 
 # build
-cd "$tmpdir"
-#svn co $maps_location
-cp -r ~/build-workspace/org.qooxdoo.qxdt.releng/maps $tmpdir
 cd "$homedir"
 java -jar $launcher -application org.eclipse.pde.build.Build
