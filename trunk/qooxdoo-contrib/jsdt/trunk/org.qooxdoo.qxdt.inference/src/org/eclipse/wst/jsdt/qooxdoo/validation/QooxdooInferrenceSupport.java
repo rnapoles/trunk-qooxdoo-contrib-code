9d355746220316b386c517c94b8f87c22f707926
mkempka <mkempka@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-04-21 15:35:59 +0000

    deliberately breaking the build to check the CI feedback mechanism

--- trunk/qooxdoo-contrib/jsdt/trunk/org.qooxdoo.qxdt.inference/src/org/eclipse/wst/jsdt/qooxdoo/validation/QooxdooInferrenceSupport.java
+++ trunk/qooxdoo-contrib/jsdt/trunk/org.qooxdoo.qxdt.inference/src/org/eclipse/wst/jsdt/qooxdoo/validation/QooxdooInferrenceSupport.java
@@ -21,6 +21,8 @@ import org.eclipse.wst.jsdt.internal.compiler.classfmt.ClassFileConstants;
 
 public class QooxdooInferrenceSupport extends InferEngine {
 
+  asdf
+  
   private CompilationUnitDeclaration compilationUnit;
   private Stack<InferredType> classDefinitionStack = new Stack<InferredType>();
   private Stack<IObjectLiteralField> memberTypeStack = new Stack<IObjectLiteralField>();
