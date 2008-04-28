d87566576989fb5babecbb541a03479ae10b4c3f
jonathan_rass <jonathan_rass@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-04-28 13:32:40 +0000

    - Added images  - Built demo application

--- qooxdoo-contrib/HtmlArea/0.2/source/class/htmlarea/Application.js
+++ qooxdoo-contrib/HtmlArea/0.2/source/class/htmlarea/Application.js
@@ -14,15 +14,32 @@
 
    Authors:
      * Alexander Back (aback)
+     * Jonathan Rass (jonathan_rass)
+
+   ======================================================================
+
+   This application contain images based on the following work:
+
+   * Richtext Editor: Fork (RTEF)
+       http://www.rtef.info/
+       Version 1.8.1
+
+     Copyright:
+       (c) 2006 Timothy Bell
+
+     License:
+       http://www.rtef.info/#contributors
+
+     Author:
+       Timothy Bell
 
 ************************************************************************ */
 
 /* ************************************************************************
 
-#resource(image:image)
+#resource(htmlarea.image:image)
 
 ************************************************************************ */
-
 /**
  * htmlarea Example application
  */
@@ -51,103 +68,106 @@ qx.Class.define("htmlarea.Application",
     main : function(e)
     {
       this.base(arguments);
-      
+
+      var demoContent = '<h1>About</h1><p>qooxdoo (pronounced [ku:ksdu:]) is a comprehensive and innovative Ajax application framework. Leveraging object-oriented JavaScript allows developers to build impressive cross-browser applications. No <acronym title="HyperText Markup Language">HTML</acronym>, <acronym title="Cascading Style Sheets">CSS</acronym> nor <acronym title="Document Object Model">DOM</acronym> knowledge is needed. qooxdoo includes a platform-independent development tool chain, a state-of-the-art <acronym title="Graphical User Interface">GUI</acronym> toolkit and an advanced client-server communication layer. It is Open Source under an <acronym title="GNU Lesser General Public License">LGPL</acronym>/<acronym title="Eclipse Public License">EPL</acronym> dual <a href="http://qooxdoo.org/license" class="wikilink1" title="license">license</a>.';
+      var debugStyles = "p { margin:0px;padding:0px; border:dotted 1px lime; background-color:#dfdfdf; } ";
+
       qx.Theme.patch(qx.theme.classic.Appearance, htmlarea.theme.classic.Appearance);
+
+      qx.io.Alias.getInstance().add("htmlarea", qx.core.Setting.get("htmlarea.resourceUri"));
       
       var vb = new qx.ui.layout.VerticalBoxLayout;
-      vb.set({ top : 50, left : 20, width : "65%", height : "auto", spacing : 20 });
+      vb.set({ top : 20, left : 20, width : "65%", height : "auto", spacing : 20 });
       vb.addToDocument();
 
-      var htmlArea = new htmlarea.HtmlArea(null);
+      var htmlArea = new htmlarea.HtmlArea(demoContent, debugStyles);
+      ha = htmlArea;
       htmlArea.set({ width : "100%", height : 400, focused : true });
       //vb.add(htmlArea);
 
       var hb = new qx.ui.layout.HorizontalBoxLayout;
       hb.set({ width : "100%", height : "auto", spacing : 10 });
 
-      var boldButton = new qx.ui.form.Button("Bold");
+      var boldButton = new qx.ui.form.Button("", "htmlarea/image/text_bold.gif");
       boldButton.addEventListener("execute", function(e){
         this.setBold();
       }, htmlArea);
 
-      var italicButton = new qx.ui.form.Button("Italic");
+      var italicButton = new qx.ui.form.Button("", "htmlarea/image/text_italic.gif");
       italicButton.addEventListener("execute", function(e){
         this.setItalic();
       }, htmlArea);
 
-      var fontSizeButton = new qx.ui.form.Button("FontSize");
+      var underButton = new qx.ui.form.Button("", "htmlarea/image/text_underline.gif");
+      underButton.addEventListener("execute", function(e){
+        this.setUnderline();
+      }, htmlArea);
+
+      var strikeButton = new qx.ui.form.Button("", "htmlarea/image/text_strikethrough.gif");
+      strikeButton.addEventListener("execute", function(e){
+        this.setStrikeThrough();
+      }, htmlArea);
+
+      
+      var fontSizeButton = new qx.ui.form.Button("", "htmlarea/image/fontsize.gif");
       fontSizeButton.addEventListener("execute", function(e){
         var result = window.prompt("FontSize: ", "");
         this.setFontSize(parseInt(result));
       }, htmlArea);
 
-      var colorButton = new qx.ui.form.Button("Color");
+      var colorButton = new qx.ui.form.Button("", "htmlarea/image/color_text.gif");
       colorButton.addEventListener("execute", function(e){
         var result = window.prompt("Color (Hex): ", "#");
         this.setTextColor(result);
       }, htmlArea);
       
-      var bgcolorButton = new qx.ui.form.Button("BgColor");
+      var bgcolorButton = new qx.ui.form.Button("", "htmlarea/image/color_bg.gif");
       bgcolorButton.addEventListener("execute", function(e){
         var result = window.prompt("BgColor (Hex): ", "#");
         this.setTextBackgroundColor(result);
       }, htmlArea);
       
-      var olButton = new qx.ui.form.Button("ordered List");
+      var olButton = new qx.ui.form.Button("", "htmlarea/image/list_ordered.gif");
       olButton.addEventListener("execute", function(e){
+        this.insertUnorderedList();
+      }, htmlArea);
+      
+      var ulButton = new qx.ui.form.Button("", "htmlarea/image/list_numbered.gif");
+      ulButton.addEventListener("execute", function(e){
         this.insertOrderedList();
       }, htmlArea);
       
-      var ulButton = new qx.ui.form.Button("unordered List");
-      ulButton.addEventListener("execute", function(e){
-        this.insertUnorderedList();
+      var undoButton = new qx.ui.form.Button("", "htmlarea/image/undo.gif");
+      undoButton.addEventListener("execute", function(e){
+        this.undo();
       }, htmlArea);
-
-      var removeFormatButton = new qx.ui.form.Button("Remove format");
+      
+      var redoButton = new qx.ui.form.Button("", "htmlarea/image/redo.gif");
+      redoButton.addEventListener("execute", function(e){
+        this.redo();
+      }, htmlArea);
+      
+      var removeFormatButton = new qx.ui.form.Button("", "htmlarea/image/remove_format.gif");
       removeFormatButton.addEventListener("execute", function(e){
         this.removeFormat();
       }, htmlArea);
 
-      var setContentButton = new qx.ui.form.Button("getValue");
-      setContentButton.addEventListener("execute", function(e){
-        alert(this.getValue());
-      }, htmlArea);
+      hb.add(boldButton, italicButton, underButton, strikeButton, fontSizeButton, colorButton, bgcolorButton, olButton, ulButton, removeFormatButton, undoButton, redoButton);
 
-      var getContentButton = new qx.ui.form.Button("getComputedValue");
-      getContentButton.addEventListener("execute", function(e){
-        alert(this.getComputedValue());
-      }, htmlArea);
-
-      hb.add(boldButton, italicButton, fontSizeButton, colorButton, bgcolorButton, olButton, ulButton, removeFormatButton, setContentButton, getContentButton);
-
-      var hb2 = new qx.ui.layout.HorizontalBoxLayout;
-      hb2.set({ width : "100%", height : "auto", spacing : 10 });
-
-      var getHtmlButton = new qx.ui.form.Button("getHtml");
-      getHtmlButton.addEventListener("execute", function(e){
-        var html = htmlArea.getHtml();
-        textArea.setValue(textArea.getValue() + "\n" + html);
-      }, this);
-
-      var innerHtmlButton = new qx.ui.form.Button("innerHtml");
-      innerHtmlButton.addEventListener("execute", function(e){
-        var html = htmlArea.getContentDocument().body.innerHTML;
-        textArea.setValue(textArea.getValue() + "\n" + html);
-      }, this);
-
-      var clearButton = new qx.ui.form.Button("clearTextarea");
-      clearButton.addEventListener("execute", function(e){
-        textArea.setValue("");
-      }, this);
-
-      hb2.add(getHtmlButton, innerHtmlButton, clearButton);
-      vb.add(hb, htmlArea, hb2);
-
-      var textArea = new qx.ui.form.TextArea;
-      textArea.set({ width: "100%", height : 200 });
-      vb.add(textArea);
+      vb.add(hb, htmlArea);
       
     }
+  },
+
+  /*
+  *****************************************************************************
+     SETTINGS
+  *****************************************************************************
+  */
+  
+  settings :
+  {
+    "htmlarea.resourceUri" : "./resource"
   }
   
 });
