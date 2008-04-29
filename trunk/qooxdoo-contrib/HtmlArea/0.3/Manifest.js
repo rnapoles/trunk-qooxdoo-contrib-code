8c86af12f77ad3ad3a948f2c408bbb8b232dda84
jonathan_rass <jonathan_rass@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-04-29 07:11:21 +0000

    Initial commit for 0.3. In this version we will port HTMLArea to qooxdoo 0.8.

---
+++ trunk/qooxdoo-contrib/HtmlArea/0.3/Manifest.js
@@ -0,0 +1,21 @@
+{
+  "name" : "HtmlArea",
+  
+  "summary" : "A simple HTML editing widget",
+  "description" : "...",
+  "keywords" : ["widget"],
+  "homepage" : "http://qooxdoo.org/documentation/contrib",
+  
+  "license" : "LGPL/EPL",
+  "authors" : [
+    {
+      "name" : "Alexander Back",
+      "email" : "alex DOT back AT 1und1 DOT de"
+    }
+  ],
+
+  "namespace" : "htmlarea",
+  
+  "version" : "trunk",  
+  "qooxdoo-versions": ["0.7.1"]
+}
