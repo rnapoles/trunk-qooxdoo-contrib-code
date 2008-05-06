61e165c7f6e8156ef15a8031f7c1bd4ae03994db
jonathan_rass <jonathan_rass@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-05-06 06:20:18 +0000

    Restored Manifest.js

--- trunk/qooxdoo-contrib/HtmlArea/0.3-pre/Manifest.js
+++ trunk/qooxdoo-contrib/HtmlArea/0.3-pre/Manifest.js
@@ -1,21 +1,39 @@
 {
-  "name" : "HtmlArea",
-  
-  "summary" : "A simple HTML editing widget",
-  "description" : "...",
-  "keywords" : ["widget"],
-  "homepage" : "http://qooxdoo.org/documentation/contrib",
-  
-  "license" : "LGPL/EPL",
-  "authors" : [
-    {
-      "name" : "Alexander Back",
-      "email" : "alex DOT back AT 1und1 DOT de"
-    }
-  ],
+  "info" : 
+  {
+    "name" : "HtmlArea",
 
-  "namespace" : "htmlarea",
+    "summary" : "A simple HTML editing widget",
+    "description" : "...",
+    
+    "keywords" : ["qooxdoo", "widget", "cross-browser"],
+    "homepage" : "http://qooxdoo.org/documentation/contrib",
+
+    "license" : "LGPL/EPL",
+    "authors" : 
+    [
+      {
+      "name" : "Alexander Back (aback)",
+      "email" : "alex DOT back AT 1und1 DOT de"
+      },
+      {
+        "name" : "Jonathan Rass (jonathan_rass)",
+        "email" : "jonathan DOT rass AT 1und1 DOT de"
+      }
+    ],
+
+    "version" : "trunk",
+    "qooxdoo-versions": ["0.8-pre2", "trunk"]
+  },
   
-  "version" : "trunk",  
-  "qooxdoo-versions": ["0.8-pre2"]
+  "provides" : 
+  {
+    "namespace"   : "qx",
+    "encoding"    : "utf-8",
+    "class"       : "source/class",
+    "resource"    : "source/resource",
+    "translation" : "source/translation",
+    "type"        : "library"
+  }
 }
+
