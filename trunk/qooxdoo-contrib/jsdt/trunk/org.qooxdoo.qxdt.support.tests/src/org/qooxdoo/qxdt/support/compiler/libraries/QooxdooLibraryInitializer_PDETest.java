4e87d9eff16790df092e52a9562c2730f056eb78
mkempka <mkempka@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-05-09 08:52:02 +0000

    Initial import.

---
+++ trunk/qooxdoo-contrib/jsdt/trunk/org.qooxdoo.qxdt.support.tests/src/org/qooxdoo/qxdt/support/compiler/libraries/QooxdooLibraryInitializer_PDETest.java
@@ -0,0 +1,40 @@
+package org.qooxdoo.qxdt.support.compiler.libraries;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.eclipse.wst.jsdt.core.compiler.libraries.LibraryLocation;
+import org.eclipse.wst.jsdt.support.qooxdoo.core.compiler.libraries.QooxdooLibraryInitializer;
+import org.junit.Test;
+
+public class QooxdooLibraryInitializer_PDETest {
+
+  @Test
+  public void libraryAvailable() throws Exception {
+    LibraryLocation ll = new QooxdooLibraryInitializer().getLibraryLocation();
+    char[][] cfileNames = ll.getLibraryFileNames();
+    String[] fileNames = convertToStrings( cfileNames );
+    assertContains( "qx_Class.js", fileNames );
+    assertTrue( "actual: " + fileNames.length, fileNames.length > 400 );
+  }
+
+  private void assertContains( String expected, String[] actual ) {
+    boolean found = false;
+    for( String each : actual ) {
+      if( each.equals( expected ) ) {
+        found = true;
+      }
+    }
+    assertTrue( expected + " is not contained in " + Arrays.toString( actual ),
+                found );
+  }
+
+  private String[] convertToStrings( char[][] cfileNames ) {
+    String[] result = new String[ cfileNames.length ];
+    for( int i = 0; i < result.length; i++ ) {
+      result[ i ] = new String( cfileNames[ i ] );
+    }
+    return result;
+  }
+}
