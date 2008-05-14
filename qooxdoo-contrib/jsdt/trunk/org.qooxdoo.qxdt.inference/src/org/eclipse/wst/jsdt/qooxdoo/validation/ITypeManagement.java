c3d9511cdc22c4c22923293a829f3e9aad505865
mkempka <mkempka@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-05-14 14:33:16 +0000

    refactored class modification into class TypeAssembler

---
+++ qooxdoo-contrib/jsdt/trunk/org.qooxdoo.qxdt.inference/src/org/eclipse/wst/jsdt/qooxdoo/validation/ITypeManagement.java
@@ -0,0 +1,8 @@
+package org.eclipse.wst.jsdt.qooxdoo.validation;
+
+import org.eclipse.wst.jsdt.core.infer.InferredType;
+
+public interface ITypeManagement {
+
+  public InferredType addType( char[] name );
+}
