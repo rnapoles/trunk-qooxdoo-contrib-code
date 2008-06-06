d1e5e637c8db12f02e7427b820dba440b6ed210b
ecker <ecker@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-06-06 14:14:57 +0000

    Added Crypto project

---
+++ qooxdoo-contrib/Crypto/trunk/Manifest.js
@@ -0,0 +1,23 @@
+{
+  "name" : "Crypto",
+
+  "summary" : "Collaborating on crypto related features (e.g. GnuPG, MD5, SHA, RSA, etc.). 
+",
+  "description": "Collaborating on crypto related features (e.g. GnuPG, MD5, SHA, RSA, etc.). 
+",
+  "keywords" : ["cryptography"],
+  "homepage" : "http://qooxdoo.org/documentation/contrib/Crypto",
+  
+  "license" : "LGPL/EPL",    
+  "authors" : [
+    {
+      "name" : "",
+      "email" : ""
+    }
+  ],
+  
+  "namespace" : "crypto",
+
+  "version" : "0.1",
+  "qooxdoo-versions": ["0.7", "trunk"]
+}
