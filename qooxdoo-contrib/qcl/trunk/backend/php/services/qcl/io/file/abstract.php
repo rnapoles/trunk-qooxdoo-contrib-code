1e02bd2672e7d974ec21f0fac09c84cf548bf90f
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-07-13 12:22:23 +0000

    added qcl_io_file package. just the structure, classes are not functional yet.

---
+++ qooxdoo-contrib/qcl/trunk/backend/php/services/qcl/io/file/abstract.php
@@ -0,0 +1,24 @@
+<?php
+
+require "qcl/jsonrpc/model.php";
+
+class qcl_io_file_abstract extends qcl_jsonrpc_model
+{
+  /**
+   * The file resource path
+   *
+   * @var string
+   */
+  var $resourcePath;
+
+  /**
+   * Gets the file's resource path
+   * @return string
+   */
+  function getResourcePath() 
+  {
+    return $this->resourcePath;
+  }  
+}
+
+?>
