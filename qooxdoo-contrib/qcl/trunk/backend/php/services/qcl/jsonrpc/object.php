ae9046637eff916f4544032972ea5230b9799d5f
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-07-13 12:23:02 +0000

    added global registry methods

--- qooxdoo-contrib/qcl/trunk/backend/php/services/qcl/jsonrpc/object.php
+++ qooxdoo-contrib/qcl/trunk/backend/php/services/qcl/jsonrpc/object.php
@@ -46,26 +46,6 @@ class qcl_jsonrpc_object extends qcl_object
   //-------------------------------------------------------------
   // public methods
   //-------------------------------------------------------------
-  /**
-   * get include path for a class name
-   * @return 
-   * @param $classname
-   */
-  function getClassPath($classname)
-  {
-    // delete JsonRpcClassPrefix
-    if ( substr($classname,0,strlen(JsonRpcClassPrefix)) == JsonRpcClassPrefix )
-    {
-	    $pathname = substr($classname,strlen(JsonRpcClassPrefix));        	
-    }
-    else
-    {
-    	$pathname = $classname;
-    }
-        
-    // determine path name
-    return SERVICE_PATH . implode( "/", explode("_",$pathname ) ) . ".php";
-  }
   
   /**
    * gets file path to binary executables depending on operating system
