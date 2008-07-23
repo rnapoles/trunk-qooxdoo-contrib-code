47ff4ef950cd06d9ff540e16750e9deb3f132232
ecker <ecker@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-07-23 15:07:48 +0000

    Minor changes (Manifest, Readme, ...)

--- trunk/qooxdoo-contrib/QWT/trunk/Manifest.js
+++ trunk/qooxdoo-contrib/QWT/trunk/Manifest.js
@@ -5,12 +5,13 @@
   "description" : "Similar to GWT: an application is written in Java and QWT translates the client part to JavaScript.",
   
   "keywords" : ["toolkit", "java"],
-  "homepage" : "http://qooxdoo.org/documentation/contrib",
+  "homepage" : "http://contrib.qooxdoo.org/QWT",
 
   "license" : "LGPL/EPL",
   "authors" : [
     {
-      "name" : "Michael Hartmeier (mlhartme)",
+      "name" : "Michael Hartmeier",
+      "user" : "mlhartme",
       "email" : "michael DOT hartmeier AT 1und1 DOT de"
     }
   ],
@@ -18,5 +19,5 @@
   "namespace" : "qwt",
   
   "version" : "trunk",
-  "qooxdoo-versions": ["0.7.2-pre"]
+  "qooxdoo-versions": ["0.7.3"]
 }
