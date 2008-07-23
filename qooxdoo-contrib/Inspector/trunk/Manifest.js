5df4d1d18dcc93bc612aca86a7c9de0bfc87f261
ecker <ecker@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-07-23 15:03:18 +0000

    Minor changes (Manifest, Readme, ...)

--- qooxdoo-contrib/Inspector/trunk/Manifest.js
+++ qooxdoo-contrib/Inspector/trunk/Manifest.js
@@ -4,18 +4,19 @@
   "summary" : "A inspector and debugger tool",
   "description" : "...",
   "keywords" : ["inspector"],
-  "homepage" : "http://qooxdoo.org/documentation/contrib",
+  "homepage" : "http://contrib.qooxdoo.org/Inspector",
 
   "license" : "LGPL/EPL",
   "authors" : [
     {
       "name" : "Martin Wittemann",
-      "user" : "martin_wittemann"
+      "user" : "martin_wittemann",
+      "email" : "martin DOT wittemann AT 1und1 DOT de"
     }
   ],
 
   "namespace" : "inspector",
         
-  "version" : "0.1-pre",
+  "version" : "trunk",
   "qooxdoo-versions": ["0.7", "0.7.1"]
 }
