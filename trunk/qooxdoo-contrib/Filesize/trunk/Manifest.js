84930c16568f7e8074ded74be3fe1412d3174b95
ecker <ecker@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-07-23 15:16:38 +0000

    Minor changes (Manifest, Readme, ...)

--- trunk/qooxdoo-contrib/Filesize/trunk/Manifest.js
+++ trunk/qooxdoo-contrib/Filesize/trunk/Manifest.js
@@ -4,22 +4,22 @@
   "summary" : "A filesize format class and corresponding cell renderer.",
   "description" : "The FilesizeFormat enables the conversion of 500 to 500kb, 1500 to 15 KB, etc.  The Filesize cellrender enables data formatted as Filesize to be included in a table.",
   "keywords" : ["widget"],
-  "homepage" : "http://qooxdoo.org/documentation/contrib",
+  "homepage" : "http://contrib.qooxdoo.org",
 
   "license" : "LGPL/EPL",
   "authors" : [
     {
-      "name" : "Til Schneider",
-      "user" : "til132"
+      "name" : "Michael Rea",
+      "user" : "mikerea",
+      "email": "mike1 AT tartansolutions DOT com"
     },
     {
       "name" : "Gaetan de Menten",
       "user" : "ged"
     },
     {
-      "name" : "Michael Rea",
-      "user" : "rea725",
-      "email": "mike1 AT tartansolutions DOT com"
+      "name" : "Til Schneider",
+      "user" : "til132"
     }
   ],
 
