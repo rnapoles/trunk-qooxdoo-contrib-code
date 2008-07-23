f78079edd93959b4c9bce883470b04cd1e11bd34
ecker <ecker@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-07-23 15:02:05 +0000

    Minor changes (Manifest, Readme, ...)

--- trunk/qooxdoo-contrib/CouchDB/trunk/Manifest.js
+++ trunk/qooxdoo-contrib/CouchDB/trunk/Manifest.js
@@ -4,18 +4,19 @@
   "summary" : "CRUD based remote io-api for a CouchDB server",
   "description": "CRUD based remote io-api for a CouchDB server",
   "keywords" : ["couchdb", "rest"],
-  "homepage" : "http://qooxdoo.org/documentation/contrib",
+  "homepage" : "http://contrib.qooxdoo.org/CouchDB",
   
   "license" : "LGPL/EPL",    
   "authors" : [
     {
       "name" : "Ralf Nieuwenhuijsen",
+      "user" : "meneerr",
       "email" : "ralf.nieuwenhuijsen@gmail.com"
     }
   ],
   
   "namespace" : "couch",  
 
-  "version" : "2008.05.26",
+  "version" : "trunk",
   "qooxdoo-versions": ["0.7", "trunk"]
 }
