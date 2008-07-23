03823d2bd808959d17aed341c37361c6b1ecea5f
ecker <ecker@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-07-23 15:04:19 +0000

    Added missing files (Manifest, Readme, License)

---
+++ trunk/qooxdoo-contrib/jsdt/README.txt
@@ -0,0 +1,6 @@
+===== QXDT =====
+
+qooxdoo Development Toolkit for Eclipse
+
+This project provides qooxdoo-specific development support for the Eclipse IDE 
+and is based on the JSDT project.
---
+++ trunk/qooxdoo-contrib/jsdt/trunk/Manifest.js
@@ -0,0 +1,25 @@
+{
+  "name" : "QXDT",
+
+  "summary" : "qooxdoo Development Toolkit for Eclipse", 
+  "description" : "This project provides qooxdoo-specific development support for the Eclipse IDE and is based on the JSDT project.",
+  "keywords" : ["ide"],
+  "homepage" : "http://contrib.qooxdoo.org/QXDT",
+
+  "license" : "LGPL/EPL",
+  "authors" : [
+    {
+      "name" : "Matthias Kempka",
+      "user" : "mkempka"
+    },
+    {
+      "name" : "Andreas Ecker",
+      "user" : "ecker"
+    }
+  ],
+
+  "namespace" : "org.qooxdoo.qxdt",
+        
+  "version" : "trunk",
+  "qooxdoo-versions": ["0.7", "trunk"]
+}
