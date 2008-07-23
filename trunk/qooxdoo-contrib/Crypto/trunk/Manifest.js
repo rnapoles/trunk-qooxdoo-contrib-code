04941d5ed0289815c99f7dca373ed60e0a35fbd6
ecker <ecker@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-07-23 15:30:08 +0000

    Minor changes (Manifest, Readme, ...). Renaming of files.

--- trunk/qooxdoo-contrib/Crypto/trunk/Manifest.js
+++ trunk/qooxdoo-contrib/Crypto/trunk/Manifest.js
@@ -1,23 +1,24 @@
 {
   "name" : "Crypto",
 
-  "summary" : "Collaborating on crypto related features (e.g. GnuPG, MD5, SHA, RSA, etc.). 
+  "summary" : "Collaborating on crypto related features (e.g. OpenPGP, MD5, SHA, RSA, etc.). 
 ",
-  "description": "Collaborating on crypto related features (e.g. GnuPG, MD5, SHA, RSA, etc.). 
+  "description": "Collaborating on crypto related features (e.g. OpenPGP, MD5, SHA, RSA, etc.). 
 ",
-  "keywords" : ["cryptography"],
-  "homepage" : "http://qooxdoo.org/documentation/contrib/Crypto",
+  "keywords" : ["cryptography", "crypto", "OpenPGP", "GnuPG"],
+  "homepage" : "http://contrib.qooxdoo.org/Crypto",
   
   "license" : "LGPL/EPL",    
   "authors" : [
     {
-      "name" : "",
-      "email" : ""
+      "name" : "Bill Adams",
+  	  "user" : "badams5116",
+      "email" : "badams AT mywebserve DOT com"
     }
   ],
   
   "namespace" : "crypto",
 
-  "version" : "0.1",
-  "qooxdoo-versions": ["0.7", "trunk"]
+  "version" : "trunk",
+  "qooxdoo-versions": ["0.7"]
 }
