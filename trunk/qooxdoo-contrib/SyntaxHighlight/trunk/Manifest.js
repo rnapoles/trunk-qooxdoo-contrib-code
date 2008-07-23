4cced03660beacc15f266e1494872db6fb6670a4
ecker <ecker@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-07-23 15:11:22 +0000

    Added missing files (Manifest, Readme, License)

---
+++ trunk/qooxdoo-contrib/SyntaxHighlight/trunk/Manifest.js
@@ -0,0 +1,22 @@
+{
+  "name" : "SyntaxHighlight",
+
+  "summary" : "Widget to show syntax-highlighted JavaScript code.",
+  "description" : "",
+  "keywords" : ["widget"],
+  "homepage" : "http://contrib.qooxdoo.org",
+
+  "license" : "LGPL/EPL",
+  "authors" : [
+    {
+      "name" : "Michael Rea",
+      "user" : "mikerea",
+      "email": "mike1 AT tartansolutions DOT com"
+    }
+  ],
+
+  "namespace" : "syntaxhighlight",
+        
+  "version" : "trunk",
+  "qooxdoo-versions": ["0.7"]
+}
