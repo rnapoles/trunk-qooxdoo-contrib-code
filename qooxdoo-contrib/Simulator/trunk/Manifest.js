efa9e69cb4c1089cdcbeebe4181faa05fa9b3b3a
ecker <ecker@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-07-23 15:08:42 +0000

    Minor changes (Manifest, Readme, ...)

--- qooxdoo-contrib/Simulator/trunk/Manifest.js
+++ qooxdoo-contrib/Simulator/trunk/Manifest.js
@@ -5,17 +5,18 @@
   "description" : "This module provides various pieces to utilize Selenium (http://www.openqa.org/selenium) to automate qooxdoo applications.",
   
   "keywords" : ["selenium", "test", "simulation", "automation"],
-  "homepage" : "http://qooxdoo.org/documentation/contrib/simulator",
+  "homepage" : "http://contrib.qooxdoo.org/Simulator",
 
   "license" : "LGPL/EPL",
   "authors" : [
     {
-      "name" : "Thomas Herchenroeder (thron7)",
+      "name" : "Thomas Herchenroeder",
+      "user" : "thron7",
       "email" : "thron7@users.sourceforge.net"
     }
   ],
 
-  "namespace" : "custom",
+  "namespace" : "simulator",
   
   "version" : "trunk",
   "qooxdoo-versions": ["0.7.2"]
