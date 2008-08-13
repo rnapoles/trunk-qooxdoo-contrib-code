b2d199d9303eaea61eb8c6d88f00a5fc0017b601
derrell <derrell@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-08-13 13:25:19 +0000

    Initial Graphics contrib structure
      This contains the base structure of a "widget" contrib and the associated
      (edited) Manifest, LICENSE, and README files.
    
    Derrell

---
+++ trunk/qooxdoo-contrib/Graphics/trunk/Manifest.js
@@ -0,0 +1,22 @@
+{
+  "name" : "Graphics",
+
+  "summary" : "Graphics classes",
+  "description": "Render graphics in qooxdoo widgets",
+  "keywords" : ["graphics"],
+  "homepage" : "http://contrib.qooxdoo.org",
+  
+  "license" : "LGPL",    
+  "authors" : [
+    {
+      "name" : "Derrell Lipman",
+      "email" : "derrell.lipman@unwireduniverse.com"
+    }
+  ],
+  
+  "namespace" : "graphics",
+  "resource-uri-setting" : "graphics.resourceUri",
+
+  "version" : "trunk",
+  "qooxdoo-versions": ["0.7"]
+}
