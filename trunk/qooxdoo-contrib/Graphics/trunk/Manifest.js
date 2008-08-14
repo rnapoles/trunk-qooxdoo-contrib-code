8ef891f9744c146076db78dd3f7af118a57e5f06
derrell <derrell@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-08-14 00:57:48 +0000

    Attempt to make Graphics contrib project work for 0.8 too.
    - The code changes are fairly minimal.  The demo application now dynamically
      determines whether it's running with a 0.7 library or one from 0.8, and
      issues the appropriate calls.
    
    - Manifest.js now provides both a 0.7 section and a 0.8 section.
    
    - I've added a config.json but it's likely in the wrong place.  I think it
      belongs in the source/demo directory, but I can find no other examples of
      qooxdoo-contrib projects which support 0.8 except for HtmlArea, and that
      project doesn't provide a demo application.  I could use some help with
      getting this stuff set up for 0.8.  Please?
    
    Derrell

--- trunk/qooxdoo-contrib/Graphics/trunk/Manifest.js
+++ trunk/qooxdoo-contrib/Graphics/trunk/Manifest.js
@@ -1,4 +1,5 @@
 {
+  "0.7" : "configuration follows...",
   "name" : "Graphics",
 
   "summary" : "Simple Scalable Vector Graphics",
@@ -18,5 +19,41 @@
   "resource-uri-setting" : "ssvg.resourceUri",
 
   "version" : "trunk",
-  "qooxdoo-versions": ["0.7"]
+  "qooxdoo-versions": ["0.7"],
+
+
+  "0.8" : "configuration follows...",
+  "info" : 
+  {
+    "name" : "Graphics",
+
+    "summary" : "Simple Scalable Vector Graphics",
+    "description" : "Render graphics in qooxdoo widgets",
+    
+    "keywords" : ["graphics"],
+    "homepage" : "http://contrib.qooxdoo.org",
+
+    "license" : "LGPL",
+    "authors" : 
+    [
+      {
+        "name" : "Derrell Lipman",
+        "email" : "derrell DOT lipman AT unwireduniverse DOT com"
+      }
+    ],
+
+    "version" : "trunk",
+    "qooxdoo-versions": ["trunk"]
+  },
+  
+  "provides" : 
+  {
+    "namespace"   : "ssvg",
+    "encoding"    : "utf-8",
+    "class"       : "source/class",
+    "resource"    : "source/resource",
+    "translation" : "source/translation",
+    "type"        : "library"
+  }
 }
+
