a4a7970a0439f359c952ca31b2b1c8979ff2dc3b
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-08-23 21:03:53 +0000

    added qcl_registry package

---
+++ trunk/qooxdoo-contrib/qcl/trunk/backend/php/services/qcl/registry/Session.php
@@ -0,0 +1,76 @@
+<?php
+
+require_once "qcl/jsonrpc/object.php";
+
+/**
+ * Class which maintains a registry which is valid during one 
+ * PHP session
+ */
+class qcl_registry_Session extends qcl_jsonrpc_object
+{
+
+  /**
+   * key of variable container in $_SESSION
+   */
+  var $_session_key = "qcl_session_registry";
+ 
+
+  /**
+   * Returns class singleton instance
+   * @access static
+   * @return qcl_registry_PageLoad singleton instance
+   */
+  function &getInstance( $class=__CLASS__ )
+  {
+    return parent::getInstance( $class );
+  }  
+  
+  /**
+   * resets the page load registry. this needs to be
+   * called, for example, when a user logs out
+   */
+  function reset()
+  {
+    $_SESSION[ $this->_session_key ] = array();
+  }
+  
+  /**
+   * Sets the registry value
+   *
+   * @param string $key
+   * @param mixed $value
+   */
+  function set( $key, $value )
+  {
+    $_SESSION[ $this->_session_key ][$key] = $value;
+  }
+
+  /**
+   * Gets the registry value
+   *
+   * @param string $key
+   * @return mixed
+   */
+  function get( $key )
+  {
+    return $_SESSION[ $this->_session_key ][$key];
+  }  
+  
+  
+  /**
+   * Check if registry value is set
+   *
+   * @param string $key
+   * @return mixed
+   */
+  function has( $key )
+  {
+    return isset( $_SESSION[ $this->_session_key ][$key] );
+  }
+
+  
+}
+
+
+
+?>
