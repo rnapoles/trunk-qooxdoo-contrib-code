f2cd344a510530910c82cf494823f94d3970b6e4
derrell <derrell@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-09-14 20:29:21 +0000

    Correct am/pm and 24-hour calculations

--- trunk/qooxdoo-contrib/TimeChooser/trunk/demo/source/class/custom/Application.js
+++ trunk/qooxdoo-contrib/TimeChooser/trunk/demo/source/class/custom/Application.js
@@ -33,7 +33,7 @@ qx.Class.define("custom.Application",
 
       var d = new Date();
       var chooser = new timechooser.TimeChooser(Math.floor(d.getTime()/1000));
-      var chooser = new timechooser.TimeChooser("10:31:20 pm");
+      var chooser = new timechooser.TimeChooser("12:31:20");
       container.add(chooser);
     }
   },
