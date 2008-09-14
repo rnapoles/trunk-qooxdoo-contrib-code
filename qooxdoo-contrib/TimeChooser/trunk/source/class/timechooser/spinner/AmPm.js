99ab2ad15c547b927b6001c2ed527834375972fb
derrell <derrell@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-09-14 02:45:51 +0000

    Add capability for setting time in various formats

--- qooxdoo-contrib/TimeChooser/trunk/source/class/timechooser/spinner/AmPm.js
+++ qooxdoo-contrib/TimeChooser/trunk/source/class/timechooser/spinner/AmPm.js
@@ -45,6 +45,23 @@ qx.Class.define("timechooser.spinner.AmPm",
     // Set an initial value to use in case the parameter value is invalid
     this.setValue(this.__am);
     this.setValue(value);
+
+    //
+    // Put the up/down buttons below the text field instead of to the right
+    //
+
+    // Get the various controls in the spinner
+    var upButton = this._getChildControl("upbutton");
+    var downButton = this._getChildControl("downbutton");
+    var textField = this._getChildControl("textfield");
+
+    // Remove all of them from the grid layout
+    this._removeAll();
+
+    // Re-insert them so in the preferred locations
+    this._add(textField,  { column: 0, row: 0, colSpan: 2, rowSpan : 1 });
+    this._add(downButton, { column: 0, row: 1, colSpan: 1, rowSpan : 1 });
+    this._add(upButton,   { column: 1, row: 1, colSpan: 1, rowSpan : 1 });
   },
 
   members :
