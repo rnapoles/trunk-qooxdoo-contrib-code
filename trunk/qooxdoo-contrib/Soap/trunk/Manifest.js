12125da380eb15b9c19f53a3490b91fb674ace26
burakarslan <burakarslan@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-09-24 23:15:13 +0000

    Soap module initial revision

---
+++ trunk/qooxdoo-contrib/Soap/trunk/Manifest.js
@@ -0,0 +1,17 @@
+{
+   "name"        : "Soap"
+  ,"summary"     : "Soap Module"
+  ,"description" : "This application contains the soap module, a demo application, and a python backend."
+  
+  ,"keywords" : ["soap"]
+  ,"homepage" : "http://qooxdoo.org/documentation/contrib"
+
+  ,"license" : "LGPL/EPL, BSD and CC"
+  ,"authors" : [
+    {"name" : "Burak Arslan", "email" : "burak.arslan@arskom.com.tr"}
+  ]
+
+  ,"namespace" : "soap"
+  ,"version"   : "trunk"
+  ,"qooxdoo-versions" : ["0.7"]
+}
