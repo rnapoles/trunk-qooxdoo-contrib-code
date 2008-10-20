a774be92b78030dbba06c2e8268043e9e66e335d
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-10-20 10:29:24 +0000

    Adding test class

---
+++ trunk/qooxdoo-contrib/qcl/trunk/backend/php/services/qcl/xml/Tests.php
@@ -0,0 +1,61 @@
+<?php
+require_once "qcl/datasource/controller.php";
+
+/**
+ * Service class containing test methods
+ */
+class class_qcl_xml_Tests extends qcl_datasource_controller
+{
+  
+ function method_testSimpleXml()
+  {
+    require_once("qcl/xml/simpleXML.php");
+    
+    $testfile = "../var/tmp/test.xml"; 
+    unlink($testfile);
+    
+    $parser = new qcl_xml_simpleXML;
+    $parser->createIfNotExists($testfile);
+    $parser->load($testfile,true);
+    
+    $doc =& $parser->getDocument();
+   
+    $record  =& $doc->addChild("record"); 
+    $record->setAttribute("id","first record");
+    $child   =& $record->addChild("child","boo!");
+    $child->setAttribute("id","child or first record"); 
+    
+    //$this->info($doc->asXml()); 
+    
+    $record2 =& $doc->addChild("record");
+    $record2->setAttribute("id","second record");
+    $child2  =& $record2->addChild("child");
+    $child2->setAttribute("id","child of second record"); 
+    
+    
+    //$this->Info($doc->tagChildren);
+    
+    $doc->recordModel[0]->setCDATA("CDATA of first record");
+    $doc->recordModel[0]->child->setAttribute("foo","yeah!");
+    
+    //$this->info($root->asXML());  
+    $parser->setData("/record[2]","Oder nicht?");
+    $parser->setAttribute("/record[1]/child","visible","false");
+    
+    //$this->info($doc->toArray());
+    $this->info($doc->asXML());
+    
+    $parser->save();
+    
+    $parser2 = new qcl_xml_simpleXML; 
+    $parser2->load($testfile,true,array("id"));
+    
+    $doc = $parser2->getDocument();
+    $this->info($doc->toArray());
+    $find = $parser2->getNodesByAttributeValue("id","second record");
+    
+  }
+  
+}
+
+?>
