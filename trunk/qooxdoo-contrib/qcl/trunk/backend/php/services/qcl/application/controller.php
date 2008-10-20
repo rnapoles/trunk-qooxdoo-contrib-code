f76bc4f514d53c231a93609d14fd4b1a38d7d812
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-10-20 14:58:07 +0000

    Added code to remotely interact with the user

--- trunk/qooxdoo-contrib/qcl/trunk/backend/php/services/qcl/application/controller.php
+++ trunk/qooxdoo-contrib/qcl/trunk/backend/php/services/qcl/application/controller.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once "qcl/datasource/controller.php";
+require_once "qcl/application/Client.php";
 
 /**
  * QCL Application Controller
@@ -20,47 +21,23 @@ class qcl_application_controller extends qcl_datasource_controller
    * Remotely alerts on the client. You need to call this
    * in the method like so: 
    * return $this->alert("foo!");
-   * 
    * @override
    * @param string response data
    */ 
   function alert($message)
   {
     $this->info($message);
-    $this->dispatchMessage("qcl.commands.alert", $message);
+    $this->dispatchMessage("qcl.commands.remote.alert", $message);
     return $this->getResponseData();  
   }
   
   /**
-   * Dispatches a message to the frontend, which will display a confirmation
-   * message and return the result to the rpc method. the signature of the method
-   * and the parameters transferred are the same plus an  boolean "true" as 
-   * additional last parameter. You need to call this method like so:
-   * return $this->confirmRemote("What do you want to do?", "service.name", $params);
-   * 
-   * @param string $display Message to be displayed
-   * @param string $service Full dot-separated service name including service method
-   * @param array  $params Array of mixed type parameters.
+   * @see qcl_application_Client::confirm
    */
   function confirmRemote ( $display, $service=null, $params=null )
   {
-    if ( is_null($service) )
-    {
-      $service = $this->getServicePath();
-    }
-    
-    if ( is_null($params) )
-    {
-      $service = $this->getParams();
-    }
-    
-    array_shift($params); // remove first parameter (form data)
-    $this->dispatchMessage("qcl.commands.confirmRemote",array( 
-      'display'  => $display,
-      'service'  => $service,
-      'params'   => $params
-    ) );
-   return $this->getResponseData();  
+    $client = new qcl_application_Client(&$this);
+    return $client->confirm ( $display, $service=null, $params=null );
   }
   
   /**
