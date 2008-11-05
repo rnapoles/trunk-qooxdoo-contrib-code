2ebfc52bd143a23fb6871c49ab4d126ec7b43d0f
fjakobs <fjakobs@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-11-05 13:02:08 +0000

    minor changes

---
+++ trunk/qooxdoo-contrib/TextMate/trunk/qooxdoo.tmbundle/Support/util.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+util.py
+
+Created by Fabian Jakobs on 2008-11-05.
+Copyright (c) 2008 1&1 Internet AG. All rights reserved.
+"""
+
+import os
+import sys
+
+def getQooxdoo():
+    qxPath = os.environ['QOOXDOO_PATH']
+    if not qxPath:
+        os.system("""CocoaDialog msgbox --title "qooxdoo SDK not found" --button1 "OK" --text "qooxdoo SDK not found" --informative-text "The path to the qooxdoo SDK could not be found. Please configure the QOOXDOO_PATH shell variable to point to the the qooxdoo SDK directory. You can do this in the Advanced tab of the Preferences dialog." """)
+        sys.exit()
+
+#        os.system("""CocoaDialog msgbox --title "qooxdoo SDK not found" -‑text "The path to the qooxdoo SDK could not be found. Please configure the QOOXDOO_PATH shell variable to point to the the qooxdoo SDK directory. You can do this in the Advanced tab of the Preferences dialog."  --button1 OK""")
+    return qxPath
+
+def addQooxdooClassPath():
+    path = getQooxdoo()
+    sys.path.append(path + "/tool/pylib")
+    sys.path.append(path + "/tool/bin")
+
+
