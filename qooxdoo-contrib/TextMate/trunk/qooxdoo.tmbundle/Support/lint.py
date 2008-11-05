23ce80fc5dd7503ac6127014bb40b5931e16c499
fjakobs <fjakobs@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-11-05 13:19:03 +0000

    minor changes

--- qooxdoo-contrib/TextMate/trunk/qooxdoo.tmbundle/Support/lint.py
+++ qooxdoo-contrib/TextMate/trunk/qooxdoo.tmbundle/Support/lint.py
@@ -13,73 +13,12 @@ import sys
 import getopt
 
 import util
+from util import TextMateLogger
 util.addQooxdooClassPath()
 
 from ecmascript.frontend import treegenerator
 import ecmalint
 
-# careful editing these, they are format strings
-TXMT_URL1_FORMAT = r"txmt://open?url=file://%s&line=%s"
-TXMT_URL2_FORMAT = r"txmt://open?url=file://%s&line=%s&col=%s"
-
-HTML_HEADER_FORMAT = r"""<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>%s</title>
-<style type="text/css">
-<!--
-
-body {
-  background-color: #D8E2F1;
-  margin: 0;
-}
-
-div#body {
-  border-style: dotted;
-  border-width: 1px 0;
-  border-color: #666;
-  margin: 10px 0;
-  padding: 10px;
-  background-color: #C9D9F0;
-}
-
-div#output{
-  padding: 0;
-  margin: 0;
-  font-family: Monaco;
-  font-size: 8pt;
-}
-
-strong.title { font-size: 11pt; }
-span.stderr { color: red; }
-p {margin: 0; padding: 2px 0; }
-
--->
-</style>
-</head>
-<body>
-<div id="body">
-<p><strong class="title">%s</strong></p><br>
-<div id="output">
-"""
-
-HTML_FOOTER = """</div>
-</div>
-</body>
-</html>
-"""
-
-
-class TextMateLogger:
-    def printHeader(self):
-        print HTML_HEADER_FORMAT % ("qooxdoo JavaScript lint", "qooxdoo JavaScript lint")
-
-    def log(self, filename, row, column, msg):
-        print "<a href='txmt://open?url=file://%s&line=%s&column=%s'>%s</a><br>" % (filename, row, column, msg)
-        
-    def printFooter(self):
-        print HTML_FOOTER
-        
 
 def lint(file):
     
@@ -90,7 +29,7 @@ def lint(file):
             break
     
     logger = TextMateLogger()
-    logger.printHeader()
+    logger.printHeader("qooxdoo JavaScript lint", "qooxdoo JavaScript lint")
     try:
         lint = ecmalint.Lint(file, logger)
         lint.checkMaps()
--- qooxdoo-contrib/TextMate/trunk/qooxdoo.tmbundle/Support/util.py
+++ qooxdoo-contrib/TextMate/trunk/qooxdoo.tmbundle/Support/util.py
@@ -11,21 +11,83 @@ import os
 import sys
 
 def getQooxdoo():
-    
     if "QOOXDOO_PATH" in os.environ:
         qxPath = os.environ['QOOXDOO_PATH']
-        if qx.path.isdir(qxPath):
+        if os.path.isdir(qxPath):
             return qxPath
 
-    os.system("""CocoaDialog msgbox --title "qooxdoo SDK not found" --button1 "OK" --text "qooxdoo SDK not found" --informative-text "The path to the qooxdoo SDK could not be found. Please configure the QOOXDOO_PATH shell variable to point to the the qooxdoo SDK directory. You can do this in the Advanced tab of the Preferences dialog." """)
+    logger = TextMateLogger()    
+    logger.printHeader("qooxdoo SDK not found", "qooxdoo SDK not found")
+    logger.printFooter()
+    print "The path to the qooxdoo SDK could not be found. Please configure the QOOXDOO_PATH shell variable to point to the the qooxdoo SDK directory. You can do this in the Advanced tab of the Preferences dialog."
+    
+    #os.system("""CocoaDialog msgbox --title "qooxdoo SDK not found" --button1 "OK" --text "qooxdoo SDK not found" --informative-text "The path to the qooxdoo SDK could not be found. Please configure the QOOXDOO_PATH shell variable to point to the the qooxdoo SDK directory. You can do this in the Advanced tab of the Preferences dialog." """)
     sys.exit()
-
-#        os.system("""CocoaDialog msgbox --title "qooxdoo SDK not found" -‑text "The path to the qooxdoo SDK could not be found. Please configure the QOOXDOO_PATH shell variable to point to the the qooxdoo SDK directory. You can do this in the Advanced tab of the Preferences dialog."  --button1 OK""")
-    return qxPath
 
 def addQooxdooClassPath():
     path = getQooxdoo()
     sys.path.append(path + "/tool/pylib")
     sys.path.append(path + "/tool/bin")
 
+
+# careful editing these, they are format strings
+TXMT_URL1_FORMAT = r"txmt://open?url=file://%s&line=%s"
+TXMT_URL2_FORMAT = r"txmt://open?url=file://%s&line=%s&col=%s"
+
+HTML_HEADER_FORMAT = r"""<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>%s</title>
+<style type="text/css">
+<!--
+
+body {
+  background-color: #D8E2F1;
+  margin: 0;
+}
+
+div#body {
+  border-style: dotted;
+  border-width: 1px 0;
+  border-color: #666;
+  margin: 10px 0;
+  padding: 10px;
+  background-color: #C9D9F0;
+}
+
+div#output{
+  padding: 0;
+  margin: 0;
+  font-family: Monaco;
+  font-size: 8pt;
+}
+
+strong.title { font-size: 11pt; }
+span.stderr { color: red; }
+p {margin: 0; padding: 2px 0; }
+
+-->
+</style>
+</head>
+<body>
+<div id="body">
+<p><strong class="title">%s</strong></p><br>
+<div id="output">
+"""
+
+HTML_FOOTER = """</div>
+</div>
+</body>
+</html>
+"""
+
+class TextMateLogger:
+    def printHeader(self, title, heading):
+        print HTML_HEADER_FORMAT % (title, heading)
+
+    def log(self, filename, row, column, msg):
+        print "<a href='txmt://open?url=file://%s&line=%s&column=%s'>%s</a><br>" % (filename, row, column, msg)
+
+    def printFooter(self):
+        print HTML_FOOTER
 
