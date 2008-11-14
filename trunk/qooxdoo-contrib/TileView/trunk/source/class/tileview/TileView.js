46b733b8d49894a28e23f3ce11899a8d9585205f
noggin182 <noggin182@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-11-14 21:36:42 +0000

    Initial copy of TileView contrib

---
+++ trunk/qooxdoo-contrib/TileView/trunk/source/class/tileview/TileView.js
@@ -0,0 +1,48 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+   http://qooxdoo.org
+
+   Copyright:
+     2008 CELCAT, http://www.celcat.com
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Matthew Gregory
+
+************************************************************************ */
+
+/**
+ * TileView
+ * Similar to the windows list view when displaying tiles
+ */
+
+qx.Class.define("tileview.TileView",
+{
+  extend : qx.ui.form.List,
+  construct : function()
+  {
+    this.base(arguments);
+    var flow = new flowlayout.FlowLayout();
+    this.getChildrenContainer().setLayout(flow);
+    this.getChildrenContainer().setHeight(800);
+  },
+  members :
+  {
+    _onAddChild : function(e)
+    {
+      var el = e.getData();
+      el.set(
+      {
+        width    : 250,
+        minWidth : 250,
+        maxWidth : 250
+      });
+      this.base(arguments, e);
+    }
+  }
+});
