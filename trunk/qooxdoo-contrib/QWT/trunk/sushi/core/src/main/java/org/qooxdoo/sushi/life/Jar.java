3abaa040b0baa4f385257c961b11b36ce131d677
mlhartme <mlhartme@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-11-17 11:26:37 +0000

    tweaks

--- trunk/qooxdoo-contrib/QWT/trunk/sushi/core/src/main/java/org/qooxdoo/sushi/life/Jar.java
+++ trunk/qooxdoo-contrib/QWT/trunk/sushi/core/src/main/java/org/qooxdoo/sushi/life/Jar.java
@@ -1,19 +1,27 @@
 package org.qooxdoo.sushi.life;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.qooxdoo.sushi.fs.Node;
 import org.qooxdoo.sushi.metadata.annotation.Sequence;
 import org.qooxdoo.sushi.metadata.annotation.Type;
 import org.qooxdoo.sushi.metadata.annotation.Value;
 
 @Type
 public class Jar {
+    public static String hash(Node node) throws IOException {
+        return node.sha();
+    }
+    
+    //--
+    
     @Value
     private Id id;
 
     @Value
-    private String sha;
+    private String hash;
     
     @Sequence(String.class)
     private List<String> directories;
@@ -22,9 +30,9 @@ public class Jar {
         this(new Id(), "");
     }
     
-    public Jar(Id id, String sha) {
+    public Jar(Id id, String hash) {
         this.id = id;
-        this.sha = sha;
+        this.hash = hash;
         this.directories = new ArrayList<String>();
     }
     
@@ -36,11 +44,11 @@ public class Jar {
         this.id = id;
     }
     
-    public String getSha() {
-        return sha;
+    public String getHash() {
+        return hash;
     }
-    public void setSha(String sha) {
-        this.sha = sha;
+    public void setHash(String hash) {
+        this.hash = hash;
     }
     
     public List<String> directories() {
