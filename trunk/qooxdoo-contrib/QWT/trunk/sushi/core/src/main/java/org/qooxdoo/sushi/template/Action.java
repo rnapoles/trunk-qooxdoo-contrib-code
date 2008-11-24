aa818dc877825abc407ba2108a0bf37ae14ed0b7
mlhartme <mlhartme@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-11-24 20:12:21 +0000

    fix file mode for unmodified files

--- trunk/qooxdoo-contrib/QWT/trunk/sushi/core/src/main/java/org/qooxdoo/sushi/template/Action.java
+++ trunk/qooxdoo-contrib/QWT/trunk/sushi/core/src/main/java/org/qooxdoo/sushi/template/Action.java
@@ -6,5 +6,9 @@ import org.qooxdoo.sushi.fs.Node;
 
 public abstract class Action {
     public abstract void directory(Node dir) throws IOException;
+    /**
+     * @prev null if file does not yet exist
+     * @next null if content is not changed, only the mode 
+     */
     public abstract void file(Node file, String prev, String next, int mode) throws IOException;
 }
