cd44451fee27527fb29e0b39af6bb01c6d407b9b
aback <aback@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-11-27 15:16:29 +0000

    Created 0.4-pre branch for bugfixing

---
+++ qooxdoo-contrib/HtmlArea/0.4-pre/Manifest.js
@@ -0,0 +1,22 @@
+{
+  "name" : "HtmlArea",
+  
+  "summary" : "A simple HTML editing widget",
+  "description" : "...",
+  "keywords" : ["widget"],
+  "homepage" : "http://contrib.qooxdoo.org/HtmlArea",
+  
+  "license" : "LGPL/EPL",
+  "authors" : [
+    {
+      "name" : "Alexander Back",
+      "user" : "aback",
+      "email" : "alex DOT back AT 1und1 DOT de"
+    }
+  ],
+
+  "namespace" : "htmlarea",
+  
+  "version" : "trunk",  
+  "qooxdoo-versions": ["0.7.x"]
+}
