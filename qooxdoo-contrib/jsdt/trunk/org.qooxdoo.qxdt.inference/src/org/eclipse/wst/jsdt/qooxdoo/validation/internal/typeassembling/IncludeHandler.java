9007eed8c061936719862c385ad4fea75ab09c3b
darkovarju <darkovarju@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-12-05 15:48:04 +0000

    using jsdt mixin support

--- qooxdoo-contrib/jsdt/trunk/org.qooxdoo.qxdt.inference/src/org/eclipse/wst/jsdt/qooxdoo/validation/internal/typeassembling/IncludeHandler.java
+++ qooxdoo-contrib/jsdt/trunk/org.qooxdoo.qxdt.inference/src/org/eclipse/wst/jsdt/qooxdoo/validation/internal/typeassembling/IncludeHandler.java
@@ -1,78 +1,29 @@
 package org.eclipse.wst.jsdt.qooxdoo.validation.internal.typeassembling;
 
-import java.util.Iterator;
-
 import org.eclipse.wst.jsdt.core.ast.IExpression;
 import org.eclipse.wst.jsdt.core.ast.IFieldReference;
 import org.eclipse.wst.jsdt.core.ast.IObjectLiteralField;
 import org.eclipse.wst.jsdt.core.ast.ISingleNameReference;
 import org.eclipse.wst.jsdt.core.infer.InferredAttribute;
-import org.eclipse.wst.jsdt.core.infer.InferredMethod;
 import org.eclipse.wst.jsdt.core.infer.InferredType;
 import org.eclipse.wst.jsdt.qooxdoo.validation.internal.ITypeManagement;
 import org.eclipse.wst.jsdt.qooxdoo.validation.internal.mixins.Mixin;
 
 public class IncludeHandler extends AbstractTypeConfigurationHandler {
 
-  private final ITypeManagement typeManager;
-
   public IncludeHandler( ITypeManagement typeManager ) {
     super( "include" );
-    this.typeManager = typeManager;
   }
 
   public void visit( IObjectLiteralField field,
                      InferredType currentClassDefinition )
   {
     IExpression initializer = field.getInitializer();
-    char[] name = getMixinName( initializer );
-    InferredType mixinType = typeManager.addType( name );
-    addMixinMethodsToClass( mixinType, currentClassDefinition );
-    addMixinAttributesToClass( mixinType, currentClassDefinition );
-    typeManager.getMixin( mixinType ).mixedIn( currentClassDefinition );
-  }
+    currentClassDefinition.addMixin( getMixinName( initializer ) );
+	}  
 
   public void visit( IObjectLiteralField field, Mixin type ) {
     // FIXME m_kempka IMPLEMENT ME May 15, 2008
-  }
-
-  private void addMixinAttributesToClass( InferredType mixinType,
-                                          InferredType classType )
-  {
-    if( mixinType.attributes != null ) {
-      for( InferredAttribute each : mixinType.attributes ) {
-        if( each != null ) {
-          if( !hasAttribute( classType, each ) ) {
-            classType.addAttribute( each );
-          }
-        }
-      }
-    }
-  }
-
-  private boolean hasAttribute( InferredType classType,
-                                InferredAttribute attribute )
-  {
-    boolean result = false;
-    for( InferredAttribute each : classType.attributes ) {
-      if( attribute.equals( each ) ) {
-        result = true;
-        break;
-      }
-    }
-    return result;
-  }
-
-  private void addMixinMethodsToClass( InferredType mixinType,
-                                       InferredType classType )
-  {
-    if( mixinType.methods != null ) {
-      Iterator<InferredMethod> iter = mixinType.methods.iterator();
-      while( iter.hasNext() ) {
-        InferredMethod each = iter.next();
-        classType.addMethod( each.name, each.getFunctionDeclaration(), false );
-      }
-    }
   }
 
   private char[] getMixinName( IExpression initializer ) {
