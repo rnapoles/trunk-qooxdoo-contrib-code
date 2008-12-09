9e79a4abfc7b33bb22ef24aa377f2f955ba65ce1
mkempka <mkempka@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-12-09 15:43:46 +0000

    improved documentation

--- trunk/qooxdoo-contrib/jsdt/trunk/org.qooxdoo.qxdt.releng.library/src/org/qooxdoo/qxdt/releng/Main.java
+++ trunk/qooxdoo-contrib/jsdt/trunk/org.qooxdoo.qxdt.releng.library/src/org/qooxdoo/qxdt/releng/Main.java
@@ -6,8 +6,8 @@ import java.io.File;
  * Fixes "@type" parameter of qooxdoo versions 0.7.3 and older to comply with
  * JSDoc. The first argument to this program has to be a directory that contains
  * only the qooxdoo classes and no subdirectories.<br/> These bash commands can
- * construct such a directory:</br> <code>BLA=`find . -name "*.js"`</code><br/>
- * <code>for II in $BLA; do NEWNAME=`echo ${II} | sed 's/\//_/g' | sed 's/^\._//'`; echo "${II} -> ${NEWNAME}"; cp ${II} /tmp/qxclasses/${NEWNAME}; done</code>
+ * construct such a directory:</br> <code>FILES=`find . -name "*.js"`</code><br/>
+ * <code>for II in $FILES; do NEWNAME=`echo ${II} | sed 's/\//_/g' | sed 's/^\._//'`; echo "${II} -> ${NEWNAME}"; cp ${II} /tmp/qxclasses/${NEWNAME}; done</code>
  * 
  * @version $Id: Main.java,v 1.0 May 7, 2008 5:59:00 PM user Exp $
  */
