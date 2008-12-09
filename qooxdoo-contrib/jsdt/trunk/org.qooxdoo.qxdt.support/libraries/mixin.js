c6f1e5cfad8464f1275e9c5ff376e1399d8970b1
mkempka <mkempka@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2008-12-09 16:34:25 +0000

    initial commit

---
+++ qooxdoo-contrib/jsdt/trunk/org.qooxdoo.qxdt.support/libraries/mixin.js
@@ -0,0 +1,26 @@
+/**
+* Object Window
+* @super Global
+* @type constructor
+* @class Window
+* @since FireFox 2.0.0.3
+*/
+function qx(){};
+qx.prototype=new Object();
+/**
+* property Mixin
+* @type Object
+* @class qx
+* @since qooxdoo 0.7.2
+*/
+qx.Mixin=new Object();
+
+/**
+* function define(arg1)
+* @type String
+* @param name String
+* @param config Object
+* @class Mixin
+* @since FireFox 2.0.0.3
+*/
+qx.Mixin.define=new function(name, config){};
