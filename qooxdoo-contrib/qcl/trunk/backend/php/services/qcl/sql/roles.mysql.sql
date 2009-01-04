dd72335a359e280f2fb10e57b5e3a2565f71b318
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-01-04 00:38:20 +0000

    Deleting the sql folder which is no longer needed

--- qooxdoo-contrib/qcl/trunk/backend/php/services/qcl/sql/roles.mysql.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `roles` (
-  `id` int(10) unsigned NOT NULL auto_increment,
-  `namedId` varchar(50) character set utf8 NOT NULL default '',
-  `name` varchar(50) character set utf8 NOT NULL default '',
-  `note` varchar(255) character set utf8 default NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
