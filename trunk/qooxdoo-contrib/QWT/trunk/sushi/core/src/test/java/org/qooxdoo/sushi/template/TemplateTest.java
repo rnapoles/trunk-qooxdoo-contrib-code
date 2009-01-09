bbf670a338b3a0513c9f055ec25143cc0187f274
mlhartme <mlhartme@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-01-09 15:43:47 +0000

    cleanup

--- trunk/qooxdoo-contrib/QWT/trunk/sushi/core/src/test/java/org/qooxdoo/sushi/template/TemplateTest.java
+++ trunk/qooxdoo-contrib/QWT/trunk/sushi/core/src/test/java/org/qooxdoo/sushi/template/TemplateTest.java
@@ -1,16 +1,12 @@
 package org.qooxdoo.sushi.template;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.fail;
 
 import java.io.IOException;
 
 import org.junit.Test;
 import org.qooxdoo.sushi.fs.IO;
 import org.qooxdoo.sushi.fs.Node;
-import org.qooxdoo.sushi.template.Template;
-import org.qooxdoo.sushi.template.TemplateException;
 
 public class TemplateTest {
 	private Template template;
@@ -19,42 +15,6 @@ public class TemplateTest {
         template = new Template(new IO().getTemp().createTempDirectory());
 	}
 
-	@Test
-	public void replace() throws TemplateException {
-		String str;
-		
-		template.variables().put("1", "one");
-		template.variables().put("2", "two");
-		
-		assertEquals("", template.replace("a", "b", ""));
-		str = "xy";
-		assertSame(str, template.replace("a", "b", str));
-        assertEquals("one", template.replace("a", "b", "a1b"));
-		assertEquals("onetwo", template.replace("(", ")", "(1)(2)"));
-		assertEquals("aonebtwoc", template.replace("(", ")", "a(1)b(2)c"));
-		assertEquals("onetwo", template.replace("_", "_", "_1__2_"));
-		try {
-			template.replace("_", "_", "_3_");
-			fail();
-		} catch (TemplateException e) {
-			// ok
-		}
-		try {
-			template.replace("_", "_", "_");
-			fail();
-		} catch (TemplateException e) {
-			// ok
-		}
-	}
-
-    @Test
-    public void escape() throws TemplateException {
-        template.variables().put("1", "one");
-        template.variables().put("2", "two");
-        
-        assertEquals("a1b", template.replace("a", "b", "\\a1b"));
-        assertEquals("a1bab", template.replace("a", "b", "\\a1b\\ab"));
-    }
 
 	@Test
     public void copy() throws IOException {
