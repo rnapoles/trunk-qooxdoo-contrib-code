abecae1b2919307d6618f8b9867b57a182fef695
mlhartme <mlhartme@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-01-11 09:32:11 +0000

    move

--- qooxdoo-contrib/QWT/trunk/sushi/core/src/main/java/org/qooxdoo/sushi/fs/template/StatusAction.java
+++ qooxdoo-contrib/QWT/trunk/sushi/core/src/main/java/org/qooxdoo/sushi/fs/template/StatusAction.java
@@ -1,34 +1,36 @@
-package org.qooxdoo.sushi.template;
+package org.qooxdoo.sushi.fs.template;
 
 import java.io.IOException;
 
 import org.qooxdoo.sushi.fs.Node;
-import org.qooxdoo.sushi.util.Diff;
-import org.qooxdoo.sushi.util.Strings;
 
-public class DiffAction extends Action {
+public class StatusAction extends Action {
     private final Node base;
     private final StringBuilder status;
     
-    public DiffAction(Node base) {
+    public StatusAction(Node base) {
         this.base = base;
         this.status = new StringBuilder();
     }
     
     @Override
     public void directory(Node dest) throws IOException {
+    	add('A', dest);
     }
 
     @Override
     public void file(Node dest, String prev, String next, int mode) throws IOException {
-        status.append("### ").append(dest.getRelative(base)).append('\n');
         if (prev == null) {
-        	status.append(Strings.indent(next, "+ "));
+        	add('A', dest);
         } else if (next != null) {
-        	status.append(Diff.diff(prev, next));
+        	add('M', dest);
         } else {
-            status.append("[chmod " + Integer.toOctalString(mode) + "]\n");
+        	add('m', dest);
         }
+    }
+
+    private void add(char flag, Node dest) throws IOException {
+        status.append(flag).append(' ').append(dest.getRelative(base)).append('\n');
     }
 
     public String get() {
