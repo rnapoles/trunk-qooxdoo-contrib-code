523f41cbda4eeadacb562b0a6413b9c04da753b0
mlhartme <mlhartme@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-01-12 20:40:11 +0000

    cleanup

--- trunk/qooxdoo-contrib/QWT/trunk/sushi/core/src/test/java/org/qooxdoo/sushi/fs/CopyDiffTest.java
+++ trunk/qooxdoo-contrib/QWT/trunk/sushi/core/src/test/java/org/qooxdoo/sushi/fs/CopyDiffTest.java
@@ -11,12 +11,12 @@ import org.qooxdoo.sushi.fs.IO;
 import org.qooxdoo.sushi.fs.Node;
 import org.qooxdoo.sushi.util.Substitution;
 
-public class CopyTest {
+public class CopyDiffTest {
 	private IO io;
 	private Map<String, String> variables;
 	private Copy copy;
 	
-	public CopyTest() throws IOException {
+	public CopyDiffTest() throws IOException {
 		io = new IO();
 		variables = new HashMap<String, String>();
         copy = new Copy(io.getTemp().createTempDirectory(),
@@ -33,34 +33,34 @@ public class CopyTest {
         variables.put("home", "mhm");
         variables.put("machine", "walter");
         
-        assertEquals("", status(destdir));
+        assertEquals("", brief(destdir));
         assertEquals("", diff(destdir));
 
         copy.getSourceDir().join("file").writeLines("home: ${home}", "machine: ${machine}");
-        assertEquals("A file\n", status(destdir));
+        assertEquals("A file\n", brief(destdir));
         assertEquals("### file\n" +
         		"+ home: mhm\n" +
                 "+ machine: walter\n", diff(destdir));
         copy.copy(destdir);
-        assertEquals("", status(destdir));
+        assertEquals("", brief(destdir));
         
         copy.getSourceDir().join("folder").mkdir();
-        assertEquals("A folder\n", status(destdir));
+        assertEquals("A folder\n", brief(destdir));
         copy.copy(destdir);
-        assertEquals("", status(destdir));
+        assertEquals("", brief(destdir));
         
         copy.getSourceDir().join("superdir/subdir").mkdirs();
-        assertEquals("A superdir\nA superdir/subdir\n", status(destdir));
+        assertEquals("A superdir\nA superdir/subdir\n", brief(destdir));
         copy.copy(destdir);
-        assertEquals("", status(destdir));
+        assertEquals("", brief(destdir));
 
         copy.getSourceDir().join("folder/file").writeLines("home: ${home}", "machine: ${machine}");
-        assertEquals("A folder/file\n", status(destdir));
+        assertEquals("A folder/file\n", brief(destdir));
         copy.copy(destdir);
-        assertEquals("", status(destdir));
+        assertEquals("", brief(destdir));
         
         variables.put("machine", "fritz");
-        assertEquals("M file\nM folder/file\n", status(destdir));
+        assertEquals("M file\nM folder/file\n", brief(destdir));
         assertEquals("### file\n" +
                 "- machine: walter\n" +
                 "+ machine: fritz\n" +
@@ -68,7 +68,7 @@ public class CopyTest {
                 "- machine: walter\n" +
                 "+ machine: fritz\n", diff(destdir));
         copy.copy(destdir);
-        assertEquals("", status(destdir));
+        assertEquals("", brief(destdir));
         assertEquals("", diff(destdir));
     }
 	
@@ -86,19 +86,19 @@ public class CopyTest {
         assertEquals(0700, destdir.join("file").getMode());
 
         file.setMode(0655);
-        assertEquals("m file\n", status(destdir));
+        assertEquals("m file\n", brief(destdir));
         copy.copy(destdir);
         assertEquals(0655, destdir.join("file").getMode());
-    }
-    
-    private String status(Node destdir) throws IOException {
-        return doDiff(destdir, true);
     }
     
     private String diff(Node destdir) throws IOException {
         return doDiff(destdir, false);
     }
 
+    private String brief(Node destdir) throws IOException {
+        return doDiff(destdir, true);
+    }
+    
     private String doDiff(Node destdir, boolean brief) throws IOException {
         Node tmp = io.getTemp().createTempDirectory();
         copy.copy(tmp);
