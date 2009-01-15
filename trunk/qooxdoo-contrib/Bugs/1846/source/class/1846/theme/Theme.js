8c921cc68fd023c328837c6f439b4142a0d59995
aback <aback@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-01-15 16:11:42 +0000

    Added skeleton for bug #1846

---
+++ trunk/qooxdoo-contrib/Bugs/1846/source/class/1846/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("1846.theme.Theme",
+{
+  meta :
+  {
+    color : 1846.theme.Color,
+    decoration : 1846.theme.Decoration,
+    font : 1846.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : 1846.theme.Appearance
+  }
+});
