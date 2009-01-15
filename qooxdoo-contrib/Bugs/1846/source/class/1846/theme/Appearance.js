94887f7d49368fba40b6b7db3769f4eea7cbf64f
aback <aback@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-01-15 16:11:42 +0000

    Added skeleton for bug #1846

---
+++ qooxdoo-contrib/Bugs/1846/source/class/1846/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("1846.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
