62319ec0b2deb85f81d28823a10fab1e3b975b84
jonathan_rass <jonathan_rass@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-02-02 09:04:48 +0000

    [BUG #1881 ASSIGNED]: CheckBox states are not updated after mouseup http://bugzilla.qooxdoo.org/show_bug.cgi?id=1881
    - Added test case

---
+++ qooxdoo-contrib/Bugs/1881/source/class/1881/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("1881.theme.Decoration",
+{
+  extend : qx.theme.modern.Decoration,
+
+  decorations :
+  {
+  }
+});
