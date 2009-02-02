7e137a992e7a5c726cf130f3bfd3fab60994441b
thron7 <thron7@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-02-02 13:50:39 +0000

    [BUG #1207] test skeleton

---
+++ trunk/qooxdoo-contrib/Bugs/1207/gui2/source/class/gui2/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("gui2.theme.Theme",
+{
+  meta :
+  {
+    color : gui2.theme.Color,
+    decoration : gui2.theme.Decoration,
+    font : gui2.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : gui2.theme.Appearance
+  }
+});
