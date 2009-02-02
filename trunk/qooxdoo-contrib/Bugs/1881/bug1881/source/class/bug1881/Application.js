beb5f9282984f27456de14b751a93617af35097b
jonathan_rass <jonathan_rass@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-02-02 09:18:33 +0000

    [BUG #1881 ASSIGNED]: CheckBox states are not updated after mouseup http://bugzilla.qooxdoo.org/show_bug.cgi?id=1881

--- trunk/qooxdoo-contrib/Bugs/1881/bug1881/source/class/bug1881/Application.js
+++ trunk/qooxdoo-contrib/Bugs/1881/bug1881/source/class/bug1881/Application.js
@@ -55,19 +55,41 @@ qx.Class.define("bug1881.Application",
       -------------------------------------------------------------------------
       */
 
-      // Create a button
-      var button1 = new qx.ui.form.Button("First Button", "bug1881/test.png");
-
       // Document is the application root
       var doc = this.getRoot();
 			
-      // Add button to document at fixed coordinates
-      doc.add(button1, {left: 100, top: 50});
 
-      // Add an event listener
-      button1.addListener("execute", function(e) {
-        alert("Hello World!");
-      });
+      var label = new qx.ui.basic.Label("What do you need for the beach?");
+
+      // create the main layout
+      var mainLayout = new qx.ui.layout.VBox();
+      mainLayout.setSpacing(10);
+
+      // add the main layout to a container widget and to the document root
+      var container = new qx.ui.container.Composite(mainLayout);
+      container.setPadding(20);
+
+      this.getRoot().add(container, {left:0,top:0});
+
+      container.add(label);
+
+      // Create some radio buttons
+      var cbOil = new qx.ui.form.CheckBox("Sun Oil");
+      var cbTowel = new qx.ui.form.CheckBox("Towel");
+      var cbBeer = new qx.ui.form.CheckBox("Beer");
+      var cbBT =  new qx.ui.form.CheckBox("Bathing togs");
+
+      this._checkBoxes = [ cbOil, cbTowel, cbBeer, cbBT ];
+
+      // Add them to the container
+      container.add(cbOil);
+      container.add(cbTowel);
+      container.add(cbBeer);
+      container.add(cbBT);
+
+      // Add button to document at fixed coordinates
+      doc.add(container, {left: 100, top: 50});
+
     }
   }
 });
