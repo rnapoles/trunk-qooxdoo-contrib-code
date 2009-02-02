3565934ff9c924e924e4fe9fae4084f73efc2f51
d_wagner <d_wagner@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-02-02 17:26:24 +0000

    Grid layout

--- qooxdoo-contrib/Simulator/trunk/source/class/custom/simulator/Shell.js
+++ qooxdoo-contrib/Simulator/trunk/source/class/custom/simulator/Shell.js
@@ -31,92 +31,72 @@ qx.Class.define("custom.simulator.Shell",
 
     this.w = {}; // widget registry
 
+    this.setCaption("Simulator Shell");
     this.setMaxWidth(450);
     this.setMaxHeight(400);
 
     // command input
-    this.setLayout(new qx.ui.layout.VBox(10));
+    var gridLayout = new qx.ui.layout.Grid(5,10);
+    gridLayout.setColumnAlign(0, "left", "middle");
+    gridLayout.setColumnAlign(1, "left", "middle");
+    gridLayout.setColumnAlign(2, "left", "middle");
+    gridLayout.setColumnAlign(3, "left", "middle");    
+    this.setLayout(gridLayout);    
     
     // 1st row
-    var r1 = new qx.ui.container.Composite(new qx.ui.layout.HBox());
-    this.add(r1)
-
-    r1.add(new qx.ui.basic.Label("Command: "));
+    this.add(new qx.ui.basic.Label("Command:"), {row: 0, column: 0});
     this.w.cmd = new qx.ui.form.TextField();
-    r1.add(this.w.cmd);
-    r1.add(new qx.ui.basic.Label(" sessionId: "));
+    this.add(this.w.cmd, {row: 0, column: 1});    
+    this.add(new qx.ui.basic.Label(" sessionId:"), {row: 0, column: 2});
     this.w.sessid = new qx.ui.form.TextField();
-    r1.add(this.w.sessid);
-
-    // 3rd row
-    var r3 = new qx.ui.container.Composite(new qx.ui.layout.HBox());
-    this.add(r3);
-
-    r3.add(new qx.ui.basic.Label("Param1: "));
-    var tf3 = new qx.ui.form.TextField();
-    r3.add(tf3);
-    this.w.parm1 = tf3;
-
-    // 4th row
-    var r4 = new qx.ui.container.Composite(new qx.ui.layout.HBox());
-    this.add(r4);
+    this.add(this.w.sessid, {row: 0, column: 3});
     
-    r4.add(new qx.ui.basic.Label("Param2: "));
-    var tf4 = new qx.ui.form.TextField();
-    r4.add(tf4);
-    this.w.parm2 = tf4;
-
-    // 2nd row
-    var r2 = new qx.ui.container.Composite(new qx.ui.layout.HBox());
-    this.add(r2);
-    
-    r2.add(new qx.ui.basic.Label("QuickCommand (hit return):"));
-
+    // 2nd row    
+    this.add(new qx.ui.basic.Label("QuickCommand (hit return):"), {row: 1, column: 0});
     var t1 = new qx.ui.form.TextField();
-    this.add(t1);
+    this.add(t1, {row: 1, column: 1, colSpan: 3});
     this.w.quickcmd = t1;
     t1.addListener("keydown",this._processCmd,this);
-
+    
+    // 3rd row
+    this.add(new qx.ui.basic.Label("Param1: "), {row: 2, column: 0});
+    var tf3 = new qx.ui.form.TextField();
+    this.add(tf3, {row: 2, column: 1, colSpan: 3});
+    this.w.parm1 = tf3;
+    
+    // 4th row
+    this.add(new qx.ui.basic.Label("Param2: "), {row: 3, column: 0});
+    var tf4 = new qx.ui.form.TextField();
+    this.add(tf4, {row: 3, column: 1, colSpan: 3});
+    this.w.parm2 = tf4;
+    
+    // 5th row
     // command output
     var cmdout = new qx.ui.form.TextArea();
-    this.add(cmdout);
+    cmdout.setHeight(100);
+    cmdout.setReadOnly(true);
+    this.add(cmdout,{row: 4, column: 0, colSpan: 4});
     this.w.cmdout = cmdout;
-    cmdout.set({
-      overflow : "auto",
-      height   : 100
-    });
-
-    // 5th row
-    var r5 = new qx.ui.container.Composite(new qx.ui.layout.HBox());
-    this.add(r5);    
-
-
+    
+    // 6th row
     // commit button
     var commitb = new qx.ui.form.Button("Send");
-    r5.add(commitb);
-    commitb.set({
-      horizontalAlign : "left"
-      });
-
+    commitb.setAllowGrowX(false);
+    commitb.setWidth(60);
     commitb.addListener("execute", this._processSend, this);
-
+    this.add(commitb, {row: 5, column: 1});
+    
     // reset button
     var resetb = new qx.ui.form.Button("Reset");
-    r5.add(resetb);
-    resetb.set({
-      horizontalAlign : "right"
-      });
-
+    resetb.setAllowGrowX(false);
+    resetb.setWidth(60);
     resetb.addListener("execute", this._resetCmdFields, this);
-
-    // 6th row
-    var r6 = new qx.ui.container.Composite(new qx.ui.layout.HBox());
-    this.add(r6);
+    this.add(resetb, {row: 5, column: 2});
     
+    // 7th row
     this.w.status = new qx.ui.basic.Label();
     var st = this.w.status;
-    r6.add(st);
-
+    this.add(st, {row: 6, column: 0, colSpan: 4});
 
   }, // construct()
 
