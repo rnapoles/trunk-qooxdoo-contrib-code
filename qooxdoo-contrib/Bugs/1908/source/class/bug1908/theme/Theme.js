60f55b1813dc3dde3001e1bc6a1d005eaf49a8fe
aback <aback@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-02-04 17:17:59 +0000

    Added sample applications for Bug #1864 and #1908

---
+++ qooxdoo-contrib/Bugs/1908/source/class/bug1908/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug1908.theme.Theme",
+{
+  meta :
+  {
+    color : bug1908.theme.Color,
+    decoration : bug1908.theme.Decoration,
+    font : bug1908.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug1908.theme.Appearance
+  }
+});
