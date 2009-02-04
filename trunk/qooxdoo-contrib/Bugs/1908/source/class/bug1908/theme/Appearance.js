59fafa676b9ea81f6b0cf585504fb317794fd768
aback <aback@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-02-04 17:17:59 +0000

    Added sample applications for Bug #1864 and #1908

---
+++ trunk/qooxdoo-contrib/Bugs/1908/source/class/bug1908/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug1908.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
