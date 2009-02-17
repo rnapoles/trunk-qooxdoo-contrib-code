a7be985e79b839ed0ac6c7642970eee29ba5cee3
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-02-17 21:52:09 +0000

    NEW - bug 1963: Make the qcl php backend PHP5-compatible http://bugzilla.qooxdoo.org/show_bug.cgi?id=1963

--- qooxdoo-contrib/qcl/trunk/backend/php/services/qcl/db/PropertyModel.php
+++ qooxdoo-contrib/qcl/trunk/backend/php/services/qcl/db/PropertyModel.php
@@ -5,6 +5,10 @@
  */
 require_once "qcl/db/__init__.php";
 require_once "qcl/jsonrpc/model.php";
+if ( phpversion() < 5 ) 
+  require_once "qcl/db/Overload.php4";
+else 
+  require_once "qcl/db/Overload.php5";
 
 
 /**
@@ -15,7 +19,7 @@ require_once "qcl/jsonrpc/model.php";
  * the whole model or all records. "getFoo" should only be used for
  * model data.
  */
-class qcl_db_PropertyModel extends qcl_jsonrpc_model
+class qcl_db_PropertyModel extends qcl_db_Overload
 {
 
  /**
@@ -206,92 +210,7 @@ class qcl_db_PropertyModel extends qcl_jsonrpc_model
     return $this->name;
   }
   
-  //-------------------------------------------------------------
-  // overloading
-  //-------------------------------------------------------------   
 
-  /**
-   * method called when called method does not exist. This will check whether 
-   * method name is 
-   * 
-   * - getXxx or setXxx and then call getProperty("xxx") 
-   *    or setProperty("xxx", $arguments[0]). 
-   * - findByXxx to call findBy("xxx",...)
-   * 
-   * Otherwise, raise an error.
-   * @param string $function  Method name
-   * @param array  $arguments Array or parameters passed to the method
-   * @param mixed  $return    call-time reference to return value (PHP4-only)
-   * @return mixed return value (PHP5 only) 
-   */
-  function __call( $function, $arguments, &$return) 
-  {
-    /*
-     * we need to reimplement the mixin behavior from 
-     * qcl_core_object because we cannot call the parent 
-     * class method
-     * @see qcl_core_object::__call()
-     */
-    if ( phpversion() >= 5 and isset($this->_mixinlookup[$method] ) )
-    {
-      $elems = array();
-      for ($i=0, $_i=count($args); $i<$_i; $i++) $elems[] = "\$args[$i]";
-      eval("\$result = ".$this->_mixinlookup[$method]."::"
-          .$method."(".implode(',',$elems).");");
-      return $result;
-    }
-    
-    /*
-     * php4 or no matching mixin methods found.
-     * Now we intercept method calls
-     */
-    $startsWith = strtolower( substr( $function, 0, 3 ) );
-    $endsWith   = strtolower( substr( $function, 3 ) );
-    
-    /*
-     * get.. and set... for property access
-     * @todo correct calling of method with variable arguments
-     */
-    if ( $startsWith == "set" )
-    {
-      //$this->debug("setting $endsWith = " . $arguments[0] . "(" . gettype($arguments[0]) . ")." . print_r($arguments,true));
-      $this->setProperty( $endsWith, $arguments[0], $arguments[1], $arguments[2] );
-    }
-    elseif ( $startsWith == "get" )
-    { 
-      $return = $this->getProperty( $endsWith, $arguments[0], $arguments[1], $arguments[2] );
-      //$this->debug("getting $endsWith = $return");
-    }
-    
-    /*
-     * findBy...
-     */
-    elseif ( strtolower( substr( $function, 0, 6 ) ) == "findby" )
-    {
-      $propName = strtolower( substr( $function, 6 ) );
-      $return = $this->findBy($propName,$arguments[0],$arguments[1],$arguments[2]);
-    }
-    
-    /*
-     * method not known, raise error
-     */
-    else
-    {
-      $this->raiseError("Unknown method " . get_class($this) . "::$function().");
-    }
-    
-    /*
-     * return result
-     */
-    if ( phpversion() < 5) 
-    {
-      return true; // PHP 4: return value is in &$return
-    }
-    else
-    {
-      return $return; // PHP 5  
-    }
-  } 
       
   //-------------------------------------------------------------
   // Initialization
