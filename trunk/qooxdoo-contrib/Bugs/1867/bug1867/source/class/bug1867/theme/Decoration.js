f0d8e8203cee0b971839c7acb58eaa4275c69a8a
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-02-23 14:19:51 +0000

    [BUG ASSIGNED 1867]: "changeSelection" event is fired at every click http://bugzilla.qooxdoo.org/show_bug.cgi?id=1867
    -commited infrastructure for bug

---
+++ trunk/qooxdoo-contrib/Bugs/1867/bug1867/source/class/bug1867/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug1867.theme.Decoration",
+{
+  extend : qx.theme.modern.Decoration,
+
+  decorations :
+  {
+  }
+});
