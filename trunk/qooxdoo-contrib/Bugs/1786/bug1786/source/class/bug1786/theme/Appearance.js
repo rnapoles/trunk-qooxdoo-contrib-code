643f85b93491e393202ac6b0ec87b9f2bfaff4f7
jonathan_rass <jonathan_rass@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-02-23 15:23:14 +0000

    [BUG #1786 ASSIGNED]: Virtual: Create an architecture for HTML base cell renderer http://bugzilla.qooxdoo.org/show_bug.cgi?id=1786
    Initial commit

---
+++ trunk/qooxdoo-contrib/Bugs/1786/bug1786/source/class/bug1786/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug1786.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
---
+++ trunk/qooxdoo-contrib/Bugs/1786/bug1786/source/class/bug1786/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug1786.theme.Theme",
+{
+  meta :
+  {
+    color : bug1786.theme.Color,
+    decoration : bug1786.theme.Decoration,
+    font : bug1786.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug1786.theme.Appearance
+  }
+});
