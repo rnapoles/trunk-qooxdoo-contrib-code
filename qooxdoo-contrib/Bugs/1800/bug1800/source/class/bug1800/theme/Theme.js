98567120f6f4f9eafb580dec783b2bd350ea2703
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-02-23 16:35:15 +0000

    [BUG ASSIGNED 1800]: The CheckBox has an other background color if the CheckBox is checked by keyboard. http://bugzilla.qooxdoo.org/show_bug.cgi?id=1800
    - committed infrastructure for bug

---
+++ qooxdoo-contrib/Bugs/1800/bug1800/source/class/bug1800/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug1800.theme.Theme",
+{
+  meta :
+  {
+    color : bug1800.theme.Color,
+    decoration : bug1800.theme.Decoration,
+    font : bug1800.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug1800.theme.Appearance
+  }
+});
