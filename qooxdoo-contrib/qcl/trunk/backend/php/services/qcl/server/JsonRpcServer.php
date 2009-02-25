322fce5c67ca69ceff6086c207fa12604eb387bf
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-02-25 11:54:02 +0000

    Incomplete - taskRewrite Server OO-Style

--- qooxdoo-contrib/qcl/trunk/backend/php/services/qcl/server/JsonRpcServer.php
+++ qooxdoo-contrib/qcl/trunk/backend/php/services/qcl/server/JsonRpcServer.php
@@ -23,6 +23,11 @@
  * dot-separated path format and expect to find the class containing the
  * service in a file of the service name (with dots converted to slashes and
  * ".php" appended).
+ * 
+ * Usage:
+ * 
+ * $server = new JsonRpcServer;
+ * $server->start();
  */
 
 
@@ -45,7 +50,7 @@ define("ScriptTransport_NotInUse", -1);
  */
 if (! defined("JsonRpcErrorHandling"))
 {
-  define("JsonRpcErrorHandling",             "on");
+  define("JsonRpcErrorHandling", "on");
 }
 
 /**
@@ -103,6 +108,45 @@ class JsonRpcServer extends AbstractServer
     $this->debug("Server initialized.");
   }  
 
+  /**
+   * Overriding the start() method to do error handling
+   * The main idea of this code is to keep PHP from messing up 
+   * the JSONRPC response if a parsing or runtime error occurs, 
+   * and to allow the client application to handle those errors nicely
+   */
+  function start()
+  {
+      /**
+     * main error handling code
+     */
+    if (JsonRpcErrorHandling == "on")
+    {
+      /*
+       * start buffering to catch errors with handler function
+       */
+      ob_start( array($this,"jsonrpc_catch_errors") );
+    
+      /*
+       * This will not always work, so do some more hacking to 
+       * comment out uncaught errors. You'll need to examine the
+       * http response to see the uncaught errors!
+       */
+      ini_set('error_prepend_string', "/*");
+      ini_set('error_append_string', "*/");
+    
+      /*
+       * error handler function for php jsonrpc
+       */
+      set_error_handler( array($this,"jsonRpcErrorHandler") );
+    }
+    
+    /*
+     * call parent method
+     */
+    parent::start();
+  }
+  
+  
   function getServerData()
   {
     return $this->input->server_data;
@@ -225,6 +269,141 @@ class JsonRpcServer extends AbstractServer
     );
 
     return $this->json->encode($ret);
+  }  
+  
+  /**
+   * error handling callback function
+   * php4 cannot handle all errors, that's why we have to use a
+   * workaround using output buffering (see post by
+   * smp at ncoastsoft dot com at
+   *   http://www.php.net/manual/en/function.set-error-handler.php
+   */
+  function jsonrpc_catch_errors( $buffer )
+  {
+    if (ereg("(error</b>:)(.+)(<br)", $buffer, $regs) )
+    {
+      /*
+       * parse error string from PHP error message
+       */
+      $err = html_entity_decode(preg_replace("/<.*?>/","",$regs[2]));
+      
+      /*
+       * log error message
+       */
+      $this->debug("*** Error: $err");
+      
+      /*
+       * return error formatted as a JSONRPC error response
+       */
+      return
+        '{' .
+        '  error:' .
+        '  {' .
+        '    "origin":' . JsonRpcError_Origin_Server . ',' . 
+        '    "code":' .  JsonRpcError_ScriptError . ',' .
+        '    "message":"Fatal PHP Error: '. addslashes($err) .
+        ' "}' .
+        '}';
+      
+    }
+    else
+    {
+      /*
+       * Buffer does not contain a php error message, so return
+       * it unmodified.
+       */
+      return $buffer;
+    }
+  }
+  
+  
+  /**
+   * jsonrpc error handler to output json error response messages
+   */
+  function jsonRpcErrorHandler($errno, $errstr, $errfile, $errline)
+  {
+    /*
+     * Determine error type
+     * @todo: remove those which are not captured by set_error_handler()
+     */
+    switch($errno){
+      case E_ERROR:
+        $errtype= "Error";
+        break;
+  
+      case E_WARNING:
+        $errtype= "Warning";
+        break;
+  
+      case E_PARSE:
+        $errtype= "Parse Error";
+        break;
+  
+      case E_NOTICE:
+        $errtype= "Notice";
+        break;
+  
+      case E_CORE_ERROR:
+        $errtype= "Core Error";
+        break;
+  
+      case E_CORE_WARNING:
+        $errtype= "Core Warning";
+        break;
+  
+      case E_COMPILE_ERROR:
+        $errtype= "Compile Error";
+        break;
+  
+      case E_COMPILE_WARNING:
+        $errtype= "Compile Warning";
+        break;
+  
+      case E_USER_ERROR:
+        $errtype= "User Error";
+        break;
+  
+      case E_USER_WARNING:
+        $errtype= "User Warning";
+        break;
+  
+      case E_USER_NOTICE:
+        $errtype= "User Notice";
+        break;
+  
+      case E_STRICT:
+        $errtype= "Strict Notice";
+        break;
+  
+      case E_RECOVERABLE_ERROR:
+        $errtype= "Recoverable Error";
+        break;
+  
+      default:
+        $errtype= "Unknown error ($errno)";
+        break;
+    }
+  
+    /*
+     * respect error_reporting level
+     */
+    $errno = $errno & error_reporting();
+    if ($errno == 0) return true;
+  
+    $errmsg =   "*** Error: $errtype in $errfile, line $errline: $errstr";
+  
+    /*
+     * log error message
+     */
+    $this->debug( $errmsg );
+    
+    /*
+     * return jsonified error message
+     */
+    $error->SetError($errno, $errmsg);
+    $error->SendAndExit();
+
+    // never gets here
   }  
 }
 ?>
--- qooxdoo-contrib/qcl/trunk/backend/php/services/qcl/server/test/testService.php
+++ qooxdoo-contrib/qcl/trunk/backend/php/services/qcl/server/test/testService.php
@@ -6,5 +6,20 @@ class class_testService
     return $params;
   }
   
+  function method_testError()
+  {
+    thisFunctionDoesNotExist();
+  }
+  
+  
+  function method_testErrorPhp5()
+  {
+    /*
+     * using eval here to avoid compile error in php4
+     */
+    eval('
+    throw new Exception("This is an Error exception");
+    ');
+  }  
 }
 ?>
