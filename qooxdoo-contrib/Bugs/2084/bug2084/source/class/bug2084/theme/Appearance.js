228531a3c7297ef4938ebed53f1b6870b921397f
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-03-10 09:42:08 +0000

    Initial commit for bug report.

---
+++ qooxdoo-contrib/Bugs/2084/bug2084/source/class/bug2084/theme/Appearance.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2084.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+    "window/textfield" : "textfield",
+    "window/button-ok" : "button",
+    "window/button-abort" : "button"  
+  }
+});
