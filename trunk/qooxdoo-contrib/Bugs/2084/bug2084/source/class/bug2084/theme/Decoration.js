c090dee37ceca13df6cafd31772c3007e798dbf1
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-03-10 09:42:08 +0000

    Initial commit for bug report.

---
+++ trunk/qooxdoo-contrib/Bugs/2084/bug2084/source/class/bug2084/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2084.theme.Decoration",
+{
+  extend : qx.theme.modern.Decoration,
+
+  decorations :
+  {
+  }
+});
