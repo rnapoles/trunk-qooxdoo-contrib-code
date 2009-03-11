309577dc57aa6eebbb2e95e71977180815cb91e2
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-03-11 11:45:28 +0000

    Initial commit for bug.

---
+++ qooxdoo-contrib/Bugs/1839/bug1839/source/class/bug1839/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug1839.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
---
+++ qooxdoo-contrib/Bugs/1839/bug1839/source/class/bug1839/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug1839.theme.Theme",
+{
+  meta :
+  {
+    color : bug1839.theme.Color,
+    decoration : bug1839.theme.Decoration,
+    font : bug1839.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug1839.theme.Appearance
+  }
+});
