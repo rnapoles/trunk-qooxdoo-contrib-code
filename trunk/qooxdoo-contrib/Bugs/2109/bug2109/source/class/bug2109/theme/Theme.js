c540acc51dcdd3ba870dd87dd2e0092f2d9b5641
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-03-17 09:56:03 +0000

    Initial commit for bug.

---
+++ trunk/qooxdoo-contrib/Bugs/2109/bug2109/source/class/bug2109/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2109.theme.Theme",
+{
+  meta :
+  {
+    color : bug2109.theme.Color,
+    decoration : bug2109.theme.Decoration,
+    font : bug2109.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug2109.theme.Appearance
+  }
+});
