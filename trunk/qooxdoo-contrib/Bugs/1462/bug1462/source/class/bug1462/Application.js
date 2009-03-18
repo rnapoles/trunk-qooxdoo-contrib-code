20a7fd64823c63024a51acfba5eb08119b954266
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-03-18 13:16:36 +0000

    Updated example.

--- trunk/qooxdoo-contrib/Bugs/1462/bug1462/source/class/bug1462/Application.js
+++ trunk/qooxdoo-contrib/Bugs/1462/bug1462/source/class/bug1462/Application.js
@@ -20,6 +20,7 @@
 qx.Class.define("bug1462.Application",
 {
   extend : qx.application.Inline,
+  //extend : qx.application.Standalone,
 
   members :
   {
@@ -33,12 +34,15 @@ qx.Class.define("bug1462.Application",
         qx.log.appender.Console;
       }
 
+      var button = new qx.ui.form.Button("button")
+      
       var win = new qx.ui.window.Window("First Window");
       win.setWidth(300);
       win.setHeight(50);
       win.open();
       
       var doc = this.getRoot();
+      doc.add(button, {left: 100, top: 100});
       doc.add(win, {left: 20, top: 20});
     }
   }
