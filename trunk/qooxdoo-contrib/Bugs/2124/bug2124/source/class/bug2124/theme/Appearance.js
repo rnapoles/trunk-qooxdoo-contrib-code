7e600da7e7ab2c5d82e2edd3eaf1d564d42d2156
jonathan_rass <jonathan_rass@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-03-26 14:10:27 +0000

    [BUG #2124 ASSIGNED]: Scroll-Wheel support still seems to going crazy in ScrollContainer http://bugzilla.qooxdoo.org/show_bug.cgi?id=2124

---
+++ trunk/qooxdoo-contrib/Bugs/2124/bug2124/source/class/bug2124/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2124.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
