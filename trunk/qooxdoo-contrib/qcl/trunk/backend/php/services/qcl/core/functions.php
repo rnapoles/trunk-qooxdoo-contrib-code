0ce4ce220a075cd919c5de16f09070e5e5d07156
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-03-30 20:46:36 +0000

    added ellipsify function

--- trunk/qooxdoo-contrib/qcl/trunk/backend/php/services/qcl/core/functions.php
+++ trunk/qooxdoo-contrib/qcl/trunk/backend/php/services/qcl/core/functions.php
@@ -487,4 +487,120 @@ if ( ! function_exists("microtime_float" ) )
   }
 }
 
+function ellipsify( $string, $length, $mode="right" )
+{
+  if ( $length and strlen( $string ) > $length )
+  {
+    switch ( $mode )
+    {
+      case "right":
+        return substr( $string, 0, $length-3 ) . "...";
+        
+      case "left" : 
+        return "..." . substr( $string, strlen($string)-$length+3 );
+        
+      case "center":
+        return substr( $string, 0, floor($length/2) -2 ) . "..." . substr( $string, floor($length/2) + 1 );
+        
+      default:
+        trigger_error("Invalid ellipsify mode '$mode'. Valid values are 'right|left|center'");
+    }
+  } 
+  return $string;
+}
+
+/*
+function ellipsify_pixel( $string, $width, $mode="right" )
+{
+  $strWidth = get_str_width( $string );
+  
+  if ( $width and $strWidth > $width )
+  {
+    $length = strlen(truncate_str_at_width( $string, $width, "" ));
+    
+    switch ( $mode )
+    {
+      case "right":
+        return substr( $string, 0, $length-3 ) . "...";
+        
+      case "left" : 
+        return "..." . substr( $string, strlen($string)-$length+3 );
+        
+      case "center":
+        return substr( $string, 0, floor($length/2) -2 ) . "..." . substr( $string, floor($length/2) + 1 );
+        
+      default:
+        trigger_error("Invalid ellipsify mode '$mode'. Valid values are 'right|left|center'");
+    }
+  } 
+  return $string;
+}
+
+// posted by 'sanneschaap' dot $del dot 'gmail dot com'
+// on http://us2.php.net/wordwrap
+
+
+//the width of the biggest char @
+$fontwidth = 11;
+
+//each chargroup has char-ords that have the same proportional displaying width
+$chargroup[0] = array(64);
+$chargroup[1] = array(37,87,119);
+$chargroup[2] = array(65,71,77,79,81,86,89,109);
+$chargroup[3] = array(38,66,67,68,72,75,78,82,83,85,88,90);
+$chargroup[4] = array(35,36,43,48,49,50,51,52,53,54,55,56,57,60,61,62,63, 69,70,76,80,84,95,97,98,99,100,101,103,104,110,111,112, 113,115,117,118,120,121,122,126);
+$chargroup[5] = array(74,94,107);
+$chargroup[6] = array(34,40,41,42,45,96,102,114,123,125);
+$chargroup[7] = array(44,46,47,58,59,91,92,93,116);
+$chargroup[8] = array(33,39,73,105,106,108,124);
+   
+//how the displaying width are compared to the biggest char width
+$chargroup_relwidth[0] = 1; //is char @
+$chargroup_relwidth[1] = 0.909413854;
+$chargroup_relwidth[2] = 0.728241563;
+$chargroup_relwidth[3] = 0.637655417;
+$chargroup_relwidth[4] = 0.547069272;
+$chargroup_relwidth[5] = 0.456483126;
+$chargroup_relwidth[6] = 0.36589698;
+$chargroup_relwidth[7] = 0.275310835;
+$chargroup_relwidth[8] = 0.184724689;
+
+//build fast array
+$char_relwidth = null;
+for ($i=0;$i<count($chargroup);$i++){
+    for ($j=0;$j<count($chargroup[$i]);$j++){
+        $char_relwidth[$chargroup[$i][$j]] = $chargroup_relwidth[$i];
+    }
+}
+
+//get the display width (in pixels) of a string
+function get_str_width($str){
+    global $fontwidth,$char_relwidth;
+    $result = 0;
+    for ($i=0;$i<strlen($str);$i++){
+        $result += $char_relwidth[ord($str[$i])];
+    }
+    $result = $result * $fontwidth;
+    return $result;   
+}
+
+//truncates a string at a certain displaying pixel width
+function truncate_str_at_width($str, $width, $trunstr='...')
+{
+    global $fontwidth,$char_relwidth;       
+    $trunstr_width = get_str_width($trunstr);
+    $width -= $trunstr_width;
+    $width = $width/$fontwidth;
+    $w = 0;
+    for ($i=0;$i<strlen($str);$i++){
+        $w += $char_relwidth[ord($str[$i])];
+        if ($w > $width)
+            break;   
+    }
+    $result = substr($str,0,$i).$trunstr;
+    return $result;
+    // texas is the reason rules at 10am :)
+}
+*/
+
 ?>
