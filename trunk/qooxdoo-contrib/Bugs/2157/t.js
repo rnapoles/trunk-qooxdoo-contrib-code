b45a1457e37e7d8fdd1db147636a4a0d4adf72a1
fjakobs <fjakobs@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-04-01 09:50:01 +0000

    [BUG #2157 NEW]: Ecmalint: Detect implicit use of undeclared private members not only in assignments http://bugzilla.qooxdoo.org/show_bug.cgi?id=2157
    fixed

--- trunk/qooxdoo-contrib/Bugs/2157/t.js
+++ trunk/qooxdoo-contrib/Bugs/2157/t.js
@@ -1,6 +1,23 @@
 var test = function () {
   this.__containerElement.setStyle("foo", "bar");
   this.__containerElement.style.foo = "bar";
+  this.__containerElement = "bar";
 }
 
 test();
+
+
+qx.Class.define("t", 
+{
+  extend : qx.core.Object,
+  
+  members :
+  {
+    foo : function()
+    {
+      this.__containerElement.setStyle("foo", "bar");
+      this.__containerElement.style.foo = "bar";
+      this.__containerElement = "bar";
+    }
+  }
+});
