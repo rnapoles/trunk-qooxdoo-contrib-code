0f2dc00395fe611ff222391eb3bcd5e5bd9091f9
ianhorst <ianhorst@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-04-01 20:40:12 +0000

    adding basic folder structure

---
+++ qooxdoo-contrib/Egg/README.txt
@@ -0,0 +1,8 @@
+===== Egg =====
+
+Incubator to develop experimental or missing code in qooxdoo.
+
+Stage 1 implementations:
+* input filters,
+* input validation
+* TimeChooser & TimeField
---
+++ qooxdoo-contrib/Egg/trunk/source/Manifest.js
@@ -0,0 +1,23 @@
+{
+   "name"         : "Egg",
+
+   "summary"      : "I.Horst's qooxdoo contributions",
+   "description"  : "Implementation of form input filters&validatation and TimeChooser",
+   "keywords"     : ["widget", "filter", "validate", "timechooser", "timefield"],
+   "homepage"     : "http://contrib.qooxdoo.org",
+  
+   "license"      : "LGPL/EPL",
+   "authors"      :
+   [
+      {
+         "name"   : "Ian Horst",
+         "email"  : "ian.horst@googlemail.com"
+      }
+   ],
+  
+   "namespace"             : "egg",
+   "resource-uri-setting"  : "egg.resourceUri",
+
+   "version"               : "trunk",
+   "qooxdoo-versions"      : ["trunk"]
+}
