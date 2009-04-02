ec4a46641bb43afa3ce98290997f9edd578c6c90
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-04-02 08:45:27 +0000

    Initial commit for bug report. http://bugzilla.qooxdoo.org/show_bug.cgi?id=2191

---
+++ trunk/qooxdoo-contrib/Bugs/2191/bug2191/source/class/bug2191/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2191.theme.Decoration",
+{
+  extend : qx.theme.modern.Decoration,
+
+  decorations :
+  {
+  }
+});
