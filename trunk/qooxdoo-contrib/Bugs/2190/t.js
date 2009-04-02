22dece5d45c6d53d39af7e36573013f633278540
thron7 <thron7@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-04-02 16:07:27 +0000

    [BUG \#2190] test data

---
+++ trunk/qooxdoo-contrib/Bugs/2190/t.js
@@ -0,0 +1,16 @@
+// syntax: compile.py --variant=qx.debug:off t.js
+function() {
+  if (true) {
+  } else 
+    if (qx.core.Variant.isSet("qx.debug", "on")) 
+    {
+      var pw = 3;
+    } else {}
+  foo();
+}
+
+/*
+This is generated:
+function(){if(true){}else{}var pwfoo();
+}
+*/
