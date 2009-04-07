d4ebe5bc714f465fbcb6e3df084697fc1fd3d87f
jonathan_rass <jonathan_rass@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-04-07 09:25:55 +0000

    [BUG #2077 ASSIGNED]: Commands: Overwriting browser shortcuts does not work properly http://bugzilla.qooxdoo.org/show_bug.cgi?id=2077
    Initial commmit of a test application

---
+++ qooxdoo-contrib/Bugs/2077/bug2077/source/class/bug2077/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2077.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
