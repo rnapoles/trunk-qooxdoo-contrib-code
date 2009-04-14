6f044d26709144a885d86b9ed38ce960aeb6e8de
jonathan_rass <jonathan_rass@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-04-14 08:00:05 +0000

    [BUG #730 FIXED]: Large Context menus are cut off at browser window edges http://bugzilla.qooxdoo.org/show_bug.cgi?id=730
    Initial commit of a sandbox for this issue.

---
+++ trunk/qooxdoo-contrib/Bugs/730/bug730/source/class/bug730/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug730.theme.Theme",
+{
+  meta :
+  {
+    color : bug730.theme.Color,
+    decoration : bug730.theme.Decoration,
+    font : bug730.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug730.theme.Appearance
+  }
+});
