19472d2e15052fb3d04f454077f92f3771c973a6
d_wagner <d_wagner@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-04-17 10:53:33 +0000

    [BUG #2239] Use classname property to identify article list widget.

--- qooxdoo-contrib/Simulator/trunk/tool/selenium/simulation/feedreader/test_feedreader.js
+++ qooxdoo-contrib/Simulator/trunk/tool/selenium/simulation/feedreader/test_feedreader.js
@@ -347,7 +347,7 @@ function runTests()
   print("Selecting first item from new feed.");
   
   try {
-    sel.qxClick("qxh=app:child[7]/qx.ui.form.List/child[0]");  
+    sel.qxClick('qxh=app:[@classname="feedreader.view.List"]/qx.ui.form.List/child[0]');  
   }
   catch(ex) {
     totalErrors ++;
