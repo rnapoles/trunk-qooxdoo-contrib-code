ee8cc681f1d2d02b9e2137f9571191e41c475cd6
jonathan_rass <jonathan_rass@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-04-23 14:24:32 +0000

    [BUG #2192 ASSIGNED]: Selecting a TextField via tab key is extremly slow http://bugzilla.qooxdoo.org/show_bug.cgi?id=2192
    The charCode property of an keyup and keydown event is always 0 in Firefox.

---
+++ trunk/qooxdoo-contrib/Bugs/2192/bug2192/source/class/bug2192/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2192.theme.Theme",
+{
+  meta :
+  {
+    color : bug2192.theme.Color,
+    decoration : bug2192.theme.Decoration,
+    font : bug2192.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug2192.theme.Appearance
+  }
+});
