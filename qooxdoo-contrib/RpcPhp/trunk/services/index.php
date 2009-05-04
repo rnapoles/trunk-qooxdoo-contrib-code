74499c4bd00b66d263a6aae864e1a3a468de6a4d
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-05-04 20:38:40 +0000

    bug fix

--- qooxdoo-contrib/RpcPhp/trunk/services/index.php
+++ qooxdoo-contrib/RpcPhp/trunk/services/index.php
@@ -14,7 +14,7 @@ if ( $_SERVER["REQUEST_METHOD"] != "POST" or isset($_REQUEST['service'])  )
  * start jsonrpc server
  */
 require dirname(__FILE__) . "/server/JsonRpcServer.php";
-JsonRpcServer::start();
+JsonRpcServer::run();
 
 
 ?>
