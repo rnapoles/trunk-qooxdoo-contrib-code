4d4387be8389f4160654bfa8dbac8269cdd55756
burakarslan <burakarslan@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-05-09 12:47:57 +0000

    revamp the trunk for qooxdoo 0.8

---
+++ qooxdoo-contrib/Soap/trunk/client/source/class/soapdemo/soap/RemoteImpl.js
@@ -0,0 +1,63 @@
+
+qx.Class.define("soapdemo.soap.RemoteImpl", { extend : qx.ui.table.model.Remote
+    ,construct : function(service_instance, row_count_method_name, row_data_method_name, service_arguments, session_id) {
+        this.base(arguments);
+        this.setRowCountMethodName(row_count_method_name);
+        this.setRowDataMethodName(row_data_method_name);
+        this.setServiceInstance(service_instance);
+        this.setServiceArguments(service_arguments);
+        this.setSessionId(session_id)
+    }
+    ,properties : {
+         rowCountMethodName: { check: "String", nullable: false}
+        ,rowDataMethodName:  { check: "String", nullable: false}
+        ,serviceInstance:    { check: "arskom.soap.client", nullable: false }
+        ,serviceArguments:   { check: "arskom.soap.parameters", nullable: false }
+        ,sessionId:          { check: "String", nullable: false}
+    }
+    ,members : {
+        _loadRowCount : function() {
+            var ctx = this;
+
+            var params = this.getServiceArguments();
+            var req = new arskom.soap.Request();
+            
+            req.setWho(this.getSessionId());
+            params.add("req",req);
+
+            var svc = this.getServiceInstance();
+            ctx.SoapRunning = svc.callAsync( this.getRowCountMethodName(), params, function(r) {
+                ctx.SoapRunning = null;
+                if (r instanceof Error) {
+                    ctx.warn("An error has occured!\r\n\r\n" + r.fileName + " line " + r.lineNumber);
+                    ctx._onRowCountLoaded(0);
+                }
+                else {
+                    ctx._onRowCountLoaded(r);
+                }
+            });
+        }
+
+        ,_loadRowData : function(firstRow, lastRow) {
+            var ctx=this;
+
+            var params = ctx.getServiceArguments();
+            var req = new arskom.soap.Request();
+            req.setStartRow(firstRow);
+            req.setWho(this.getSessionId());
+            params.add("req",req);
+
+            var svc = this.getServiceInstance();
+            ctx.SoapRunning = svc.callAsync( this.getRowDataMethodName(), params, function(r) {
+                ctx.SoapRunning = null;
+                if (r instanceof Error) {
+                    ctx.warn("An error has occured!\r\n\r\n" + r.fileName + " line " + r.lineNumber);
+                    ctx._onRowDataLoaded([]);
+                }
+                else {
+                    ctx._onRowDataLoaded(r);
+                }
+            });
+        }
+    }
+});
---
+++ qooxdoo-contrib/Soap/trunk/client/source/class/soapdemo/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("soapdemo.theme.Theme",
+{
+  meta :
+  {
+    color : soapdemo.theme.Color,
+    decoration : soapdemo.theme.Decoration,
+    font : soapdemo.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : soapdemo.theme.Appearance
+  }
+});
