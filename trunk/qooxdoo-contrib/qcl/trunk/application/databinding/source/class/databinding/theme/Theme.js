ecfc078ec2f511fb9499b1e47b5d842c73989c69
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-05-13 12:50:54 +0000

    Adding sample application to demonstrate databinding

---
+++ trunk/qooxdoo-contrib/qcl/trunk/application/databinding/source/class/databinding/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("databinding.theme.Theme",
+{
+  meta :
+  {
+    color : databinding.theme.Color,
+    decoration : databinding.theme.Decoration,
+    font : databinding.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : databinding.theme.Appearance
+  }
+});
