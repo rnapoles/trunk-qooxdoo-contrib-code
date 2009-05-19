2282cae0c887e056bed2950d20a2e152f685710d
jonathan_rass <jonathan_rass@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-05-19 14:07:13 +0000

    [BUG #2361 ASSIGNED]: TextArea#maxLength has no effect http://bugzilla.qooxdoo.org/show_bug.cgi?id=2361
    Testcase

---
+++ qooxdoo-contrib/Bugs/2361/bug2361/source/class/bug2361/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2361.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
