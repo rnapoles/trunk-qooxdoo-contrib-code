00365e5268bd304b188d7e06943919fa58b1f427
d_wagner <d_wagner@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-05-28 07:15:27 +0000

    Test rewritten as an instance of simulation.Simulation

--- qooxdoo-contrib/Simulator/trunk/tool/selenium/simulation/testrunner/test_testrunner.js
+++ qooxdoo-contrib/Simulator/trunk/tool/selenium/simulation/testrunner/test_testrunner.js
@@ -1,215 +1,191 @@
-importClass(Packages.com.thoughtworks.selenium.QxSelenium);
-
-// - Config Section ------------------------------------------------------------
-var config = {
-  selServer   : "localhost",
-  selPort     : 4444,
-  testBrowser : "*custom /usr/lib/firefox-3.0.5/firefox -no-remote -P selenium-3",
-  autHost     : "http://172.17.12.142",
-  autPath     : "/~dwagner/workspace/qooxdoo.trunk/framework/test/"
+var baseConf = {
+  'autName' : 'Testrunner',
+  'globalTimeout' : 120000,
+  'stepSpeed' : '250',
+  'selServer' : 'localhost',
+  'selPort' : 4444,
+  'testBrowser' : '*custom /usr/lib/firefox-3.0.10/firefox -no-remote -P selenium-3',
+  'autHost' : 'http://localhost',
+  'autPath' : '/~dwagner/workspace/qooxdoo.trunk/framework/test/index.html',
+  'simulatorSvn' : '/home/dwagner/workspace/qooxdoo.contrib/Simulator' 
 };
 
-var stepSpeed  = "250"; // millisecs after each command
-var testPause = 1800000; // millisecs to wait for all tests to finish
-var selWin = 'selenium.browserbot.getCurrentWindow()'; // get application iframe
-var qxAppInst = 'qx.core.Init.getApplication().viewer'; // get demobrowser instance
-var qxStatusText = '.widgets["statuspane.systeminfo"].getContent().toString()'; // content of status text field
-var isStatusReady = selWin + '.' + qxAppInst + qxStatusText + ' == "Ready" || ' + selWin + '.' + qxAppInst + qxStatusText + ' == "Enabled auto-reload"'; // check if test run has finished
-var testResults = selWin + '.' + qxAppInst + '.f1.getContentElement().getDomElement().innerHTML'; // content of result iframe
-var testResultsLength = selWin + '.' + qxAppInst + '.f1.getContentElement().getDomElement().innerHTML.length';
-var isQxReady = 'var qxReady = false; try { if (selenium.browserbot.getCurrentWindow().qx.core.Init.getApplication().viewer) { qxReady = true; } } catch(e) {} qxReady;'; // check if testrunner instance exists
-var usrAgent = 'navigator.userAgent';
-var platform = 'navigator.platform';
-
-var packageFunc= 'function getPkg() {';
-packageFunc+= '  var temp = {};';
-packageFunc+= '  var iter = ' + selWin + '.' + qxAppInst + '.tests.handler.ttree.getIterator("depth");';
-packageFunc+= '  var curr;';
-packageFunc+= '  while (curr = iter()) {';
-packageFunc+= '    var path = curr.pwd();';
-packageFunc+= '    if (path[3]) {';
-packageFunc+= '      var pack = path[1] + "." + path[2] + "." + path[3];';
-packageFunc+= '      temp[pack] = "";';
-packageFunc+= '    }';
-packageFunc+= '  }';
-packageFunc+= '  return temp;';
-packageFunc+= '}';
-packageFunc+= 'var packObj = getPkg();';
-packageFunc+= 'var packStr = "";';
-packageFunc+= 'for (pack in packObj) {';
-packageFunc+= '  packStr += pack + ",";';
-packageFunc+= '}';
-packageFunc+= 'packStr;';
-
-var ignore = ["qx.test.Xml"];
-var include = [];
-
-// - Config End ----------------------------------------------------------------
-
-var currentDate = new Date();
-var errWarn = 0;
-var testFailed = false;
-
-// store command line parameters in config object
-for (i in arguments) {
-  if (arguments[i].indexOf("=") >0) {
-    var tempArr = arguments[i].split("=");
-    config[tempArr[0]] = tempArr[1];
+var args = arguments ? arguments : "";
+var simSvn = baseConf.simulatorSvn;
+for (var i=0; i<args.length; i++) {
+  if (args[i].indexOf('simulatorSvn') >= 0) {
+    simSvn = args[i].substr(args[i].indexOf('simulatorSvn=') + 13);
   }
 }
 
-/*
- * Open/create a log file and return the file object.
+load([simSvn + "/trunk/tool/selenium/simulation/Simulation.js"]);
+
+var mySim = new simulation.Simulation(baseConf,args);
+
+var selWin = simulation.Simulation.SELENIUMWINDOW;
+var qxAppInst = simulation.Simulation.QXAPPINSTANCE + '.viewer';
+// Content of the "status" text field
+var qxStatusText = '.widgets["statuspane.systeminfo"].getContent().toString()';
+// Check if a test run has finished
+var isStatusReady = selWin + '.' + qxAppInst + qxStatusText + ' == "Ready" || ' + selWin + '.' + qxAppInst + qxStatusText + ' == "Enabled auto-reload"';
+// HTML content of the result iframe
+var testResults = selWin + '.' + qxAppInst + '.f1.getContentElement().getDomElement().innerHTML';
+
+var ignore = [];
+var include = [];
+
+/**
+ * Runs a function in the AUT context that reads the available test packages
+ * from the tree.
+ * 
+ * @returns {Array} an array of test package names
  */
-function getLogFile()
+simulation.Simulation.prototype.getPackageArray = function()
 {
-  var logFileName = config.logFile ? config.logFile :  "testrunner_" + currentDate.getTime() + ".log";
-  var fstream = new java.io.FileWriter(logFileName, true);
-  var out = new java.io.BufferedWriter(fstream);
-  return out;
-}
+  var packageFunction = function()  {
+    var packObj = {};
+    var iter = selenium.browserbot.getCurrentWindow().qx.core.Init.getApplication().viewer.tests.handler.ttree.getIterator("depth");
+    var curr;
+    while (curr = iter()) {
+      var path = curr.pwd();
+      if (path[3]) {
+        var pack = path[1] + "." + path[2] + "." + path[3];
+        packObj[pack] = "";
+      }    
+    }
+    var packStr = "";
+    for (pack in packObj) {
+      packStr += pack + ",";
+    }
+    return packStr;
+  };
+
+  var packs = this.getEval('(' + packageFunction + ')();', 'Getting test package names');
+  packs = String(packs);
+  if (packs == "false" || packs == "null") {
+    return false;
+  }
+  packs = packs.substr(0,packs.length-1);
+  var packArr = packs.split(',');
+  return packArr;
+};
 
 /*
-*  Write a message to Selenium's browser side log and the local log file.
-*/
-function browserLog(msg)
+ * Main test method.
+ */
+simulation.Simulation.prototype.runTestsSteps = function()
 {
-  msg = msg ? msg : "";
-  msg = String(msg);
-  msg = msg.replace(/\n/g,'<br/>');
-  msg = msg.replace(/\r/g,'<br/>');
-  msg = msg.replace(/'/g, '&quot;');
-  msg = msg.replace(/ä/g, '&auml;');
-  msg = msg.replace(/ö/g, '&ouml;');
-  msg = msg.replace(/ü/g, '&uuml;');
-  msg = msg.replace(/Ä/g, '&Auml;');
-  msg = msg.replace(/Ö/g, '&Ouml;');
-  msg = msg.replace(/Ü/g, '&Uuml;');
-  msg = msg.replace(/ß/g, '&szlig;');
-  var prefix = 'qxSimulator_' + currentDate.getTime();
-  var logFile = getLogFile();
-  logFile.write(prefix + ': ' + msg);
-  logFile.newLine();
-  logFile.close();
-  return 'LOG.error("' + prefix + ': " + \'' + msg + '\');';
-}
+  var packages = this.getPackageArray();
+
+  if (!packages) {
+    this.log("ERROR: Unable to get list of test packages.", "error");
+    this.testFailed = true;
+    return;
+  }
+
+  if (this.debug) {
+    print("TEST PACKAGES: " + packages);
+  }
+  
+  if (include.length > 0) {
+    packages = include;
+  }
+  else if (ignore.length > 0) {
+
+    for (var x = 0; x < packages.length; x++) {
+      for (var j = 0; j < ignore.length; j++) {
+        if (packages[x] == ignore[j]) {
+          packages.splice(x, 1);
+        }
+      }
+    }
+    
+  }
+
+  if (this.debug) {
+    print("TESTING PACKAGES: " + packages);
+  }
+
+  // Get the current test application's URI from the input field.
+  var autUri = this.getEval(selWin + '.document.getElementsByTagName("input")[0].value', 'Getting AUT URI');
+  autUri = autUri.substring(0,autUri.indexOf('=')+1);
+
+  var elapsedTotal = 0;
+  for (var i=0; i<packages.length; i++) {
+    if (this.debug) {
+      print("Loading package: " + packages[i]);
+    }
+    // Enter the test app URI with the current package's name after 'testclass='.
+    this.type("dom=document.getElementsByTagName('input')[0]", autUri + packages[i]);
+    this.runScript(qxAppInst + '.reloadTestSuite();', "Calling reloadTestSuite");        
+    
+    var isAutReady = this.waitForCondition(isStatusReady, 120000, 
+                                           "Waiting for test package to load");
+
+    if (!isAutReady) {
+      return;
+    }
+
+    if (this.debug) {
+      print("Starting tests in package " + packages[i]);
+    }
+    var packageStartDate = new Date();    
+
+    this.runScript(qxAppInst + '.runTest();', "Calling runTest");
+
+    var isPackageDone = mySim.waitForCondition(isStatusReady, 360000, 
+                      "Waiting for test package " + packages[i] + " to finish");
+
+    if (!isPackageDone) {
+      return;
+    }
+
+    if (this.debug) {
+      this.logTestDuration(packageStartDate, "Test package " + packages[i]);
+    } 
+
+    var result = this.getEval(testResults, 'Getting result HTML');
+
+    if (!result) {
+      return;
+    }
+
+    this.logErrors(result);
+
+  }
+
+};
 
 /*
-* Generated HTML elements are uppercase in IE
-*/
-function getLogArray(result)
-{
+ * Takes HTML content from the result iframe, splits it up into individual test 
+ * results, removes successful results and logs the rest. 
+ */
+simulation.Simulation.prototype.logErrors = function(result)
+{  
   var logArray = [];
   if (result.indexOf("</div>") >0 ) {
     logArray = result.split("</div>");
   } else if (result.indexOf("</DIV>") >0 ) {
     logArray = result.split("</DIV>");
   }
-  return logArray;
-}
 
-function getResultChunk(startAt, chars)
-{
-  return selWin + '.' + qxAppInst + '.f1.getContentElement().getDomElement().innerHTML.substr(' + startAt + ',' + chars + ')';
-}
-
-function getPackageArray()
-{
-  var packs = sel.getEval(packageFunc);
-  packs = String(packs);
-  packs = packs.substr(0,packs.length-1);
-  var packArr = packs.split(',');
-  return packArr;
-}
-
-function logBrowserInfo()
-{
-  var agent = sel.getEval(usrAgent);
-  var plat = sel.getEval(platform);
-
-  sel.getEval(browserLog("<h1>Test Runner results from " + currentDate.toLocaleString() + "</h1>"));
-  sel.getEval(browserLog("<p>Application under test: <a href=\"" + config.autHost + config.autPath + "\">" + config.autHost + config.autPath + "</a>"));
-  sel.getEval(browserLog("<p>Platform: " + plat + "</p>"));
-  sel.getEval(browserLog("<p>User agent: " + agent + "</p>"));
-}
-
-function isAutReady()
-{
-  print("Waiting for application to load...");
-  try {
-    sel.waitForCondition(isStatusReady,"240000");
-    return true;
+  if (this.debug) {
+    print("Split result into " + logArray.length + " array entries.");
   }
-  catch(ex) {
-    testFailed = true;
-    print("Test file not loaded: " + ex);
-    sel.getEval(browserLog("<DIV>ERROR: Test file not loaded: " + ex + "</DIV>"));
-    return false;
-  }
-}
 
-function isRunnerReady()
-{
-  try {
-    sel.waitForCondition(isQxReady, "240000");
-    return true;
-  }
-  catch(ex) {
-    testFailed = true;
-    print("Couldn't find qx instance in AUT window. " + ex);
-    sel.getEval(browserLog("<DIV>ERROR: Unable to find qx instance in AUT window: " + ex + "</DIV>"));
-    return false;
-  }
-}
-
-function isTestsDone()
-{
-  print("Waiting for tests to finish...");
-  try {
-    sel.waitForCondition(isStatusReady,testPause);
-    return true;
-  }
-  catch(ex) {
-    testFailed = true;
-    print("Test run did not finish correctly: " + ex);
-    sel.getEval(browserLog("<DIV>ERROR: Test run did not finish correctly: " + ex + "</DIV>"));
-    return false;
-  }
-}
-
-function getTestDuration(startTime)
-{
-  var stopTime = new Date();
-  var elapsed = stopTime.getTime() - startTime.getTime();
-  return elapsed;
-}
-
-function logTestDuration(elapsed, description)
-{
-  var desc = description || "Test run";
-  elapsed = (elapsed / 1000);
-  min = Math.floor(elapsed / 60);
-  sec = Math.round(elapsed % 60);
-  if (sec < 10) {
-    sec = "0" + sec;
-  }
-  print(desc + " finished in: " + min + " minutes " + sec + " seconds.");
-  sel.getEval(browserLog("<p>" + desc + " finished in: " + min + " minutes " + sec + " seconds.</p>"));
-}
-
-function logErrors(result)
-{
-  var logArray = getLogArray(result);
-  print("Split result into " + logArray.length + " array entries.");
   // we can speed this up since we don't have to wait for the browser
-  sel.setSpeed("500");
+  this.__sel.setSpeed("500");
+
   for (var i=0, l=logArray.length; i<l; i++) {
     var line = logArray[i];
+
     // Workaround for (rhino?) issue with replace() and single quotes.
     line = line.split("'");
     line = line.join("\'");
+
     // only log warnings and errors
     if ( (line.indexOf('<div') >= 0 || line.indexOf('<DIV') >= 0) && line.indexOf('testResult success') < 0) {
+
       // strip uninformative stack trace
       if (line.indexOf('Stack trace:') > 0) {
         if (line.indexOf('<DIV class="trace') >= 0) {
@@ -222,6 +198,7 @@ function logErrors(result)
           line = line.substring(0,line.indexOf('<div class="trace'));
         }
       }
+
       try {
         line = line.replace(/\<br\>/gi, "<br/>");
         line = line.replace(/\'/g, "\\'");
@@ -231,6 +208,7 @@ function logErrors(result)
       catch(ex) {
         print("Error while replacing: " + ex);
       }
+
       // make sure all div tags are closed
       var odivs = line.match(/\<div/gi);
       var cdivs = line.match(/\<\/div/gi);
@@ -243,143 +221,67 @@ function logErrors(result)
           }
         }
       }
-      errWarn++;
-      print("Logging line " + line);
-      sel.getEval(browserLog(line));
-    }
-  }
-  print(errWarn + " lines logged");
-}
 
-function getResultLog()
-{
-  print("Getting log...");
-  try {
-    var result = sel.getEval(testResults);
-    print("Got log");
-    return result;
-  }
-  catch(ex) {
-    testFailed = true;
-    print("Could not get test results: " + ex);
-    sel.getEval(browserLog("<DIV>ERROR: Could not get test results: " + ex + "</DIV>"));
-    return false;
-  }
-}
-
-function runTests()
-{
-  if (!isAutReady()) {
-    return;
-  }
-
-  print("Starting tests for " + config.autHost + config.autPath);
-  var startTime = new Date();  
-  sel.runScript(qxAppInst + '.runTest();');
-  
-  if (!isTestsDone()) {
-    return;
-  }
-
-  logTestDuration(getTestDuration(startTime)); 
-
-  var result = getResultLog();
-  
-  if (!result) {
-    return;
-  }
-
-  logErrors(result);
-}
-
-function runTestsSteps()
-{
-  if (!isAutReady()) {
-    sel.getEval(browserLog("<DIV>Timeout while loading test suite.</DIV>"));
-    return;
-  }
-
-  var packages = getPackageArray();
-  print("TEST PACKAGES: " + packages);
-  
-  if (include.length > 0) {
-    packages = include;
-  }
-  else if (ignore.length > 0) {
-  
-    for (var x = 0; x < packages.length; x++) {
-      for (var j = 0; j < ignore.length; j++) {
-        if (packages[x] == ignore[j]) {
-          packages.splice(x, 1);
-        }
+      this.errWarn++;
+      if (this.debug) {
+        print("Logging line " + line);
       }
+      this.log(line);
     }
-    
   }
 
-  print("TESTING PACKAGES: " + packages);
+  if (this.debug) {
+    print(this.errWarn + " lines logged");
+  }
 
-  var autUri = sel.getEval(selWin + '.document.getElementsByTagName("input")[0].value');
-  autUri = autUri.substring(0,autUri.indexOf('=')+1);
-    
-  var elapsedTotal = 0;
-
-  for (var i=0; i<packages.length; i++) {
-    print("Loading package: " + packages[i]);
-    sel.type("dom=document.getElementsByTagName('input')[0]", autUri + packages[i]);
-    sel.runScript(qxAppInst + '.reloadTestSuite();');
-    
-    if (!isAutReady()) {
-      sel.getEval(browserLog("<DIV>Failed while attempting to load package " + packages[i] + "</DIV>"));
-      return;
-    }
-  
-    print("Starting tests in package " + packages[i]);
-    var startTime = new Date();
-    sel.runScript(qxAppInst + '.runTest();');
-    
-    if (!isTestsDone()) {
-      return;
-    }
-    
-    var packageDuration = getTestDuration(startTime);    
-  
-    elapsedTotal += packageDuration; 
-  
-    var result = getResultLog();    
-    
-    if (!result) {
-      return;
-    }
-  
-    logErrors(result);
-
-    //logTestDuration(packageDuration, packages[i]);
-
-  }  
-  logTestDuration(elapsedTotal);
-
-}
+};
 
 // - Main --------------------------------------------------------------------
 
-print("Starting Test Runner session with browser " + config.testBrowser);
-browserLog("<h1>Test Runner results from " + currentDate.toLocaleString() + "</h1>");
-var sel = new QxSelenium(config.selServer,config.selPort,config.testBrowser,config.autHost);
-sel.start();
-sel.setTimeout(120000);
-sel.open(config.autHost + config.autPath);
-sel.setSpeed(stepSpeed);
+(function() { 
+  mySim.testFailed = false;
+  mySim.errWarn = 0;
 
-if (isRunnerReady()) {
-  logBrowserInfo();
-  runTestsSteps();
-}
+  mySim.startSession();    
+  mySim.logEnvironment();   
+  var isAppReady = mySim.waitForCondition(simulation.Simulation.ISQXAPPREADY, 240000, 
+                                          "Waiting for qooxdoo application");
 
-if (!testFailed) {
-  print("Test run finished successfully.");
-  sel.getEval(browserLog("<p>Tests with warnings or errors: " + errWarn + "</p>"));
-}
 
-sel.stop();
-print("Test Runner session finished.");
+  if (!isAppReady) {
+    mySim.testFailed = true;
+    mySim.stop();
+    return;
+  }  
+
+  var isSuiteReady = mySim.waitForCondition(isStatusReady, 240000, 
+                                            "Waiting for test suite to load");
+
+  if (!isSuiteReady) {
+    mySim.testFailed = true;
+    mySim.stop();
+    return;
+  }
+
+  try {
+    mySim.runTestsSteps();
+  }
+  catch(ex) {
+    var msg = "Unexpected error while running tests!";
+    if (mySim.debug) {
+      print(msg + "\n" + ex);
+    }
+    mySim.log(msg, "error");
+  }
+
+  if (!mySim.testFailed) {
+    if (mySim.debug) {
+      print("Test run finished successfully.");
+    }
+    mySim.log("Tests with warnings or errors: " + mySim.errWarn, "info");
+  }
+
+  mySim.logTestDuration();
+  mySim.stop();
+
+})();
