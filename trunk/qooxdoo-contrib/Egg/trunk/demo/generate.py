5e9e34a46939759c18f46d6ed40509c8cb58f519
ianhorst <ianhorst@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-05-28 21:51:07 +0000

    made a draft TimeField form

--- trunk/qooxdoo-contrib/Egg/trunk/demo/generate.py
+++ trunk/qooxdoo-contrib/Egg/trunk/demo/generate.py
@@ -25,7 +25,7 @@
 import sys, os, re, subprocess
 
 CMD_PYTHON = 'python'
-QOOXDOO_PATH = '../../../ajax/qooxdoo/trunk'
+QOOXDOO_PATH = '../../ajax/qooxdoo/trunk'
 
 def getQxPath():
     path = QOOXDOO_PATH
--- trunk/qooxdoo-contrib/Egg/trunk/demo/source/class/eggdemo/Application.js
+++ trunk/qooxdoo-contrib/Egg/trunk/demo/source/class/eggdemo/Application.js
@@ -32,47 +32,32 @@ qx.Class.define("eggdemo.Application",
   members :
   {
     /**
-     * This method contains the initial application code and gets called 
-     * during startup of the application
+     * 
      */
     main : function()
     {
-      // Call super class
       this.base(arguments);
 
-      // Enable logging in debug variant
-      if (qx.core.Variant.isSet("qx.debug", "on"))
-      {
-        // support native logging capabilities, e.g. Firebug for Firefox
+      if (qx.core.Variant.isSet("qx.debug", "on")) {
         qx.log.appender.Native;
-        // support additional cross-browser console. Press F7 to toggle visibility
         qx.log.appender.Console;
       }
-
-      /*
-      -------------------------------------------------------------------------
-        Below is your actual application code...
-      -------------------------------------------------------------------------
-      */
       
-      var tabview = new qx.ui.tabview.TabView;
-      
-      var demos =
-      [
-         {
-            label:   "filters",
-            content: new eggdemo.Filters
-         }
-      ];
-      
-      for (var i = 0; i < demos.length; i++)
-      {
-         var page = new qx.ui.tabview.Page(demos[i].label);
-         page.setLayout(new qx.ui.layout.VBox);
-         page.add(demos[i].content);
-         tabview.add(page, {flex: 1});
-      }
-      this.getRoot().add(tabview, {edge: 1});
+      var time = Math.floor(new Date().getTime() % 86400000 / 1000);
+      this.getRoot().add(new egg.ui.form.TimeField(time));
     }
   }
 });
+/*
+h:mm a
+10:08 PM
+
+h:mm:ss a
+10:08:20 PM
+
+h:mm:ss a z
+10:08:00 PM GMT+1:00
+
+h:mm:ss a v
+10:07:41 PM ?
+ */
