d0ca70cd3eb34ab4f7691fea9fde0fc6d6935435
burakarslan <burakarslan@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-06-02 11:53:17 +0000

    releasing 0.4.1 -- a maintenance release

---
+++ trunk/qooxdoo-contrib/Soap/0.4.1/client/source/class/soapdemo/theme/Theme.js
@@ -0,0 +1,10 @@
+
+qx.Theme.define("soapdemo.theme.Theme", {
+    meta : {
+        color : soapdemo.theme.Color,
+        decoration : soapdemo.theme.Decoration,
+        font : soapdemo.theme.Font,
+        icon : qx.theme.icon.Tango,
+        appearance : soapdemo.theme.Appearance
+    }
+});
