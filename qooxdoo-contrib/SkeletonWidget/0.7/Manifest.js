4b1e5dac06932054f03007d08a0f1b1f352f87f1
thron7 <thron7@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-06-09 14:55:20 +0000

    copied trunk to 0.7, as this is a qooxdoo 0.7 compatible structure

---
+++ qooxdoo-contrib/SkeletonWidget/0.7/Manifest.js
@@ -0,0 +1,22 @@
+{
+  "name" : "SkeletonWidget",
+
+  "summary" : "A Sample widget.",
+  "description": "This widget can be used as basis for other contributions",
+  "keywords" : ["widget", "skeleton"],
+  "homepage" : "http://contrib.qooxdoo.org",
+  
+  "license" : "LGPL/EPL",    
+  "authors" : [
+    {
+      "name" : "",
+      "email" : ""
+    }
+  ],
+  
+  "namespace" : "skeletonwidget",
+  "resource-uri-setting" : "skeletonwidget.resourceUri",
+
+  "version" : "trunk",
+  "qooxdoo-versions": ["0.7"]
+}
