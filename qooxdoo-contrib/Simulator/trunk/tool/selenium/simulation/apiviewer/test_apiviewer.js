3be27687bb12f8552cd39e9951ce9f531a2b1e4c
d_wagner <d_wagner@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-06-10 12:22:15 +0000

    [BUG #2216] Log classes with documentation errors.

--- qooxdoo-contrib/Simulator/trunk/tool/selenium/simulation/apiviewer/test_apiviewer.js
+++ qooxdoo-contrib/Simulator/trunk/tool/selenium/simulation/apiviewer/test_apiviewer.js
@@ -27,11 +27,15 @@ var selWin = simulation.Simulation.SELENIUMWINDOW;
 var qxAppInst = simulation.Simulation.QXAPPINSTANCE;
 var logHtml = selWin + "." + qxAppInst + ".logelem.innerHTML";
 
+var ignoreDocErrors = ["qx.ui.virtual"];
+
 simulation.Simulation.prototype.runTest = function()
 {  
   // Make sure the locale is 'en' to simplify dealing with log messages.
   var setLocale = "qx.locale.Manager.getInstance().setLocale('en')";  
   this.runScript(setLocale, "Setting application locale to EN");
+  
+  this.logDocErrors();
   
   this.qxClick("qxh=app:viewer/qx.ui.toolbar.ToolBar/qx.ui.toolbar.Part/child[1]", "", "Clicking search button");
   
@@ -43,7 +47,7 @@ simulation.Simulation.prototype.runTest = function()
   
   this.qxTableClick("qxh=app:viewer/[@_searchView]/qx.ui.table.Table","row=0");
   
-  Packages.java.lang.Thread.sleep(5000);
+  Packages.java.lang.Thread.sleep(4000);
 
   // Check if the HTML embed's content has changed.
   var classViewerHtmlScript = 'selenium.browserbot.getCurrentWindow().document.getElementById("ClassViewer").innerHTML';
@@ -56,6 +60,62 @@ simulation.Simulation.prototype.runTest = function()
   }
   
   // Select a specific class from the tree, expand properties, ...
+  
+};
+
+simulation.Simulation.prototype.logDocErrors = function() 
+{
+  if (this.getConfigSetting("debug")) {
+    print("Getting classes with documentation errors");
+  }
+  var getDocWarnings = function(){  
+    function checkItems(items, parentLabel){
+      for (var i = 0; i < items.length; i++) {
+        var icon = items[i].getIcon();
+        if (icon.indexOf("_warning") >= 0) {
+          if (icon.indexOf("class") >= 0) {
+            warn.push(parentLabel + "." + items[i].getLabel());
+          }
+          else {
+            items[i].setOpen(true);
+            var newParentLabel = false;
+            if (parentLabel) {
+              newParentLabel = parentLabel + "." + items[i].getLabel();
+            }
+            var kids = items[i].getChildren();
+            checkItems(kids, newParentLabel);
+          }
+        }
+      }
+    }
+    var warn = [];
+    initialItems = selenium.browserbot.getCurrentWindow().qx.core.Init.getApplication().viewer.getChildren()[2].getChildren()[0].getChildren()[0].getChildren()[0].getChildren()[0].getChildren();
+    checkItems(initialItems, "qx");
+    return warn;
+  };
+  
+  this.addOwnFunction("getDocWarnings", getDocWarnings);
+  var docWarnings = this.getEval("selenium.browserbot.getCurrentWindow().qx.Simulation.getDocWarnings();");
+  
+  // docWarnings returns an array, but Rhino gives us a string-like object.
+  var docArray = String(docWarnings).split(",");
+    
+  function filterArr(item) {
+    for (var i=0; i<ignoreDocErrors.length; i++) {
+      if (item.indexOf(ignoreDocErrors[i]) >= 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  var filteredArray = docArray.filter(filterArr);
+  
+  if (filteredArray.length > 0) {
+    this.log("Classes with documentation errors (Excluded: " 
+             + ignoreDocErrors.join() + "):<br\/>" 
+             + filteredArray.join(", "), "info");    
+  }
   
 };
 
@@ -87,14 +147,14 @@ simulation.Simulation.prototype.runTest = function()
   catch(ex) {
     mySim.testFailed = true;
     var msg = "Unexpected error while running test!";
-    if (mySim.debug) {
+    if (mySim.getConfigSetting("debug")) {
       print(msg + "\n" + ex);
     }
     mySim.log(msg + "<br/>" + ex, "error");
   }
 
   if (!mySim.testFailed) {
-    if (mySim.debug) {
+    if (mySim.getConfigSetting("debug")) {
       print("Test run finished successfully.");
     }
     var errors = mySim.getTotalErrorsLogged() + mySim.errWarn;
