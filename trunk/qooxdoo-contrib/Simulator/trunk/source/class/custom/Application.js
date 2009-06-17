b17a667b8035aa00c3faf868a983cf8d26a67c38
d_wagner <d_wagner@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-06-17 13:22:27 +0000

    minor changes

--- trunk/qooxdoo-contrib/Simulator/trunk/source/class/custom/Application.js
+++ trunk/qooxdoo-contrib/Simulator/trunk/source/class/custom/Application.js
@@ -40,49 +40,54 @@ qx.Class.define("custom.Application",
     /**
      * TODOC
      *
-     * @type member
+     * @return {void} 
      */
     main : function()
     {
       this.base(arguments);
 
       // Define alias for custom resource path
-      //qx.io.Alias.getInstance().add("custom", qx.core.Setting.get("custom.resourceUri"));
+      // qx.io.Alias.getInstance().add("custom", qx.core.Setting.get("custom.resourceUri"));
 
       var doc = this.getRoot();
-      var container = new qx.ui.container.Composite(new qx.ui.layout.VBox)
-      doc.add(container)   
+      var container = new qx.ui.container.Composite(new qx.ui.layout.VBox);
+      doc.add(container);
 
       // Create button
       var button1 = new qx.ui.form.Button("First Button", "custom/image/test.png");
+      button1.selId = "foo";
       container.add(button1);
       this.b1 = button1;
 
       // Attach a tooltip
       button1.setToolTip(new qx.ui.tooltip.ToolTip("A nice tooltip", "icon/32/status/dialog-information.png"));
-      
+
       button1.getContentElement().setAttribute("id", "button");
       button1.setUserData("thefatbutton", "this is for the simulator");
 
       // textfield
       var tf = new qx.ui.form.TextField();
-      container.add(tf);      
+      container.add(tf);
       tf.getContentElement().setAttribute("id", "tf");
+      this.tf = tf;
 
       // Add an event listener
-      button1.addListener("execute", function(e) {
-        tf.setValue("I was pressed!" + e);
-      });
+      button1.addListener("execute", function(e)
+      {
+        this.debug("Button 1 clicked.");
+        this.tf.setValue("I was pressed!" + e);
+      },
+      this);
 
-
-      //# tabview
+      // # tabview
       var tv = new qx.ui.tabview.TabView();
       container.add(tv);
-      tv.set({
-        height : 200,
-        width  : 300,
-        padding: 10
-      });                  
+      tv.set(
+      {
+        height  : 200,
+        width   : 300,
+        padding : 10
+      });
 
       var p1 = new qx.ui.tabview.Page("First Tab");
       p1.getButton().getContentElement().setAttribute("id", "First");
@@ -93,44 +98,37 @@ qx.Class.define("custom.Application",
       tv.add(p2);
 
       // create some arbitrary object nestings
-      this.c1={};
+      this.c1 = {};
       this.c1.c2 = {};
-      this.c1.c2.c3={};
-      this.c1.c2.c3.First=p1.getButton();
-      this.c1.c2.Second=p2.getButton();
+      this.c1.c2.c3 = {};
+      this.c1.c2.c3.First = p1.getButton();
+      this.c1.c2.Second = p2.getButton();
 
-
-      //this.tracker = new custom.MouseTracker();
-      //this.tracker.window.open();
-
+      // this.tracker = new custom.MouseTracker();
+      // this.tracker.window.open();
       this.selsh = new custom.simulator.Shell();
-      doc.add(this.selsh, {right: 0});
+      doc.add(this.selsh, { right : 0 });
       this.selsh.open();
+    },  // construct
 
-      // activate Inspector
-      //inspector.Inspector.init();
-
-    }, //construct
-
-
+    // activate Inspector
+    // inspector.Inspector.init();
     /**
      * TODOC
      *
-     * @type member
+     * @return {void} 
      */
-    close : function()
-    {
+    close : function() {
       this.base(arguments);
 
-      // Prompt user
-      // return "Do you really want to close the application?";
+    // Prompt user
+    // return "Do you really want to close the application?";
     },
-
 
     /**
      * TODOC
      *
-     * @type member
+     * @return {void} 
      */
     terminate : function() {
       this.base(arguments);
@@ -141,12 +139,10 @@ qx.Class.define("custom.Application",
 
 
   /*
-  *****************************************************************************
-     SETTINGS
-  *****************************************************************************
-  */
+      *****************************************************************************
+         SETTINGS
+      *****************************************************************************
+      */
 
-  settings : {
-    "custom.resourceUri" : "./resource"
-  }
+  settings : { "custom.resourceUri" : "./resource" }
 });
