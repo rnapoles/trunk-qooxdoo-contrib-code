fc786064eb717868824c17f1d250128b106fbf4d
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-06-17 10:20:49 +0000

    [BUG RESOLVED 1857]: Buttons do not work while a tooltip is showing http://bugzilla.qooxdoo.org/show_bug.cgi?id=1857
    Application to reproduce bug report.

---
+++ qooxdoo-contrib/Bugs/1857/bug1857/source/class/bug1857/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug1857.theme.Appearance",
+{
+  extend : qx.theme.classic.Appearance,
+
+  appearances :
+  {
+  }
+});
---
+++ qooxdoo-contrib/Bugs/1857/bug1857/source/class/bug1857/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug1857.theme.Decoration",
+{
+  extend : qx.theme.classic.Decoration,
+
+  decorations :
+  {
+  }
+});
