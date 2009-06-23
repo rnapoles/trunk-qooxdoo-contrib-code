67bd2cc6d323af7d3569edfed80a6f7d28bc0431
d_wagner <d_wagner@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-06-23 15:12:03 +0000

    [BUG #2301] Minor corrections for browser-side message filtering

--- trunk/qooxdoo-contrib/Simulator/trunk/tool/selenium/simulation/Simulation.js
+++ trunk/qooxdoo-contrib/Simulator/trunk/tool/selenium/simulation/Simulation.js
@@ -754,11 +754,9 @@ simulation.Simulation.prototype.addObjectGetter = function()
 simulation.Simulation.prototype.addSanitizer = function()
 {
   var sanitize = function(text)
-  {    
-    text = text.replace(/\n/g,'<br/>');
-    text = text.replace(/\r/g,'<br/>');
+  {
     text = text.replace(/'/g, '&quot;');
-    text = text.replace(/[^\w\d\-_:;\.,\!\?\(\)\[\]#$%&= \/\<\> ]?/gi, ''); 
+    text = text.replace(/[^\w\d\-_:;\.,\"\!\?\(\)\[\]#$%&= \/\<\> ]?/gi, ''); 
     return text;
   };
   
