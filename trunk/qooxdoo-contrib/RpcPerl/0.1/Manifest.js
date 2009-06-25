d77bce22befae3201110c0ca4d6c1a3197c0ddcf
oetiker <oetiker@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-06-25 21:04:47 +0000

    tag first release

---
+++ trunk/qooxdoo-contrib/RpcPerl/0.1/Manifest.js
@@ -0,0 +1,25 @@
+{
+  "name" : "RpcPerl",
+
+  "summary" : "qooxdoo RPC server in Perl",
+  "description": "",
+  "keywords" : ["RPC"],
+  "homepage" : "http://qooxdoo.org/documentation/RPC",
+  
+  "license" : "LGPL/EPL",    
+  "authors" : [
+    {
+      "name" : "Nick Glencross",
+      "user" : "",
+      "email" : "nick DOT glencross AT gmail DOT com"
+    },
+    {
+      "name" : "Tobi Oetiker",
+      "user" : "oetiker",
+      "email" : "tobi AT oetiker DOT ch"
+    }
+  ],
+  
+  "version" : "trunk",
+  "qooxdoo-versions": ["0.7","0.8"]
+}
