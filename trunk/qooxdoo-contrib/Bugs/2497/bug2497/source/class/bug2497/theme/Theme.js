3033c2cbfecbd25f00b007661054788e9590d9f0
jonathan_rass <jonathan_rass@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-06-26 08:39:09 +0000

    [BUG #2497 ASSIGNED]: Removing the element an effect is running on in its finish callback. http://bugzilla.qooxdoo.org/show_bug.cgi?id=2497
    Demo for this issue.

---
+++ trunk/qooxdoo-contrib/Bugs/2497/bug2497/source/class/bug2497/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2497.theme.Theme",
+{
+  meta :
+  {
+    color : bug2497.theme.Color,
+    decoration : bug2497.theme.Decoration,
+    font : bug2497.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug2497.theme.Appearance
+  }
+});
