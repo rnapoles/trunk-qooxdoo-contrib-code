067043399c780f55db747b4768348b5a6500fc2b
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-07-08 15:14:11 +0000

    Initial commit for bug report.

---
+++ qooxdoo-contrib/Bugs/2563/bug2563/source/class/bug2563/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2563.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
