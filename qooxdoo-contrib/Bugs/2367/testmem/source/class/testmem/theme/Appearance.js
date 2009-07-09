43137341ada43d20dc98c6171c88b7be8a0f242d
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-07-09 15:55:56 +0000

    Performance tests for bug report.

---
+++ qooxdoo-contrib/Bugs/2367/testmem/source/class/testmem/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("testmem.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
