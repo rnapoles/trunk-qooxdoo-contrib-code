7889ad8550432eb396cb5077bda813d6faab948e
til132 <til132@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-07-10 12:55:25 +0000

    Added sleeps to the form handler pages for simulating server-side work (in order to show the blocker pane)

--- qooxdoo-contrib/Hijax/trunk/demo/default/source/contact-form.php
+++ qooxdoo-contrib/Hijax/trunk/demo/default/source/contact-form.php
@@ -1,5 +1,10 @@
 <?php include("header.php"); ?>
 
+<?php
+// Simulating server-side work
+sleep(1);
+?>
+
 <h2>Message sent</h2>
 <p>Subject: <?php echo $_REQUEST["subject_text"]; ?></p>
 <p>E-Mail Address: <?php echo $_REQUEST["email_email"]; ?></p>
