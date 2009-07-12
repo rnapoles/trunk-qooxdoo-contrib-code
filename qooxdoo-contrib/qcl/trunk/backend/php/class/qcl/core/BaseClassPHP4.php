b8c6298c70b1e0853797aef86c4e39928533012a
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-07-12 08:52:41 +0000

    set() method can now take a map as argument

--- qooxdoo-contrib/qcl/trunk/backend/php/class/qcl/core/BaseClassPHP4.php
+++ qooxdoo-contrib/qcl/trunk/backend/php/class/qcl/core/BaseClassPHP4.php
@@ -105,14 +105,23 @@ class qcl_core_BaseClass
   }
 
   /**
-   * Generic getter for properties
-   * @param string $property
+   * Generic setter for properties. Behaves like qooxdoo setter.
+   * @param string|array $property If string, set the corresponding property to $value.
+   *   If array, assume it is a map and set each key-value pair.
    * @param mixed $value
    * @return unknown_type
    * @todo type check
    */
-  function set( $property, $value )
+  function set( $property, $value=null )
   {
+    if ( is_array($property) )
+    {
+      foreach ( $property as $key => $value )
+      {
+        $this->set( $key, $value );
+      }
+      return;
+    }
     $this->check( $property );
     $this->$property = $value;
   }
