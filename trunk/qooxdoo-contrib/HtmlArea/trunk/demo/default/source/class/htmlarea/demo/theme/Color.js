a46f0dd405b0d4645242ffe49824189a3566566e
aback <aback@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-07-16 14:06:17 +0000

    [BUG 1958 ASSIGNED] Create a low-level variant http://bugzilla.qooxdoo.org/show_bug.cgi?id=1958
    Added two new demos (low-level and high-level demo)

---
+++ trunk/qooxdoo-contrib/HtmlArea/trunk/demo/default/source/class/htmlarea/demo/theme/Color.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("htmlarea.demo.theme.Color",
+{
+  extend : qx.theme.modern.Color,
+
+  colors :
+  {
+  }
+});
