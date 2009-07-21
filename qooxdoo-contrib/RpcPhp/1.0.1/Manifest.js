bfdc25fbdf13b3748b31387244ce03553e31c27a
derrell <derrell@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-07-21 14:03:55 +0000

    Make initial, obvious changes required for PHP6 compatibility

---
+++ qooxdoo-contrib/RpcPhp/1.0.1/Manifest.js
@@ -0,0 +1,20 @@
+{
+  "name" : "RpcPhp",
+
+  "summary" : "qooxdoo RPC server in PHP",
+  "description": "",
+  "keywords" : ["RPC"],
+  "homepage" : "http://qooxdoo.org/documentation/RPC",
+  
+  "license" : "LGPL/EPL",    
+  "authors" : [
+    {
+      "name" : "Derrell Lipman",
+      "user" : "derrell",
+      "email" : "derrell DOT lipman AT unwireduniverse DOT com"
+    }
+  ],
+  
+  "version" : "1.0.0",
+  "qooxdoo-versions": ["0.7", "0.8" ]
+}
