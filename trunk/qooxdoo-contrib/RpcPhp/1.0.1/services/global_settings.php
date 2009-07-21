ac6b7750e3afac9e9a3cda27b71a0f15a7c60703
derrell <derrell@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-07-21 14:03:55 +0000

    Make initial, obvious changes required for PHP6 compatibility

---
+++ trunk/qooxdoo-contrib/RpcPhp/1.0.1/services/global_settings.php
@@ -0,0 +1,3 @@
+<?php
+  //define("defaultAccessibility", Accessibility_Public);
+?>
---
+++ trunk/qooxdoo-contrib/RpcPhp/1.0.1/services/qooxdoo/test.php
@@ -0,0 +1,294 @@
+<?php
+  /*
+   * qooxdoo - the new era of web development
+   *
+   * http://qooxdoo.org
+   *
+   * Copyright:
+   *   2006 Derrell Lipman
+   *
+   * License:
+   *   LGPL: http://www.gnu.org/licenses/lgpl.html
+   *   EPL: http://www.eclipse.org/org/documents/epl-v10.php
+   *   See the LICENSE file in the project's top-level directory for details.
+   *
+   * Authors:
+   *  * Derrell Lipman (derrell)
+   */
+
+/*
+ * This is the standard qooxdoo test class.  There are tests for each of the
+ * primitive types here, along with standard named tests "echo", "sink" and
+ * "sleep".
+ */
+
+class class_test
+{
+    /**
+     * Specify method accessibility.  Default value is configured in server,
+     * but may be overridden on a per-method basis here.
+     *
+     * @param method
+     *   The name of the method (without the leading "method_") to be tested
+     *   for accessibility.
+     *
+     * @param defaultAccessibility
+     *   The default accessibility configured in the server.  (See @return for
+     *   possible values.)
+     *
+     * @param bScriptTransportInUse (not yet implemented)
+     *   Boolean indicating whether the current request was issued via
+     *   ScriptTransport.
+     *
+     * @param bDefaultScriptTransportAllowed (not yet implemented)
+     *   Boolean specifying the default value for allowing requests via
+     *   ScriptTransport. 
+     *
+     * @return
+     *   One of these values:
+     *     Accessibility_Public
+     *     Accessibility_Domain
+     *     Accessibility_Session
+     *     Accessibility_Fail
+     */
+/*
+    public function GetAccessibility($method, $defaultAccessibility)
+    {
+        switch($method)
+        {
+        case "echo":
+            return Accessibility_Domain;
+
+        case "getInteger":
+            return Accessibility_Session;
+
+        case "getString":
+            return Accessibility_Public;
+        }
+
+        return $defaultAccessibility;
+    }
+*/
+
+    /**
+     * Echo the (one and only) parameter.
+     *
+     * @param params
+     *   An array containing the parameters to this method
+     *
+     * @param error
+     *   An object of class JsonRpcError.
+     *
+     * @return
+     *   Success: The object containing the result of the method;
+     *   Failure: null
+     */
+    public function method_echo($params, $error)
+    {
+        if (count($params) != 1)
+        {
+            $error->SetError(JsonRpcError_ParameterMismatch,
+                             "Expected 1 parameter; got " . count($params));
+            return $error;
+        }
+        return "Client said: [" . $params[0] . "]";
+    }
+    
+    /**
+     * Sink all data and never return.
+     *
+     * @param params
+     *   An array containing the parameters to this method (none expected)
+     *
+     * @param error
+     *   An object of class JsonRpcError.
+     *
+     * @return
+     *   "Never"
+     */
+    public function method_sink($params, $error)
+    {
+        /* We're never supposed to return.  Just sleep for a very long time. */
+        sleep(240);
+    }
+    
+    /**
+     * Sleep for the number of seconds specified by the parameter.
+     *
+     * @param params
+     *   An array containing the parameters to this method (one expected)
+     *
+     * @param error
+     *   An object of class JsonRpcError.
+     *
+     * @return
+     *   Success: The object containing the result of the method;
+     *   Failure: null
+     */
+    public function method_sleep($params, $error)
+    {
+        if (count($params) != 1)
+        {
+            $error->SetError(JsonRpcError_ParameterMismatch,
+                             "Expected 1 parameter; got " . count($params));
+            return $error;
+        }
+        
+        sleep(intval($params[0]));
+        return $params[0];
+    }
+    
+
+    /*************************************************************************/
+
+    /*
+     * The remainder of the functions test each individual primitive type, and
+     * test echoing arbitrary types.  Hopefully the name is self-explanatory.
+     */
+
+    public function method_getInteger($params, $error)
+    {
+        return 1;
+    }
+    
+    public function method_getFloat($params, $error)
+    {
+        return 1/3;
+    }
+    
+    public function method_getString($params, $error)
+    {
+        return "Hello world";
+    }
+    
+    public function method_getBadString($params, $error)
+    {
+        return "<!DOCTYPE HTML \"-//IETF//DTD HTML 2.0//EN\">";
+    }
+    
+    public function method_getArrayInteger($params, $error)
+    {
+        return array(1, 2, 3, 4);
+    }
+    
+    public function method_getArrayString($params, $error)
+    {
+        return array("one", "two", "three", "four");
+    }
+    
+    public function method_getObject($params, $error)
+    {
+        return new JSON(); // some arbitrary object
+    }
+    
+    public function method_getTrue($params, $error)
+    {
+        return true;
+    }
+    
+    public function method_getFalse($params, $error)
+    {
+        return false;
+    }
+    
+    public function method_getNull($params, $error)
+    {
+        return null;
+    }
+
+    public function method_isInteger($params, $error)
+    {
+        return is_int($params[0]);
+    }
+    
+    public function method_isFloat($params, $error)
+    {
+        return is_float($params[0]);
+    }
+    
+    public function method_isString($params, $error)
+    {
+        return is_string($params[0]);
+    }
+    
+    public function method_isBoolean($params, $error)
+    {
+        return is_bool($params[0]);
+    }
+    
+    public function method_isArray($params, $error)
+    {
+        return is_array($params[0]);
+    }
+    
+    public function method_isObject($params, $error)
+    {
+        return is_object($params[0]);
+    }
+    
+    public function method_isNull($params, $error)
+    {
+        return is_null($params[0]);
+    }
+    
+    public function method_getParams($params, $error)
+    {
+        return $params;
+    }	
+    
+    public function method_getParam($params, $error)
+    {
+        return $params[0];
+    }	
+    
+    public function method_getCurrentTimestamp()
+    {
+        $now = time();
+        $obj = new stdClass();
+        $obj->now = $now;
+        $obj->json = new JSON_Date($now);
+        return $obj;
+    }
+
+    // utility function used by method_getError()
+    public function getErrorThrown($params)
+    {
+        // In this example we don't use the provided $error object.
+        // Instead, we throw a new error. This could be done from any depth
+        // of recursion or function call graph without having to pass around
+        // the $error object.
+        throw new JsonRpcError(23, "This is an application-provided error");
+    }
+
+    // utility function used by method_getError()
+    public function getErrorTraditional($params, $error)
+    {
+        // Here we demonstrate the traditional method of returning the
+        // provided $error object after specifying the error code and message.
+        $error->SetError(42, "This is an application-provided error");
+
+        // just for fun, test the GetError() method too
+        $errorInfo = $error->GetError();
+
+        // override the previous error message with a new one.
+        $error->SetError($errorInfo["code"],
+                         "origin=" . $errorInfo["origin"] .
+                         ", code=" . $errorInfo["code"] .
+                         ", message=" . $errorInfo["message"]);
+        return $error;
+    }
+
+    public function method_getError($params, $error)
+    {
+        if (count($params) > 0 && $params[0])
+        {
+            return $this->getErrorThrown($params);
+        }
+        else
+        {
+            return $this->getErrorTraditional($params, $error);
+        }
+    }
+}
+
+?>
