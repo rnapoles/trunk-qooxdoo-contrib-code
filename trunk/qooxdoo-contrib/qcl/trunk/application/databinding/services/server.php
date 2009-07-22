e1af59950ae26628098169e330dd91286aaf327a
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-07-22 12:43:23 +0000

    Various cosmetic fixes and removal of empty directories

--- trunk/qooxdoo-contrib/qcl/trunk/application/databinding/services/server.php
+++ trunk/qooxdoo-contrib/qcl/trunk/application/databinding/services/server.php
@@ -1,7 +1,25 @@
 <?php
+/* ************************************************************************
+
+   qcl - the qooxdoo component library
+
+   http://qooxdoo.org/contrib/project/qcl/
+
+   Copyright:
+     2007-2009 Christian Boulanger
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+   *  Christian Boulanger (cboulanger)
+
+************************************************************************ */
+
 /**
- * Sample application to show qcl access control system, using
- * RpcPhp trunk version and qcl library
+ * Sample application to show qcl access databinding and synchronization
  */
 
 /*
