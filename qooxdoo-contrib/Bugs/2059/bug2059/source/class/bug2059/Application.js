41852fe941fa508f8e6142a54793f179be4df369
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-07-22 14:32:03 +0000

    Initial commit for bug report.

---
+++ qooxdoo-contrib/Bugs/2059/bug2059/source/class/bug2059/Application.js
@@ -0,0 +1,51 @@
+/* ************************************************************************
+
+#asset(qx/icon/Tango/16/actions/edit-cut.png)
+#asset(qx/icon/Tango/16/actions/edit-copy.png)
+#asset(qx/icon/Tango/16/actions/edit-paste.png)
+
+************************************************************************ */
+
+qx.Class.define("bug2059.Application",
+{
+  extend : qx.application.Standalone,
+
+  members :
+  {
+    main : function()
+    {
+      this.base(arguments);
+
+      if (qx.core.Variant.isSet("qx.debug", "on"))
+      {
+        qx.log.appender.Native;
+        qx.log.appender.Console;
+      }
+
+      var list = new qx.ui.form.List;
+      list.setContextMenu(this.getContextMenu());
+
+      for (var i=0; i<20; i++) {
+        list.add(new qx.ui.form.ListItem("Item " + i));
+      }
+
+      var doc = this.getRoot();
+      doc.add(list, {left: 100, top: 50});
+    },
+
+    getContextMenu : function()
+    {
+      var menu = new qx.ui.menu.Menu;
+
+      var cutButton = new qx.ui.menu.Button("Cut", "icon/16/actions/edit-cut.png");
+      var copyButton = new qx.ui.menu.Button("Copy", "icon/16/actions/edit-copy.png");
+      var pasteButton = new qx.ui.menu.Button("Paste", "icon/16/actions/edit-paste.png");
+
+      menu.add(cutButton);
+      menu.add(copyButton);
+      menu.add(pasteButton);
+
+      return menu;
+    }
+  }
+});
