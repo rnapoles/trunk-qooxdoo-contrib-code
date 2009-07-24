9c658323dcb4f7ec7555a0a3fe69dbbb0a019767
dmbaggett <dmbaggett@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-07-24 22:27:31 +0000

    smart table model; initial contrib -- needs demo framework written around it

---
+++ qooxdoo-contrib/smart/source/class/smart/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("smart.theme.Theme",
+{
+  meta :
+  {
+    color : smart.theme.Color,
+    decoration : smart.theme.Decoration,
+    font : smart.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : smart.theme.Appearance
+  }
+});
