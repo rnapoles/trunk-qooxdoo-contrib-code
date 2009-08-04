d4934f8d368b1d4eb09f53ce1024c6422f2171d4
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-08-04 23:10:32 +0000

    Added support for RadioGroup for qcl.ui.dialog.Form.

--- trunk/qooxdoo-contrib/qcl/trunk/frontend/source/class/qcl/ui/dialog/FormRenderer.js
+++ trunk/qooxdoo-contrib/qcl/trunk/frontend/source/class/qcl/ui/dialog/FormRenderer.js
@@ -45,7 +45,9 @@ qx.Class.define("qcl.ui.dialog.FormRenderer",
      * @param title {String?} A title of the group you are adding.
      */
     addItems : function(items, names, title) {
-      // add the header
+      /*
+       * add the header
+       */
       if (title != null) {
         this._add(
           this._createHeader(title), {row: this._row, column: 0, colSpan: 2}
@@ -53,24 +55,104 @@ qx.Class.define("qcl.ui.dialog.FormRenderer",
         this._row++;
       }
       
-      // add the items, which can be form elements or 
+      /*
+       * add the items
+       */ 
       for (var i = 0; i < items.length; i++) 
       {
+        /*
+         * current item
+         */
+        var item = items[i];
 
-        if ( items[i].getUserData("excluded") )
+        /*
+         * radio group
+         */
+        if ( item instanceof qx.ui.form.RadioGroup )
+        {
+          /*
+           * create horizontal radio group for a small
+           * number of radio buttons 
+           */
+          if ( item.getUserData("orientation") == "horizontal" )
+          {
+            var widget = this._createHBoxForRadioGroup( item );
+          }
+          else
+          {
+            var widget = this._createWidgetForRadioGroup( item );
+          }       
+        }
+        
+        /*
+         * other form widgets
+         */
+        else
+        {
+          var widget = item;
+        }
+        
+        /*
+         * Excluded form elements, used for full-width
+         * labels. this should be implemented differently,
+         * though
+         */
+        if ( names[i] && item.getUserData("excluded") )
         {
           var label = new qx.ui.basic.Label( names[i] );
           label.setRich(true);
-          this._add( label, {row: this._row, column: 0, colSpan : 2 } );
+          this._add( label,  {row: this._row, column: 0, colSpan : 2 } );
         }
+        
+        /*
+         * if the label is null, use the full width for the widget
+         * doesn't work yet
+         */
+        else if ( ! names[i]  )
+        {
+          this._add( widget, {row: this._row, column: 0, colSpan : 2 } );
+        }
+        
+        /*
+         * normal case: label in col 0, form element in col 2
+         */
         else
         {
-          var label = this._createLabel(names[i], items[i]);
-          this._add(label, {row: this._row, column: 0});
-          this._add(items[i], {row: this._row, column: 1});
+          var label = this._createLabel( names[i], item);
+          label.setRich(true);          
+          this._add( label, {row: this._row, column: 0} );
+          this._add( widget, {row: this._row, column: 1} );
         }
-        this._row++;
+        
+        /*
+         * increment row
+         */
+         this._row++;
+        
       }
+    },
+
+    
+    /**
+     * Takes the items of the given RadioGroup and adds the to a Composite. 
+     * The composite has a HBox layout so the RadioButtons will be alligned
+     * horizontally. This is only useful for a small number of RadioButtons
+     * such as 2 or 3 buttons with labels. 
+     * 
+     * @param group {qx.ui.form.RadioGroup} The RadioGroup which needs to be 
+     *   added.
+     * @return {qx.ui.container.Composite} A composite containing the items of 
+     *   the RadioGroup.
+     */
+    _createHBoxForRadioGroup : function(group) {
+      var widget = new qx.ui.container.Composite(new qx.ui.layout.HBox(5));
+      var items = group.getItems();
+      for (var i = 0; i < items.length; i++) {
+        widget.add(items[i]);
+      }
+      return widget;
     }
+    
+    
   }
 });
