67ca46ff1787ca6daf73e8c41d968940593df783
oetiker <oetiker@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-08-18 21:10:32 +0000

    Cropper contrib. This is the initial commit. See http://qooxdoo.org/contrib/project/cropper

---
+++ trunk/qooxdoo-contrib/Cropper/trunk/source/class/cropper/__init__.js
@@ -0,0 +1,9 @@
+/** <h3> cropper API Documentation </h3>
+ *
+ * The cropper object positions itself over a widget of your choice and
+ * activates upon clicking on that widget. You can then drag the mouse to
+ * select a portion of the underlying widget. As the mouse button is
+ * released, an event is generated, telling about the the coordinates
+ * chosen. Press the space bar to move the selection area.
+ *
+ */
