9914c10588825f8505f8d4f7b1fc187e72a7646c
thron7 <thron7@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-08-27 18:25:32 +0000

    [BUG #2469] test data

---
+++ qooxdoo-contrib/Bugs/2469/testoptvar/source/class/testoptvar/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("testoptvar.theme.Decoration",
+{
+  extend : qx.theme.modern.Decoration,
+
+  decorations :
+  {
+  }
+});
