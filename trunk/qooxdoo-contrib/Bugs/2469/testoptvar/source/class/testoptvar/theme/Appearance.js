18d99e8def490bbdf35753a42c9ded89885c0508
thron7 <thron7@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-08-27 18:25:32 +0000

    [BUG #2469] test data

---
+++ trunk/qooxdoo-contrib/Bugs/2469/testoptvar/source/class/testoptvar/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("testoptvar.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
---
+++ trunk/qooxdoo-contrib/Bugs/2469/testoptvar/source/class/testoptvar/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("testoptvar.theme.Theme",
+{
+  meta :
+  {
+    color : testoptvar.theme.Color,
+    decoration : testoptvar.theme.Decoration,
+    font : testoptvar.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : testoptvar.theme.Appearance
+  }
+});
