ecf936851002d66d7ae46d6604a9fe782bc00e9b
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-09-03 14:58:03 +0000

    [RESOLVED - bug 2748]: Broken images in IE7 (Checkbox) http://bugzilla.qooxdoo.org/show_bug.cgi?id=2748
    - Example code to reproduce issue.

---
+++ qooxdoo-contrib/Bugs/2748/bug2748/source/class/bug2748/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2748.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
