7fff6e6e546b8d73cbda6326168fe47749abcb3a
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-09-07 15:22:46 +0000

    [BUG RESOLVED 2602]: Textfield keypress events stop firing after all characters selected by 'swiping' across them. http://bugzilla.qooxdoo.org/show_bug.cgi?id=2602
    - Application to reproduce issue.

---
+++ trunk/qooxdoo-contrib/Bugs/2602/bug2602/source/class/bug2602/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2602.theme.Theme",
+{
+  meta :
+  {
+    color : bug2602.theme.Color,
+    decoration : bug2602.theme.Decoration,
+    font : bug2602.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug2602.theme.Appearance
+  }
+});
