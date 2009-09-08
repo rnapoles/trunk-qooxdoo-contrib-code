b46634366dd9520cba3a13bd89243b0a342fe229
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-09-08 09:23:04 +0000

    [BUG ASSIGNED 2600]: Unable to do SelectAll on focus of text field http://bugzilla.qooxdoo.org/show_bug.cgi?id=2600
    - example application to reproduce issue

---
+++ qooxdoo-contrib/Bugs/2600/bug2600/source/class/bug2600/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2600.theme.Theme",
+{
+  meta :
+  {
+    color : bug2600.theme.Color,
+    decoration : bug2600.theme.Decoration,
+    font : bug2600.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug2600.theme.Appearance
+  }
+});
