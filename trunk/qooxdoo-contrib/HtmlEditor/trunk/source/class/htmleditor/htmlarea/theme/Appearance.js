51fcd20e7e121cc96280f854bddc6890c903d895
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-09-08 18:41:57 +0000

    Added 0.8.3-style theme files to suppress "missing theme" error

---
+++ trunk/qooxdoo-contrib/HtmlEditor/trunk/source/class/htmleditor/htmlarea/theme/Appearance.js
@@ -0,0 +1,39 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2007 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Alexander Back (aback)
+     * Jonathan Weiß (jonathan_rass)
+
+************************************************************************ */
+
+qx.Theme.define("htmleditor.htmlarea.theme.Appearance",
+{
+  title : "HtmlArea appearance theme",
+ 
+  appearances :
+  {
+    "html-area" : 
+    {
+      style : function(states)
+      {
+        return {
+          width           : 600,
+          height          : 400,
+          backgroundColor : "white"
+        };
+      }
+    }
+  }
+});
---
+++ trunk/qooxdoo-contrib/HtmlEditor/trunk/source/class/htmleditor/htmlarea/theme/HtmlArea.js
@@ -0,0 +1,33 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2007 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Alexander Back (aback)
+     * Jonathan Weiß (jonathan_rass)
+
+************************************************************************ */
+
+qx.Theme.define("htmleditor.htmlarea.theme.HtmlArea",
+{
+  title : "HtmlArea meta theme",
+ 
+  meta :
+  {
+    color : qx.theme.modern.Color,
+    decoration : qx.theme.modern.Decoration,
+    font : qx.theme.modern.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : qx.theme.modern.Appearance
+  }
+});
