49d98a6da10d868daa1422ee8ac11f649680e07f
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-09-15 11:50:44 +0000

    [BUG ASSIGNED 2766]: IE7 fatal error when setting invalid style (setBlockerColor) http://bugzilla.qooxdoo.org/show_bug.cgi?id=2766
    - example application

---
+++ qooxdoo-contrib/Bugs/2766/bug2766/source/class/bug2766/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2766.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
