d54e273430cebf32871c496de9406155be67acfe
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-09-18 12:20:55 +0000

    [BUG ASSIGNED 2796]: Cell Click and Click events of the table are not happening in IE6 http://bugzilla.qooxdoo.org/show_bug.cgi?id=2796
    - added application to reproduce issue.

---
+++ qooxdoo-contrib/Bugs/2796/bug2796/source/class/bug2796/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2796.theme.Decoration",
+{
+  extend : qx.theme.modern.Decoration,
+
+  decorations :
+  {
+  }
+});
