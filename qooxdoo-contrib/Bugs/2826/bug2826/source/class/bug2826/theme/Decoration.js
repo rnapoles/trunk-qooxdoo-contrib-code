ec75cceb20adc4ddacdcd20c3ed6d430e2420a80
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-09-22 14:34:37 +0000

    [ASSIGNED - bug 2826]: It is possible to select a disabled menu item http://bugzilla.qooxdoo.org/show_bug.cgi?id=2826
    - application to reproduce bug report

---
+++ qooxdoo-contrib/Bugs/2826/bug2826/source/class/bug2826/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2826.theme.Decoration",
+{
+  extend : qx.theme.modern.Decoration,
+
+  decorations :
+  {
+  }
+});
