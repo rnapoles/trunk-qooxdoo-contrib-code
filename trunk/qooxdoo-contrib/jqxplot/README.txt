aa6a0c8bdb37dcc2060b6953b8658093a3f3039a
zaucker <zaucker@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-09-30 13:35:31 +0000

    Complete demo application for using jqPlot with Qooxdoo.

---
+++ trunk/qooxdoo-contrib/jqxplot/README.txt
@@ -0,0 +1,7 @@
+Demo: Using jqPlot with Qooxdoo
+===============================
+
+This is an example how the jQuery (http://jquery.com) based plotting
+library jqPlot (http://www.jqplot.com) can be used with Qooxdoo.
+
+See trunk/demo/default/readme.txt
---
+++ trunk/qooxdoo-contrib/jqxplot/trunk/demo/default/source/class/jqxplot/Application.js
@@ -0,0 +1,148 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#asset(test/*)
+
+************************************************************************ */
+
+/**
+ * This is the main application class of your custom application "test"
+ */
+qx.Class.define("jqxplot.Application",
+{
+  extend : qx.application.Standalone,
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    /**
+     * This method contains the initial application code and gets called
+     * during startup of the application
+     */
+    main : function()
+    {
+        // Call super class
+        this.base(arguments);
+
+        // Enable logging in debug variant
+        if (qx.core.Variant.isSet("qx.debug", "on")) {
+            // support native logging capabilities, e.g. Firebug for Firefox
+            qx.log.appender.Native;
+            // support additional cross-browser console. Press F7 to toggle visibility
+            qx.log.appender.Console;
+	}
+
+        // Document is the application root
+        var doc = this.getRoot();
+
+        var graphBox =
+        new qx.ui.container.Composite(new qx.ui.layout.VBox(20));
+        graphBox.setPadding(20);
+
+        var graphCanvas =
+            new qx.ui.container.Composite(new qx.ui.layout.Canvas()).set({
+                                       padding: 20,
+                                       width:500,
+                                       height:400});
+
+        var graphSelect = new qx.ui.form.SelectBox();
+        graphSelect.setMaxWidth(100);
+        graphSelect.add(new qx.ui.form.ListItem('*** Select ***'));
+        graphSelect.add(new qx.ui.form.ListItem('Pie chart'));
+        graphSelect.add(new qx.ui.form.ListItem('Bar graph'));
+
+        graphBox.add(graphSelect);
+        graphBox.add(graphCanvas);
+        doc.add(graphBox);
+
+        var myGraph = null;
+        graphSelect.addListener("changeSelection", function(e) {
+ 	    var graphType = e.getData()[0].getLabel();
+	    var dom = graphCanvas.getContainerElement().getDomElement();
+	    qx.bom.element.Attribute.set(dom, 'id', 'jqxPlotBox');
+ 	    var title = 'Testing jqPlot with Qooxdoo';
+
+            switch (graphType) {
+	      case 'Bar graph':
+	      var data = [ [500, 350], [200, 300], [800, 800], [900, 650], [400, 100] ];
+	      var labels = [ {label: 'frogs'}, {label: 'buzzards'}, {label: 'cows'}, {label: 'deer'}, {label: 'mice'}];
+	      var xLabels = ['2005', '2009'];
+              myGraph =
+                  jQuery.jqplot('jqxPlotBox', data,
+                              { title: { text: title, fontSize: '12pt'},
+				legend: {show: true, location: 'ne', fontSize: '8pt'},
+                                seriesDefaults: {
+                                    renderer: jQuery.jqplot.BarRenderer,
+                                    rendererOptions: {barPadding: 8, barMargin: 20}
+                                },
+                                series: labels,
+                                axes: {
+                                    xaxis: {tickOptions: { enableFontSupport: true,
+							   fontFamily: 'Georgia',
+							   fontSize: '10pt' },
+                                            renderer: jQuery.jqplot.CategoryAxisRenderer,
+                                            ticks: xLabels,
+                                            enableFontSupport: true,
+                                            fontSize: '10pt'
+					   },
+                                    yaxis: { min: 0, max: 1100, numberTicks: 12,
+					     tickOptions: { enableFontSupport: true,
+							    fontFamily: 'Georgia',
+							    fontSize: '10pt' },
+					     labelRenderer: jQuery.jqplot.CanvasAxisLabelRenderer,
+                                             labelOptions: { enableFontSupport: true,
+							     fontFamily: 'Georgia',
+							     fontSize: '12pt' },
+					     label: 'y-axis',
+                                             fontSize: '10pt'
+					   }
+                                },
+                                cursor: { zoom: true, showTooltip: true,
+					  followMouse: true, clickReset: true
+					}
+                              }
+		  );
+	        break;
+
+	      case 'Pie chart':
+		var data = [ ['frogs', 500], ['buzzards', 200], ['cows', 800], ['deer', 900], ['mice', 400] ];
+
+ 	        myGraph =
+                  jQuery.jqplot('jqxPlotBox', [data],
+                                { title: { text: title, fontSize: '12pt'},
+                                  seriesDefaults: {
+                                    renderer:        jQuery.jqplot.PieRenderer,
+                                    rendererOptions: { sliceMargin: 8 }
+                                  },
+                                  legend: {show: true, fontSize: '8pt'},
+                                  // disable zoom
+                                  cursor: {zoom:false }
+                              });
+		break;
+
+	      default:
+	        break;
+	    }
+	    myGraph.redraw();
+	}, this);
+
+
+    }
+  }
+});
