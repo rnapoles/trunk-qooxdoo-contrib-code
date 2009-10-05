5e62c9de85ffd7a0dcf7f8b2490f2aa0a53f6792
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-10-05 08:50:06 +0000

    [BUG 2876]: qx.bom.Document.isStandardMode() return always false http://bugzilla.qooxdoo.org/show_bug.cgi?id=2876
    - Application to reproduce issue

---
+++ trunk/qooxdoo-contrib/Bugs/2876/bug2876/source/class/bug2876/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2876.theme.Decoration",
+{
+  extend : qx.theme.modern.Decoration,
+
+  decorations :
+  {
+  }
+});
