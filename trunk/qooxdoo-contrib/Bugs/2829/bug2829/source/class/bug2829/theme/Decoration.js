fe3ca3ed80c6d46c7d8f8729e2dddac7c6c5ec92
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-10-14 15:34:04 +0000

    [ASSIGNED - bug 2829]: MenuButtons should ignore tooltips http://bugzilla.qooxdoo.org/show_bug.cgi?id=2829
    - Application to reproduce issue.

---
+++ trunk/qooxdoo-contrib/Bugs/2829/bug2829/source/class/bug2829/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2829.theme.Decoration",
+{
+  extend : qx.theme.modern.Decoration,
+
+  decorations :
+  {
+  }
+});
