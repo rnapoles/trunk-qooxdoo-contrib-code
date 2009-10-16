52f20da5eac7cbef87d4f13beb6c02b6b10c5e69
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-10-16 13:09:47 +0000

    [BUG ASSIGNED 2716]: MBlocker block() prevents focusable elements to get the correct appearance http://bugzilla.qooxdoo.org/show_bug.cgi?id=2716
    Application to reproduce issue.

---
+++ trunk/qooxdoo-contrib/Bugs/2716/bug2716/source/class/bug2716/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2716.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
---
+++ trunk/qooxdoo-contrib/Bugs/2716/bug2716/source/class/bug2716/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2716.theme.Theme",
+{
+  meta :
+  {
+    color : bug2716.theme.Color,
+    decoration : bug2716.theme.Decoration,
+    font : bug2716.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug2716.theme.Appearance
+  }
+});
