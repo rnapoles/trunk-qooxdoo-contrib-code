6e52caa8ba55210152008f31c9174d85cf1ba345
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-10-16 10:20:37 +0000

    [BUG FIXED 2713]: qx.ui.core.MBlocker.blockContent doesn't make the widget anonymous http://bugzilla.qooxdoo.org/show_bug.cgi?id=2713
    Application to reproduce issue.

---
+++ trunk/qooxdoo-contrib/Bugs/2713/bug2713/source/class/bug2713/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2713.theme.Theme",
+{
+  meta :
+  {
+    color : bug2713.theme.Color,
+    decoration : bug2713.theme.Decoration,
+    font : bug2713.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug2713.theme.Appearance
+  }
+});
