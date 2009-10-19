4c9e864ba2bf2da4c44ec4a09f0158723d4564e8
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-10-19 15:38:51 +0000

    Application to reproduce issue.

---
+++ trunk/qooxdoo-contrib/Bugs/2925/bug2925/source/class/bug2925/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2925.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
