125375241231e224a55524d392e8a25e6956ac15
jonathan_rass <jonathan_rass@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-10-20 12:11:50 +0000

    [BUG #2402 ASSIGNED]: Umlauts only fire keypress event in TextArea, but no keyup or keydown http://bugzilla.qooxdoo.org/show_bug.cgi?id=2402
    Initial commit.

---
+++ qooxdoo-contrib/Bugs/2402/bug2402/source/class/bug2402/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2402.theme.Decoration",
+{
+  extend : qx.theme.modern.Decoration,
+
+  decorations :
+  {
+  }
+});
