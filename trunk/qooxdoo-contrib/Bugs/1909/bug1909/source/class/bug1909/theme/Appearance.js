4c512d07f9d399eb774febb073a9fbf109962db5
aback <aback@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-10-21 14:45:10 +0000

    Committed sample app for #1909

---
+++ trunk/qooxdoo-contrib/Bugs/1909/bug1909/source/class/bug1909/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug1909.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
---
+++ trunk/qooxdoo-contrib/Bugs/1909/bug1909/source/class/bug1909/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug1909.theme.Decoration",
+{
+  extend : qx.theme.modern.Decoration,
+
+  decorations :
+  {
+  }
+});
