e21df140f39f0dbd7076497ae7c1e280fb5ad925
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-10-25 12:58:47 +0000

    Finished scriptiong support. Ready for beta release.

---
+++ qooxdoo-contrib/RpcConsole/trunk/demo/default/source/script/rpcconsole.testData.js
@@ -0,0 +1,156 @@
+/**
+ * Calls the setTestData() method of the rpcconsole.demo.Application app with
+ * a map of test data from which the test menu will be populated. The map
+ * must have the following structure:
+ * <pre>
+ * {
+ *   testName : {
+ *     label : "The label in the menu",
+ *     /**
+ *      * Optional initialization function called when the 
+ *      * button is created. If the function returns boolean false,
+ *      * the button is not attached to the menu.
+ *      * The menu button, the testData map and the name of test are 
+ *      * passed as arguments. 
+ *      * /
+ *     init : function( button, testData, testName ){
+ *      // "this" refers to the main application
+ *      return true;
+ *     },
+ *     /**
+ *      * Optional function called when the menu button is executed.
+ *      * If the function returns boolean false, the request is not
+ *      * send. The testData map and the name of test are 
+ *      * passed as arguments.
+ *      * /
+ *     execute : function( testData, testName )
+ *     {
+ *        // "this" refers to the main application
+ *        return true;
+ *     },
+ *     /**
+ *      * The request model data. Each value overrides an existing
+ *      * property. Other properties are kept.
+ *      * / 
+ *     requestData : {
+ *       url     : "http://", 
+ *       service : "my.service.name",
+ *       method : "myMethodName",
+ *       params : ["arg1", "arg2", [ "arg", 3] ]
+ *     },
+ *     /**
+ *      * Optional callback function called after the result of the
+ *      * request has returned from the server.
+ *      * /
+ *     callback : function( response ){
+ *       this.getActiveConsole().getRequestModel().setServerData(
+ *        // do something with the response, for example, change
+ *        // the server data like here
+ *        // "this" refers to the main application
+ *       );
+ *     }
+ *   },
+ *   
+ *   testName2 : {
+ *    ...
+ *   },
+ *   
+ *   testName3 : {
+ *    ...
+ *   }
+ *   ...
+ * }
+ * </pre>
+ */
+qx.core.Init.getApplication().setTestData(
+{  
+  
+   /**
+   * Call echo service
+   * @type 
+   */
+  qooxdooTestEcho : {
+    label       : "qooxdoo.test.echo - Send a simple 'hello world' message to be echoed by the server.",
+    init        : function(button,testData,testName)
+    {
+      var data = testData[testName];
+      this.info( "Intializing " + testName + ", calling " + data.requestData.service );
+    },
+    requestData : {
+      service : "qooxdoo.test",
+      method : "echo",
+      params : ["Hello World!"],
+      serverData : {
+        authentication : {
+          username : "Foo",
+          password : "Bar"
+        }
+      }
+    }
+  },   
+  
+  /**
+   * Use the trunk server
+   */
+  useTrunk : {
+    label : "Use RpcPhp trunk server ",
+    requestData : {
+      url : "../../../../../RpcPhp/trunk/services/index.php"
+    }
+  },
+  
+  /**
+   * Use the stable server
+   * @type 
+   */
+  useStable : {
+    label : "Use RpcPhp stable (1.0.1) server ",
+    requestData : {
+      url : "../../../../../RpcPhp/1.0.1/services/index.php"
+    }
+  },  
+  
+  /**
+   * Demo showing how to connect several test data items.
+   * @type 
+   */
+  combineJobs :
+  {
+    label : "Test Combined Jobs",  
+    execute: function( testData, testName)
+    {
+      this.info("First call.")
+      this.getActiveConsole().sendRequest( testData.qooxdooTestEcho.requestData, function(result)
+      {
+        this.info("Second call.")
+        this.getActiveConsole().sendRequest( testData.qooxdooTestEcho.requestData, function(result)
+        {
+          this.info("Test completed.");
+        }, this);
+      },this);
+    }
+  },  
+  
+  /**
+   * load test data from file
+   */
+  loadTestData : {
+    label : "Load test data from file",
+    init : function(){
+      this.info("Test data ready.")
+    },
+    execute : function(){
+      var testDataUrl = prompt("Enter Test Data URL:");
+      if ( ! testDataUrl ) return;
+      try 
+      {
+        qx.util.Validate.url()(testDataUrl); 
+        this.loadTestData( testDataUrl );  
+      }
+      catch(e)
+      {
+        alert("Please enter a valid URL");
+      }
+    }
+  }
+});
--- qooxdoo-contrib/RpcConsole/trunk/readme.txt
+++ qooxdoo-contrib/RpcConsole/trunk/readme.txt
@@ -1,9 +1,101 @@
-Your qooxdoo Contribution
-=========================
+RpcConsole
+==========
 
-This is a qooxdoo contribution skeleton which is used as a template. The 
-'create-application.py' script (usually under tool/bin/create-application.py)
-will use this and expand it into a self-contained qooxdoo contribution which 
-can then be further extended. Please refer to the script and other documentation
-for further information.
+This contribution is a scriptable test client for your JSON-RPC backend. 
 
+It contains an embeddable widget which allows the user to input request data
+and see the response returned by the client (rpccconsole.RpcConsole), and a
+demo application (rpcconsole.demo.Application), which can be used run tests
+manually or by user-supplied scripts. 
+
+The demo allows to open multiple windows with independent consoles, and to 
+load test data from a given url. Since qx.io2.ScriptLoader is used, you can 
+use the contribution demo app to debug the backend of applications which 
+live both on the same or on a different domain. 
+
+Note: For this to work, this bug needs to be fixed:
+http://bugzilla.qooxdoo.org/show_bug.cgi?id=2978
+
+Example test data is loaded from script/rpcconsole.testData.js, but
+you can override this with adding '?testDataUrl=http://path/to/my/testData.js' 
+to the build or source URL of this app. To change the default server url,
+add '?serverUrl=http://path/to/my/server/'.
+
+The test data file populates the "Tests" menu and  must look like this:
+
+qx.core.Init.getApplication().setTestData(
+{
+  testName : {
+    label : "The label in the menu",
+    /**
+     * Optional initialization function called when the 
+     * button is created. If the function returns boolean false,
+     * the button is not attached to the menu.
+     * The menu button, the testData map and the name of test are 
+     * passed as arguments. 
+    */
+    init : function( button, testData, testName ){
+     // "this" refers to the main application
+     return true;
+    },
+    /**
+     * Optional function called when the menu button is executed.
+     * If the function returns boolean false, the request is not
+     * send. The testData map and the name of test are 
+     * passed as arguments.
+     */
+    execute : function( testData, testName )
+    {
+       // "this" refers to the main application
+       return true;
+    },
+    /**
+     * The request model data. Each value overrides an existing
+     * property. Other properties are kept.
+     */ 
+    requestData : {
+      url     : "http://", 
+      service : "my.service.name",
+      method : "myMethodName",
+      params : ["arg1", "arg2", [ "arg", 3] ]
+    },
+    /**
+     * Optional callback function called after the result of the
+     * request has returned from the server.
+     */
+    callback : function( response ){
+      this.getActiveConsole().getRequestModel().setServerData(
+       // do something with the response, for example, change
+       // the server data like here
+       // "this" refers to the main application
+      );
+    }
+  },
+  
+  testName2 : {
+   ...
+  },
+  
+  testName3 : {
+   ...
+  }
+  ...
+});
+
+As the testData map is passed as an argument to init and execute functions,
+you combine several jobs by the following code
+
+testName1 :
+{
+  label : "Combine Jobs 2 and 3",  
+  execute: function( testData, testName)
+  {
+    this.sendRequest( testData.testName2.requestData, function(result)
+    {
+      this.sendRequest( testData.testName3.requestData, function(result)
+      {
+        this.info("Tests completed.");
+      }, this);
+    },this);
+  }
+}
