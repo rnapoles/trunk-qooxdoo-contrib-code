650e0943f1fbe8a4a0d79ef7afbf791259abcae6
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-10-27 10:04:19 +0000

    [BUG RESOLVED 2981]: blocker not disabling tabbing through the text fields http://bugzilla.qooxdoo.org/show_bug.cgi?id=2981
    Application to reproduce issue.

---
+++ qooxdoo-contrib/Bugs/2981/bug2981/source/class/bug2981/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2981.theme.Theme",
+{
+  meta :
+  {
+    color : bug2981.theme.Color,
+    decoration : bug2981.theme.Decoration,
+    font : bug2981.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug2981.theme.Appearance
+  }
+});
