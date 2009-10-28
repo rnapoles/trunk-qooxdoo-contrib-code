7705fbeba85b8cbe1272987b8728918529246ed6
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-10-28 15:18:01 +0000

    [Bug 2941] Application to reproduce issue.

---
+++ trunk/qooxdoo-contrib/Bugs/2941/bug2941/source/class/bug2941/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2941.theme.Decoration",
+{
+  extend : qx.theme.modern.Decoration,
+
+  decorations :
+  {
+  }
+});
