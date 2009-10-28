23921e21b35e2ed5d3957cf86538b5f1a5d55b7a
derrell <derrell@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-10-28 19:57:54 +0000

    qooxit: save previously unsaved project files

---
+++ trunk/qooxdoo-contrib/qooxit/trunk/source/class/qooxit/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("qooxit.theme.Theme",
+{
+  meta :
+  {
+    color : qooxit.theme.Color,
+    decoration : qooxit.theme.Decoration,
+    font : qooxit.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : qooxit.theme.Appearance
+  }
+});
