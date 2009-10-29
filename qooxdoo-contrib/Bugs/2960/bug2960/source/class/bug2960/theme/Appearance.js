bc5cb8d53f937011cc47305ebeb596d20f61386f
aback <aback@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-10-29 16:15:53 +0000

    Added sample applications for bugs #2921 and #2960

---
+++ qooxdoo-contrib/Bugs/2960/bug2960/source/class/bug2960/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2960.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
