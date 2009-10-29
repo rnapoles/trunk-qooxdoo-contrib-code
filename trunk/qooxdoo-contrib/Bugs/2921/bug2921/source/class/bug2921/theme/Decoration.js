85431581642c81d686d7e4347fd3ffe3780cd659
aback <aback@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-10-29 16:15:53 +0000

    Added sample applications for bugs #2921 and #2960

---
+++ trunk/qooxdoo-contrib/Bugs/2921/bug2921/source/class/bug2921/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2921.theme.Decoration",
+{
+  extend : qx.theme.modern.Decoration,
+
+  decorations :
+  {
+  }
+});
---
+++ trunk/qooxdoo-contrib/Bugs/2960/bug2960/source/class/bug2960/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2960.theme.Decoration",
+{
+  extend : qx.theme.modern.Decoration,
+
+  decorations :
+  {
+  }
+});
