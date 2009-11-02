e451bc077757b2c4c7bdd5640fcb8fa9f1371d36
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-11-02 00:18:55 +0000

    Cleaned up interfaces

--- trunk/qooxdoo-contrib/qcl/trunk/backend/php/class/qcl/data/model/ITreeNodeModel.php
+++ trunk/qooxdoo-contrib/qcl/trunk/backend/php/class/qcl/data/model/ITreeNodeModel.php
@@ -25,38 +25,43 @@ interface qcl_data_model_ITreeNodeModel
 {
 
   /**
+   * Returns the id of the parent node.
+   * @return int
+   */
+  function getParentId();
+
+  /**
    * Returns the data of child nodes of a branch ordered by the order field
-   * @param int $parentId
+   *
    * @param string|null $orderBy Optional propert name by which the returned
    *   data should be ordered
    * @return array
    */
-  function getChildren ( $parentId, $orderBy=null );
+  function getChildren ( $orderBy=null );
 
   /**
    * Returns the ids of the child node ids optionally ordered by a property
-   * @param int $parentId
+   *
    * @param string|null $orderBy Optional propert name by which the returned
    *   data should be ordered
    * @return array
    */
-  function getChildIds ( $parentId, $orderBy=null );
+  function getChildIds ( $orderBy=null );
 
   /**
    * Returns the number of children of the given node
-   * @param int $parentId
    * @return int
    */
-  function getChildCount ( $parentId );
+  function getChildCount ();
 
   /**
    * Reorders the position of the child node. If the tree data in the
    * model does not support reordering, implement as empty stub.
-   * @param int $parentId parent folder id
+   *
    * @param string|null $orderBy defaults to position column
    * @return void
    */
-  function reorder ( $parentId, $orderBy=null );
+  function reorder ( $orderBy=null );
 
   /**
    * Whether tree model supports positioning
@@ -67,39 +72,34 @@ interface qcl_data_model_ITreeNodeModel
    /**
     * Change position the absolute position of the node among
     *   the node siblings
-    * @param int $nodeId
-    * @param int $parentId
-    * @param int $position  New position
+    *
     * @return void
     */
-  function changePosition ( $nodeId, $parentId, $position );
+  function changePosition ( $position );
 
    /**
     * Change parent node
-    * @param int $nodeId  Node id
     * @param int $parentId  New parent node id
     * @return int Old parent id
     */
-  function changeParent( $nodeId, $parentId );
+  function changeParent( $parentId );
 
   /**
    * Returns the path of a node in the folder hierarchy as a
-   * string of the node labels.
-   * separated by the a given character
-   * @param int $nodeId
-   * @param string $separator
+   * string of the node labels, separated by the a given character
+   *
+   * @param string $separator Separator character, defaults to "/"
    * @return string
    */
-  function getLabelPath( $nodeId, $separator );
+  function getLabelPath( $separator="/" );
 
   /**
    * Returns the path of a node in the folder hierarchy,
-   * as an array of ids
-   * @param int $nodeId
-   * @param string $separator
+   * as an array of ids.
+   *
    * @return string
    */
-  function getIdPath( $nodeId );
+  function getIdPath();
 
   /**
    * Returns the id of a node given its label path
