f76d044cf540522ff26bffa1cd2c2f7d4d0b2107
aback <aback@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-11-04 14:57:40 +0000

    Added sample application for Bug #2756

---
+++ trunk/qooxdoo-contrib/Bugs/2756/bug2756/source/class/bug2756/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2756.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
