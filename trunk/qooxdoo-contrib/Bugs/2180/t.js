caca574d8ae8d6bb02534daf1de102e6d56f1dab
thron7 <thron7@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-11-11 16:44:20 +0000

    [BUG #2180] add test

---
+++ trunk/qooxdoo-contrib/Bugs/2180/t.js
@@ -0,0 +1,4 @@
+if(!dojo.date.stamp._isoRegExp){
+    dojo.date.stamp._isoRegExp =
+      /^(?:(\d{4})(?:-(\d{2})(?:-(\d{2}))?)?)?(?:T(\d{2}):(\d{2})(?::(\d{2})(.\d+)?)?((?:[+-](\d{2}):(\d{2}))|Z)?)?$/;
+  }
