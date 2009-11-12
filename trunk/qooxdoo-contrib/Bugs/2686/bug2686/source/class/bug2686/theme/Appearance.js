2fa0f356b14534bc95c0e6ea727a0165d7f34456
jonathan_rass <jonathan_rass@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-11-12 10:29:36 +0000

    [BUG #2686 ASSIGNED]: The disappear event never gets fired http://bugzilla.qooxdoo.org/show_bug.cgi?id=2686
    Initial commit.

---
+++ trunk/qooxdoo-contrib/Bugs/2686/bug2686/source/class/bug2686/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug2686.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
