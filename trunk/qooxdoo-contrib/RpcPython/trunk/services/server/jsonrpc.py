58caf9b4549028cde3445c4e2cf1d5600597cb1d
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-11-22 00:40:46 +0000

    Use already instantiated and persisted service class instead of instantiating a new one on each request.

--- trunk/qooxdoo-contrib/RpcPython/trunk/services/server/jsonrpc.py
+++ trunk/qooxdoo-contrib/RpcPython/trunk/services/server/jsonrpc.py
@@ -42,6 +42,9 @@ from qxjsonrpc._error import *
 
 class JsonRpcServer(qxjsonrpc.http.HTTPServer):
     '''HTTP JSON-RPC server backend for qooxdoo and other json-rpc clients'''
+    
+    serviceObjects = {}
+    
     def __init__(self, host='127.0.0.1', port=8000, debug=True):
         qxjsonrpc.http.HTTPServer.__init__(self, host, port, debug=debug)
         
@@ -60,16 +63,21 @@ class JsonRpcServer(qxjsonrpc.http.HTTPServer):
         # Add the capitalized part to the class name 
         name = name + "." + lastPart.capitalize()
         
-        # Return an instance of the loaded class
-        return self._get_class(name)()
+        # Create an instance of the class if not already created
+        try: 
+            serviceObject = self.serviceObjects[name]
+        except KeyError:
+            serviceObject = self._get_class(name)() 
+            self.serviceObjects[name] = serviceObject
+            
+        # return the instance
+        return serviceObject
     
     # the following is adapted from http://code.activestate.com/recipes/223972/
     def _get_mod(self, modulePath):
         """Import a module programmatically"""
         try:
             aMod = sys.modules[modulePath]
-            if aMod is None:
-                raise KeyError
         except KeyError:
             # The last [''] is very important!
             try:
