a2f7a010a4a908818abf588aeb084528e81c917a
thron7 <thron7@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-11-23 16:14:32 +0000

    [BUG #2178] updated test

--- qooxdoo-contrib/Bugs/2178/t.js
+++ qooxdoo-contrib/Bugs/2178/t.js
@@ -1,3 +1,8 @@
 dojo._disconnect = function(obj, event, handle, listener){
 		([dojo._listener, del, node_listener][listener]).remove(obj, event, handle);
 	}
+
+[0, 0].sort(function(){
+	baseHasDuplicate = false;
+	return 0;
+});
