ad4e97a017bfe6b3e816f968115f05c02990fb96
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-11-24 08:45:41 +0000

    [BUG ASSIGNED 3037]: shortcut command tip not shown in case of RadioButtons and CheckBoxes http://bugzilla.qooxdoo.org/show_bug.cgi?id=3037
    updeted application

--- qooxdoo-contrib/Bugs/3037/bug3037/source/class/bug3037/Application.js
+++ qooxdoo-contrib/Bugs/3037/bug3037/source/class/bug3037/Application.js
@@ -45,42 +45,51 @@ qx.Class.define("bug3037.Application",
         qx.log.appender.Console;
       }
 
-      // Edit menu
-      var editMenu = new qx.ui.menu.Menu();
-      var edit = new qx.ui.menubar.Button("Edit", null, editMenu);
-      var copyCommand = new qx.ui.core.Command("Alt+1");
-      var pasteCommand = new qx.ui.core.Command("Alt+2");
-      copyCommand.addListener("execute", this.onExecute, this);
-      pasteCommand.addListener("execute", this.onExecute, this);
-
-      var copyButton = new qx.ui.menu.Button("copy",null,copyCommand);
-      var pasteButton = new qx.ui.menu.Button("paste",null,pasteCommand);
-      editMenu.add(copyButton);
-      editMenu.add(pasteButton);
-      
-      // View menu
-      var viewMenu = new qx.ui.menu.Menu();
-      var view = new qx.ui.menubar.Button("View", null, viewMenu);
-      var viewFixedCommand = new qx.ui.core.Command("Alt+3");
-      var viewFluidCommand = new qx.ui.core.Command("Alt+4");
-      viewFixedCommand.addListener("execute", this.onExecute, this);
-      viewFluidCommand.addListener("execute", this.onExecute, this);
-      
-      var fixedButton = new qx.ui.menu.RadioButton("fixed");
-      var fluidButton = new qx.ui.menu.RadioButton("fluid");
-      fixedButton.setCommand(viewFixedCommand);
-      fluidButton.setCommand(viewFluidCommand);
-      viewMenu.add(fixedButton);
-      viewMenu.add(fluidButton);
-
-      new qx.ui.form.RadioGroup(fixedButton, fluidButton);
+      // Commands
+      var command1 = new qx.ui.core.Command("Alt+1");
+      var command2 = new qx.ui.core.Command("Alt+2");
+      var command3 = new qx.ui.core.Command("Alt+3");
+      var command4 = new qx.ui.core.Command("Alt+4");
+      var command5 = new qx.ui.core.Command("Alt+5");
+      var command6 = new qx.ui.core.Command("Alt+6");
+      command1.addListener("execute", this.onExecute, this);
+      command2.addListener("execute", this.onExecute, this);
+      command3.addListener("execute", this.onExecute, this);
+      command4.addListener("execute", this.onExecute, this);
+      command5.addListener("execute", this.onExecute, this);
+      command6.addListener("execute", this.onExecute, this);
       
       // Menu
+      var menu = new qx.ui.menu.Menu();
+      var edit = new qx.ui.menubar.Button("Edit", null, menu);
+
+      // Menu items
+      var button1 = new qx.ui.menu.Button("copy", null);
+      var button2 = new qx.ui.menu.Button("paste", null);
+      var radioButton1 = new qx.ui.menu.RadioButton("fixed");
+      var radioButton2 = new qx.ui.menu.RadioButton("fluid");
+      var checkBox1 = new qx.ui.menu.CheckBox("check1");
+      var checkBox2 = new qx.ui.menu.CheckBox("check2");
+      button1.setCommand(command1);
+      button2.setCommand(command2);
+      radioButton1.setCommand(command3);
+      radioButton2.setCommand(command4);
+      checkBox1.setCommand(command5);
+      checkBox2.setCommand(command6);
+      menu.add(button1);
+      menu.add(button2);
+      menu.add(radioButton1);
+      menu.add(radioButton2);
+      menu.add(checkBox1);
+      menu.add(checkBox2);
+
+      new qx.ui.form.RadioGroup(radioButton1, radioButton2);
+      
+      // MenuBar
       var menubar = new qx.ui.menubar.MenuBar;
       menubar.setPaddingRight(10);
       menubar.add(edit);
-      menubar.add(view);
-      this.getRoot().add(menubar, {top:10, left: 10});
+      this.getRoot().add(menubar, {top:10, left: 10, right: 10});
     },
     
     onExecute : function(e) {
