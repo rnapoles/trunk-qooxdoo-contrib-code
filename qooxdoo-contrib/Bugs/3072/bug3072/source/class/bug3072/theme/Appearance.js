496f48432207ab7eb7d0395dd4c036fd115fa5df
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-11-24 13:37:16 +0000

    [BUG RESOLVED 3072]: Popups broken on IE 7 if the root Qx element has text-align: center property http://bugzilla.qooxdoo.org/show_bug.cgi?id=3072
    - Application to reproduce issue.

---
+++ qooxdoo-contrib/Bugs/3072/bug3072/source/class/bug3072/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug3072.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
