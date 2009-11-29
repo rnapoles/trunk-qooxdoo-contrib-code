0c4c46cbedcf2a094c81f97e7cca4d6ef21c4847
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-11-29 20:25:39 +0000

    First release

---
+++ qooxdoo-contrib/RpcConsole/0.1/demo/default/source/class/rpcconsole/demo/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("rpcconsole.demo.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
---
+++ qooxdoo-contrib/RpcConsole/0.1/demo/default/source/class/rpcconsole/demo/theme/Color.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("rpcconsole.demo.theme.Color",
+{
+  extend : qx.theme.modern.Color,
+
+  colors :
+  {
+  }
+});
