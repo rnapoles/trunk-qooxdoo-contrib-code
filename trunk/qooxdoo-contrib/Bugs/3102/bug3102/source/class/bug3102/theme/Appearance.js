6e68b9b5b099adcbb03336d9cb0dea75669f8faa
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-12-02 10:07:23 +0000

    [Bug #3102]
    - Application to reproduce issue.

---
+++ trunk/qooxdoo-contrib/Bugs/3102/bug3102/source/class/bug3102/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug3102.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
