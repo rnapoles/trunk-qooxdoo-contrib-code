e99ea6129b22bef578c341c60af763913c8ba670
thron7 <thron7@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-12-04 09:25:13 +0000

    [BUG #1589] test application

---
+++ trunk/qooxdoo-contrib/Bugs/1589/höräßlich/source/class/höräßlich/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("höräßlich.theme.Decoration",
+{
+  extend : qx.theme.modern.Decoration,
+
+  decorations :
+  {
+  }
+});
---
+++ trunk/qooxdoo-contrib/Bugs/1589/höräßlich/source/class/höräßlich/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("höräßlich.theme.Theme",
+{
+  meta :
+  {
+    color : höräßlich.theme.Color,
+    decoration : höräßlich.theme.Decoration,
+    font : höräßlich.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : höräßlich.theme.Appearance
+  }
+});
