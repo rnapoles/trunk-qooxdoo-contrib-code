b91d839d8d23f16c81deeb3f0a4947b4867e5778
thron7 <thron7@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-12-04 09:25:13 +0000

    [BUG #1589] test application

---
+++ qooxdoo-contrib/Bugs/1589/höräßlich/source/class/höräßlich/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("höräßlich.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
