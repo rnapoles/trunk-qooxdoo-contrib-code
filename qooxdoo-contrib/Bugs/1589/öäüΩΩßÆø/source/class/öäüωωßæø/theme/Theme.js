381f33892e0566248c65b3f1aa496d4a9bb27684
thron7 <thron7@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-12-04 09:56:26 +0000

    [BUG #1589] another test app - shows issues on OSX

---
+++ qooxdoo-contrib/Bugs/1589/öäüΩΩßÆø/source/class/öäüωωßæø/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("öäüωωßæø.theme.Theme",
+{
+  meta :
+  {
+    color : öäüωωßæø.theme.Color,
+    decoration : öäüωωßæø.theme.Decoration,
+    font : öäüωωßæø.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : öäüωωßæø.theme.Appearance
+  }
+});
