2ea9150aec235fd23e89130f60600b8a639f6829
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-12-09 11:04:00 +0000

    [BUG #3162 ASSIGNED]: Blocker linked to a qx.ui.menu.Menu() has a size of (0,0) when rendering a toolbar in an inline application http://bugzilla.qooxdoo.org/show_bug.cgi?id=3162
    - Application to reproduce issue

---
+++ trunk/qooxdoo-contrib/Bugs/3162/bug3162/source/class/bug3162/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug3162.theme.Decoration",
+{
+  extend : qx.theme.modern.Decoration,
+
+  decorations :
+  {
+  }
+});
