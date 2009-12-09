9bfbfb2f140ec819aaf88414e343b89b491cb5bf
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-12-09 10:13:40 +0000

    [BUG #3196 FIXED]: vertical separator not being displayed in 20972 http://bugzilla.qooxdoo.org/show_bug.cgi?id=3196
    - Application to reproduce issue

---
+++ qooxdoo-contrib/Bugs/3196/bug3196/source/class/bug3196/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug3196.theme.Theme",
+{
+  meta :
+  {
+    color : bug3196.theme.Color,
+    decoration : bug3196.theme.Decoration,
+    font : bug3196.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug3196.theme.Appearance
+  }
+});
