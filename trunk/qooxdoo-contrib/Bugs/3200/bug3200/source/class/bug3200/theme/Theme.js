67d688ee4bfaaf1ea476ee4e0ef8ade6e27ad36e
d_wagner <d_wagner@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-12-09 09:09:57 +0000

    Application to reproduce bug #3200

---
+++ trunk/qooxdoo-contrib/Bugs/3200/bug3200/source/class/bug3200/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug3200.theme.Theme",
+{
+  meta :
+  {
+    color : bug3200.theme.Color,
+    decoration : bug3200.theme.Decoration,
+    font : bug3200.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug3200.theme.Appearance
+  }
+});
