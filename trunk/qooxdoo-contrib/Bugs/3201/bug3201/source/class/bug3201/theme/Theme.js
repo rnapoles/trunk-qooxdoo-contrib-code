2b6131b5001c7d519089d40f393327038dd5d34f
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-12-10 08:38:28 +0000

    [BUG #3201 ASSIGNED]: Keypress event doesn't trigger listener for escape key http://bugzilla.qooxdoo.org/show_bug.cgi?id=3201
    - Application to reproduce issue

---
+++ trunk/qooxdoo-contrib/Bugs/3201/bug3201/source/class/bug3201/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug3201.theme.Theme",
+{
+  meta :
+  {
+    color : bug3201.theme.Color,
+    decoration : bug3201.theme.Decoration,
+    font : bug3201.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug3201.theme.Appearance
+  }
+});
