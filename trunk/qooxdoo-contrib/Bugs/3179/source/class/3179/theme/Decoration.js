43d3e57c6ef9c9719181f55643effcef370a03ec
fjakobs <fjakobs@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2009-12-11 15:55:30 +0000

    [Bug #3179] add example app

---
+++ trunk/qooxdoo-contrib/Bugs/3179/source/class/3179/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("3179.theme.Decoration",
+{
+  extend : qx.theme.modern.Decoration,
+
+  decorations :
+  {
+  }
+});
