59d502d0dbc15ff4fb47d2e67c8ecc0115b7b70e
jonathan_rass <jonathan_rass@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-01-11 10:57:42 +0000

    [BUG #3302 ASSIGNED]: qx.application.Standalone.close() method does not work with Opera http://bugzilla.qooxdoo.org/show_bug.cgi?id=3302
    Demo for this bug.

---
+++ qooxdoo-contrib/Bugs/3302/bug3302/source/class/bug3302/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug3302.theme.Decoration",
+{
+  extend : qx.theme.modern.Decoration,
+
+  decorations :
+  {
+  }
+});
