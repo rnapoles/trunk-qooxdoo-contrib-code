bceca92eba7da21662f8c34692e815305e3f5a06
jonathan_rass <jonathan_rass@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-01-18 09:54:00 +0000

    [BUG #3283 ASSIGNED]: Horizontal scrolling not working using mouse wheel http://bugzilla.qooxdoo.org/show_bug.cgi?id=3283
    Demo for this issue

---
+++ trunk/qooxdoo-contrib/Bugs/3283/bug3283/source/class/bug3283/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug3283.theme.Theme",
+{
+  meta :
+  {
+    color : bug3283.theme.Color,
+    decoration : bug3283.theme.Decoration,
+    font : bug3283.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug3283.theme.Appearance
+  }
+});
