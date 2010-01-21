3d5c3ed90b7aa96a2c59c1ffd5ac85abc1a74923
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-01-21 15:38:04 +0000

    [BUG #3331 ASSIGNED]: Using qx.ui.core.Blocker for the second time is blocking browser http://bugzilla.qooxdoo.org/show_bug.cgi?id=3331
    - Application to reproduce issue

---
+++ trunk/qooxdoo-contrib/Bugs/3331/bug3331/source/class/bug3331/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug3331.theme.Theme",
+{
+  meta :
+  {
+    color : bug3331.theme.Color,
+    decoration : bug3331.theme.Decoration,
+    font : bug3331.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug3331.theme.Appearance
+  }
+});
