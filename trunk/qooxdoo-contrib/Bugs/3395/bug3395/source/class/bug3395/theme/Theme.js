1760244797ee33bea95f034b16969438e2b25f9e
jonathan_rass <jonathan_rass@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-02-01 15:27:13 +0000

    [BUG #3395 ASSIGNED]: getScrollY() and getScrollX() on qx.ui.core.scroll.AbstractScrollArea return other values than in 1.0 http://bugzilla.qooxdoo.org/show_bug.cgi?id=3395
    Added sample to show/fix bug.

---
+++ trunk/qooxdoo-contrib/Bugs/3395/bug3395/source/class/bug3395/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug3395.theme.Theme",
+{
+  meta :
+  {
+    color : bug3395.theme.Color,
+    decoration : bug3395.theme.Decoration,
+    font : bug3395.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug3395.theme.Appearance
+  }
+});
