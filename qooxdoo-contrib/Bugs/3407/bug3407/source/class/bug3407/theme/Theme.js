01a7ebf47d3d0e2c4a9071bf3a83c0c216b3ea52
aback <aback@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-02-10 14:32:07 +0000

    Added sample application for bug #3407

---
+++ qooxdoo-contrib/Bugs/3407/bug3407/source/class/bug3407/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug3407.theme.Theme",
+{
+  meta :
+  {
+    color : bug3407.theme.Color,
+    decoration : bug3407.theme.Decoration,
+    font : bug3407.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug3407.theme.Appearance
+  }
+});
