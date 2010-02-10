4096aa28c9217af0292b38c463693ec5f3ac99c1
aback <aback@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-02-10 14:32:07 +0000

    Added sample application for bug #3407

---
+++ trunk/qooxdoo-contrib/Bugs/3407/bug3407/source/class/bug3407/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug3407.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
