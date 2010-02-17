784d2ff6670fe8f17ade92589e7dd58c83df71f5
d_wagner <d_wagner@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-02-17 16:25:55 +0000

    Initial check-in of contribDemobrowser

---
+++ qooxdoo-contrib/qooxdoo/contribDemobrowser/source/class/contribdemobrowser/Application.js
@@ -0,0 +1,78 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#asset(contribdemobrowser/*)
+#asset(demobrowser/*)
+
+************************************************************************ */
+
+/**
+ * This is the main application class of your custom application "contribdemobrowser"
+ */
+qx.Class.define("contribdemobrowser.Application",
+{
+  extend : qx.application.Standalone,
+
+  construct : function()
+  {
+    this.base(arguments);
+
+    // Include CSS files
+    qx.bom.Stylesheet.includeFile("demobrowser/css/style.css");
+    qx.bom.Stylesheet.includeFile("demobrowser/css/sourceview.css");
+  },
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    // overridden
+    main : function()
+    {
+      this.base(arguments);
+
+      // Enable logging in source (or debug build)
+      if (qx.core.Variant.isSet("qx.debug", "on"))
+      {
+        qx.log.appender.Native;
+        qx.log.appender.Console;
+      }
+
+      // Initialize the viewer
+      this.viewer = new demobrowser.DemoBrowser;
+      this.getRoot().add(this.viewer, {edge:0});
+    },
+
+    // overridden
+    finalize : function()
+    {
+      this.base(arguments);
+
+      this.viewer.dataLoader("script/demodata.json");
+    }
+  },
+
+
+  /*
+  *****************************************************************************
+     DESTRUCTOR
+  *****************************************************************************
+  */
+
+  destruct : function() {
+    this._disposeObjects("viewer");
+  }
+});
