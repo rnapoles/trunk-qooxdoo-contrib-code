b86d8d1eecd650aa6c9d065b6374a8c0562e660b
aback <aback@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-03-23 13:17:55 +0000

    Added demo application for Bug#3462

---
+++ trunk/qooxdoo-contrib/Bugs/3462/bug3462/source/class/bug3462/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug3462.theme.Theme",
+{
+  meta :
+  {
+    color : bug3462.theme.Color,
+    decoration : bug3462.theme.Decoration,
+    font : bug3462.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug3462.theme.Appearance
+  }
+});
