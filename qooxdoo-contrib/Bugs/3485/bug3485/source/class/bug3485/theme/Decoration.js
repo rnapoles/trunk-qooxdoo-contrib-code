7418ef9740438c3acd3b90196b584669489816a3
jonathan_rass <jonathan_rass@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-04-12 12:32:41 +0000

    [BUG #3485 ASSIGNED]: The popup placement computation does not take into account the shadow decorator dimensions http://bugzilla.qooxdoo.org/show_bug.cgi?id=3485
    Forgot to add source.

---
+++ qooxdoo-contrib/Bugs/3485/bug3485/source/class/bug3485/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug3485.theme.Decoration",
+{
+  extend : qx.theme.modern.Decoration,
+
+  decorations :
+  {
+  }
+});
---
+++ qooxdoo-contrib/Bugs/3485/bug3485/source/class/bug3485/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug3485.theme.Theme",
+{
+  meta :
+  {
+    color : bug3485.theme.Color,
+    decoration : bug3485.theme.Decoration,
+    font : bug3485.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug3485.theme.Appearance
+  }
+});
