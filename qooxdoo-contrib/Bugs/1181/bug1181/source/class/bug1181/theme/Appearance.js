be46a037874e2db2d4f88e2ce3f33496bf6a951c
jonathan_rass <jonathan_rass@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-04-12 12:34:14 +0000

    [BUG #1181 NEW]: Locale changing leads to IE problem that is related to displaying of previously invisible widgets http://bugzilla.qooxdoo.org/show_bug.cgi?id=1181
    Sample for this issue. Use this source with legacy_0.7.x when working on this bug.

---
+++ qooxdoo-contrib/Bugs/1181/bug1181/source/class/bug1181/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug1181.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
