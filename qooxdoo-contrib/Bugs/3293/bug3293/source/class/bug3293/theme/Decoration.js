36d7e644dd604522d9cf7f9c708e55ac124aa495
d_wagner <d_wagner@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-04-12 09:31:39 +0000

    Application to reproduce bug #3293

---
+++ qooxdoo-contrib/Bugs/3293/bug3293/source/class/bug3293/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug3293.theme.Decoration",
+{
+  extend : qx.theme.modern.Decoration,
+
+  decorations :
+  {
+  }
+});
