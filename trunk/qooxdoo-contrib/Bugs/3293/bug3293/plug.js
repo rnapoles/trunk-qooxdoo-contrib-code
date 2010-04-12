f6d83384db826381caae95f5198ae72ceb8cb619
d_wagner <d_wagner@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-04-12 09:31:39 +0000

    Application to reproduce bug #3293

---
+++ trunk/qooxdoo-contrib/Bugs/3293/bug3293/plug.js
@@ -0,0 +1,1 @@
+alert("decode-uris-plug");
---
+++ trunk/qooxdoo-contrib/Bugs/3293/bug3293/source/class/bug3293/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug3293.theme.Theme",
+{
+  meta :
+  {
+    color : bug3293.theme.Color,
+    decoration : bug3293.theme.Decoration,
+    font : bug3293.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug3293.theme.Appearance
+  }
+});
