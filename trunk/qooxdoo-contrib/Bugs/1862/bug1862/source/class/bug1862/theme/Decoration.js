d52ecc53c5257f6ceda4281f3e9fe82965420829
jonathan_rass <jonathan_rass@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-04-12 12:50:34 +0000

    [BUG #1862 NEW]: [ScrollBar] value not updated when size changes in IE http://bugzilla.qooxdoo.org/show_bug.cgi?id=1862
    Sample for this issue. Use this source with legacy_0.7.x when working on this bug.

---
+++ trunk/qooxdoo-contrib/Bugs/1862/bug1862/source/class/bug1862/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug1862.theme.Decoration",
+{
+  extend : qx.theme.modern.Decoration,
+
+  decorations :
+  {
+  }
+});
