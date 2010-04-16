c124a12b153396048d18dbd97914ef771df93690
jonathan_rass <jonathan_rass@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-04-16 12:59:09 +0000

    [BUG #3089 ASSIGNED]: table cell editor is not deactivated by clicking in other meta column http://bugzilla.qooxdoo.org/show_bug.cgi?id=3089
    Sample for this issue.

---
+++ trunk/qooxdoo-contrib/Bugs/3089/bug3089/source/class/bug3089/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug3089.theme.Decoration",
+{
+  extend : qx.theme.modern.Decoration,
+
+  decorations :
+  {
+  }
+});
