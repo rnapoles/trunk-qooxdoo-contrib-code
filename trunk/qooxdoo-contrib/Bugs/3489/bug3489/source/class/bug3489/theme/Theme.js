347bb8ae1b14c1f1593712638cc330409906d860
jonathan_rass <jonathan_rass@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-04-19 12:38:53 +0000

    [BUG #3489 ASSIGNED]: Table doesn't swallow Enter key http://bugzilla.qooxdoo.org/show_bug.cgi?id=3489
    Sample for this issue.

---
+++ trunk/qooxdoo-contrib/Bugs/3489/bug3489/source/class/bug3489/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug3489.theme.Theme",
+{
+  meta :
+  {
+    color : bug3489.theme.Color,
+    decoration : bug3489.theme.Decoration,
+    font : bug3489.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug3489.theme.Appearance
+  }
+});
