df4fa110041c0ae0e7e23109822379cf8b5a5297
jonathan_rass <jonathan_rass@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-04-20 09:10:56 +0000

    [BUG #3504 ASSIGNED]: Command with meta modifier fires event only once http://bugzilla.qooxdoo.org/show_bug.cgi?id=3504
    Sample for this issue.

---
+++ trunk/qooxdoo-contrib/Bugs/3504/bug3504/source/class/bug3504/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug3504.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
---
+++ trunk/qooxdoo-contrib/Bugs/3504/bug3504/source/class/bug3504/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug3504.theme.Theme",
+{
+  meta :
+  {
+    color : bug3504.theme.Color,
+    decoration : bug3504.theme.Decoration,
+    font : bug3504.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug3504.theme.Appearance
+  }
+});
