4635b8c9d1173136e2cae2282b6a67c6f8d01f15
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-04-24 14:55:55 +0000

    removed batch file that doesn't belong into the repository..

--- qooxdoo-contrib/qcl/trunk/sync.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-#./generate.py api
-#rsync -aC -e ssh api cboulanger@frs.sourceforge.net:userweb/htdocs/qooxdoo-contrib/qcl/trunk
-
-cd demo/access
-#./generate.py build
-rsync -aC -e ssh --exclude=config.php build services contrib cboulanger@frs.sourceforge.net:userweb/htdocs/qooxdoo-contrib/qcl/trunk/demo/access
-
