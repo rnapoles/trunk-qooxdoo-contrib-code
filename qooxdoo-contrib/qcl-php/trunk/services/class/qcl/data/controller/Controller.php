bb1894b0775a4e8b41f975785699198058e256c4
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-04-26 21:52:55 +0000

    Various fixes, particularly bugs in TreeController

--- qooxdoo-contrib/qcl-php/trunk/services/class/qcl/data/controller/Controller.php
+++ qooxdoo-contrib/qcl-php/trunk/services/class/qcl/data/controller/Controller.php
@@ -850,6 +850,175 @@ class qcl_data_controller_Controller
     }
   }
 
+  //-------------------------------------------------------------
+  // creating and evaluating form data
+  //-------------------------------------------------------------
+
+  /**
+   * Returns data for a dialog.Form widget based on a model
+   * @param qcl_data_model_AbstractActiveRecord $model
+   * @param int $width The default width of the form in pixel (defaults to 300)
+   * @return unknown_type
+   */
+  protected function createFormData( qcl_data_model_AbstractActiveRecord $model, $width=300 )
+  {
+    $modelFormData = $model->formData();
+
+    if ( ! is_array( $modelFormData) or ! count( $modelFormData ) )
+    {
+      throw new JsonRpcException( "Element $type has no editable properties.");
+    }
+
+    $formData = array();
+
+    foreach( $modelFormData as $name => $elementData )
+    {
+
+      /*
+       * check property data
+       */
+      qcl_assert_valid_string( $elementData['label'] );
+
+      /*
+       * type
+       */
+      if ( ! isset( $elementData['type'] ) )
+      {
+        $elementData['type']  = "TextField";
+      }
+
+      /*
+       * width
+       */
+      if ( ! isset( $elementData['width'] ) )
+      {
+        $elementData['width'] = $width;
+      }
+
+      /*
+       * get value from model or default value
+       */
+      if ( ! isset( $elementData['value'] ) )
+      {
+         $elementData['value'] = $model->get( $name );
+      }
+      if ( isset( $elementData['default'] ) )
+      {
+        if ( ! $elementData['value'] )
+        {
+          $elementData['value'] = $elementData['default'];
+        }
+        unset( $elementData['default'] );
+      }
+
+      /*
+       * marshal value
+       */
+      if ( isset( $elementData['marshaler'] ) )
+      {
+        if ( isset( $elementData['marshaler']['marshal'] ) )
+        {
+          $marshaler = $elementData['marshaler']['marshal'];
+          if( isset( $marshaler['function'] ) )
+          {
+            $this->debug(" ",__CLASS__,__LINE__);
+            $elementData['value'] = $marshaler['function']( $elementData['value'] );
+          }
+          elseif( isset( $marshaler['callback'] ) )
+          {
+            $callback = $marshaler['callback'];
+            qcl_assert_array( $callback );
+            if ( $callback[0] == "this" )
+            {
+              $callback[0] = $model;
+            }
+            qcl_assert_method_exists( $callback[0], $callback[1] );
+            $elementData['value'] = $callback[0]->$callback[1]( $elementData['value'] );
+          }
+          else
+          {
+            throw new InvalidArgumentException("Invalid marshalling data");
+          }
+        }
+        unset( $elementData['marshaler'] );
+      }
+      $formData[ $name ] = $elementData;
+    }
+    return $formData;
+  }
+
+  /**
+   * Parses data returned by  dialog.Form widget based on a model
+   * @param qcl_data_model_AbstractActiveRecord $model
+   * @param object $data;
+   * @return unknown_type
+   */
+  protected function parseFormData( qcl_data_model_AbstractActiveRecord $model, $data )
+  {
+    $data = object2array( $data ) ;
+    $modelFormData = $model->formData();
+
+    if ( ! is_array( $modelFormData) or ! count( $modelFormData ) )
+    {
+      throw new JsonRpcException( "Element $type has no editable properties.");
+    }
+    foreach( $data as $property => $value )
+    {
+      /*
+       * is it an editable property?
+       */
+      if (! isset( $modelFormData[$property] ) )
+      {
+        throw new InvalidJsonRpcArgumentException( "Invalid form data property");
+      }
+
+      /*
+       * should I ignore it?
+       */
+      if ( isset( $modelFormData[$property]['ignore'] ) and $modelFormData[$property]['ignore'] === true )
+      {
+        unset( $data[$property] );
+        continue;
+      }
+
+      /*
+       * marshaler
+       */
+      if ( isset( $modelFormData[$property]['marshaler']['unmarshal'] ) )
+      {
+        $marshaler = $modelFormData[$property]['marshaler']['unmarshal'];
+        if( isset( $marshaler['function'] ) )
+        {
+          $value = $marshaler['function']( $value );
+        }
+        elseif( isset( $marshaler['callback'] ) )
+        {
+          $callback = $marshaler['callback'];
+          qcl_assert_array( $callback );
+          if ( $callback[0] === "this" )
+          {
+            $callback[0] = $model;
+          }
+          qcl_assert_method_exists( $callback[0], $callback[1] );
+          $value = $callback[0]->$callback[1]( $value );
+        }
+        else
+        {
+          throw new InvalidArgumentException("Invalid marshaler data");
+        }
+        $data[$property] = $value;
+      }
+
+      /*
+       * remove null values from data
+       */
+      if ( $value === null )
+      {
+        unset( $data[$property] );
+      }
+    }
+    return $data;
+  }
 
   //-------------------------------------------------------------
   // response data, deprecated methods
