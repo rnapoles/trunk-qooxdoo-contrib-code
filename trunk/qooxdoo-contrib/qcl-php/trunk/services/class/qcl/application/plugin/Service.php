5dab4f15752958cfbc97b5803e58e0d71d1e998f
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-04-29 13:03:54 +0000

    Finished basic implementation of plugin system

--- trunk/qooxdoo-contrib/qcl-php/trunk/services/class/qcl/application/plugin/Service.php
+++ trunk/qooxdoo-contrib/qcl-php/trunk/services/class/qcl/application/plugin/Service.php
@@ -16,7 +16,9 @@
  *  * Christian Boulanger (cboulanger)
  */
 
+qcl_import("qcl_data_controller_Controller");
 qcl_import("qcl_application_plugin_RegistryModel");
+qcl_import("qcl_ui_dialog_Alert");
 
 /**
  * The plugin Service
@@ -36,6 +38,11 @@ class qcl_application_plugin_Service
     $plugin_path = $app->pluginPath();
     $formData = array();
 
+    $registryModel = new qcl_application_plugin_RegistryModel();
+
+    /*
+     * scan plugin directory
+     */
     foreach ( scandir($plugin_path) as $namedId )
     {
       $file = "$plugin_path/$namedId/Plugin.php";
@@ -44,14 +51,27 @@ class qcl_application_plugin_Service
         require_once ( $file );
         $class  = "{$namedId}_Plugin";
         $plugin = new $class();
-        $name        = $plugin->getName();
+        $name   = $plugin->getName();
+
+        if ( ! $registryModel->namedIdExists( $namedId ) )
+        {
+          $options = array(
+            array( 'label'  => $this->tr("Plugin is not installed"), 'value' => "" ),
+            array( 'label'  => $this->tr("Install plugin"), 'value' => "install" )
+          );
+        }
+        else
+        {
+          $options = array(
+            array( 'label'  => $this->tr("Plugin is installed"), 'value' => "" ),
+            array( 'label'  => $this->tr("Uninstall plugin"), 'value' => "uninstall" )
+          );
+        }
 
         $formData[$namedId] = array(
           'type'    => "selectbox",
-          'options' => array(
-            array( 'label'  => $this->tr("Install plugin"), 'value' => "install" ),
-            array( 'label'  => $this->tr("Uninstall plugin"), 'value' => "install" )
-          ),
+          'width'   => 300,
+          'options' => $options,
           'label'   => $name,
           'value'   => $namedId
         );
@@ -64,6 +84,81 @@ class qcl_application_plugin_Service
       $formData, true,
       $this->serviceName(), "handlePluginForm"
     );
+  }
+
+  public function method_handlePluginForm( $data )
+  {
+    if ( $data === null )
+    {
+      return "ABORTED";
+    }
+    $registryModel = new qcl_application_plugin_RegistryModel();
+    $messages = array();
+
+    foreach( $data as $namedId => $action )
+    {
+      $plugin_path = $this->getApplication()->pluginPath();
+      $file = "$plugin_path/$namedId/Plugin.php";
+      require_once ( $file );
+      $class  = "{$namedId}_Plugin";
+      $plugin = new $class();
+      $msg = "";
+
+      switch ( $action )
+      {
+        case "install":
+          $this->getLogger()->log(sprintf(
+            "Installing plugin '%s'", $plugin->getName()
+          ), QCL_LOG_PLUGIN );
+          try
+          {
+            $plugin->install();
+            $registryModel->create( $namedId, array(
+              'name'        => $plugin->getName(),
+              'description' => $plugin->getDescription()
+            ));
+          }
+          catch( qcl_application_plugin_Exception $e )
+          {
+            $msg = sprintf(
+              "Installation of plugin '%s' failed: %s",
+              $plugin->getName(), $e->getMessage()
+            );
+            $this->getLogger()->log($msg, QCL_LOG_PLUGIN );
+          }
+          break;
+
+        case "uninstall":
+          $this->getLogger()->log(sprintf(
+            "Uninstalling plugin '%s'", $plugin->getName()
+          ), QCL_LOG_PLUGIN );
+          try
+          {
+            $plugin->uninstall();
+            $registryModel->load( $namedId );
+            $registryModel->delete();
+          }
+          catch( qcl_application_plugin_Exception $e )
+          {
+            $msg = sprintf(
+              "Uninstallation of plugin '%s' failed: %s",
+              $plugin->getName(), $e->getMessage()
+            );
+            $this->getLogger()->log($msg, QCL_LOG_PLUGIN );
+          }
+          break;
+      }
+
+      if ( $msg)
+      {
+        $messages[] = $msg;
+      }
+    }
+    if ( count( $messages) )
+    {
+      return new qcl_ui_dialog_Alert( implode("<br/>", $messages ) );
+    }
+    return "OK";
   }
 }
 ?>
