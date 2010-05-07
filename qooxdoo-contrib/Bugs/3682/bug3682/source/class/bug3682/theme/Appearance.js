f6ce04d1208bf0920625d90edeb1d0274705be05
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-05-07 11:53:55 +0000

    Added application to reproduce bug report #3682.

---
+++ qooxdoo-contrib/Bugs/3682/bug3682/source/class/bug3682/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug3682.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
