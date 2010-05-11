a41d9983e07cf0a9eb4de72d5e8c98d966bebf90
jonathan_rass <jonathan_rass@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-05-11 13:03:06 +0000

    [BUG #3689 ASSIGNED]: FF3.6 prevents click on native scrollbars http://bugzilla.qooxdoo.org/show_bug.cgi?id=3689
    Sample for this issue.

---
+++ trunk/qooxdoo-contrib/Bugs/3689/bug3689/source/class/bug3689/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug3689.theme.Theme",
+{
+  meta :
+  {
+    color : bug3689.theme.Color,
+    decoration : bug3689.theme.Decoration,
+    font : bug3689.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug3689.theme.Appearance
+  }
+});
