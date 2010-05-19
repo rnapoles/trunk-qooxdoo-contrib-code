4685010a27e16bf53bf5ff15e82e94cbcaf6b546
d_wagner <d_wagner@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-05-19 14:16:04 +0000

    [BUG #3701] Application to reproduce the issue

---
+++ trunk/qooxdoo-contrib/Bugs/3701/bug3701/source/class/bug3701/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug3701.theme.Theme",
+{
+  meta :
+  {
+    color : bug3701.theme.Color,
+    decoration : bug3701.theme.Decoration,
+    font : bug3701.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug3701.theme.Appearance
+  }
+});
