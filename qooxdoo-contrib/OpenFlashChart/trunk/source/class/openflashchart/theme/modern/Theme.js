48c7b7b6028429c1dc9d0dbcd02614175a873bb9
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-05-21 15:30:59 +0000

    Initial commit for the new open flash chart contribution.

---
+++ qooxdoo-contrib/OpenFlashChart/trunk/source/class/openflashchart/theme/modern/Theme.js
@@ -0,0 +1,20 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("openflashchart.theme.modern.Theme",
+{
+  meta :
+  {
+    color : openflashchart.theme.modern.Color,
+    decoration : openflashchart.theme.modern.Decoration,
+    font : openflashchart.theme.modern.Font,
+    appearance : openflashchart.theme.modern.Appearance
+  }
+});
