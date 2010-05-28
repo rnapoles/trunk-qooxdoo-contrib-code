8e264856881f563b6911a133be7e309a37657663
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-05-28 19:06:19 +0000

    Created 0.2 tag

---
+++ trunk/qooxdoo-contrib/RpcConsole/0.2/source/class/rpcconsole/theme/classic/Theme.js
@@ -0,0 +1,20 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("rpcconsole.theme.classic.Theme",
+{
+  meta :
+  {
+    color : rpcconsole.theme.classic.Color,
+    decoration : rpcconsole.theme.classic.Decoration,
+    font : rpcconsole.theme.classic.Font,
+    appearance : rpcconsole.theme.classic.Appearance
+  }
+});
