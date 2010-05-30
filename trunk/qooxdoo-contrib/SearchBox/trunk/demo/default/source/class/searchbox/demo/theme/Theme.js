86ed1b198fd049644b8a2107a05cb09e2029daa9
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-05-30 19:12:54 +0000

    Initial Commit. Not yet functional

---
+++ trunk/qooxdoo-contrib/SearchBox/trunk/demo/default/source/class/searchbox/demo/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("searchbox.demo.theme.Theme",
+{
+  meta :
+  {
+    color : searchbox.demo.theme.Color,
+    decoration : searchbox.demo.theme.Decoration,
+    font : searchbox.demo.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : searchbox.demo.theme.Appearance
+  }
+});
---
+++ trunk/qooxdoo-contrib/SearchBox/trunk/source/class/searchbox/theme/classic/Theme.js
@@ -0,0 +1,20 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("searchbox.theme.classic.Theme",
+{
+  meta :
+  {
+    color : searchbox.theme.classic.Color,
+    decoration : searchbox.theme.classic.Decoration,
+    font : searchbox.theme.classic.Font,
+    appearance : searchbox.theme.classic.Appearance
+  }
+});
