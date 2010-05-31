ad00f1fa36fa1bf03fd41498f1281dad77d59868
jonathan_rass <jonathan_rass@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-05-31 08:39:14 +0000

    [BUG #3733 ASSIGNED]: Table: hover effect for rows http://bugzilla.qooxdoo.org/show_bug.cgi?id=3733
    Sample for this issue.

---
+++ trunk/qooxdoo-contrib/Bugs/3733/bug3733/source/class/bug3733/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("bug3733.theme.Theme",
+{
+  meta :
+  {
+    color : bug3733.theme.Color,
+    decoration : bug3733.theme.Decoration,
+    font : bug3733.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : bug3733.theme.Appearance
+  }
+});
