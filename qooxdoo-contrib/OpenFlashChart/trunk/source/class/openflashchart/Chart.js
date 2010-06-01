e7e8f36d6339d02728921aabfc0236c358bc30ef
chris_schmidt <chris_schmidt@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-06-01 08:03:39 +0000

    Added API documentation.

--- qooxdoo-contrib/OpenFlashChart/trunk/source/class/openflashchart/Chart.js
+++ qooxdoo-contrib/OpenFlashChart/trunk/source/class/openflashchart/Chart.js
@@ -1,15 +1,19 @@
 /* ************************************************************************
 
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
    Copyright:
-     2009 ACME Corporation -or- Your Name, http://www.example.com
-     
+     2010 1&1 Internet AG, Germany, http://www.1und1.de
+
    License:
      LGPL: http://www.gnu.org/licenses/lgpl.html
      EPL: http://www.eclipse.org/org/documents/epl-v10.php
      See the LICENSE file in the project's top-level directory for details.
 
    Authors:
-     * Your Name (username)
+     * Christian Hagendorn (chris_schmidt)
 
 ************************************************************************ */
 
@@ -20,16 +24,22 @@
 
 ************************************************************************ */
 /**
- * This is the main class of contribution "OpenFlashChart"
+ * qooxdoo based wrapper for the Open Flash Chart (OFC) charting solution.
  * 
- * TODO: Replace the sample code of a custom button with the actual code of 
- * your contribution.
+ * The OFC uses a Flash movie to render the chart. The chart is described in 
+ * a JSON file structure, the definition is defined from OFC and could be found
+ * on the <a href="http://teethgrinder.co.uk/open-flash-chart-2/" target="_blank">
+ * project homepage</a>.
  * 
+ * To {@link #load} method can be used to load the JSON structure into the Flash movie. 
  */
 qx.Class.define("openflashchart.Chart",
 {
   extend : qx.ui.embed.Flash,
 
+  /**
+   * Constructs the wrapper class.
+   */
   construct : function()
   {
     this.base(arguments, "openflashchart/open-flash-chart.swf");
@@ -40,6 +50,13 @@ qx.Class.define("openflashchart.Chart",
 
   members :
   {
+    /**
+     * Loads the Flash movie with the passed JSON structure. The chart definition
+     * could be found on the Open Flash chart <a href="http://teethgrinder.co.uk/open-flash-chart-2/" target="_blank">
+     * project homepage</a>.
+     * 
+     * @param data {Object} Object which describes the chart to draw.
+     */
     load : function(data)
     {
       var chart = this.getFlashElement();
@@ -51,6 +68,11 @@ qx.Class.define("openflashchart.Chart",
       }
     },
     
+    /**
+     * Loads the data deferred this is needed, when the movie is not ready at runtime.
+     * 
+     * @param data {Object} Object which describes the chart to draw.
+     */
     __deferredLoad : function(data) {
       qx.event.Timer.once(function() {
         this.load(data);
