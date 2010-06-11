d0a74faaab613ca4791a2ceed86b52d165166d02
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-06-11 15:25:12 +0000

    Re-implemented download server

--- trunk/qooxdoo-contrib/qcl-php/trunk/services/class/qcl/server/Download.php
+++ trunk/qooxdoo-contrib/qcl-php/trunk/services/class/qcl/server/Download.php
@@ -16,45 +16,61 @@
  *  * Christian Boulanger (cboulanger)
  */
 
+qcl_import("qcl_server_Upload");
+
 /**
- * Download server
- * -- not yet functional --
- *
+ * Download server.
  */
 class qcl_server_Download
-  extends qcl_core_Object
+  extends qcl_server_Upload
 {
-  /**
-   * Not used, only here for compatibility
-   * @var array
-   */
-  public $servicePaths = array();
 
   /**
-   * Download a file, given its datasource
-   * @return unknown_type
+   * Download a file as an attachment.
+   * Expects the following GET or POST parameters:
+   *    'download=true'
+   *    'sessionId=d9dfjdkfsdfs...' The session id.
+   *    'application=applicationNamespace'
+   *    'datasource=nameOfDatasource'
+   *    'id=/path/to/file/or/id' The id of the file or a path
+   *    'name=nameOfTheFile' The name of the file as it is downloaded
+   *     to the file.
+   * There can be an optional 'delete=(true|false)' parameter if the file should
+   * be deleted after download.
+   * @return void
    */
   public function start()
   {
 
-    //$this->debug("Starting download ...");
+    $this->log("Starting upload ...",QCL_LOG_REQUEST);
 
-    $filename   = $_REQUEST['download'];
-    $datasource = $_REQUEST['datasource'];
+    qcl_assert_array_keys( $_REQUEST, array( 'sessionId', 'datasource','id', ) );
+
     $sessionId  = $_REQUEST['sessionId'];
+    $datasource = $_REQUEST['datasource'];
+    $filename   = $_REQUEST['id'];
 
-    if ( ! $filename or ! $datasource or ! $sessionId )
+    if ( isset( $_REQUEST['delete'] ) )
     {
-      echo "Invalid parameters.";
-      exit;
+      $delete = qcl_parseBoolString( $_REQUEST['delete'] );
     }
 
     /*
      * authentication
      */
-    $accessController = $this->getUserController();
-    if ( ! $sessionId or
-         ! $accessController->isValidUserSession( $sessionId ) )
+    if ( ! isset( $_REQUEST['sessionId'] ) )
+    {
+      $this->abort("Missing paramenter 'sessionId'");
+    }
+    $application = $this->getApplication();
+    $accessController = $application->getAccessController();
+    $sessionId = $_REQUEST['sessionId'];
+
+    try
+    {
+      $userId = $accessController->getUserIdFromSession( $sessionId );
+    }
+    catch( qcl_access_InvalidSessionException $e )
     {
       /*
        * check http basic authentication
@@ -72,27 +88,20 @@ class qcl_server_Download
 
     /*
      * Check active user
-     * @todo add config key to allow anonymous downloads
      */
-    $activeUser  = $accessController->getActiveUser();
-    //$configModel = $accessController->getConfigModel();
-    if ( $activeUser->isAnonymous() )
-    {
-      $this->abort( "Anonymous downloads are not permitted.");
-    }
-
-    $this->info(
-      "Downlaod of '$filename' from '$datasource' authorized for " .
-       $activeUser->username() .
-      " (Session #" . $accessController->getSessionId() . ")."
+    $userModel = $accessController->getUserModel();
+    $userModel->load( $userId );
+    $this->log(
+      "Download authorized for " . $userModel->username() .
+      "(Session #" . $sessionId . ").", QCL_LOG_REQUEST
     );
 
     /*
      * get datasource model
      */
-    require_once "qcl/data/datasource/Manager.php";
-    $dsController = new qcl_data_datasource_Manager( $this );
-    $dsModel      = $dsController->getDatasourceModel( $datasource );
+    qcl_import("qcl_data_datasource_Manager") ;
+    $dsController = qcl_data_datasource_Manager::getInstance();
+    $dsModel = $dsController->getDatasourceModelByName( $datasource );
     if ( ! $dsModel->isFileStorage() )
     {
       $this->abort( "'$datasource' is not a file storage!");
@@ -101,28 +110,37 @@ class qcl_server_Download
     /*
      * check access
      */
-    if ( ! $dsModel->isActive() or
-         ( $dsModel->isHidden() and $dsModel->getOwner() != $activeUser->username() )
-    )
+    if ( ! $dsModel->isActive() or $dsModel->isHidden() )
     {
       $this->abort( "Access to '$datasource' forbidden." );
     }
+
+    // @todo alternative access control by using username/password
 
     /*
      * get file
      */
     $folder = $dsModel->getFolderObject();
-    if ( ! $folder->has( $filename ) )
+    $file   = $folder->get( $filename );
+    if ( ! $file->exists() )
     {
-      $this->abort( "File '$filename' does not exist in storage '$datasource'" );
+      $this->abort( sprintf( _( "File '%s' does not exist in datasource '%s'" ), $filename, $datasource ) );
     }
-    $file = $folder->get( $filename );
 
     /*
      * send headers
      */
-    header("Content-Type: application/octet-stream");
-    header("Content-Disposition: attachment; filename=\"$filename\"");
+    $name = isset( $_REQUEST['name'] ) ? $_REQUEST['name'] : $filename;
+    $contentType = $this->getContentType( $name );
+    $size = $file->size();
+
+    header("Pragma: public");
+    header("Expires: 0");
+    header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+    header("Content-Type: $contentType");
+    header("Content-Disposition: attachment; filename=\"$name\"");
+    header("Content-Transfer-Encoding: binary");
+    header("Content-Length: $size" );
 
     /*
      * stream file content to client
@@ -133,9 +151,39 @@ class qcl_server_Download
       echo $data;
     }
     $file->close();
+
+    /*
+     * delete if requested
+     */
+    if ( $delete )
+    {
+      $file->delete();
+    }
+
     exit;
   }
 
-
+  /**
+   * Returns the content type according to the file extension
+   */
+  protected function getContentType( $file )
+  {
+    $file_extension = strtolower(substr(strrchr($file,"."),1));
+    switch( $file_extension )
+    {
+      case "pdf": $ctype="application/pdf"; break;
+      case "txt": $ctype="text/plain"; break;
+      case "exe": die("Not allowed");
+      case "zip": $ctype="application/zip"; break;
+      case "doc": $ctype="application/msword"; break;
+      case "xls": $ctype="application/vnd.ms-excel"; break;
+      case "ppt": $ctype="application/vnd.ms-powerpoint"; break;
+      case "gif": $ctype="image/gif"; break;
+      case "png": $ctype="image/png"; break;
+      case "jpg": $ctype="image/jpg"; break;
+      default: $ctype="application/octet-stream";
+    }
+    return $ctype;
+  }
 }
 ?>
