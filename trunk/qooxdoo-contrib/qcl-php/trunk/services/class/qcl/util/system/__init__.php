00da4397f486c48d4614597a6c538d9af3c90c94
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-06-12 12:42:13 +0000

    Added qcl_util_system_Mail class

--- trunk/qooxdoo-contrib/qcl-php/trunk/services/class/qcl/util/system/__init__.php
+++ trunk/qooxdoo-contrib/qcl-php/trunk/services/class/qcl/util/system/__init__.php
@@ -18,4 +18,37 @@
 
 ************************************************************************ */
 
+/*
+ * exceptions
+ */
+class qcl_util_system_ShellException extends LogicException {}
+class qcl_util_system_MailException extends LogicException {}
+
+/*
+ * log filters
+ */
+$logger = qcl_log_Logger::getInstance();
+define("QCL_LOG_SHELL","shell");
+$logger->registerFilter( QCL_LOG_PROPERTIES, "Shell commands",false);
+
+define("QCL_LOG_MAIL","mail");
+$logger->registerFilter( QCL_LOG_PROPERTIES, "Sending mails",true);
+
+/*
+ * configure mail
+ */
+if( ! defined('QCL_MAIL_SMTP_HOST') )
+{
+  define( 'QCL_MAIL_SMTP_HOST', $_SERVER["SERVER_NAME"] );
+}
+else
+{
+  ini_set("SMTP", QCL_MAIL_SMTP_HOST );
+}
+
+if( ! defined( 'QCL_MAIL_SENDER' ) )
+{
+  define( 'QCL_MAIL_SENDER', $_SERVER["SERVER_ADMIN"] );
+}
+ini_set('sendmail_from', QCL_MAIL_SENDER );
 ?>
