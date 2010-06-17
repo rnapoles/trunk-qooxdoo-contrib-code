8f65c27b781154bd176bdb3242cdf6b18fba674a
simply-notes <simply-notes@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-06-17 13:23:15 +0000

    initial version of accordion menu style widget

---
+++ trunk/qooxdoo-contrib/accordion/trunk/demo/default/source/class/accordion/demo/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("accordion.demo.theme.Theme",
+{
+  meta :
+  {
+    color : accordion.demo.theme.Color,
+    decoration : accordion.demo.theme.Decoration,
+    font : accordion.demo.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : accordion.demo.theme.Appearance
+  }
+});
---
+++ trunk/qooxdoo-contrib/accordion/trunk/source/class/accordion/theme/classic/Theme.js
@@ -0,0 +1,20 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("accordion.theme.classic.Theme",
+{
+  meta :
+  {
+    color : accordion.theme.classic.Color,
+    decoration : accordion.theme.classic.Decoration,
+    font : accordion.theme.classic.Font,
+    appearance : accordion.theme.classic.Appearance
+  }
+});
