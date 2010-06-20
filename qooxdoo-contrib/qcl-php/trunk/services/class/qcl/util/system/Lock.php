06f318f77913509f44570b3cf0fa212d13339f55
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-06-20 18:43:42 +0000

    Added qcl_system_Lock to solve concurrency issues.

---
+++ qooxdoo-contrib/qcl-php/trunk/services/class/qcl/util/system/Lock.php
@@ -0,0 +1,76 @@
+<?php
+/*
+ * qcl - the qooxdoo component library
+ *
+ * http://qooxdoo.org/contrib/project/qcl/
+ *
+ * Copyright:
+ *   2007-2009 Christian Boulanger
+ *
+ * License:
+ *   LGPL: http://www.gnu.org/licenses/lgpl.html
+ *   EPL: http://www.eclipse.org/org/documents/epl-v10.php
+ *   See the LICENSE file in the project's top-level directory for details.
+ *
+ * Authors:
+ *  * Christian Boulanger (cboulanger)
+ */
+
+/**
+ * A simple locking system to make sure two users aren't accessing
+ * the same resource at the same time.
+ *
+ */
+class qcl_util_system_Lock
+{
+
+  /**
+   * A file pointer for this lock
+   * @var resource
+   */
+  private $fp;
+
+  /**
+   * The path to the temporary file
+   * @var string
+   */
+  private $file;
+
+  /**
+   * Constructor
+   * @return unknown_type
+   */
+  public function __construct( $name )
+  {
+    $this->file = sys_get_temp_dir() . "/$name";
+  }
+
+  /**
+   * Tries to get the lock
+   * @return bool
+   *    True if the lock for this file was available,
+   *    false if it was already locked.
+   */
+  public function getExclusive()
+  {
+    if( ! file_exists( $this->file ) )
+    {
+      touch( $this->file );
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+  /**
+   * Releases the lock
+   * @return void
+   */
+  public function release()
+  {
+    unlink( $this->file );
+  }
+}
+?>
