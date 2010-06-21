61c10dd1286139c04880b4afbfec57cbc69e303f
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-06-21 20:59:55 +0000

    Updated trunk to current contribution structure.

---
+++ qooxdoo-contrib/ProgressBar/trunk/demo/default/source/class/progressbar/demo/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("progressbar.demo.theme.Theme",
+{
+  meta :
+  {
+    color : progressbar.demo.theme.Color,
+    decoration : progressbar.demo.theme.Decoration,
+    font : progressbar.demo.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : progressbar.demo.theme.Appearance
+  }
+});
