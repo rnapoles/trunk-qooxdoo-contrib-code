7290c4b463f266255826c922c55aa3af83c706fb
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-06-21 20:06:17 +0000

    Updating tree demo for contribDemobrowser

--- qooxdoo-contrib/VirtualData/trunk/demo/treedemo/source/class/virtualdata/demo/Application.js
+++ qooxdoo-contrib/VirtualData/trunk/demo/treedemo/source/class/virtualdata/demo/Application.js
@@ -71,11 +71,13 @@ qx.Class.define("virtualdata.demo.Application",
             dataModel : new virtualdata.model.SimpleTreeDataModel()
           }
       );
-      tree.set(
-        {
-          width  : 350
-        });
-      tree.setAlwaysShowOpenCloseSymbol(true);
+      tree.set({
+        width  : 350,
+        showCellFocusIndicator : false
+      });
+      
+      tree.getSelectionModel().setSelectionMode(qx.ui.treevirtual.TreeVirtual.SelectionMode.MULTIPLE_INTERVAL);
+
 
       // Obtain the resize behavior object to manipulate
       var resizeBehavior = tree.getTableColumnModel().getBehavior();
@@ -89,11 +91,6 @@ qx.Class.define("virtualdata.demo.Application",
       var dataModel = tree.getDataModel();
 
       var te1 = dataModel.addBranch(null, "Desktop", true);
-      tree.nodeSetLabelStyle(te1,
-                             "background-color: red; " +
-                             "color: white;" +
-                             "font-weight: bold;");
-
       var te1_1;
 
       dataModel.addBranch(te1, "Files", true);
@@ -109,7 +106,6 @@ qx.Class.define("virtualdata.demo.Application",
       dataModel.addBranch(te1, "Network", true);
 
       te = dataModel.addBranch(te1, "Trash", true);
-      tree.nodeSetCellStyle(te, "background-color: cyan;");
 
       var te2 = dataModel.addBranch(null, "Inbox", true);
 
@@ -127,23 +123,13 @@ qx.Class.define("virtualdata.demo.Application",
 
       dataModel.setData();
 
-      var commandFrame = new qx.ui.groupbox.GroupBox("Control");
-      commandFrame.setLayout(new qx.ui.layout.Canvas());
-
+      var commandFrame = new qx.ui.groupbox.GroupBox("Binding two trees");
+      commandFrame.setLayout(new qx.ui.layout.VBox(5));
       hBox.add(commandFrame);
 
-      var o = new qx.ui.basic.Atom("Current Selection: ");
-      commandFrame.add(o, { left: 0, top: 6 });
-
-      o = new qx.ui.form.TextField();
-      o.set({ readOnly: true });
-      commandFrame.add(o, { left : 4, right : 0, top : 20 });
-
-
-
-      var buttonRemove = new qx.ui.form.Button("Remove");
-      buttonRemove.set({ enabled: false });
-      commandFrame.add(buttonRemove, { top : 50, left : 0 });
+      var buttonRemove = new qx.ui.form.Button("Remove node");
+      //buttonRemove.set({ enabled: false });
+      commandFrame.add(buttonRemove);
       buttonRemove.addListener(
         "execute",
         function(e)
@@ -157,9 +143,9 @@ qx.Class.define("virtualdata.demo.Application",
           dataModel.setData();
         });
         
-      var buttonAdd = new qx.ui.form.Button("Add");
-      buttonAdd.set({ enabled: false });
-      commandFrame.add(buttonAdd, { top : 50, left : 100 });
+      var buttonAdd = new qx.ui.form.Button("Add node");
+      //buttonAdd.set({ enabled: false });
+      commandFrame.add(buttonAdd);
       var nodeCounter = 1;
       buttonAdd.addListener(
         "execute",
@@ -173,86 +159,18 @@ qx.Class.define("virtualdata.demo.Application",
           }
         });   
         
-      tree.addListener(
-        "changeSelection",
-        function(e)
-        {
-          // Get the list of selected nodes.  We're in single-selection mode,
-          // so there will be only one of them.
-          var nodes = e.getData();
-          if (nodes.length)
-          {
-            this.setValue(tree.getHierarchy(nodes[0].nodeId).join('/'));
-            buttonRemove.setEnabled(true);
-            buttonAdd.setEnabled(true);
-          }
-          else
-          {
-            this.setValue("");
-            buttonRemove.setEnabled(false);
-            buttonAdd.setEnabled(false);
-          }
-        },
-        o);        
-
-      o = new qx.ui.form.CheckBox("Use tree lines if theme supports them?");
-      o.set({ value: true });
-      commandFrame.add(o, { top : 80, left : 0 });
-      o.addListener("changeValue",
-                    function(e)
-                    {
-                      tree.setUseTreeLines(e.getData());
-                    });
-
-      o = new qx.ui.form.CheckBox("Exclude first-level tree lines?");
-      o.set({ value: false });
-      commandFrame.add(o, { top : 100, left : 0 });
-      o.addListener("changeValue",
-                    function(e)
-                    {
-                      tree.setExcludeFirstLevelTreeLines(e.getData());
-                    });
-
-      o = new qx.ui.form.CheckBox("Always show open/close symbol?");
-      o.set({ value: true });
-      commandFrame.add(o, { top : 120, left : 0 });
-      o.addListener("changeValue",
-                    function(e)
-                    {
-                      tree.setAlwaysShowOpenCloseSymbol(e.getData());
-                    });
-
-      o = new qx.ui.form.CheckBox("Remove open/close if found empty?");
-      o.set({ value: true });
-      commandFrame.add(o, { top : 140, left : 0 });
-      tree.addListener("treeOpenWhileEmpty",
-                       function(e)
-                       {
-                         if (this.getValue())
-                         {
-                           var node = e.getData();
-                           tree.nodeSetHideOpenClose(node, true);
-                         }
-                       },
-                       o);
-
-      o = new qx.ui.form.CheckBox("Open/close click selects row?");
-      o.set({ value: false });
-      commandFrame.add(o, { top : 160, left : 0 });
-      o.addListener("changeValue",
-                    function(e)
-                    {
-                      tree.setOpenCloseClickSelectsRow(e.getData());
-                    });
-
-      o = new qx.ui.form.CheckBox("Disable the tree?");
-      o.set({ value: false });
-      commandFrame.add(o, { top : 180, left : 0 });
-      o.addListener("changeValue",
-                    function(e)
-                    {
-                      tree.setEnabled(! e.getData());
-                    });
+      var label = new qx.ui.basic.Label(
+        "<p>Select the 'workspace' folder in the left tree and click on 'Add node' repeatedly." +
+        "You should see the added nodes appear immediately in the right tree.</p> " +
+        "<p>Similarly, if you remove a node in the left tree, it should disappear in the right tree."+
+        "This way, you can keep several instances of a tree widget in one application in sync (for example, in a filesystem browser).</p>"
+      );
+      label.set({
+        width : 150,
+        rich  : true
+      });
+      
+      commandFrame.add( label );
                     
       ////// Databinding the second tree
                     
