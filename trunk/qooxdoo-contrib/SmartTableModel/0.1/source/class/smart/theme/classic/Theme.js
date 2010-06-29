f79ede8c0a9dd8cc92e4467271467c384dd26f5e
dmbaggett <dmbaggett@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-06-29 13:36:54 +0000

    0.1 version

---
+++ trunk/qooxdoo-contrib/SmartTableModel/0.1/source/class/smart/theme/classic/Theme.js
@@ -0,0 +1,20 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("smart.theme.classic.Theme",
+{
+  meta :
+  {
+    color : smart.theme.classic.Color,
+    decoration : smart.theme.classic.Decoration,
+    font : smart.theme.classic.Font,
+    appearance : smart.theme.classic.Appearance
+  }
+});
