30c061cb56cf88b6b91c5e142ceea35b82734a59
derrell <derrell@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-06-30 19:25:14 +0000

    Initial commit of TimezoneDate contrib (still work in progress)

---
+++ trunk/qooxdoo-contrib/TimezoneDate/trunk/source/class/timezonedate/theme/modern/Theme.js
@@ -0,0 +1,20 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("timezonedate.theme.modern.Theme",
+{
+  meta :
+  {
+    color : timezonedate.theme.modern.Color,
+    decoration : timezonedate.theme.modern.Decoration,
+    font : timezonedate.theme.modern.Font,
+    appearance : timezonedate.theme.modern.Appearance
+  }
+});
