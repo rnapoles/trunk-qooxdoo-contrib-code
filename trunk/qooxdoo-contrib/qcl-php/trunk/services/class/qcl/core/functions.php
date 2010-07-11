eefc6aa55df858be1a21c541608d85c1fab85911
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-07-11 17:53:20 +0000

    Added qcl_generate_password() function

--- trunk/qooxdoo-contrib/qcl-php/trunk/services/class/qcl/core/functions.php
+++ trunk/qooxdoo-contrib/qcl-php/trunk/services/class/qcl/core/functions.php
@@ -1029,6 +1029,26 @@ if(!function_exists('get_called_class'))
 }
 
 /**
+ * Generates a password with the given length
+ * @see http://www.inside-php.de/scripte/PHP-Code%20Ausschnitte-18/Erweiterter-Passwort-Generator.html
+ * @param int $length
+ * @return string
+ */
+function qcl_generate_password( $length=8 )
+{
+  $dummy = array_merge(range('0', '9'), range('a', 'z'), range('A', 'Z') );
+  mt_srand((double)microtime()*1000000);
+  for ($i = 1; $i <= (count($dummy)*2); $i++)
+  {
+    $swap = mt_rand(0,count($dummy)-1);
+    $tmp = $dummy[$swap];
+    $dummy[$swap] = $dummy[0];
+    $dummy[0] = $tmp;
+  }
+  return substr( implode('',$dummy), 0, $length);
+}
+
+/**
  * Encodes as json and pretty-prints the resulting json data.
  * Posted by umbrae at gmail dot com on
  * http://www.php.net/manual/de/function.json-encode.php#80339
