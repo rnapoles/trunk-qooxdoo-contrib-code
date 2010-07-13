8b73be814add8cbe79a544af2e24bfaaebfdb154
derrell <derrell@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-07-13 00:33:46 +0000

    Get rid of appearance warnings by adding an appearance theme.

--- qooxdoo-contrib/SmartTableModel/trunk/source/class/smart/theme/modern/Appearance.js
+++ qooxdoo-contrib/SmartTableModel/trunk/source/class/smart/theme/modern/Appearance.js
@@ -10,7 +10,13 @@
 
 qx.Theme.define("smart.theme.modern.Appearance",
 {
+  extend : qx.theme.modern.Appearance,
+  title : "Smart Demo",
+
   appearances :
   {
+    "table-header-cell/menu-view-button" :
+    {
+    }
   }
 });
