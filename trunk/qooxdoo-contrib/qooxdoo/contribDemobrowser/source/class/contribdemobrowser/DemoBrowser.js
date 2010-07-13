986b27d88d681b9bb241fa7c7289d5db2110a4e3
d_wagner <d_wagner@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-07-13 10:02:04 +0000

    Moved more contrib-specific code to contribdemobrowser.DemoBrowser

--- trunk/qooxdoo-contrib/qooxdoo/contribDemobrowser/source/class/contribdemobrowser/DemoBrowser.js
+++ trunk/qooxdoo-contrib/qooxdoo/contribDemobrowser/source/class/contribdemobrowser/DemoBrowser.js
@@ -22,10 +22,21 @@ qx.Class.define("contribdemobrowser.DemoBrowser",
   construct : function()
   {
     this.base(arguments);
+    
+    this.__versionSelect.addListener("changeSelection", function(ev) {
+      this.__versionFilter = ev.getData()[0].getModel();
+      this.filter(this.__searchTextField.getValue() || "");
+  }, this);
   },
   
   members :
   {
+    __versionSelect : null,
+    __infoView : null,
+    __readmeView : null,
+    __demoView : null,
+    __demoStack : null,
+    
     /**
      * Creates the application header.
      */
@@ -39,6 +50,123 @@ qx.Class.define("contribdemobrowser.DemoBrowser",
     
       header.add(title);    
       return header;
+    },
+    
+    
+    __makeDemoStack : function(demoView)
+    {
+      var demoStack = new qx.ui.container.Stack();
+      this.__infoView = new demobrowser.Manifest();
+      this.__readmeView = new demobrowser.Readme();
+      demoStack.add(demoView);
+      demoStack.add(this.__infoView);
+      demoStack.add(this.__readmeView);
+      return demoStack;
+    },
+    
+    __makeVersionSelect : function(leftComposite)
+    {
+      var versionComposite = new qx.ui.container.Composite();
+      versionComposite.setLayout(new qx.ui.layout.HBox(3));
+      leftComposite.add(versionComposite);
+      var versionLabel = new qx.ui.basic.Label("Compatible with: ")
+      versionLabel.setPadding(4, 5, 0, 2);
+      versionComposite.add(versionLabel);
+
+      this.__versionSelect = new qx.ui.form.SelectBox();
+      versionComposite.add(this.__versionSelect,{flex: 1});
+
+      var itemAll = new qx.ui.form.ListItem("Any qooxdoo version");
+      itemAll.setModel(null);
+      this.__versionSelect.add(itemAll);
+    },
+    
+    
+    /**
+     * Add a library version's "qxVersion" tags to the list of version tags
+     *
+     * @param tagList {Array} A tree node's tags
+     */
+    __getVersionTags : function(tagList)
+    {
+      if (!this.__versionTags) {
+        this.__versionTags = {};
+      }
+      for (var i=0,l=tagList.length; i<l; i++) {
+        var tag = tagList[i];
+        if (tag.indexOf("qxVersion") == 0) {
+          if (!(tag in this.__versionTags)) {
+            this.__versionTags[tag] = "";
+          }
+        }
+      }
+    },
+    
+
+    /**
+     * Add an option for each version to the version select box
+     */
+    __getVersionItems : function()
+    {
+      var versions = [];
+      for (var tag in this.__versionTags) {
+        versions.push(tag.substr(tag.indexOf("_") + 1) );
+      }
+      versions.sort();
+      versions.reverse();
+
+      for (var i=0,l=versions.length; i<l; i++) {
+        var li = new qx.ui.form.ListItem(versions[i]);
+        li.setModel("qxVersion_" + versions[i]);
+        this.__versionSelect.add(li);
+      }
+    },
+    
+    treeGetSelection : function(e)
+    {
+      var treeNode = this.tree.getSelection()[0];
+      var modelNode = treeNode.getUserData("modelLink");
+      this.tests.selected = this.tests.handler.getFullName(modelNode);
+      if (modelNode) {
+        if (modelNode.manifest) {
+          this.__infoView.setManifestData(modelNode.manifest);
+          this.__demoStack.setSelection([this.__infoView]);
+        }
+        else if (modelNode.readme) {
+          this.__readmeView.setTitle(treeNode.getLabel());
+          this.__readmeView.setReadmeData(modelNode.readme);
+          this.__demoStack.setSelection([this.__readmeView]);
+        }
+        else {
+          this.__demoStack.setSelection([this.__demoView]);
+        }
+      }
+      else {
+        this.__demoStack.setSelection([this.__demoView]);
+      }
+    },
+    
+    /**
+     * event handler for the Run Test button - performs the tests
+     *
+     * @param e {Event} TODOC
+     * @return {void}
+     */
+    runSample : function(e)
+    {
+      this.__demoStack.setSelection([this.__demoView]);
+      
+      this._runbutton.setVisibility("excluded");
+      this._stopbutton.setVisibility("visible");
+
+      if (this.tests.selected.indexOf(".html") > 0) {
+        var file = this.tests.selected.replace(".", "/");
+        // contribDemobrowser has an additional hierarchy level
+        file = file.replace(".", "/");
+        this.setCurrentSample(file);
+      } else {
+        this.playNext();
+      }
     },
     
     /**
@@ -110,5 +238,19 @@ qx.Class.define("contribdemobrowser.DemoBrowser",
         this._runbutton.setVisibility("visible");
       }
     }
+  },
+  
+  
+  
+  /*
+  *****************************************************************************
+     DESTRUCTOR
+  *****************************************************************************
+  */
+
+  destruct : function()
+  {
+    this._disposeObjects("__demoStack", "__infoView", "__readmeView", 
+      "__versionTags", "__versionSelect");
   }
 });
