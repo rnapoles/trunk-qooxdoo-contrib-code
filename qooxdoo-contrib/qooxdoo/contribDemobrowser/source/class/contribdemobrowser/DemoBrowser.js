54550f7abb7a766cd1c9ce235dbb965b711f3bf0
d_wagner <d_wagner@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-07-13 08:22:32 +0000

    Moved contrib-specific previous/next navigation to contribdemobrowser.DemoBrowser

--- qooxdoo-contrib/qooxdoo/contribDemobrowser/source/class/contribdemobrowser/DemoBrowser.js
+++ qooxdoo-contrib/qooxdoo/contribDemobrowser/source/class/contribdemobrowser/DemoBrowser.js
@@ -39,6 +39,76 @@ qx.Class.define("contribdemobrowser.DemoBrowser",
     
       header.add(title);    
       return header;
+    },
+    
+    /**
+     * Loads the sample preceding the currently selected one
+     * 
+     * @param {Event} e
+     */
+    playPrev : function(e)
+    {
+      var currSamp = this.tree.getSelection()[0];  // widget
+
+      if (!currSamp) {
+        return;
+      }
+
+      var otherSamp = null;
+      var sample = currSamp;
+      while (sample) {
+        var prev = this.tree.getPreviousNodeOf(sample);
+        if (prev instanceof qx.ui.tree.TreeFile) {
+          otherSamp = prev;
+          break;
+        } else {
+          sample = prev;
+        }
+      }
+
+      if (otherSamp) {
+        this.tree.setSelection([otherSamp]);
+        this.runSample();
+      } else {
+        // Remove stop button, display run button
+        this._stopbutton.setVisibility("excluded");
+        this._runbutton.setVisibility("visible");
+      }
+    },
+    
+    /**
+     * Loads the sample following the currently selected one
+     * 
+     * @param {Event} e
+     */
+    playNext : function(e)
+    {
+      var currSamp = this.tree.getSelection()[0];  // widget
+
+      if (!currSamp) {
+        return;
+      }
+
+      var otherSamp = null;
+      var sample = currSamp;
+      while (sample) {
+        var next = this.tree.getNextNodeOf(sample);
+        if (next instanceof qx.ui.tree.TreeFile) {
+          otherSamp = next;
+          break;
+        } else {
+          sample = next;
+        }
+      }
+
+      if (otherSamp) {
+        this.tree.setSelection([otherSamp]);
+        this.runSample();
+      } else {
+        // Remove stop button, display run button
+        this._stopbutton.setVisibility("excluded");
+        this._runbutton.setVisibility("visible");
+      }
     }
   }
 });
