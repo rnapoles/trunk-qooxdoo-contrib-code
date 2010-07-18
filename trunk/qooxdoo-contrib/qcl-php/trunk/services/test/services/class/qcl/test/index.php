d0d01f8d7fb140b3a4455e54ef412102bc73c74c
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-07-18 21:26:17 +0000

    Update to current trunk

--- trunk/qooxdoo-contrib/qcl-php/trunk/services/test/services/class/qcl/test/index.php
+++ trunk/qooxdoo-contrib/qcl-php/trunk/services/test/services/class/qcl/test/index.php
@@ -17,185 +17,35 @@
      * Chritian Boulanger (cboulanger)
 
 ************************************************************************ */
-/*
- * The parent directory containing subfolders with test classes.
- * Must be the top-level "class" folder containing the top namespace,
- * otherwise the class names cannot be correctly determined.
- */
-if( ! defined( "QCL_TEST_CLASS_DIR" ) )
-{
-  define( "QCL_TEST_CLASS_DIR" , realpath( "../.." ) );
-}
 
 /*
- * The path to the directory containing the server that provides access
- * to the test classes
+ * This script scans this folder and subfolders of the containing
+ * folder and looks for classes that extend from qcl_test_AbstractTestController
+ * The methods of these classes are checked
+ * as to whether their doc comments contain the @rpctest tag. If so,
+ * the content of this tag is parsed and returned as part of the json
+ * data.
  */
-if( ! defined( "QCL_TEST_SERVER_DIR" ) )
-{
-  define( "QCL_TEST_SERVER_DIR" , "../../.." );
-}
-
-/*
- * The parent class that test service classes must extend in order to
- * be included in the test suite
- */
-if( ! defined( "QCL_TEST_SERVICE_PARENT_CLASS" ) )
-{
-  define( "QCL_TEST_SERVICE_PARENT_CLASS" , "qcl_test_AbstractTestController" );
-}
-
-/*
- * we're delivering a javascript file
- */
-header("Content-Type: text/javascript");
-
-/*
- * urls
- */
-$serverUrl =  "http://" . $_SERVER["HTTP_HOST"] .
-  dirname( dirname( dirname( dirname( $_SERVER["SCRIPT_NAME"] ) ) ) ) . "/server.php";
-$testDataUrl = "http://" . $_SERVER["HTTP_HOST"] . $_SERVER["SCRIPT_NAME"];
-
-/*
- * introduction
- */
-echo <<<EOF
-/* ************************************************************************
-
-   qcl - the qooxdoo component library
-
-   http://qooxdoo.org/contrib/project/qcl/
-
-   Copyright:
-     2007-2010 Christian Boulanger
-
-   License:
-     LGPL: http://www.gnu.org/licenses/lgpl.html
-     EPL: http://www.eclipse.org/org/documents/epl-v10.php
-     See the LICENSE file in the project's top-level directory for details.
-
-   Authors:
-     * Christian Boulanger (cboulanger)
-
-   The following file contains data that can be fed into the RpcConsole
-   contribution to automatically run all tests contained in the
-   subdirectories. Load the RpcConsole with the following GET parameters:
-
-   ?serverUrl=$serverUrl
-   &testDataUrl=$testDataUrl
-
-   You can also load the data from the testDataUrl with the "Load/Edit Tests"
-   -> "Load test data..." command or copy and paste the data into the editor
-   using the "Load/Edit Tests" -> "Edit test data" menu command.
-
-************************************************************************ */
-
-EOF;
-
-/*
- * change the working directory to the server's and load required
- * libraries
- */
-chdir( QCL_TEST_SERVER_DIR );
-require_once "config.php";
-require_once "qcl/bootstrap.php";
 
 /**
- * Recursively collect all files that end with ".php"
- * @param $dir
- * @return array
+ * The  absolute path to the top-level "class" folder containing
+ * the namespace folders with the test classes.
  */
-function getServiceClassFiles( $dir = QCL_TEST_CLASS_DIR )
-{
-  $serviceClasses = new ArrayList();
-  foreach( scandir( $dir ) as $file )
-  {
-    $path = $dir . "/" . $file;
-    if (  $file[0] == "." ) continue;
-    if ( is_dir( $path) )
-    {
-      $serviceClasses->addAll( getServiceClassFiles( $path ) );
+define( "QCL_TEST_CLASS_DIR" , realpath( "../.." ) );
 
-    }
-    elseif ( get_file_extension( $file ) == "php" )
-    {
-      $serviceClasses->add( $path );
-    }
-  }
-  return $serviceClasses->toArray();
-}
+/**
+ * The absolute path to the directory containing the server script.
+ * Usually the parent directory of QCL_TEST_CLASS_DIR
+ */
+define( "QCL_TEST_SERVER_DIR" , realpath(  "../../.." ) );
 
-require_once "services/server/JsonRpcServer.php";
+/*
+ * configure paths
+ */
+require QCL_TEST_SERVER_DIR . "/config.php";
 
-$tests = array();
-$files = getServiceClassFiles();
-$packages = array();
-$packageCount = 0;
-
-echo <<<EOF
-qx.core.Init.getApplication().setTestData(
-{
-EOF;
-
-foreach( $files as $path )
-{
-  /*
-   * package
-   */
-  $packageDir = dirname( $path );
-  $package = str_replace( "/",".", substr( $packageDir, strlen(QCL_TEST_CLASS_DIR) +1 ) ) . ".*";
-  if ( ! isset( $packages[$package] ) )
-  {
-    $packageCount++;
-    echo <<<EOF
-
-      "runPackage{$packageCount}":{
-        "label":"Execute $package test suite",
-        "execute":function (){
-          this.info( "Starting test suite $package ");
-          this.runTests("$package");
-        }
-      },
-
-EOF;
-    $packages[$package] = true;
-  }
-
-
-
-  $file_content = file_get_contents( $path );
-  if ( strstr( $file_content, "extends ". QCL_TEST_SERVICE_PARENT_CLASS) )
-  {
-    /*
-     * convert path into class name
-     */
-    $import_class = substr(
-       str_replace("/","_", substr( $path, strlen(QCL_TEST_CLASS_DIR) +1 ) ), 0, -4
-    );
-    qcl_import( $import_class );
-
-    $className = JsonRpcClassPrefix . $import_class;
-    $class = new $className;
-
-    $testJson = $class->rpcConsoleClassTestJson();
-
-    $testJson = implode("\n  ",explode("\n",$testJson) );
-
-
-    if ( $testJson != "" )
-    {
-      $test = "\n";
-      $test .= "  //=================================================================\n";
-      $test .= "  // Class $className \n";
-      $test .= "  //=================================================================\n";
-      $test .= "  " . $testJson;
-      $tests[] = $test;
-    }
-
-  }
-}
-echo implode(",\n",$tests) . "\n});";
-
-
+/*
+ * load the script that creates the test data
+ */
+require QCL_CLASS_PATH . "/qcl/test/testdata.php";
 ?>
