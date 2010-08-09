d8d4221224a25a53c2e2e37ff2bb8e8a011f750f
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-08-09 20:11:10 +0000

    Initial checkin of RpcNode contribution
    This is a json-rpc server based on the JavaScript-based node.js runtime.
    
    The server will implement json-rpc specs 1.0 and 2.0 and be backwards-compatible
    to the existing, non-standard json-rpc servers in qooxdoo-contrib.
    
    The code is currently being worked on and still unfunctional.

---
+++ qooxdoo-contrib/RpcNode/trunk/demo/default/source/class/rpcnode/demo/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("rpcnode.demo.theme.Theme",
+{
+  meta :
+  {
+    color : rpcnode.demo.theme.Color,
+    decoration : rpcnode.demo.theme.Decoration,
+    font : rpcnode.demo.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : rpcnode.demo.theme.Appearance
+  }
+});
