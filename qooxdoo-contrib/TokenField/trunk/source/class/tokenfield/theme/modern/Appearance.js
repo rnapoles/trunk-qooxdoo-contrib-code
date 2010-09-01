68c914cc5cdb4b9f9826ef66e1ae8d3d4b15ffba
guilhermeaiolfi <guilhermeaiolfi@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-09-01 19:03:17 +0000

    Included the appearance entries

--- qooxdoo-contrib/TokenField/trunk/source/class/tokenfield/theme/modern/Appearance.js
+++ qooxdoo-contrib/TokenField/trunk/source/class/tokenfield/theme/modern/Appearance.js
@@ -1,11 +1,39 @@
 /* ************************************************************************
 
    Copyright:
-
+     2010 Guilherme R. Aiolfi
+     
    License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
 
-   Authors:
+   Authors: Guilherme R. Aiolfi (gradinf AT gmail DOT com)
 
 ************************************************************************ */
 
-qx.Theme.define("tokenfield.theme.modern.Appearance", { appearances : {} });
+qx.Theme.define("tokenfield.demo.theme.Appearance",
+{
+  extend      : qx.theme.modern.Appearance,
+  appearances : 
+  {
+		'token' : 'combobox',
+	
+	  'tokenitem' :
+	  {
+	    include : 'listitem',
+	
+	    style : function(states)
+	    {
+	      return {
+	        decorator : 'group',
+	        textColor : states.hovered ? '#314a6e' : '#000000',
+	        height    : 18,
+	        padding   : [ 1, 6, 1, 6 ],
+	        margin    : 0,
+	        icon      : states.close ? "decoration/window/close-active.png" : "decoration/window/close-inactive.png"
+	      };
+	    }
+	  }
+	}
+});
