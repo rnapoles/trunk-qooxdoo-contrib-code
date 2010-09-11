e01734798bff8971310b43087d186f3369a62713
oetiker <oetiker@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-09-11 22:02:05 +0000

    initial

---
+++ trunk/qooxdoo-contrib/QxProtovis/trunk/demo/default/source/class/qxprotovis/demo/theme/Color.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("qxprotovis.demo.theme.Color",
+{
+  extend : qx.theme.modern.Color,
+
+  colors :
+  {
+  }
+});
