dbf5169f85c3173f91a6d50e9d7cdef19e555f47
oetiker <oetiker@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-09-11 22:02:05 +0000

    initial

---
+++ qooxdoo-contrib/QxProtovis/trunk/demo/default/source/class/qxprotovis/demo/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("qxprotovis.demo.theme.Theme",
+{
+  meta :
+  {
+    color : qxprotovis.demo.theme.Color,
+    decoration : qxprotovis.demo.theme.Decoration,
+    font : qxprotovis.demo.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : qxprotovis.demo.theme.Appearance
+  }
+});
