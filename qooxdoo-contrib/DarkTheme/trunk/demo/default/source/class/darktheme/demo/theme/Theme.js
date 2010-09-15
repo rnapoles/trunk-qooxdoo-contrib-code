2f30e464c644cb953d99d6b619d34c0158ea870c
ecker <ecker@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-09-15 13:02:00 +0000

    Added initial version of DarkTheme contrib on behalf of Norbert Schroeder. Thanks!

---
+++ qooxdoo-contrib/DarkTheme/trunk/demo/default/source/class/darktheme/demo/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("darktheme.demo.theme.Theme",
+{
+  meta :
+  {
+    color : darktheme.demo.theme.Color,
+    decoration : darktheme.demo.theme.Decoration,
+    font : darktheme.demo.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : darktheme.demo.theme.Appearance
+  }
+});
