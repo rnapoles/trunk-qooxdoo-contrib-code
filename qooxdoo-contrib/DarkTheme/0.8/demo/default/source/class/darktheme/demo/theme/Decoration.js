07ade39d55d2844724dbc2a4abdecab00a27cb15
SCRO34 <SCRO34@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-09-16 08:39:01 +0000

    DarkTheme version 0.8

---
+++ qooxdoo-contrib/DarkTheme/0.8/demo/default/source/class/darktheme/demo/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("darktheme.demo.theme.Decoration",
+{
+  extend : qx.theme.modern.Decoration,
+
+  decorations :
+  {
+  }
+});
