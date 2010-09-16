9fd31c4ae46e9d1220da5400e2ec194438cf601b
guilhermeaiolfi <guilhermeaiolfi@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-09-16 13:07:32 +0000

    Classic theme.

--- trunk/qooxdoo-contrib/TokenField/trunk/source/class/tokenfield/theme/classic/Appearance.js
+++ trunk/qooxdoo-contrib/TokenField/trunk/source/class/tokenfield/theme/classic/Appearance.js
@@ -1,11 +1,40 @@
 /* ************************************************************************
 
    Copyright:
+     2010 Guilherme R. Aiolfi
 
    License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
 
-   Authors:
+   Authors: Guilherme R. Aiolfi (guilhermeaiolfi)
 
 ************************************************************************ */
 
-qx.Theme.define("tokenfield.theme.classic.Appearance", { appearances : {} });
+qx.Theme.define("tokenfield.theme.classic.Appearance", 
+{ 
+	extend      : qx.theme.classic.Appearance,
+	appearances : 
+	{
+    'token' : 'combobox',
+
+    'tokenitem' :
+    {
+      include : 'listitem',
+
+      style : function(states)
+      {
+        return {
+          decorator : 'main',
+          textColor : states.hovered ? '#314a6e' : states.head? "text-selected" : '#000000',
+          height    : 18,
+          padding   : [0, 5, 0, 5],
+          margin    : [1, 0, 1, 1],
+          backgroundColor : states.head? "background-selected" : undefined,
+          icon      : "decoration/window/close.gif"
+        };
+      }
+    }
+  }
+});
