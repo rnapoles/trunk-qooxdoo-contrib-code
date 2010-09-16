15de0c3202d3173f952cf1a2bccbfa16564254a1
SCRO34 <SCRO34@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-09-16 08:39:01 +0000

    DarkTheme version 0.8

---
+++ trunk/qooxdoo-contrib/DarkTheme/0.8/demo/default/source/class/darktheme/demo/theme/Color.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("darktheme.demo.theme.Color",
+{
+  extend : qx.theme.modern.Color,
+
+  colors :
+  {
+  }
+});
