6a5e3cfc2db6b956738c762f8b7188f37615e3e3
guilhermeaiolfi <guilhermeaiolfi@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-09-16 13:07:32 +0000

    Classic theme.

--- qooxdoo-contrib/TokenField/trunk/source/class/tokenfield/theme/classic/Color.js
+++ qooxdoo-contrib/TokenField/trunk/source/class/tokenfield/theme/classic/Color.js
@@ -1,11 +1,21 @@
 /* ************************************************************************
 
    Copyright:
+     2010 Guilherme R. Aiolfi
 
    License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
 
-   Authors:
+   Authors: Guilherme R. Aiolfi (guilhermeaiolfi)
 
 ************************************************************************ */
 
-qx.Theme.define("tokenfield.theme.classic.Color", { colors : {} });
+qx.Theme.define("tokenfield.theme.classic.Color", 
+{ 
+	extend : qx.theme.classic.Color,
+	colors : 
+	{
+	} 
+});
