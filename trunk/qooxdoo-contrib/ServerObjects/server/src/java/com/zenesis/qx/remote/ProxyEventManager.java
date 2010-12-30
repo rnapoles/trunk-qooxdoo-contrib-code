3fe3f7f944b969a4a1aad75d1c0e451a99da4542
spackers <spackers@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2010-12-30 17:31:52 +0000

    reorganising the code

---
+++ trunk/qooxdoo-contrib/ServerObjects/server/src/java/com/zenesis/qx/remote/ProxyEventManager.java
@@ -0,0 +1,77 @@
+/**
+ * ************************************************************************
+ * 
+ *    server-objects - a contrib to the Qooxdoo project that makes server 
+ *    and client objects operate seamlessly; like Qooxdoo, server objects 
+ *    have properties, events, and methods all of which can be access from
+ *    either server or client, regardless of where the original object was
+ *    created.
+ * 
+ *    http://qooxdoo.org
+ * 
+ *    Copyright:
+ *      2010 Zenesis Limited, http://www.zenesis.com
+ * 
+ *    License:
+ *      LGPL: http://www.gnu.org/licenses/lgpl.html
+ *      EPL: http://www.eclipse.org/org/documents/epl-v10.php
+ *      
+ *      This software is provided under the same licensing terms as Qooxdoo,
+ *      please see the LICENSE file in the Qooxdoo project's top-level directory 
+ *      for details.
+ * 
+ *    Authors:
+ *      * John Spackman (john.spackman@zenesis.com)
+ * 
+ * ************************************************************************
+ */
+package com.zenesis.qx.remote;
+
+import com.zenesis.qx.event.EventListener;
+import com.zenesis.qx.event.EventManager;
+
+/**
+ * Specialisation of EventManager for better event detection
+ * @author John Spackman [john.spackman@zenesis.com]
+ */
+public class ProxyEventManager extends EventManager {
+	
+	private final ProxyTypeManager typeManager;
+
+	public ProxyEventManager(ProxyTypeManager typeManager) {
+		super();
+		this.typeManager = typeManager;
+	}
+
+	public ProxyEventManager(ProxyTypeManager typeManager, boolean setGlobal) {
+		super(setGlobal);
+		this.typeManager = typeManager;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.zenesis.qx.event.EventManager#supportsEvent(java.lang.Object, java.lang.String)
+	 */
+	@Override
+	public boolean supportsEvent(Object obj, String eventName) {
+		if (obj instanceof Proxied) {
+			Proxied proxied = (Proxied)obj;
+			ProxyType type = typeManager.getProxyType(proxied.getClass());
+			return type.supportsEvent(eventName);
+		}
+		return super.supportsEvent(obj, eventName);
+	}
+
+	@Override
+	public boolean addListener(Object keyObject, String eventName,
+			EventListener listener) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return super.addListener(keyObject, eventName, listener);
+	}
+
+	@Override
+	public boolean removeListener(Object keyObject, String eventName, EventListener listener) {
+		// TODO Auto-generated method stub
+		return super.removeListener(keyObject, eventName, listener);
+	}
+
+}
