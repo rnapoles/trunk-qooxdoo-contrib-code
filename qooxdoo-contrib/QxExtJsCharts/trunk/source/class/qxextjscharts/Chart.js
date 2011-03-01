c4e7fe06dc92242b57c8f929e587e5318a27bb92
oetiker <oetiker@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2011-03-01 23:05:28 +0000

    * moved resources to a better place * fixed delayed execution

--- qooxdoo-contrib/QxExtJsCharts/trunk/source/class/qxextjscharts/Chart.js
+++ qooxdoo-contrib/QxExtJsCharts/trunk/source/class/qxextjscharts/Chart.js
@@ -11,47 +11,14 @@
 
 ************************************************************************ */
 /*
-#asset(qxextjscharts/*)
+#asset(ExtJs4/*)
 */
 
 /**
  * Create a ExtJS Chart 'Widget' providing access to the ExtJS charting functions
  */
 
-/* load the ExtJS scripts */
-if (! window.Ext ){
-    var todo = [];
-    var rm = qx.util.ResourceManager.getInstance();
-    var head = document.getElementsByTagName("head")[0];
-    var el = document.createElement("link");
-    el.type = "text/css";
-    el.rel="stylesheet";
-    el.href=rm.toUri('qxextjscharts/ext-all.css');
-    setTimeout(function() {
-        head.appendChild(el);
-    }, 0);
-    var sl = new qx.io.ScriptLoader();
-    sl.load(rm.toUri('qxextjscharts/ext-core-debug.js'),function(status){
-        if (status == 'success'){
-            var sl2 = new qx.io.ScriptLoader();
-            sl2.load(rm.toUri('qxextjscharts/ext-all-debug.js'),function(status){
-                if (status == 'success'){
-                    // seems not to get initialized when running on chrome
-                    Ext.supports.init();
-                    for (var i=0;i<todo.length;i++){
-                        todo[i]();
-                    }
-                }
-                else {
-                    alert("Faild to load ext-all-debug.js");
-                }
-            });
-        }
-        else {
-            alert("Faild to load ext-core-debug.js");
-        }
-    });
-}    
+var todo = [];
 
 qx.Class.define("qxextjscharts.Chart",{
     extend : qx.ui.core.Widget,
@@ -60,6 +27,39 @@ qx.Class.define("qxextjscharts.Chart",{
         this.__map = map;
         this.addListener('resize',this.__redraw,this);      
         this.addListenerOnce('appear',this.__draw,this);
+        /* load the ExtJS scripts */
+        if (! window.Ext ){
+            var rm = qx.util.ResourceManager.getInstance();
+            var head = document.getElementsByTagName("head")[0];
+            var el = document.createElement("link");
+            el.type = "text/css";
+            el.rel="stylesheet";
+            el.href=rm.toUri('ExtJs4/ext-all.css');
+            setTimeout(function() {
+                head.appendChild(el);
+            }, 0);
+            var sl = new qx.io.ScriptLoader();
+            sl.load(rm.toUri('ExtJs4/ext-core-debug.js'),function(status){
+                if (status == 'success'){
+                    var sl2 = new qx.io.ScriptLoader();
+                    sl2.load(rm.toUri('ExtJs4/ext-all-debug.js'),function(status){
+                        if (status == 'success'){
+                            // seems not to get initialized when running on chrome
+                            Ext.supports.init();
+                            while (todo.length > 0){
+                                todo.shift()();  
+                            }
+                        }
+                        else {
+                            alert("Faild to load ext-all-debug.js");
+                        }
+                    });
+                }
+                else {
+                    alert("Faild to load ext-core-debug.js");
+                }
+            });
+        }    
     },
     properties: {
         extJsHandle: {
