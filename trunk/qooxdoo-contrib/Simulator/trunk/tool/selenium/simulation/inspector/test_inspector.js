f7b81392e9aecebf36b9f62d985c11060d4c36c7
d_wagner <d_wagner@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2011-03-09 14:28:26 +0000

    skip console test in FF4

--- trunk/qooxdoo-contrib/Simulator/trunk/tool/selenium/simulation/inspector/test_inspector.js
+++ trunk/qooxdoo-contrib/Simulator/trunk/tool/selenium/simulation/inspector/test_inspector.js
@@ -289,7 +289,8 @@ simulation.Simulation.prototype.runTest = function()
   // Simulating native key events doesn't work in WebKit
   // The Console window uses window.top to eval commands which doesn't work 
   // during IE tests because the Inspector is loaded in an Iframe 
-  if (browser.indexOf("AppleWebKit") < 0 && launcher.indexOf("*iexplore") < 0) {
+  if (browser.indexOf("AppleWebKit") < 0 && launcher.indexOf("*iexplore") < 0
+      && browser.indexOf("Firefox/4") < 0) {
     try {
       this.checkConsole();
     } catch(ex) {
