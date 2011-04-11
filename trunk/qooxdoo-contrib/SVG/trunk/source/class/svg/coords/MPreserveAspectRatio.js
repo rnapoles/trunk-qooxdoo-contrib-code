2393dbd27bfc2a454cd9a1707e3d741f18dfe321
marcputs <marcputs@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2011-04-11 14:01:29 +0000

    MPreserveAspectRatio: default did not work correctly. Fixed.

--- trunk/qooxdoo-contrib/SVG/trunk/source/class/svg/coords/MPreserveAspectRatio.js
+++ trunk/qooxdoo-contrib/SVG/trunk/source/class/svg/coords/MPreserveAspectRatio.js
@@ -34,50 +34,76 @@ qx.Mixin.define("svg.coords.MPreserveAspectRatio",
   {
     /**
      * Indicates whether or not to force uniform scaling.
+     * 
+     * To remove the preserveAspectRatio attribute, set it to null:
+     * <pre class="javascript">
+     * element.setPreserveAspectRatio(null);
+     * </pre>
      *
      * @param align {String ? "xMidYMid"} The 'align' parameter indicates whether to force uniform scaling and, if so,
-     *                the alignment method to use in case the aspect ratio of the viewBox doesn't
-     *                match the aspect ratio of the viewport. Possible values are:
-     *
-     *                <ul>
-     *                  <li>xMinYMin - align to top left</li>
-     *                  <li>xMidYMin - center on x-axis, align top</li>
-     *                  <li>xMaxYMin - align to top right</li>
-     *                  <li>xMinYMid - align to left, center on y-axis</li>
-     *                  <li>xMidYMid (default) - center on x and y-axes</li>
-     *                  <li>xMaxYMid - align to right, center on y-axis</li>
-     *                  <li>xMinYMax - align to bottom left</li>
-     *                  <li>xMidYMax - center on x-axis, align to bottom</li>
-     *                  <li>xMaxYMax - align to bottom right</li>
-     *                </ul>
+     *   the alignment method to use in case the aspect ratio of the viewBox doesn't
+     *   match the aspect ratio of the viewport. Possible values are:
+     *   <ul>
+     *     <li>xMinYMin - align to top left</li>
+     *     <li>xMidYMin - center on x-axis, align top</li>
+     *     <li>xMaxYMin - align to top right</li>
+     *     <li>xMinYMid - align to left, center on y-axis</li>
+     *     <li>xMidYMid (default) - center on x and y-axes</li>
+     *     <li>xMaxYMid - align to right, center on y-axis</li>
+     *     <li>xMinYMax - align to bottom left</li>
+     *     <li>xMidYMax - center on x-axis, align to bottom</li>
+     *     <li>xMaxYMax - align to bottom right</li>
+     *   </ul>
+     *   
      * @param meetOrSlice {String ? "meet"} Possible values are:
-     *                <ul>
-     *                  <li>meet (default)</li>
-     *                  <li>slice</li>
-     *                </ul>
+     *   <ul>
+     *     <li>_meet_ - scale the graphic such that:
+     *       <ul>
+     *         <li>aspect ratio is preserved</li>
+     *         <li>the entire viewbox is visible within the viewport</li>
+     *         <li>the viewbox is scaled up as much as possible, while still meeting the other criteria</li>
+     *       </ul>
+     *       If the aspect ratio's of the viewbox and viewport do not match, the area into
+     *       which the viewbox will draw is smaller than the viewport.
+     *     </li>
+     *     <li>_slice_ - scale the graphic such that:
+     *       <ul>
+     *         <li>aspect ratio is preserved</li>
+     *         <li>the entire viewport is covered by the viewbox</li>
+     *         <li>the viewbox is scaled down as much as possible, while still meeting the other criteria</li> 
+     *       </ul>
+     *       If the aspect ratio's of the viewbox and viewport do not match, the area into
+     *       which the viewbox will draw is larger than the viewport.
+     *     </li>
+     *   </ul>
+     *   
      * @param defer {Boolean ? false} If set to true on an 'image' element, then the value of preserveAspectRatio
-     *                on the referenced content should be used.
-     *
-     *                For preserveAspectRatio on all other elements, the 'defer' portion of the
-     *                attribute is ignored.
+     *   on the referenced content should be used. For preserveAspectRatio on all other elements, the 'defer'
+     *   portion of the attribute is ignored.
+     *   
      * @return {void}
      */
     setPreserveAspectRatio : function(align, meetOrSlice, defer)
     {
-      var value = align + " " + meetOrSlice;
-
-      if (defer) {
-        value = "defer " + value;
+      if (null == align) {
+        this.removeAttribute("preserveAspectRatio");
       }
+      else {
+        var value = align + " " + (meetOrSlice || "meet");
 
-      this.setAttribute("preserveAspectRatio", value);
+        if (defer) {
+          value = "defer " + value;
+        }
+
+        this.setAttribute("preserveAspectRatio", value);
+      }
     },
 
 
     /**
      * Gets the value of the preserveAspectRatio attribute.
      *
-     * @return {String} TODOC
+     * @return {String}
      * @see #setPreserveAspectRatio
      */
     getPreserveAspectRatio : function() {
