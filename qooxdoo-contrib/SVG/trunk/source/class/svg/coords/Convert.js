2d6657d362d98fadc4243be19e13a26878ef7bcd
marcputs <marcputs@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2011-04-13 12:48:44 +0000

    Added calculation of userspace to document coordinates.

--- qooxdoo-contrib/SVG/trunk/source/class/svg/coords/Convert.js
+++ qooxdoo-contrib/SVG/trunk/source/class/svg/coords/Convert.js
@@ -24,26 +24,62 @@ qx.Class.define("svg.coords.Convert",
      * Converts client (document) coordinates to svg userspace coordinates,
      * using the coordinate system of the specified element.
      * 
+     * This is the inverse of {#userspaceToClient}.
+     * 
      * @param el {svg.core.Element}
-     *   Element whose coordinate system to use.
+     *   Element of which to use coordinate system.
      *  
      * @param clientx {Integer}
-     *   x coordinate in client
+     *   x coordinate in client (pixels)
      * 
      * @param clienty {Integer}
-     *   y coordinate in client
+     *   y coordinate in client (pixels)
      * 
      * @return {SVGPoint}
-     *   An object containing the keys <code>x</code> and <code>y</code>. 
+     *   An object containing the converted coordinates <code>x</code> and
+     *   <code>y</code> in userspace.
      *   
      */
     clientToUserspace : function(el, clientx, clienty) {
       var vp = el.getNearestViewport() || el;
       var m = vp.getScreenCTM();
+      
       var p = vp.createPoint();
       p.x = clientx;
       p.y = clienty;
+      
       p = p.matrixTransform(m.inverse());
+      return p;
+    },
+    
+    /**
+     * Converts svg userspace coordinates to client (document) coordinates
+     * using the coordinate system of the specified element.
+     * 
+     * This is the inverse of {#clientToUserspace}.
+     * 
+     * @param el {svg.core.Element}
+     *   Element of which to use coordinate system.
+     * 
+     * @param usX {Integer}
+     *   x coordinate in userspace
+     * 
+     * @param usY {Integer}
+     *   y coordinate in userspace
+     * 
+     * @return {SVGPoint}
+     *   An object containing the converted coordinates <code>x</code> and
+     *   <code>y</code> in client (document).
+     */
+    userspaceToClient : function(el, usX, usY) {
+      var vp = el.getNearestViewport() || el;
+      var m = vp.getScreenCTM();
+      
+      var p = vp.createPoint();
+      p.x = usX;
+      p.y = usY;
+      
+      p = p.matrixTransform(m);
       return p;
     }
 
