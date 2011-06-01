c14e6c9c746cd9336d73ebf0bb7e8d445a2d383e
derrell <derrell@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2011-06-01 15:57:29 +0000

    Can't test for both Environment and Variant to avoid appender inclusion.

--- trunk/qooxdoo-contrib/TimezoneDate/trunk/demo/default/source/class/timezonedate/demo/Application.js
+++ trunk/qooxdoo-contrib/TimezoneDate/trunk/demo/default/source/class/timezonedate/demo/Application.js
@@ -1,17 +1,22 @@
 /* ************************************************************************
 
    Copyright:
-
+     (c) 2010, 2011 Derrell Lipman
+     
    License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
 
    Authors:
+     * Derrell Lipman (derrell)
 
-#asset(timezonedate.demo/*)
+#asset(timezonedate/*)
 
 ************************************************************************ */
 
 /**
- * This is the main application class of your custom application "timezonedate"
+ * This is the main application class of the demo for "timezonedate"
  */
 qx.Class.define("timezonedate.demo.Application",
 {
@@ -32,20 +37,10 @@ qx.Class.define("timezonedate.demo.Application",
       // Call super class
       this.base(arguments);
 
-      // Enable logging in debug variant
-      if (typeof(qx.core.Variant) !== "undefined")
-      {
-        if (qx.core["Variant"].isSet("qx.debug", "on"))
-        {
-          appender = qx.log.appender.Native;
-          appender = qx.log.appender.Console;
-        }
-      }
-
       // Enable logging in debug environment
       if (typeof(qx.core.Environment) !== "undefined")
       {
-        if (qx.core["Environment"].get("qx.debug"))
+        if (qx.core.Environment.get("qx.debug"))
         {
           appender = qx.log.appender.Native;
           appender = qx.log.appender.Console;
@@ -56,18 +51,27 @@ qx.Class.define("timezonedate.demo.Application",
 
       this.debug("test 1");
       tzDate = new timezonedate.TimezoneDate(0);
-      this.assertEquals((new Date(0)).getTime(), tzDate.getTime());
+      if (this.assertEquals)
+      {
+        this.assertEquals((new Date(0)).getTime(), tzDate.getTime());
+      }
       this.debug("date: " + tzDate);
 
       this.debug("test 2");
       tzDate = new timezonedate.TimezoneDate("1970-01-01T00:00Z");
-      this.assertEquals((new Date(0)).getTime(), tzDate.getTime());
+      if (this.assertEquals)
+      {
+        this.assertEquals((new Date(0)).getTime(), tzDate.getTime());
+      }
       this.debug("date: " + tzDate);
 
       this.debug("test 3");
       tzDate = new timezonedate.TimezoneDate("1970-01-01T04:00-04:00");
       tzDate.setOutputFormatter(tzDate.constructor._formatRfc2822);
-      this.assertEquals((new Date(0)).getTime(), tzDate.getTime());
+      if (this.assertEquals)
+      {
+        this.assertEquals((new Date(0)).getTime(), tzDate.getTime());
+      }
       this.debug("date: " + tzDate);
       this.debug(tzDate.format(240));
     }
