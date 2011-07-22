32f9d85564b5f037ddac1803c7bcfebc86b4678d
d_wagner <d_wagner@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2011-07-22 06:57:54 +0000

    Removed unnecessary locale change

--- trunk/qooxdoo-contrib/Simulator/1.5/tool/selenium/simulation/api/test_api.js
+++ trunk/qooxdoo-contrib/Simulator/1.5/tool/selenium/simulation/api/test_api.js
@@ -29,11 +29,7 @@ var qxAppInst = simulation.Simulation.QXAPPINSTANCE;
 
 
 simulation.Simulation.prototype.runTest = function()
-{  
-  // Make sure the locale is 'en' to simplify dealing with log messages.
-  var setLocale = "qx.locale.Manager.getInstance().setLocale('en')";  
-  this.runScript(setLocale, "Setting application locale to EN");
-  
+{
   this.logDocErrors();
   
 };
