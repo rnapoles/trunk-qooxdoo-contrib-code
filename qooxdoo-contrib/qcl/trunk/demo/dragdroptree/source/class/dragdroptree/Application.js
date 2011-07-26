5c65101285101d26aa2a2f77b66e6cda8b2a8895
cboulanger <cboulanger@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2011-07-26 18:53:42 +0000

    Updated dragdroptree demo

--- qooxdoo-contrib/qcl/trunk/demo/dragdroptree/source/class/dragdroptree/Application.js
+++ qooxdoo-contrib/qcl/trunk/demo/dragdroptree/source/class/dragdroptree/Application.js
@@ -52,20 +52,10 @@ qx.Class.define("dragdroptree.Application",
       // Call super class
       this.base(arguments);
 
-      // Enable logging in debug variant
-      if (qx.core.Variant.isSet("qx.debug", "on"))
-      {
-        // support native logging capabilities, e.g. Firebug for Firefox
-        qx.log.appender.Native;
-        // support additional cross-browser console. Press F7 to toggle visibility
-        qx.log.appender.Console;
-      }
-
-      /*
-      -------------------------------------------------------------------------
-        Below is your actual application code...
-      -------------------------------------------------------------------------
-      */
+      // support native logging capabilities, e.g. Firebug for Firefox
+      qx.log.appender.Native;
+      // support additional cross-browser console. Press F7 to toggle visibility
+      qx.log.appender.Console;
      
       /*
        * main layout
@@ -223,7 +213,7 @@ qx.Class.define("dragdroptree.Application",
       vbox.setWidth(300);
       hbox1.add(vbox);
      
-      label = new qx.ui.basic.Label("<h2>TreeVirtual Drag & Drop</h2>");
+      var label = new qx.ui.basic.Label("<h2>TreeVirtual Drag & Drop</h2>");
       label.setRich(true);
       vbox.add( label );
       
