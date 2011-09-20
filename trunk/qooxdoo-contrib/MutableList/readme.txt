7917bb11835caebe8ef2db109f2920f081417eb2
hellings <hellings@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2011-09-20 04:31:18 +0000

    Added a mutable list plugin.

---
+++ trunk/qooxdoo-contrib/MutableList/readme.txt
@@ -0,0 +1,6 @@
+MutableList
+=========================
+
+A list along with a text field and some basic operations that allow the
+user to input new entries, edit and delete old entries, and that allows
+the programmer to retrieve the list of entries.
---
+++ trunk/qooxdoo-contrib/MutableList/trunk/source/class/mutablelist/theme/classic/Theme.js
@@ -0,0 +1,20 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("mutablelist.theme.classic.Theme",
+{
+  meta :
+  {
+    color : mutablelist.theme.classic.Color,
+    decoration : mutablelist.theme.classic.Decoration,
+    font : mutablelist.theme.classic.Font,
+    appearance : mutablelist.theme.classic.Appearance
+  }
+});
