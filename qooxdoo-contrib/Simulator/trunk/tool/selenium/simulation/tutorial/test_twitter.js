3db10bc1022e3fa77236d945ac99092d60d0bf94
d_wagner <d_wagner@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2011-09-26 09:55:53 +0000

    [BUG #5621] Added a test skeleton for the Twitter tutorial apps

---
+++ qooxdoo-contrib/Simulator/trunk/tool/selenium/simulation/tutorial/test_twitter.js
@@ -0,0 +1,50 @@
+var baseConf = {
+  'autName' : 'TutorialTwitter',
+  'globalTimeout' : 300000,
+  'stepSpeed' : '500',
+  'debug' : true
+};
+
+var args = arguments ? arguments : "";
+var simSvn = baseConf.simulatorSvn;
+for (var i=0; i<args.length; i++) {
+  if (args[i].indexOf('simulatorSvn') >= 0) {
+    simSvn = args[i].substr(args[i].indexOf('simulatorSvn=') + 13);
+  }
+}
+
+load([simSvn + "/trunk/tool/selenium/simulation/Simulation.js"]);
+
+var mySim = new simulation.Simulation(baseConf,args);
+
+var selWin = simulation.Simulation.SELENIUMWINDOW;
+var qxAppInst = simulation.Simulation.QXAPPINSTANCE;
+
+// - Main --------------------------------------------------------------------
+(function() { 
+  mySim.testFailed = false;
+
+  var sessionStarted = mySim.startSession();
+  
+  if (!sessionStarted) {
+    return;
+  }
+
+  var isAppReady = mySim.waitForCondition(simulation.Simulation.ISQXAPPREADY, 60000, 
+    "Waiting for qooxdoo application");
+
+
+  if (!isAppReady) {
+    mySim.testFailed = true;
+    mySim.stop();
+    return;
+  }
+
+  mySim.addGlobalErrorHandler();
+  java.lang.Thread.sleep(5000);
+  mySim.logGlobalErrors();
+  mySim.logResults();
+
+  mySim.stop();
+
+})();
