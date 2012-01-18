3caafc74a915225be6e42bb31a131d6d565ee5ac
d_wagner <d_wagner@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2012-01-18 16:15:25 +0000

    Added tests for Page Transitions,  List, Events and  Toolbar

--- qooxdoo-contrib/Simulator/trunk/tool/selenium/simulation/mobileshowcase/test_mobileshowcase.js
+++ qooxdoo-contrib/Simulator/trunk/tool/selenium/simulation/mobileshowcase/test_mobileshowcase.js
@@ -49,9 +49,86 @@ simulation.Simulation.prototype.selectItem = function(itemName) {
   this.waitForElementPresent("//div[@class='navigationbar-backbutton']");
 };
 
-simulation.Simulation.prototype.goBack = function() {
+simulation.Simulation.prototype.goBack = function(parentPageTitle) {
   this.qxClick("//div[@class='navigationbar-backbutton']");
-  this.waitForElementPresent("//h1[text() = 'Overview']");
+  if (parentPageTitle) {
+    this.waitForElementPresent("//h1[text() = '" + parentPageTitle + "']");
+  }
+};
+
+simulation.Simulation.prototype.testPageTransitions = function()
+{
+  this.log("Testing Animations", "info");
+  this.waitForElementPresent("//h1[text() = 'Animation']");
+  var listItems = this.getListItemLabels();
+  for (var i=0, l=listItems.length; i<l; i++) {
+    this.selectItem(listItems[i]);
+    Packages.java.lang.Thread.sleep(1500);
+    this.waitForElementPresent("//strong[contains(text(), 'reverse animation')]");
+    this.goBack("Animation");
+  }
+};
+
+simulation.Simulation.prototype.testList = function()
+{
+  this.log("Testing List", "info");
+  this.waitForElementPresent("//h1[text() = 'List']");
+  this.qxClick("//div[text() = 'Selectable Item2']");
+  if (!this.__sel.isAlertPresent()) {
+    throw new Error("Clicking Selectable Item2 did not open an alert box!");
+  }
+  var expectedText = "Item Selected #2";
+  var foundText = String(this.__sel.getAlert());
+  if (foundText !== expectedText) {
+    throw new Error("Expected alert box text " + expectedText + " but found " + foundText);
+  }
+};
+
+simulation.Simulation.prototype.testEvents = function()
+{
+  this.log("Testing Events", "info");
+  var eventContainerLocator = "//div[contains(@class, 'eventcontainer')]";
+  this.waitForElementPresent(eventContainerLocator);
+  this.qxClick(eventContainerLocator);
+  this.waitForElementPresent("//div[contains(text(), 'touchstart touchend tap')]");
+};
+
+simulation.Simulation.prototype.testToolbar = function()
+{
+  this.log("Testing Toolbar", "info");
+  this.waitForElementPresent("//div[contains(@class, 'toolbar')]");
+  
+  //click search button
+  this.qxClick("//div[contains(@class, 'toolbar-button')]/descendant::div[text() = 'Search']");
+  this.waitForElementPresent("//div[contains(@class, 'dialog')]");
+  var searchDialogButtonLocator = "//div[contains(@class, 'button')]";
+  this.qxClick(searchDialogButtonLocator);
+  Packages.java.lang.Thread.sleep(500);
+  if (this.__sel.isVisible(searchDialogButtonLocator)) {
+    throw new Error("Clicking Search did not close the search dialog!");
+  }
+  
+  //click 'left arrow' button
+  var noButtonLocator = "//div[text() = 'no']";
+  this.qxClick("//img[contains(@src, 'arrowleft')]");
+  this.waitForElementPresent(noButtonLocator);
+  this.qxClick(yesButtonLocator);
+  Packages.java.lang.Thread.sleep(500);
+  if (this.__sel.isVisible(noButtonLocator)) {
+    throw new Error("Clicking No did not close the Are you sure dialog!");
+  }
+  
+  //click load button
+  /*
+  this.qxClick("//div[contains(@class, 'toolbar-button')]/descendant::div[text() = 'Load']");
+  var loadingDialogLocator = "//div[text() = 'Loading...']";
+  this.waitForElementPresent(loadingDialogLocator);
+  //the loading dialog disappears after 5sec
+  Packages.java.lang.Thread.sleep(5500);
+  if (this.__sel.isVisible(loadingDialogLocator)) {
+    throw new Error("Loading Dialog did not disappear!");
+  }
+  */
 };
 
 mySim.runTest = function()
@@ -65,13 +142,18 @@ mySim.runTest = function()
     var testMethodName = "test" + listItems[i].replace(/ /i, "");
     this.log("Selecting item " + listItems[i], "info");
     this.selectItem(listItems[i]);
-    /*
     if (this[testMethodName] && typeof this[testMethodName] == "function") {
-      this[testMethodName]();
+      //try {
+        this[testMethodName]();
+      /*
+      }
+      catch(ex) {
+        this.log("Error while testing " + listItems[i] + ": " + ex.message, "error");
+      }
+      */
     }
-    */
     this.log("Going back " + i, "info");
-    this.goBack();
+    this.goBack("Overview");
   }
 };
 
