34aed02ca9fdfac333d2e990212246e41e5a6bf3
spackers <spackers@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2012-05-09 13:03:27 +0000

    first pass at minor refresh to GUI look, providing thinner scrollbars, table header gradients, and tweaks to tabview tabs and colours

--- trunk/qooxdoo-contrib/Aristo/branches/v1.1/demo/default/source/class/aristo/demo/ScrollBar.js
+++ trunk/qooxdoo-contrib/Aristo/branches/v1.1/demo/default/source/class/aristo/demo/ScrollBar.js
@@ -37,26 +37,42 @@ qx.Class.define("aristo.demo.ScrollBar",
       });
       container.add(label, {row: 0, column: 0});
 
+      function doScroll(e) {
+          hScrollBar.setPosition(e.getData());
+          hScrollBarNative.setPosition(e.getData());
+          vScrollBar.setPosition(e.getData());
+          vScrollBarNative.setPosition(e.getData());
+          label.setValue("Value " + e.getData());
+      }
 
       var vScrollBar = new qx.ui.core.scroll.ScrollBar("vertical").set({
         height: 200,
         maximum: 500
       });
-      vScrollBar.addListener("scroll", function(e) {
-        hScrollBar.setPosition(e.getData());
-      });
+      vScrollBar.addListener("scroll", doScroll);
       container.add(vScrollBar, {row: 0, column: 1});
 
+      var vScrollBarNative = new qx.ui.core.scroll.NativeScrollBar("vertical").set({
+        height: 200,
+        maximum: 500
+      });
+      vScrollBarNative.addListener("scroll", doScroll);
+      container.add(vScrollBarNative, {row: 0, column: 2});
 
-      var hScrollBar = new qx.ui.core.scroll.NativeScrollBar("horizontal").set({
+
+      var hScrollBar = new qx.ui.core.scroll.ScrollBar("horizontal").set({
+          width: 300,
+          maximum: 500
+        });
+        hScrollBar.addListener("scroll", doScroll);
+        container.add(hScrollBar, {row: 1, column: 0});
+
+    var hScrollBarNative = new qx.ui.core.scroll.NativeScrollBar("horizontal").set({
         width: 300,
         maximum: 500
       });
-      hScrollBar.addListener("scroll", function(e) {
-        vScrollBar.setPosition(e.getData());
-        label.setValue("Value: " + e.getData());
-      });
-      container.add(hScrollBar, {row: 1, column: 0});
+      hScrollBarNative.addListener("scroll", doScroll);
+      container.add(hScrollBarNative, {row: 2, column: 0});
 
       hScrollBar.setPosition(170);
     }
--- trunk/qooxdoo-contrib/Aristo/branches/v1.1/source/class/aristo/theme/Font.js
+++ trunk/qooxdoo-contrib/Aristo/branches/v1.1/source/class/aristo/theme/Font.js
@@ -36,14 +36,14 @@ qx.Theme.define("aristo.theme.Font",
   {
     "default" :
     {
-      size : 11,
+      size : 12,
       lineHeight : 1.4,
       family : [ "Arial", "Tahoma", "Verdana", "Bitstream Vera Sans", "Liberation Sans" ]
     },
 
     "bold" :
     {
-      size : 11,
+      size : 12,
       lineHeight : 1.4,
       family : [ "Arial", "Tahoma", "Verdana", "Bitstream Vera Sans", "Liberation Sans" ],
       bold : true
@@ -51,14 +51,14 @@ qx.Theme.define("aristo.theme.Font",
 
     "small" :
     {
-      size : 10,
+      size : 11,
       lineHeight : 1.4,
       family : [ "Arial", "Tahoma", "Verdana", "Bitstream Vera Sans", "Liberation Sans" ]
     },
 
     "monospace" :
     {
-      size : 11,
+      size : 12,
       lineHeight : 1.4,
       family : [ "Courier New", "DejaVu Sans Mono", "monospace" ]
     },
