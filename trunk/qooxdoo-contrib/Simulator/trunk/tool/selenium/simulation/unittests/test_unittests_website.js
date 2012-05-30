f3bd8e1fe5f2d2718268653dab3f7f371d0efc64
d_wagner <d_wagner@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2012-05-30 12:12:50 +0000

    [BUG #6460 ] Log the amount of failed unit tests

--- trunk/qooxdoo-contrib/Simulator/trunk/tool/selenium/simulation/unittests/test_unittests_website.js
+++ trunk/qooxdoo-contrib/Simulator/trunk/tool/selenium/simulation/unittests/test_unittests_website.js
@@ -34,5 +34,27 @@ var mySim = new simulation.Simulation(baseConf,args);
   mySim.waitForElementPresent("//span[contains(text(), 'Test suite finished')]", 300000);
   Packages.java.lang.Thread.sleep(5000);
   
+  var getStatus = 'selenium.browserbot.getCurrentWindow().document.getElementById("status").innerHTML';
+  
+  var status = mySim.__sel.getEval(getStatus);
+  var failed = /Failed: (\d+)/.exec(status);
+  var totalErrors;
+  if (failed && failed.length > 0) {
+    totalErrors = failed[1];
+  }
+  else {
+    totalErrors = 1;
+  }
+  Packages.java.lang.Thread.sleep(999999);
+  
+  if (!totalErrors) {
+    this.log("Could not retrieve failed test count!", "error");
+    totalErrors = 1;
+    mySim.testFailed = true;
+  }
+
+  mySim.log("Tests with warnings or errors: " + totalErrors, "info");
+    
   mySim.stop();
+  mySim.logTestDuration();
 })();
