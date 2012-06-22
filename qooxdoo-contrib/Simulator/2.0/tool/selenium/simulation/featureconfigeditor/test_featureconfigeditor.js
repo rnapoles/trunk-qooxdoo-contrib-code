4d3cc10ecdef155bb16082b58c92ea756d6955f6
d_wagner <d_wagner@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2012-06-22 12:21:14 +0000

    added 2.0 branch

---
+++ qooxdoo-contrib/Simulator/2.0/tool/selenium/simulation/featureconfigeditor/test_featureconfigeditor.js
@@ -0,0 +1,54 @@
+var baseConf = {
+  'autName' : 'FeatureConfigEditor',
+  'globalTimeout' : 300000,
+  'stepSpeed' : '500',
+  'debug' : true
+};
+
+var args = arguments ? arguments : "";
+var simSvn = baseConf.simulatorSvn;
+for (var i=0; i<args.length; i++) {
+  if (args[i].indexOf('simulatorSvn') >= 0) {
+    simSvn = args[i].substr(args[i].indexOf('simulatorSvn=') + 13);
+  }
+}
+
+load([simSvn + "/trunk/tool/selenium/simulation/Simulation.js"]);
+
+var mySim = new simulation.Simulation(baseConf,args);
+
+var selWin = simulation.Simulation.SELENIUMWINDOW;
+var qxAppInst = simulation.Simulation.QXAPPINSTANCE;
+
+// - Main --------------------------------------------------------------------
+(function() { 
+  mySim.testFailed = false;
+
+  var sessionStarted = mySim.startSession();
+  
+  if (!sessionStarted) {
+    return;
+  }
+  
+  mySim.__sel.click("//a[contains(text(), 'Launch Configuration Editor')]");
+  mySim.setupEnvironment();
+
+  var isAppReady = mySim.waitForCondition(simulation.Simulation.ISQXAPPREADY, 60000, 
+    "Waiting for qooxdoo application");
+
+
+  if (!isAppReady) {
+    mySim.testFailed = true;
+    mySim.stop();
+    return;
+  }
+
+  mySim.setupApplicationLogging();
+  mySim.addGlobalErrorHandler();
+  java.lang.Thread.sleep(5000);
+  mySim.logGlobalErrors();
+  mySim.logResults();
+
+  mySim.stop();
+
+})();
