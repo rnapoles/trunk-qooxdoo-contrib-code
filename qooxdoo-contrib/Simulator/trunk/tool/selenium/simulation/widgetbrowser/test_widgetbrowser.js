f61db0e2ed796837985355964cbf4f1d9cbb7f45
d_wagner <d_wagner@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2012-09-28 08:39:48 +0000

    Select all tabs in each theme; test state buttons

--- qooxdoo-contrib/Simulator/trunk/tool/selenium/simulation/widgetbrowser/test_widgetbrowser.js
+++ qooxdoo-contrib/Simulator/trunk/tool/selenium/simulation/widgetbrowser/test_widgetbrowser.js
@@ -27,73 +27,44 @@ var selWin = simulation.Simulation.SELENIUMWINDOW;
 var qxAppInst = simulation.Simulation.QXAPPINSTANCE;
 var locators = {
   tabView : 'qxh=child[0]/qx.ui.container.Scroll/qx.ui.core.scroll.ScrollPane/[@classname="widgetbrowser.view.TabView"]',
-  tabContainer : '/qx.ui.container.SlideBar/qx.ui.core.scroll.ScrollPane/qx.ui.container.Composite'
+  tabContainer : '/qx.ui.container.SlideBar/qx.ui.core.scroll.ScrollPane/qx.ui.container.Composite',
+  themeSelectBox : 'qxh=child[0]/[@classname="widgetbrowser.view.Header"]/qx.ui.form.SelectBox',
+  themeListItem : 'qxh=*/qx.ui.popup.Popup/qx.ui.form.List/[@label="THEME"]',
+  stateButtonLocator : 'qxhv=qx.ui.container.Composite/*/qx.ui.tabview.TabView/*/[@label="STATE"]'
 };
 
 simulation.Simulation.prototype.runTest = function()
 {
   // Make sure the locale is 'en' to simplify dealing with log messages.
-  var setLocale = "qx.locale.Manager.getInstance().setLocale('en')";  
+  var setLocale = "qx.locale.Manager.getInstance().setLocale('en')";
   this.runScript(setLocale, "Setting application locale to EN");
-  
-  this.checkUrlParameter();
-  
+
   // Log any errors caught during showcase startup
   this.logGlobalErrors();
   this.clearGlobalErrorStore();
-  
+
   var tabNames = this.getTabNames();
-  for (var i=0,l=tabNames.length; i<l; i++) {
-    this.testTab(tabNames[i]);
+  var themeNames = this.getThemeNames();
+
+  for (var i = themeNames.length - 1; i >= 0; i--) {
+    var theme = themeNames[i];
+    this.selectTheme(theme);
+    for (var j = tabNames.length - 1; j >= 0; j--) {
+      this.testTab(tabNames[j]);
+    }
   }
 };
 
-simulation.Simulation.prototype.checkUrlParameter = function()
-{
-  var selectedTheme = "qx.theme.Simple";
-  
-  var urlWithParam = this.getConfigSetting("autHost") + "" 
-  + this.getConfigSetting("autPath") + "?qx.theme=" + selectedTheme;
-  this.qxOpen(urlWithParam);
-  var isAppReady = this.waitForCondition(simulation.Simulation.ISQXAPPREADY, 60000, 
-                                          "Waiting for qooxdoo application");
-
-  if (!isAppReady) {
-    this.testFailed = true;
-    this.log("checkUrlParameter: Application was not reloaded correctly!", "error");
-    return;
-  }
-  
-  this.addGlobalErrorHandler();
-  this.setupApplicationLogging();
-  
-  var environmentTheme = "";
-  var themeGetter = selWin + ".qx.core.Environment.get(\"qx.theme\")";
-  try {
-    environmentTheme = this.__sel.getEval(themeGetter); 
-  }
-  catch(ex) {
-    this.log("checkUrlParameter: Unable to query theme environment setting!", "error");
-    return;
-  }
-  
-  if (environmentTheme != selectedTheme) {
-    this.log("checkUrlParameter: Expected theme " + selectedTheme + " but core.environment says " + environmentTheme, "error");
-    return;
-  }
-  
-  this.log("checkUrlParameter: Theme setting from URL applied correctly", "info");
-};
 
 simulation.Simulation.prototype.getTabNames = function()
 {
-  var labelGetter = 'var labels = [];\
-  var pages = this.getChildren();\
-  for (var i=0,l=pages.length; i<l; i++) {\
-    labels.push(pages[i].getLabel());\
-  }\
-  return labels;';
-  
+  var labelGetter = 'var labels = [];' +
+    'var pages = this.getChildren();' +
+    'for (var i=0,l=pages.length; i<l; i++) {' +
+    '  labels.push(pages[i].getLabel());' +
+    '}' +
+    'return labels;';
+
   var strVal = String(this.__sel.getRunInContext(locators.tabView, labelGetter));
   try {
     return eval(strVal);
@@ -102,6 +73,36 @@ simulation.Simulation.prototype.getTabNames = function()
   }
 };
 
+simulation.Simulation.prototype.getThemeNames = function()
+{
+  var themeGetter = 'var themes = [];' +
+    'var items = this.getChildren();' +
+    'for (var i=0,l=items.length; i<l; i++) {' +
+    '  themes.push(items[i].getLabel());' +
+    '}' +
+    'return themes;';
+
+  var strVal = String(this.__sel.getRunInContext(locators.themeSelectBox, themeGetter));
+  try {
+    return eval(strVal);
+  } catch(ex) {
+    return strVal.split(",");
+  }
+};
+
+
+simulation.Simulation.prototype.selectTheme = function(themeName)
+{
+  this.log("Selecting theme: " + themeName, "info");
+  this.__sel.qxClick(locators.themeSelectBox);
+  Packages.java.lang.Thread.sleep(1000);
+  this.__sel.qxClick(locators.themeListItem.replace("THEME", themeName));
+  Packages.java.lang.Thread.sleep(3000);
+  this.logGlobalErrors();
+  this.clearGlobalErrorStore();
+};
+
+
 simulation.Simulation.prototype.testTab = function(tabName)
 {
   this.log("Clicking Tab " + tabName, "info");
@@ -109,25 +110,42 @@ simulation.Simulation.prototype.testTab = function(tabName)
     tabName = /(.*?)\//.exec(tabName)[1];
   }
   this.qxClick(locators.tabView + locators.tabContainer + '/[@label=' + tabName + ']');
-  
   // Give the demo part some time to appear
   Packages.java.lang.Thread.sleep(5000);
-  // Log any errors caught during demo startup 
+  this.testStates();
   this.logGlobalErrors();
   this.clearGlobalErrorStore();
 };
 
+
+simulation.Simulation.prototype.testStates = function(tabName)
+{
+  var states = ["Disabled", "Hovered", "Focused", "Invalid", "Hide some", "Overflow"];
+  for (var i=0,l=states.length; i<l; i++) {
+    var state = states[i];
+    var buttonLocator = locators.stateButtonLocator.replace("STATE", state);
+    if (this.__sel.isElementPresent(buttonLocator)) {
+      this.log("Testing state: " + state, "debug");
+      this.__sel.qxClick(buttonLocator);
+      Packages.java.lang.Thread.sleep(1000);
+      this.__sel.qxClick(buttonLocator);
+      Packages.java.lang.Thread.sleep(1000);
+    }
+  }
+};
+
+
 // - Main --------------------------------------------------------------------
-(function() { 
+(function() {
   mySim.testFailed = false;
 
   var sessionStarted = mySim.startSession();
-  
+
   if (!sessionStarted) {
     return;
   }
 
-  var isAppReady = mySim.waitForCondition(simulation.Simulation.ISQXAPPREADY, 60000, 
+  var isAppReady = mySim.waitForCondition(simulation.Simulation.ISQXAPPREADY, 60000,
                                           "Waiting for qooxdoo application");
 
 
